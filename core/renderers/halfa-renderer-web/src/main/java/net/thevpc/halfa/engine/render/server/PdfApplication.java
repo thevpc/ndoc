027695dc6577e3f7bec49af6bde6c23179ab4ff2
mohamed <derbely887@gmail.com>
2024-07-12 17:06:19 +0100

    Web-Render

---
+++ core/renderers/halfa-renderer-web/src/main/java/net/thevpc/halfa/engine/render/server/PdfApplication.java
@@ -0,0 +1,12 @@
+package net.thevpc.halfa.engine.render.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PdfApplication.class, args);
+    }
+}
---
+++ core/renderers/halfa-renderer-web/src/main/java/net/thevpc/halfa/engine/render/server/PdfController.java
@@ -0,0 +1,37 @@
+package net.thevpc.halfa.engine.render.server;
+import net.thevpc.halfa.api.HEngine;
+import net.thevpc.halfa.api.document.HDocument;
+import net.thevpc.halfa.engine.HEngineImpl;
+import net.thevpc.halfa.spi.renderer.HDocumentStreamRenderer;
+import net.thevpc.nuts.NSession;
+import net.thevpc.nuts.Nuts;
+import net.thevpc.nuts.io.NPath;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PdfController {
+
+    @GetMapping("/renderPdf")
+    @ResponseBody
+    public String renderPdf() {
+        NSession session = Nuts.openWorkspace();
+
+        HEngine e = new HEngineImpl(session);
+
+        NPath file = NPath.of("/home/mohamed/Desktop/stage/halfa/documentation/tson-doc/main.hd", session)
+                .toAbsolute()
+                .normalize();
+
+        HDocument doc = e.loadDocument(file, null).get();
+
+        HDocumentStreamRenderer renderer = e.newStreamRenderer("hd");
+        renderer.setOutput(NPath.ofUserHome(session).resolve("exemple.hd"));
+
+        renderer.render(doc);
+
+        return "PDF rendu avec succès!";
+    }
+}
