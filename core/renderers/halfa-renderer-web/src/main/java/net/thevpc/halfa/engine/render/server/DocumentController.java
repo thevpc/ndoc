b6a12ed704317141cb3cd913cf4209369d2c894c
mohamed <derbely887@gmail.com>
2024-07-16 16:19:36 +0100

    web_rendring

--- core/renderers/halfa-renderer-web/src/main/java/net/thevpc/halfa/engine/render/server/DocumentController.java
+++ core/renderers/halfa-renderer-web/src/main/java/net/thevpc/halfa/engine/render/server/DocumentController.java
@@ -21,6 +21,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
@@ -31,6 +32,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/document")
+@CrossOrigin(origins = "http://localhost:4200")
 public class DocumentController {
 
     private final HEngine engine;
