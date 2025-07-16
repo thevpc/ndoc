85fb28faf1532cad26762db1de0fbf1769207c25
thevpc <taha.bensalah@gmail.com>
2025-07-16 20:45:07 +0100

    [CHANGED] renaming as ndoc from halfa

--- app/ndoc-viewer/src/main/java/net/thevpc/ndoc/NDocViewer.java
+++ app/ndoc-viewer/src/main/java/net/thevpc/ndoc/NDocViewer.java
@@ -3,7 +3,12 @@ package net.thevpc.ndoc;
 import com.formdev.flatlaf.FlatLightLaf;
 import net.thevpc.ndoc.main.MainFrame;
 import net.thevpc.nuts.*;
+import net.thevpc.nuts.cmdline.NArg;
+import net.thevpc.nuts.cmdline.NCmdLine;
+import net.thevpc.nuts.io.NPath;
 import net.thevpc.nuts.nswing.NSwingUtils;
+
+import javax.swing.*;
 
 /**
  * @author vpc
@@ -21,7 +26,23 @@ public class NDocViewer  {
         NSwingUtils.setSharedWorkspaceInstance();
         FlatLightLaf.setup(new com.formdev.flatlaf.FlatDarculaLaf());
         MainFrame mainFrame=new MainFrame();
-        mainFrame.setVisible(true);
-
+        SwingUtilities.invokeLater(() -> {
+            mainFrame.setVisible(true);
+            NCmdLine cmdLine = NApp.of().getCmdLine();
+            while (!cmdLine.isEmpty()) {
+                cmdLine.matcher()
+                        .with("--reopen").matchTrueFlag(a->{
+                            NPath p = mainFrame.getService().getLatestProjectPath();
+                            if(p!=null){
+                                mainFrame.getService().openProject(NPath.of(a.stringValue()));
+                            }
+                        })
+                        .with("--open").matchEntry(a->
+                                mainFrame.getService().openProject(NPath.of(a.stringValue())))
+                        .with("--show-log").matchTrueFlag(a->
+                                mainFrame.getService().showDebug())
+                        .requireWithDefault();
+            }
+        });
     }
 }
