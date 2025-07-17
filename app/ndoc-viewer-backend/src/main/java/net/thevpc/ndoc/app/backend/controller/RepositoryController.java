7a9033134e49025ea45ecdf0d0167b631245f83f
thevpc <taha.bensalah@gmail.com>
2025-07-17 11:02:32 +0100

    [CHANGED] rename halfa to ndoc

--- app/ndoc-viewer-backend/src/main/java/net/thevpc/ndoc/app/backend/controller/RepositoryController.java
+++ app/ndoc-viewer-backend/src/main/java/net/thevpc/ndoc/app/backend/controller/RepositoryController.java
@@ -113,7 +113,7 @@ public class RepositoryController {
     public ResponseEntity<byte[]> getDocumentImages(
             @RequestParam(name = "pageNumber", defaultValue = "1") int pageNumber) {
         try {
-            NPath file = NPath.of("file:///home/mohamed/Desktop/stage/halfa/documentation/tson-doc/main.ndoc")
+            NPath file = NPath.of("file:///home/mohamed/Desktop/stage/ndoc/documentation/tson-doc/main.ndoc")
                     .toAbsolute()
                     .normalize();
 
