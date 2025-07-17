256bd89744498a80641b80775a68609c55313fd1
thevpc <taha.bensalah@gmail.com>
2025-07-17 11:26:43 +0100

    [CHANGED] rename halfa to ndoc

---
+++ core/ndoc-api/src/main/java/net/thevpc/ndoc/api/model/elem2d/primitives/NDocAbstractElement2D.java
@@ -0,0 +1,6 @@
+package net.thevpc.ndoc.api.model.elem2d.primitives;
+
+import net.thevpc.ndoc.api.model.elem2d.NDocElement2D;
+
+public abstract class NDocAbstractElement2D implements NDocElement2D {
+}
