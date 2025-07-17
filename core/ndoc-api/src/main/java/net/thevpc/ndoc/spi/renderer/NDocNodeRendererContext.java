1a3e2bef92ef460d67217220a495128cf03cb11d
thevpc <taha.bensalah@gmail.com>
2025-07-17 11:30:02 +0100

    [CHANGED] rename halfa to ndoc

--- core/ndoc-api/src/main/java/net/thevpc/ndoc/spi/renderer/NDocNodeRendererContext.java
+++ core/ndoc-api/src/main/java/net/thevpc/ndoc/spi/renderer/NDocNodeRendererContext.java
@@ -2,13 +2,13 @@ package net.thevpc.ndoc.spi.renderer;
 
 import net.thevpc.ndoc.NDocDocumentFactory;
 import net.thevpc.ndoc.api.NDocEngine;
-import net.thevpc.ndoc.api.document.HLogger;
-import net.thevpc.ndoc.api.model.elem2d.Bounds2;
-import net.thevpc.ndoc.api.model.node.HNode;
+import net.thevpc.ndoc.api.document.NDocLogger;
+import net.thevpc.ndoc.api.model.elem2d.NDocBounds2;
+import net.thevpc.ndoc.api.model.node.NDocNode;
 import net.thevpc.ndoc.api.style.HProp;
-import net.thevpc.ndoc.api.style.HProperties;
+import net.thevpc.ndoc.api.style.NDocProperties;
 import net.thevpc.ndoc.spi.model.NDocSizeRequirements;
-import net.thevpc.ndoc.spi.util.HSizeRef;
+import net.thevpc.ndoc.spi.util.NDocSizeRef;
 import net.thevpc.nuts.elem.NElement;
 import net.thevpc.nuts.io.NPath;
 import net.thevpc.nuts.util.NOptional;
@@ -20,13 +20,13 @@ public interface NDocNodeRendererContext {
     String CAPABILITY_PRINT="print";
     String CAPABILITY_ANIMATE ="animate";
 
-    default NDocSizeRequirements sizeRequirementsOf(HNode p) {
+    default NDocSizeRequirements sizeRequirementsOf(NDocNode p) {
         return manager().getRenderer(p.type()).get().sizeRequirements(p, this);
     }
 
     NDocNodeRendererManager manager();
 
-    HLogger log();
+    NDocLogger log();
 
     NDocNodeRendererContext dryMode();
 
@@ -34,36 +34,36 @@ public interface NDocNodeRendererContext {
 
     boolean isDry();
 
-    Bounds2 getGlobalBounds();
+    NDocBounds2 getGlobalBounds();
 
     NDocGraphics graphics();
 
-    Bounds2 getBounds();
+    NDocBounds2 getBounds();
 
 
-    void render(HNode p);
+    void render(NDocNode p);
 
-    void render(HNode p, NDocNodeRendererContext ctx);
+    void render(NDocNode p, NDocNodeRendererContext ctx);
 
     NDocEngine engine();
 
     NDocDocumentFactory documentFactory();
 
-    NOptional<NElement> computePropertyValue(HNode t, String s, String... synonyms);
+    NOptional<NElement> computePropertyValue(NDocNode t, String s, String... synonyms);
 
-    List<HProp> computeProperties(HNode t);
+    List<HProp> computeProperties(NDocNode t);
 
-    NPath resolvePath(NPath path, HNode node);
+    NPath resolvePath(NPath path, NDocNode node);
 
-    NPath resolvePath(NElement path, HNode node);
+    NPath resolvePath(NElement path, NDocNode node);
 
-    NDocNodeRendererContext withDefaultStyles(HNode node, HProperties defaultStyles);
+    NDocNodeRendererContext withDefaultStyles(NDocNode node, NDocProperties defaultStyles);
 
-    NDocNodeRendererContext withBounds(HNode t, Bounds2 bounds2);
+    NDocNodeRendererContext withBounds(NDocNode t, NDocBounds2 bounds2);
 
     NDocNodeRendererContext withGraphics(NDocGraphics graphics);
 
-    HSizeRef sizeRef();
+    NDocSizeRef sizeRef();
 
     boolean isPrint();
 
