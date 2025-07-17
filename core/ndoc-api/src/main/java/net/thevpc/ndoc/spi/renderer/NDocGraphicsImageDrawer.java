717ba368a1863d1706e151d65e932282ea45471e
thevpc <taha.bensalah@gmail.com>
2025-07-17 11:29:59 +0100

    [CHANGED] rename halfa to ndoc

--- core/ndoc-api/src/main/java/net/thevpc/ndoc/spi/renderer/NDocGraphicsImageDrawer.java
+++ core/ndoc-api/src/main/java/net/thevpc/ndoc/spi/renderer/NDocGraphicsImageDrawer.java
@@ -1,7 +1,7 @@
 package net.thevpc.ndoc.spi.renderer;
 
-import net.thevpc.ndoc.api.model.elem2d.HImageOptions;
+import net.thevpc.ndoc.api.model.elem2d.NDocImageOptions;
 
 public interface NDocGraphicsImageDrawer {
-    void drawImage(double x, double y, HImageOptions options, NDocGraphics graphics);
+    void drawImage(double x, double y, NDocImageOptions options, NDocGraphics graphics);
 }
