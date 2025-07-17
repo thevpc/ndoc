a0ee651983267c4611e632f9ed12adc5d9f25b3b
thevpc <taha.bensalah@gmail.com>
2025-07-17 11:28:40 +0100

    [CHANGED] rename halfa to ndoc

--- core/ndoc-api/src/main/java/net/thevpc/ndoc/api/model/elem3d/NDocMatrix3D.java
+++ core/ndoc-api/src/main/java/net/thevpc/ndoc/api/model/elem3d/NDocMatrix3D.java
@@ -1,14 +1,14 @@
 package net.thevpc.ndoc.api.model.elem3d;
 
-public class Matrix3D {
+public class NDocMatrix3D {
     double[][] m;
 
-    public Matrix3D(double[][] m) {
+    public NDocMatrix3D(double[][] m) {
         this.m = m;
     }
 
-    public static Matrix3D identity() {
-        return new Matrix3D(new double[][]{
+    public static NDocMatrix3D identity() {
+        return new NDocMatrix3D(new double[][]{
                 {1, 0, 0, 0},
                 {0, 1, 0, 0},
                 {0, 0, 1, 0},
@@ -17,8 +17,8 @@ public class Matrix3D {
     }
 
     // Translation matrix
-    public Matrix3D translate(double tx, double ty, double tz) {
-        return multiply(new Matrix3D(new double[][]{
+    public NDocMatrix3D translate(double tx, double ty, double tz) {
+        return multiply(new NDocMatrix3D(new double[][]{
                 {1, 0, 0, tx},
                 {0, 1, 0, ty},
                 {0, 0, 1, tz},
@@ -27,10 +27,10 @@ public class Matrix3D {
     }
 
     // Rotation matrix around the X axis
-    public Matrix3D rotateX(double angle) {
+    public NDocMatrix3D rotateX(double angle) {
         double cos = Math.cos(angle);
         double sin = Math.sin(angle);
-        return multiply(new Matrix3D(
+        return multiply(new NDocMatrix3D(
                 new double[][]{
                         {1, 0, 0, 0},
                         {0, cos, -sin, 0},
@@ -41,10 +41,10 @@ public class Matrix3D {
     }
 
     // Rotation matrix around the Y axis
-    public Matrix3D rotateY(double angle) {
+    public NDocMatrix3D rotateY(double angle) {
         double cos = Math.cos(angle);
         double sin = Math.sin(angle);
-        return multiply(new Matrix3D(
+        return multiply(new NDocMatrix3D(
                 new double[][]{
                         {cos, 0, sin, 0},
                         {0, 1, 0, 0},
@@ -55,10 +55,10 @@ public class Matrix3D {
     }
 
     // Rotation matrix around the Z axis
-    public Matrix3D rotateZ(double angle) {
+    public NDocMatrix3D rotateZ(double angle) {
         double cos = Math.cos(angle);
         double sin = Math.sin(angle);
-        return multiply(new Matrix3D(new double[][]{
+        return multiply(new NDocMatrix3D(new double[][]{
                 {cos, -sin, 0, 0},
                 {sin, cos, 0, 0},
                 {0, 0, 1, 0},
@@ -67,8 +67,8 @@ public class Matrix3D {
     }
 
     // Scaling matrix
-    public Matrix3D scale(double sx, double sy, double sz) {
-        return multiply(new Matrix3D(new double[][]{
+    public NDocMatrix3D scale(double sx, double sy, double sz) {
+        return multiply(new NDocMatrix3D(new double[][]{
                 {sx, 0, 0, 0},
                 {0, sy, 0, 0},
                 {0, 0, sz, 0},
@@ -76,12 +76,12 @@ public class Matrix3D {
         }));
     }
 
-    public Matrix3D multiply(Matrix3D b) {
+    public NDocMatrix3D multiply(NDocMatrix3D b) {
         return multiply(this, b);
     }
 
     // Multiply two matrices
-    public static Matrix3D multiply(Matrix3D a, Matrix3D b) {
+    public static NDocMatrix3D multiply(NDocMatrix3D a, NDocMatrix3D b) {
         double[][] result = new double[4][4];
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
@@ -91,6 +91,6 @@ public class Matrix3D {
                 }
             }
         }
-        return new Matrix3D(result);
+        return new NDocMatrix3D(result);
     }
 }
