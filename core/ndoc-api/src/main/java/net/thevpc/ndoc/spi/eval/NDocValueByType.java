aae6f89ed3936ba949b9362b217f35ecb11f38fe
thevpc <taha.bensalah@gmail.com>
2025-07-17 11:29:41 +0100

    [CHANGED] rename halfa to ndoc

--- core/ndoc-api/src/main/java/net/thevpc/ndoc/spi/eval/NDocValueByType.java
+++ core/ndoc-api/src/main/java/net/thevpc/ndoc/spi/eval/NDocValueByType.java
@@ -3,7 +3,7 @@ package net.thevpc.ndoc.spi.eval;
 import net.thevpc.ndoc.api.model.HArrow;
 import net.thevpc.ndoc.api.model.HArrowType;
 import net.thevpc.ndoc.api.model.elem2d.*;
-import net.thevpc.ndoc.api.model.node.HNode;
+import net.thevpc.ndoc.api.model.node.NDocNode;
 import net.thevpc.ndoc.spi.renderer.NDocNodeRendererContext;
 import net.thevpc.nuts.elem.NElement;
 import net.thevpc.nuts.util.NOptional;
@@ -12,73 +12,73 @@ import java.awt.*;
 
 public class NDocValueByType {
 
-    public static NOptional<HArrowType> getArrowType(HNode t, NDocNodeRendererContext ctx, String propName, String... propNames) {
+    public static NOptional<HArrowType> getArrowType(NDocNode t, NDocNodeRendererContext ctx, String propName, String... propNames) {
         return NDocObjEx.of(ctx.computePropertyValue(t, propName, propNames).orNull()).asArrowType();
     }
 
-    public static NOptional<HArrow> getArrow(HNode t, NDocNodeRendererContext ctx, String propName, String... propNames) {
+    public static NOptional<HArrow> getArrow(NDocNode t, NDocNodeRendererContext ctx, String propName, String... propNames) {
         return NDocObjEx.of(ctx.computePropertyValue(t, propName, propNames).orNull()).asArrow();
     }
 
-    public static NOptional<Boolean> getBoolean(HNode t, NDocNodeRendererContext ctx, String propName, String... propNames) {
+    public static NOptional<Boolean> getBoolean(NDocNode t, NDocNodeRendererContext ctx, String propName, String... propNames) {
         return NDocObjEx.of(ctx.computePropertyValue(t, propName, propNames).orNull()).asBoolean();
     }
 
-    public static NOptional<Integer> getIntOrBoolean(HNode t, NDocNodeRendererContext ctx, String propName, String... propNames) {
+    public static NOptional<Integer> getIntOrBoolean(NDocNode t, NDocNodeRendererContext ctx, String propName, String... propNames) {
         return NDocObjEx.of(ctx.computePropertyValue(t, propName, propNames).orNull()).asIntOrBoolean();
     }
 
-    public static NOptional<Integer> getInt(HNode t, NDocNodeRendererContext ctx, String propName, String... propNames) {
+    public static NOptional<Integer> getInt(NDocNode t, NDocNodeRendererContext ctx, String propName, String... propNames) {
         return NDocObjEx.of(ctx.computePropertyValue(t, propName, propNames).orNull()).asInt();
     }
 
-    public static NOptional<String> getStringOrName(HNode t, NDocNodeRendererContext ctx, String propName) {
+    public static NOptional<String> getStringOrName(NDocNode t, NDocNodeRendererContext ctx, String propName) {
         return NDocObjEx.of(ctx.computePropertyValue(t, propName).orNull()).asStringOrName();
     }
 
-    public static NOptional<Paint> getPaint(HNode t, NDocNodeRendererContext ctx, String propName, String... propNames) {
+    public static NOptional<Paint> getPaint(NDocNode t, NDocNodeRendererContext ctx, String propName, String... propNames) {
         NDocObjEx r = NDocObjEx.of(ctx.computePropertyValue(t, propName, propNames).orElse(null));
         return NOptional.of(r.asPaint().orElse(null));
     }
 
-    public static NOptional<Color> getColor(HNode t, NDocNodeRendererContext ctx, String propName, String... propNames) {
+    public static NOptional<Color> getColor(NDocNode t, NDocNodeRendererContext ctx, String propName, String... propNames) {
         NDocObjEx r = NDocObjEx.of(ctx.computePropertyValue(t, propName, propNames).orElse(null));
         return NOptional.of(r.asColor().orElse(null));
     }
 
-    public static NOptional<Double> getDouble(HNode t, NDocNodeRendererContext ctx, String s) {
+    public static NOptional<Double> getDouble(NDocNode t, NDocNodeRendererContext ctx, String s) {
         return NDocObjEx.of(ctx.computePropertyValue(t, s).orNull()).asDouble();
     }
 
-    public static NOptional<Double2> getDouble2(HNode t, NDocNodeRendererContext ctx, String s) {
+    public static NOptional<NDocDouble2> getDouble2(NDocNode t, NDocNodeRendererContext ctx, String s) {
         return NDocObjEx.of(ctx.computePropertyValue(t, s).orNull()).asDouble2();
     }
 
-    public static NOptional<NElement> getTson(HNode t, NDocNodeRendererContext ctx, String s) {
+    public static NOptional<NElement> getTson(NDocNode t, NDocNodeRendererContext ctx, String s) {
         return NDocObjEx.of(ctx.computePropertyValue(t, s).orNull()).asTson();
     }
 
-    public static NOptional<Double2> getDouble2OrHAlign(HNode t, NDocNodeRendererContext ctx, String s) {
+    public static NOptional<NDocDouble2> getDouble2OrHAlign(NDocNode t, NDocNodeRendererContext ctx, String s) {
         return NDocObjEx.of(ctx.computePropertyValue(t, s).orNull()).asDouble2OrHAlign();
     }
 
-    public static NOptional<ElemNumber2> getNNumberElement2Or1OrHAlign(HNode t, NDocNodeRendererContext ctx, String s) {
+    public static NOptional<NDocElemNumber2> getNNumberElement2Or1OrHAlign(NDocNode t, NDocNodeRendererContext ctx, String s) {
         return NDocObjEx.of(ctx.computePropertyValue(t, s).orNull()).asNNumberElement2Or1OrHAlign();
     }
 
-    public static NOptional<Double4> getDouble4(HNode t, NDocNodeRendererContext ctx, String s) {
+    public static NOptional<NDocDouble4> getDouble4(NDocNode t, NDocNodeRendererContext ctx, String s) {
         return NDocObjEx.of(ctx.computePropertyValue(t, s).orNull()).asDouble4();
     }
 
-    public static NOptional<double[]> getDoubleArray(HNode t, NDocNodeRendererContext ctx, String s) {
+    public static NOptional<double[]> getDoubleArray(NDocNode t, NDocNodeRendererContext ctx, String s) {
         return NDocObjEx.of(ctx.computePropertyValue(t, s).orNull()).asDoubleArray();
     }
 
-    public static NOptional<Padding> getPadding(HNode t, NDocNodeRendererContext ctx, String s) {
+    public static NOptional<Padding> getPadding(NDocNode t, NDocNodeRendererContext ctx, String s) {
         return NDocObjEx.of(ctx.computePropertyValue(t, s).orNull()).asPadding();
     }
 
-    public static NOptional<Rotation> getRotation(HNode t, NDocNodeRendererContext ctx, String s) {
+    public static NOptional<Rotation> getRotation(NDocNode t, NDocNodeRendererContext ctx, String s) {
         return NDocObjEx.of(ctx.computePropertyValue(t, s).orNull()).asRotation();
     }
 }
