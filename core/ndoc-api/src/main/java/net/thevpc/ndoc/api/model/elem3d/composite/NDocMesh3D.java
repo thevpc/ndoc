d0fe1bdeb209f09a1e5f829d8570686f1240f3b4
thevpc <taha.bensalah@gmail.com>
2025-07-17 11:27:54 +0100

    [CHANGED] rename halfa to ndoc

--- core/ndoc-api/src/main/java/net/thevpc/ndoc/api/model/elem3d/composite/NDocMesh3D.java
+++ core/ndoc-api/src/main/java/net/thevpc/ndoc/api/model/elem3d/composite/NDocMesh3D.java
@@ -1,8 +1,8 @@
 package net.thevpc.ndoc.api.model.elem3d.composite;
 
-import net.thevpc.ndoc.api.model.elem3d.HPoint3D;
-import net.thevpc.ndoc.api.model.elem3d.HVector3D;
-import net.thevpc.ndoc.api.model.elem3d.primitives.Element3DTriangle;
+import net.thevpc.ndoc.api.model.elem3d.NDocPoint3D;
+import net.thevpc.ndoc.api.model.elem3d.NDocVector3D;
+import net.thevpc.ndoc.api.model.elem3d.primitives.NDocElement3DTriangle;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,15 +10,15 @@ import java.util.List;
 /**
  * thanks to https://github.com/caosdoar/spheres/blob/master/src/spheres.cpp
  */
-public class Mesh3D {
-    List<HPoint3D> vertices = new ArrayList<>();
-    List<Mesh3DTriangleIndices> triangles = new ArrayList<>();
+public class NDocMesh3D {
+    List<NDocPoint3D> vertices = new ArrayList<>();
+    List<NDocMesh3DTriangleIndices> triangles = new ArrayList<>();
 
-    public List<HPoint3D> getVertices() {
+    public List<NDocPoint3D> getVertices() {
         return vertices;
     }
 
-    public List<Mesh3DTriangleIndices> getTriangles() {
+    public List<NDocMesh3DTriangleIndices> getTriangles() {
         return triangles;
     }
 
@@ -26,9 +26,9 @@ public class Mesh3D {
         return triangles.size();
     }
 
-    public Element3DTriangle triangleAt(int index) {
-        Mesh3DTriangleIndices t = triangles.get(index);
-        return new Element3DTriangle(
+    public NDocElement3DTriangle triangleAt(int index) {
+        NDocMesh3DTriangleIndices t = triangles.get(index);
+        return new NDocElement3DTriangle(
                 vertices.get(t.p1),
                 vertices.get(t.p2),
                 vertices.get(t.p3)
@@ -38,17 +38,17 @@ public class Mesh3D {
     }
 
     public void addTriangle(int a, int b, int c) {
-        triangles.add(new Mesh3DTriangleIndices(a, b, c));
+        triangles.add(new NDocMesh3DTriangleIndices(a, b, c));
     }
 
     public void addQuad(int a, int b, int c, int d) {
-        triangles.add(new Mesh3DTriangleIndices(a, b, c));
-        triangles.add(new Mesh3DTriangleIndices(a, c, d));
+        triangles.add(new NDocMesh3DTriangleIndices(a, b, c));
+        triangles.add(new NDocMesh3DTriangleIndices(a, c, d));
     }
 
     public void addQuadAlt(int a, int b, int c, int d) {
-        triangles.add(new Mesh3DTriangleIndices(a, b, d));
-        triangles.add(new Mesh3DTriangleIndices(b, c, d));
+        triangles.add(new NDocMesh3DTriangleIndices(a, b, d));
+        triangles.add(new NDocMesh3DTriangleIndices(b, c, d));
     }
 
     public void clear() {
@@ -56,18 +56,18 @@ public class Mesh3D {
         triangles.clear();
     }
 
-    public double distance(HPoint3D p, int tidx) {
-        Mesh3DTriangleIndices tr = triangles.get(tidx);
+    public double distance(NDocPoint3D p, int tidx) {
+        NDocMesh3DTriangleIndices tr = triangles.get(tidx);
         int idx0 = tr.p1;
         int idx1 = tr.p2;
         int idx2 = tr.p3;
-        HVector3D v0 = vertices.get(idx0).asVector();
-        HVector3D v1 = vertices.get(idx1).asVector();
-        HVector3D v2 = vertices.get(idx2).asVector();
-        HVector3D bv = v0;
-        HVector3D e0 = v1.minus(v0);
-        HVector3D e1 = v2.minus(v0);
-        HVector3D dv = bv.minus(p.asVector());
+        NDocVector3D v0 = vertices.get(idx0).asVector();
+        NDocVector3D v1 = vertices.get(idx1).asVector();
+        NDocVector3D v2 = vertices.get(idx2).asVector();
+        NDocVector3D bv = v0;
+        NDocVector3D e0 = v1.minus(v0);
+        NDocVector3D e1 = v2.minus(v0);
+        NDocVector3D dv = bv.minus(p.asVector());
         double a = e0.dot(e0);
         double b = e0.dot(e1);
         double c = e1.dot(e1);
@@ -148,25 +148,25 @@ public class Mesh3D {
         return (p.asVector().minus((v0.plus(e0.mul(s)).plus(e1.mul(t))))).norm();
     }
 
-    private HPoint3D centerPoint(Mesh3DTriangleIndices t) {
-        HPoint3D p1 = vertices.get(t.p1);
-        HPoint3D p2 = vertices.get(t.p2);
-        HPoint3D p3 = vertices.get(t.p3);
+    private NDocPoint3D centerPoint(NDocMesh3DTriangleIndices t) {
+        NDocPoint3D p1 = vertices.get(t.p1);
+        NDocPoint3D p2 = vertices.get(t.p2);
+        NDocPoint3D p3 = vertices.get(t.p3);
         double x = (p1.x + p2.x + p3.x) / 3;
         double y = (p1.y + p2.y + p3.y) / 3;
         double z = (p1.z + p2.z + p3.z) / 3;
-        return new HPoint3D(x, y, z);
+        return new NDocPoint3D(x, y, z);
     }
 
     public void sortByZ() {
         triangles.sort((a, b) -> {
-            HPoint3D aa = centerPoint(a);
-            HPoint3D bb = centerPoint(b);
+            NDocPoint3D aa = centerPoint(a);
+            NDocPoint3D bb = centerPoint(b);
             return Double.compare(aa.z, bb.z);
         });
     }
 
-    public double distance(HPoint3D p) {
+    public double distance(NDocPoint3D p) {
         double min = 10e10;
         for (int i = 0; i < triangles.size(); i += 3) {
             min = Math.min(min, distance(p, i));
