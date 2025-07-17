e08c93491ee903649cfc00ca0b009047e9c472bb
thevpc <taha.bensalah@gmail.com>
2025-07-17 10:43:45 +0100

    [CHANGED] rename halfa to ndoc

--- core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/HEngineUtils.java
+++ core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/HEngineUtils.java
@@ -3,18 +3,18 @@ package net.thevpc.ndoc.engine;
 import net.thevpc.nuts.io.NPath;
 
 public class HEngineUtils {
-    public static final String HALFA_EXT_NAME = "ndoc";
-    public static final String HALFA_EXT = ".ndoc";
+    public static final String NDOC_EXT_NAME = "ndoc";
+    public static final String NDOC_EXT = ".ndoc";
     public static final String NDOC_EXT_STAR = "*.ndoc";
     public static final String NDOC_EXT_STAR_STAR = "**/*.ndoc";
 
-    public static boolean isHalfaFile(NPath path) {
-        return path != null && isHalfaFile(path.getName());
+    public static boolean isNDocFile(NPath path) {
+        return path != null && isNDocFile(path.getName());
     }
 
-    public static boolean isHalfaFile(String name) {
+    public static boolean isNDocFile(String name) {
         if (name != null) {
-            return name.toLowerCase().endsWith(HALFA_EXT);
+            return name.toLowerCase().endsWith(NDOC_EXT);
         }
         return false;
     }
@@ -22,9 +22,9 @@ public class HEngineUtils {
     public static int comparePaths(NPath o1, NPath o2) {
         String a = o1.toString();
         String b = o2.toString();
-        if (isHalfaFile(a) && isHalfaFile(b)) {
-            a = a.substring(0, a.length() - HALFA_EXT.length());
-            b = b.substring(0, b.length() - HALFA_EXT.length());
+        if (isNDocFile(a) && isNDocFile(b)) {
+            a = a.substring(0, a.length() - NDOC_EXT.length());
+            b = b.substring(0, b.length() - NDOC_EXT.length());
         }
         return a.compareTo(b);
     }
