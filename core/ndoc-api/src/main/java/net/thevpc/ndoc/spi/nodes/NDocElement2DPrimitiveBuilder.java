40763fb959464d235896aa5bcd87c977bb1da193
thevpc <taha.bensalah@gmail.com>
2025-07-17 11:29:43 +0100

    [CHANGED] rename halfa to ndoc

--- core/ndoc-api/src/main/java/net/thevpc/ndoc/spi/nodes/NDocElement2DPrimitiveBuilder.java
+++ core/ndoc-api/src/main/java/net/thevpc/ndoc/spi/nodes/NDocElement2DPrimitiveBuilder.java
@@ -1,8 +1,8 @@
 package net.thevpc.ndoc.spi.nodes;
 
-import net.thevpc.ndoc.api.model.elem2d.Element2DPrimitive;
-import net.thevpc.ndoc.api.model.elem2d.HElement2D;
+import net.thevpc.ndoc.api.model.elem2d.NDocElement2DPrimitive;
+import net.thevpc.ndoc.api.model.elem2d.NDocElement2D;
 
 public interface NDocElement2DPrimitiveBuilder {
-    Element2DPrimitive[] toPrimitives(HElement2D e);
+    NDocElement2DPrimitive[] toPrimitives(NDocElement2D e);
 }
