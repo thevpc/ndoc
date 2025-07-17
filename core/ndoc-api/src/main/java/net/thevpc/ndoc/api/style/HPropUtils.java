5a3748e000f80bad00cec87173a0368430ff490a
thevpc <taha.bensalah@gmail.com>
2025-07-17 11:29:11 +0100

    [CHANGED] rename halfa to ndoc

--- core/ndoc-api/src/main/java/net/thevpc/ndoc/api/style/HPropUtils.java
+++ core/ndoc-api/src/main/java/net/thevpc/ndoc/api/style/HPropUtils.java
@@ -1,8 +1,8 @@
 package net.thevpc.ndoc.api.style;
 
-import net.thevpc.ndoc.api.model.elem2d.HPoint2D;
-import net.thevpc.ndoc.api.model.elem3d.HPoint3D;
-import net.thevpc.ndoc.api.model.node.HNode;
+import net.thevpc.ndoc.api.model.elem2d.NDocPoint2D;
+import net.thevpc.ndoc.api.model.elem3d.NDocPoint3D;
+import net.thevpc.ndoc.api.model.node.NDocNode;
 import net.thevpc.ndoc.api.util.NElemUtils;
 import net.thevpc.ndoc.spi.eval.NDocObjEx;
 import net.thevpc.nuts.util.NOptional;
@@ -12,31 +12,31 @@ import java.util.Arrays;
 import java.util.List;
 
 public class HPropUtils {
-    public static boolean addPoint(HNode line, HPoint2D point) {
+    public static boolean addPoint(NDocNode line, NDocPoint2D point) {
         if (point != null) {
-            NDocObjEx o = NDocObjEx.of(line.getPropertyValue(HPropName.POINTS).orNull());
-            NOptional<HPoint2D[]> hPoint2DArray = o.asHPoint2DArray();
-            List<HPoint2D> v = new ArrayList<>();
+            NDocObjEx o = NDocObjEx.of(line.getPropertyValue(NDocPropName.POINTS).orNull());
+            NOptional<NDocPoint2D[]> hPoint2DArray = o.asHPoint2DArray();
+            List<NDocPoint2D> v = new ArrayList<>();
             if (hPoint2DArray.isPresent()) {
                 v.addAll(Arrays.asList(hPoint2DArray.get()));
             }
             v.add(point);
-            line.setProperty(HPropName.POINTS, NElemUtils.toElement(v.toArray(new HPoint2D[0])));
+            line.setProperty(NDocPropName.POINTS, NElemUtils.toElement(v.toArray(new NDocPoint2D[0])));
             return true;
         }
         return false;
     }
 
-    public static boolean addPoint(HNode line, HPoint3D point) {
+    public static boolean addPoint(NDocNode line, NDocPoint3D point) {
         if (point != null) {
-            NDocObjEx o = NDocObjEx.of(line.getPropertyValue(HPropName.POINTS).orNull());
-            NOptional<HPoint3D[]> hPoint2DArray = o.asHPoint3DArray();
-            List<HPoint3D> v = new ArrayList<>();
+            NDocObjEx o = NDocObjEx.of(line.getPropertyValue(NDocPropName.POINTS).orNull());
+            NOptional<NDocPoint3D[]> hPoint2DArray = o.asHPoint3DArray();
+            List<NDocPoint3D> v = new ArrayList<>();
             if (hPoint2DArray.isPresent()) {
                 v.addAll(Arrays.asList(hPoint2DArray.get()));
             }
             v.add(point);
-            line.setProperty(HPropName.POINTS, NElemUtils.toElement(v.toArray(new HPoint3D[0])));
+            line.setProperty(NDocPropName.POINTS, NElemUtils.toElement(v.toArray(new NDocPoint3D[0])));
             return true;
         }
         return false;
