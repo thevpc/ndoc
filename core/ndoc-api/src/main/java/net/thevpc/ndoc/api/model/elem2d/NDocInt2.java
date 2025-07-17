18f84dbe09cb95a69b6700fca4f2df0626e7bd36
thevpc <taha.bensalah@gmail.com>
2025-07-17 11:27:26 +0100

    [CHANGED] rename halfa to ndoc

--- core/ndoc-api/src/main/java/net/thevpc/ndoc/api/model/elem2d/NDocInt2.java
+++ core/ndoc-api/src/main/java/net/thevpc/ndoc/api/model/elem2d/NDocInt2.java
@@ -3,11 +3,11 @@ package net.thevpc.ndoc.api.model.elem2d;
 import net.thevpc.nuts.elem.NElement;
 import net.thevpc.nuts.elem.NToElement;
 
-public class Int2 implements NToElement {
+public class NDocInt2 implements NToElement {
     private Integer x;
     private Integer y;
 
-    public Int2(Integer x, Integer y) {
+    public NDocInt2(Integer x, Integer y) {
         this.x = x;
         this.y = y;
     }
