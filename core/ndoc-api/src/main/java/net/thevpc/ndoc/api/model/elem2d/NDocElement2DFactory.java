f858f67d266d2fb2371de8f5c081e75075885459
thevpc <taha.bensalah@gmail.com>
2025-07-17 11:27:16 +0100

    [CHANGED] rename halfa to ndoc

---
+++ core/ndoc-api/src/main/java/net/thevpc/ndoc/api/model/elem2d/NDocElement2DFactory.java
@@ -0,0 +1,25 @@
+package net.thevpc.ndoc.api.model.elem2d;
+
+import net.thevpc.ndoc.api.model.elem2d.primitives.*;
+
+public class NDocElement2DFactory {
+    public static NDocElement2DLine line(NDocPoint2D from, NDocPoint2D to) {
+        return new NDocElement2DLine(from, to);
+    }
+
+    public static NDocElement2DQuadCurve quad(NDocPoint2D from, NDocPoint2D ctrl, NDocPoint2D to) {
+        return new NDocElement2DQuadCurve(from, to).setCtrl(ctrl);
+    }
+
+    public static NDocElement2DCubicCurve cubic(NDocPoint2D from, NDocPoint2D ctrl1, NDocPoint2D ctrl2, NDocPoint2D to) {
+        return new NDocElement2DCubicCurve(from, to).setCtrl1(ctrl1).setCtrl2(ctrl2);
+    }
+
+    public static NDocElement2DPolygon polygon(NDocPoint2D... points) {
+        return new NDocElement2DPolygon(points, true, true);
+    }
+
+    public static NDocElement2DPolyline polyline(NDocPoint2D... points) {
+        return new NDocElement2DPolyline(points);
+    }
+}
