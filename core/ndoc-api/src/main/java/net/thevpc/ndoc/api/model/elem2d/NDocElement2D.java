7d85eaaeb157576cb6faf8122e9fbac30ebb36dd
thevpc <taha.bensalah@gmail.com>
2025-07-17 11:27:15 +0100

    [CHANGED] rename halfa to ndoc

--- core/ndoc-api/src/main/java/net/thevpc/ndoc/api/model/elem2d/NDocElement2D.java
+++ core/ndoc-api/src/main/java/net/thevpc/ndoc/api/model/elem2d/NDocElement2D.java
@@ -2,5 +2,5 @@ package net.thevpc.ndoc.api.model.elem2d;
 
 import net.thevpc.ndoc.api.model.HElement;
 
-public interface HElement2D extends HElement {
+public interface NDocElement2D extends HElement {
 }
