69c66739a8e4bed483c694eb3fd0ac9669bd7ab9
thevpc <taha.bensalah@gmail.com>
2025-07-18 11:20:52 +0100

    [CHANGED] rename NDoc

--- core/ndoc-elements/src/main/java/net/thevpc/ndoc/elem/base/container/ol/NDocOrderedListRenderer.java
+++ core/ndoc-elements/src/main/java/net/thevpc/ndoc/elem/base/container/ol/NDocOrderedListRenderer.java
@@ -2,7 +2,7 @@ package net.thevpc.ndoc.elem.base.container.ol;
 
 import net.thevpc.ndoc.NDocDocumentFactory;
 import net.thevpc.ndoc.api.model.node.NDocNodeType;
-import net.thevpc.ndoc.api.style.HProp;
+import net.thevpc.ndoc.api.style.NDocProp;
 import net.thevpc.ndoc.api.style.NDocProperties;
 import net.thevpc.ndoc.api.style.NDocPropName;
 import net.thevpc.ndoc.api.model.node.NDocNode;
@@ -32,7 +32,7 @@ public class NDocOrderedListRenderer extends ConvertedNDocNodeRenderer {
                 .setProperty(NDocPropName.COLUMNS, NElement.ofInt(2))
                 .setProperty(NDocPropName.ROWS, NElement.ofInt(2))
                 .setProperty(NDocPropName.COLUMNS_WEIGHT, NElement.ofDoubleArray(1, 20))
-                .setProperties(p.props().toArray(new HProp[0]))
+                .setProperties(p.props().toArray(new NDocProp[0]))
                 ;
     }
 }
