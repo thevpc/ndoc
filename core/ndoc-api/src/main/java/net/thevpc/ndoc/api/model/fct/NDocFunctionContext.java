f18dc088635e5af954c660064a5a09d6fa43182d
thevpc <taha.bensalah@gmail.com>
2025-07-20 21:55:07 +0100

    [WIP] replace inheritance by simple function calls (with inner defs)

--- core/ndoc-api/src/main/java/net/thevpc/ndoc/api/model/fct/NDocFunctionContext.java
+++ core/ndoc-api/src/main/java/net/thevpc/ndoc/api/model/fct/NDocFunctionContext.java
@@ -1,14 +1,7 @@
 package net.thevpc.ndoc.api.model.fct;
 
-import net.thevpc.ndoc.api.model.node.NDocNode;
-import net.thevpc.nuts.elem.NElement;
-import net.thevpc.nuts.util.NOptional;
+
+import net.thevpc.ndoc.api.NDocEngine;
 
 public interface NDocFunctionContext {
-    Object invokeFunction(String name, NDocFunctionArg[] args);
-
-    NDocFunctionArg createNodeArg(NDocNode node);
-
-    NDocFunctionArg createUserObjectArg(Object node);
-    NDocFunctionArg createRawArg(NElement node);
 }
