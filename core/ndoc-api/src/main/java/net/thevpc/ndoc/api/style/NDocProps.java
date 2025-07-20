99dcfae87a07921bc39bc63d2ba8077a55e294f8
thevpc <taha.bensalah@gmail.com>
2025-07-20 21:54:52 +0100

    [WIP] replace inheritance by simple function calls (with inner defs)

--- core/ndoc-api/src/main/java/net/thevpc/ndoc/api/style/NDocProps.java
+++ core/ndoc-api/src/main/java/net/thevpc/ndoc/api/style/NDocProps.java
@@ -5,7 +5,9 @@ import java.awt.*;
 import net.thevpc.ndoc.api.model.elem2d.NDocDouble2;
 import net.thevpc.ndoc.api.model.elem2d.NDocAlign;
 import net.thevpc.ndoc.api.model.elem2d.NDocSize;
+import net.thevpc.ndoc.api.util.NDocUtils;
 import net.thevpc.nuts.elem.NElement;
+import net.thevpc.nuts.elem.NToElement;
 
 /**
  * @author vpc
@@ -13,19 +15,19 @@ import net.thevpc.nuts.elem.NElement;
 public class NDocProps {
 
     public static NDocProp fontSize(Number value) {
-        return new NDocProp(NDocPropName.FONT_SIZE, NElement.ofNumber(value == null ? null : value.doubleValue()));
+        return elem(NDocPropName.FONT_SIZE, NDocUtils.addCompilerDeclarationPathDummy(NElement.ofNumber(value == null ? null : value.doubleValue())));
     }
 
     public static NDocProp fontFamily(String value) {
-        return new NDocProp(NDocPropName.FONT_FAMILY, NElement.ofString(value));
+        return elem(NDocPropName.FONT_FAMILY, NDocUtils.addCompilerDeclarationPathDummy(NElement.ofString(value)));
     }
 
     public static NDocProp width(NDocSize size) {
-        return new NDocProp(NDocPropName.WIDTH, size);
+        return elem(NDocPropName.WIDTH, size);
     }
 
     public static NDocProp height(NDocSize size) {
-        return new NDocProp(NDocPropName.HEIGHT, size);
+        return elem(NDocPropName.HEIGHT, size);
     }
 
 //    public static HProp foregroundColor(Paint color) {
@@ -33,15 +35,15 @@ public class NDocProps {
 //    }
 
     public static NDocProp foregroundColor(int color) {
-        return new NDocProp(NDocPropName.FOREGROUND_COLOR, NElement.ofInt(color));
+        return elem(NDocPropName.FOREGROUND_COLOR, NDocUtils.addCompilerDeclarationPathDummy(NElement.ofInt(color)));
     }
 
     public static NDocProp backgroundColor(int rgb) {
-        return new NDocProp(NDocPropName.BACKGROUND_COLOR, NElement.ofInt(rgb));
+        return elem(NDocPropName.BACKGROUND_COLOR, NDocUtils.addCompilerDeclarationPathDummy(NElement.ofInt(rgb)));
     }
 
     public static NDocProp backgroundColor(Color color) {
-        return new NDocProp(NDocPropName.BACKGROUND_COLOR, color==null?null: NElement.ofInt(color.getRGB()));
+        return elem(NDocPropName.BACKGROUND_COLOR, color == null ? null : NDocUtils.addCompilerDeclarationPathDummy(NElement.ofInt(color.getRGB())));
     }
 
 //    public static HProp lineColor(Paint color) {
@@ -49,7 +51,7 @@ public class NDocProps {
 //    }
 
     public static NDocProp gridColor(Color color) {
-        return new NDocProp(NDocPropName.GRID_COLOR, color==null?null: NElement.ofInt(color.getRGB()));
+        return elem(NDocPropName.GRID_COLOR, color == null ? null : NDocUtils.addCompilerDeclarationPathDummy(NElement.ofInt(color.getRGB())));
     }
 
 //    public static HProp lineColor(int color) {
@@ -65,39 +67,39 @@ public class NDocProps {
     }
 
     public static NDocProp preserveShapeRatio(boolean value) {
-        return new NDocProp(NDocPropName.PRESERVE_ASPECT_RATIO, NElement.ofBoolean(value));
+        return elem(NDocPropName.PRESERVE_ASPECT_RATIO, NDocUtils.addCompilerDeclarationPathDummy((NElement.ofBoolean(value))));
     }
 
     public static NDocProp drawContour(boolean value) {
-        return new NDocProp(NDocPropName.DRAW_CONTOUR, NElement.ofBoolean(value));
+        return elem(NDocPropName.DRAW_CONTOUR, NDocUtils.addCompilerDeclarationPathDummy(NElement.ofBoolean(value)));
     }
 
     public static NDocProp drawGrid(boolean value) {
-        return new NDocProp(NDocPropName.DRAW_GRID, NElement.ofBoolean(value));
+        return elem(NDocPropName.DRAW_GRID, NDocUtils.addCompilerDeclarationPathDummy(NElement.ofBoolean(value)));
     }
 
     public static NDocProp fillBackground(boolean value) {
-        return new NDocProp(NDocPropName.FILL_BACKGROUND, NElement.ofBoolean(value));
+        return elem(NDocPropName.FILL_BACKGROUND, NDocUtils.addCompilerDeclarationPathDummy(NElement.ofBoolean(value)));
     }
 
     public static NDocProp fontBold(boolean value) {
-        return new NDocProp(NDocPropName.FONT_BOLD, NElement.ofBoolean(value));
+        return elem(NDocPropName.FONT_BOLD, NDocUtils.addCompilerDeclarationPathDummy(NElement.ofBoolean(value)));
     }
 
     public static NDocProp fontItalic(boolean value) {
-        return new NDocProp(NDocPropName.FONT_ITALIC, NElement.ofBoolean(value));
+        return elem(NDocPropName.FONT_ITALIC, NDocUtils.addCompilerDeclarationPathDummy(NElement.ofBoolean(value)));
     }
 
     public static NDocProp fontUnderlined(boolean value) {
-        return new NDocProp(NDocPropName.FONT_UNDERLINED, NElement.ofBoolean(value));
+        return elem(NDocPropName.FONT_UNDERLINED, NDocUtils.addCompilerDeclarationPathDummy(NElement.ofBoolean(value)));
     }
 
     public static NDocProp fontStrike(boolean value) {
-        return new NDocProp(NDocPropName.FONT_STRIKE, NElement.ofBoolean(value));
+        return elem(NDocPropName.FONT_STRIKE, NDocUtils.addCompilerDeclarationPathDummy(NElement.ofBoolean(value)));
     }
 
     public static NDocProp position(NDocDouble2 p) {
-        return new NDocProp(NDocPropName.POSITION, p);
+        return elem(NDocPropName.POSITION, p);
     }
 
     public static NDocProp center() {
@@ -105,86 +107,94 @@ public class NDocProps {
     }
 
     public static NDocProp origin(NDocAlign p) {
-        return new NDocProp(NDocPropName.ORIGIN, p);
+        return elem(NDocPropName.ORIGIN, p);
     }
 
     public static NDocProp origin(double x, double y) {
-        return new NDocProp(NDocPropName.ORIGIN, new NDocDouble2(x, y));
+        return elem(NDocPropName.ORIGIN, new NDocDouble2(x, y));
     }
 
     public static NDocProp position(double x, double y) {
-        return new NDocProp(NDocPropName.POSITION, new NDocDouble2(x, y));
+        return elem(NDocPropName.POSITION, new NDocDouble2(x, y));
     }
 
     public static NDocProp position(NDocAlign p) {
-        return new NDocProp(NDocPropName.POSITION, p);
+        return elem(NDocPropName.POSITION, p);
     }
 
     public static NDocProp size(double x, double y) {
-        return new NDocProp(NDocPropName.SIZE, new NDocDouble2(x, y));
+        return elem(NDocPropName.SIZE, new NDocDouble2(x, y));
     }
 
     public static NDocProp size(double x) {
-        return new NDocProp(NDocPropName.SIZE, new NDocDouble2(x, x));
+        return elem(NDocPropName.SIZE, new NDocDouble2(x, x));
     }
 
     public static NDocProp size(NDocDouble2 size) {
-        return new NDocProp(NDocPropName.SIZE, size);
+        return elem(NDocPropName.SIZE, size);
     }
 
     public static NDocProp roundCorner(NDocDouble2 o) {
-        return new NDocProp(NDocPropName.ROUND_CORNER, o);
+        return elem(NDocPropName.ROUND_CORNER, o);
     }
 
     public static NDocProp roundCorner(double x, double y) {
-        return new NDocProp(NDocPropName.ROUND_CORNER, new NDocDouble2(x, y));
+        return elem(NDocPropName.ROUND_CORNER, NDocUtils.addCompilerDeclarationPathDummy(new NDocDouble2(x, y).toElement()));
     }
 
     public static NDocProp threeD(Boolean b) {
-        return new NDocProp(NDocPropName.THEED, NElement.ofBoolean(b));
+        return elem(NDocPropName.THEED, NDocUtils.addCompilerDeclarationPathDummy(NElement.ofBoolean(b)));
     }
 
     public static NDocProp raised(Boolean b) {
-        return new NDocProp(NDocPropName.RAISED, NElement.ofBoolean(b));
+        return elem(NDocPropName.RAISED, NDocUtils.addCompilerDeclarationPathDummy(NElement.ofBoolean(b)));
     }
 
     public static NDocProp columns(int o) {
-        return new NDocProp(NDocPropName.COLUMNS, NElement.ofInt(o));
+        return elem(NDocPropName.COLUMNS, NDocUtils.addCompilerDeclarationPathDummy(NElement.ofInt(o)));
     }
 
     public static NDocProp rows(int o) {
-        return new NDocProp(NDocPropName.ROWS, NElement.ofInt(o));
+        return elem(NDocPropName.ROWS, NDocUtils.addCompilerDeclarationPathDummy(NElement.ofInt(o)));
     }
 
     public static NDocProp rowsWeight(double... w) {
-        return new NDocProp(NDocPropName.ROWS_WEIGHT, NElement.ofDoubleArray(w));
+        return elem(NDocPropName.ROWS_WEIGHT, NElement.ofDoubleArray(w));
     }
 
     public static NDocProp colspan(int c) {
-        return new NDocProp(NDocPropName.COLSPAN, NElement.ofInt(c));
+        return elem(NDocPropName.COLSPAN, NDocUtils.addCompilerDeclarationPathDummy(NElement.ofInt(c)));
     }
 
     public static NDocProp rowspan(int r) {
-        return new NDocProp(NDocPropName.ROWSPAN, NElement.ofInt(r));
+        return elem(NDocPropName.ROWSPAN, NDocUtils.addCompilerDeclarationPathDummy(NElement.ofInt(r)));
     }
 
     public static NDocProp styleClasses(String... array) {
-        return new NDocProp(NDocPropName.CLASS, NElement.ofStringArray(array));
+        return elem(NDocPropName.CLASS, NDocUtils.addCompilerDeclarationPathDummy(NElement.ofStringArray(array)));
     }
 
     public static NDocProp disabled(boolean b) {
-        return new NDocProp(NDocPropName.HIDE, NElement.ofBoolean(b));
+        return elem(NDocPropName.HIDE, NDocUtils.addCompilerDeclarationPathDummy(NElement.ofBoolean(b)));
     }
 
     public static NDocProp name(String name) {
-        return new NDocProp(NDocPropName.NAME, NElement.ofString(name));
+        return elem(NDocPropName.NAME, NDocUtils.addCompilerDeclarationPathDummy(NElement.ofString(name)));
     }
 
     public static NDocProp maxX(double x) {
-        return new NDocProp(NDocPropName.MAX_X, NElement.ofDouble(x));
+        return elem(NDocPropName.MAX_X, NDocUtils.addCompilerDeclarationPathDummy(NElement.ofDouble(x)));
     }
 
     public static NDocProp maxY(double x) {
-        return new NDocProp(NDocPropName.MAX_Y, NElement.ofDouble(x));
+        return elem(NDocPropName.MAX_Y, NDocUtils.addCompilerDeclarationPathDummy(NElement.ofDouble(x)));
+    }
+
+    public static NDocProp elem(String name, NElement v) {
+        return new NDocProp(name, v, null);
+    }
+
+    public static NDocProp elem(String name, NToElement v) {
+        return new NDocProp(name,(v==null?null:NDocUtils.addCompilerDeclarationPathDummy(v.toElement())), null);
     }
 }
