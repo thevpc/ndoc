0040327da4dcbd8cf3fba1aeb46af3592e814ace
thevpc <taha.bensalah@gmail.com>
2025-07-20 21:59:41 +0100

    [WIP] changing inheritance with functions

--- core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/fct/DefaultNDocFunctionContext.java
+++ core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/fct/DefaultNDocFunctionContext.java
@@ -1,74 +1,55 @@
 package net.thevpc.ndoc.engine.fct;
 
 import net.thevpc.ndoc.api.NDocEngine;
-import net.thevpc.ndoc.api.model.fct.NDocFunctionArg;
 import net.thevpc.ndoc.api.model.fct.NDocFunctionContext;
-import net.thevpc.ndoc.api.model.node.NDocNode;
-import net.thevpc.nuts.elem.NElement;
+import net.thevpc.ndoc.api.model.node.NDocItem;
 
 public class DefaultNDocFunctionContext implements NDocFunctionContext {
-    private NDocNode node;
-    private NDocEngine engine;
 
-    public DefaultNDocFunctionContext(NDocNode node, NDocEngine engine) {
-        this.node = node;
-        this.engine = engine;
+    public DefaultNDocFunctionContext(NDocItem node, NDocEngine engine) {
     }
 
-    @Override
-    public Object invokeFunction(String name, NDocFunctionArg[] args) {
-        return engine.findFunction(node, name, args).get().invoke(args, this);
-    }
+//    @Override
+//    public Object invokeFunction(String name, NDocFunctionArg[] args) {
+//        return engine.findFunction(node, name, args).get().invoke(args, this);
+//    }
+//
 
-
-    @Override
-    public NDocFunctionArg createNodeArg(NDocNode node) {
-        return new NDocNodeFunctionArg(node);
-    }
-
-    @Override
-    public NDocFunctionArg createUserObjectArg(Object node) {
-        return new UserObjectFunctionArg(node);
-    }
-
-    @Override
-    public NDocFunctionArg createRawArg(NElement node) {
-        return new NDocNElementFunctionArg(node);
-    }
-
-    private class NDocNodeFunctionArg implements NDocFunctionArg {
-        NDocNode node;
-
-        public NDocNodeFunctionArg(NDocNode node) {
-            this.node = node;
-        }
-
-        public NDocNode get() {
-            return node.copy();
-        }
-    }
-
-    private class NDocNElementFunctionArg implements NDocFunctionArg {
-        NElement node;
-
-        public NDocNElementFunctionArg(NElement node) {
-            this.node = node;
-        }
-
-        public Object get() {
-            return node;
-        }
-    }
-
-    private class UserObjectFunctionArg implements NDocFunctionArg {
-        Object node;
-
-        public UserObjectFunctionArg(Object node) {
-            this.node = node;
-        }
-
-        public Object get() {
-            return node;
-        }
-    }
+//    @Override
+//    public NDocFunctionArg createNodeArg(NDocNode node) {
+//        return new NDocNodeFunctionArg(node);
+//    }
+//
+//    @Override
+//    public NDocFunctionArg createUserObjectArg(Object node) {
+//        return new UserObjectFunctionArg(node);
+//    }
+//
+//
+//
+//    private class NDocNodeFunctionArg implements NDocFunctionArg {
+//        NDocNode node;
+//
+//        public NDocNodeFunctionArg(NDocNode node) {
+//            this.node = node;
+//        }
+//
+//        public NDocNode get() {
+//            return node.copy();
+//        }
+//    }
+//
+//
+//
+//    private class UserObjectFunctionArg implements NDocFunctionArg {
+//        Object node;
+//
+//        public UserObjectFunctionArg(Object node) {
+//            this.node = node;
+//        }
+//
+//        public Object get() {
+//            return node;
+//        }
+//    }
 }
