f25f0595fade3fb7f6f49c26fe17fca40b86905e
thevpc <taha.bensalah@gmail.com>
2025-07-20 21:59:04 +0100

    [WIP] changing inheritance with functions

--- core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/control/CtrlNDocNodeBase.java
+++ core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/control/CtrlNDocNodeBase.java
@@ -1,10 +1,11 @@
 package net.thevpc.ndoc.engine.control;
 
 import net.thevpc.ndoc.api.model.node.NDocNodeCtrl;
+import net.thevpc.ndoc.api.resources.NDocResource;
 import net.thevpc.ndoc.spi.base.model.DefaultNDocNode;
 
 public abstract class CtrlNDocNodeBase extends DefaultNDocNode implements NDocNodeCtrl {
-    public CtrlNDocNodeBase(String nodeType) {
-        super(nodeType);
+    public CtrlNDocNodeBase(String nodeType, NDocResource source) {
+        super(nodeType,source);
     }
 }
