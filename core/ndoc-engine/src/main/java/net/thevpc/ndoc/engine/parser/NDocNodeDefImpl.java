f1d777c866548da32dc2351890541519dd4da099
thevpc <taha.bensalah@gmail.com>
2025-07-20 21:59:52 +0100

    [WIP] changing inheritance with functions

--- core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/parser/NDocNodeDefImpl.java
+++ core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/parser/NDocNodeDefImpl.java
@@ -4,18 +4,28 @@ import net.thevpc.ndoc.api.model.node.NDocNode;
 import net.thevpc.ndoc.api.model.node.NDocNodeDef;
 import net.thevpc.ndoc.api.model.node.NDocNodeDefParam;
 import net.thevpc.ndoc.api.resources.NDocResource;
+import net.thevpc.ndoc.spi.base.model.DefaultNDocNode;
 
 public class NDocNodeDefImpl implements NDocNodeDef {
     private final String templateName;
     private final NDocNodeDefParam[] params;
     private final NDocNode[]  body;
     private final NDocResource source;
+    private final NDocNode parent;
 
-    public NDocNodeDefImpl(String templateName, NDocNodeDefParam[] params, NDocNode[] body, NDocResource source) {
+    public NDocNodeDefImpl(NDocNode parent,String templateName, NDocNodeDefParam[] params, NDocNode[] body, NDocResource source) {
         this.templateName = templateName;
         this.params = params;
         this.body = body;
+        for (NDocNode nDocNode : body) {
+            ((DefaultNDocNode)nDocNode).setParent(this);
+        }
         this.source = source;
+        this.parent = parent;
+    }
+
+    public NDocNode parent() {
+        return parent;
     }
 
     public NDocResource source() {
