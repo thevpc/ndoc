750e424a636c92609bae7fa794f80c90d30db11f
thevpc <taha.bensalah@gmail.com>
2025-07-21 10:23:42 +0100

    [CHANGED] removed messages from method parameters, now engine always has messages

--- core/ndoc-api/src/main/java/net/thevpc/ndoc/api/model/fct/NDocFunctionContext.java
+++ core/ndoc-api/src/main/java/net/thevpc/ndoc/api/model/fct/NDocFunctionContext.java
@@ -2,6 +2,9 @@ package net.thevpc.ndoc.api.model.fct;
 
 
 import net.thevpc.ndoc.api.NDocEngine;
+import net.thevpc.ndoc.api.document.NDocLogger;
 
 public interface NDocFunctionContext {
+    NDocLogger messages();
+    NDocEngine engine();
 }
