f3546eee5f5d355f7b4ca572626b7272ae01d50d
thevpc <taha.bensalah@gmail.com>
2025-07-23 09:53:35 +0100

    [CHANGED] some refactor core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/control/CtrlNDocNodeCall.java

--- core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/control/CtrlNDocNodeCall.java
+++ core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/control/CtrlNDocNodeCall.java
@@ -1,8 +1,8 @@
 package net.thevpc.ndoc.engine.control;
 
-import net.thevpc.ndoc.api.model.node.NDocNode;
-import net.thevpc.ndoc.api.model.node.NDocNodeType;
-import net.thevpc.ndoc.api.resources.NDocResource;
+import net.thevpc.ndoc.api.document.node.NDocNode;
+import net.thevpc.ndoc.api.document.node.NDocNodeType;
+import net.thevpc.ndoc.api.parser.NDocResource;
 import net.thevpc.nuts.elem.NElement;
 
 import java.util.ArrayList;
