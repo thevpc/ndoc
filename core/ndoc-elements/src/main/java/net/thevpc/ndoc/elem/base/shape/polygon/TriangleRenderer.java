7d62b53406728565076eb587ad9546d4eb83eedf
thevpc <taha.bensalah@gmail.com>
2025-07-23 09:53:34 +0100

    [CHANGED] some refactor core/ndoc-elements/src/main/java/net/thevpc/ndoc/elem/base/shape/polygon/TriangleRenderer.java

--- core/ndoc-elements/src/main/java/net/thevpc/ndoc/elem/base/shape/polygon/TriangleRenderer.java
+++ core/ndoc-elements/src/main/java/net/thevpc/ndoc/elem/base/shape/polygon/TriangleRenderer.java
@@ -1,10 +1,10 @@
 package net.thevpc.ndoc.elem.base.shape.polygon;
 
-import net.thevpc.ndoc.api.model.elem2d.NDocPoint2D;
-import net.thevpc.ndoc.api.model.node.NDocNode;
-import net.thevpc.ndoc.api.model.node.NDocNodeType;
-import net.thevpc.ndoc.api.style.NDocProperties;
-import net.thevpc.ndoc.spi.renderer.NDocNodeRendererContext;
+import net.thevpc.ndoc.api.document.elem2d.NDocPoint2D;
+import net.thevpc.ndoc.api.document.node.NDocNode;
+import net.thevpc.ndoc.api.document.node.NDocNodeType;
+import  net.thevpc.ndoc.api.document.style.NDocProperties;
+import net.thevpc.ndoc.api.renderer.NDocNodeRendererContext;
 
 public class TriangleRenderer extends NDocPolygonBaseRenderer {
     NDocProperties defaultStyles = new NDocProperties();
