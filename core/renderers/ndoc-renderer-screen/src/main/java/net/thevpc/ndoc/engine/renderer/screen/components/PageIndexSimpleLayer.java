ab7bfbe344145c3a810d311ef510dc7b29acb45e
thevpc <taha.bensalah@gmail.com>
2025-07-23 09:53:36 +0100

    [CHANGED] some refactor core/renderers/ndoc-renderer-screen/src/main/java/net/thevpc/ndoc/engine/renderer/screen/components/PageIndexSimpleLayer.java

--- core/renderers/ndoc-renderer-screen/src/main/java/net/thevpc/ndoc/engine/renderer/screen/components/PageIndexSimpleLayer.java
+++ core/renderers/ndoc-renderer-screen/src/main/java/net/thevpc/ndoc/engine/renderer/screen/components/PageIndexSimpleLayer.java
@@ -1,8 +1,8 @@
 package net.thevpc.ndoc.engine.renderer.screen.components;
 
-import net.thevpc.ndoc.api.model.elem2d.NDocAlign;
+import net.thevpc.ndoc.api.document.elem2d.NDocAlign;
 import net.thevpc.ndoc.engine.renderer.screen.DocumentView;
-import net.thevpc.ndoc.spi.renderer.NDocGraphics;
+import net.thevpc.ndoc.api.renderer.NDocGraphics;
 
 import java.awt.*;
 
