1f58de5252d8a1b36325cda768e985f63dc78281
thevpc <taha.bensalah@gmail.com>
2025-07-23 09:53:35 +0100

    [CHANGED] some refactor core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/control/CtrlNDocNodeBase.java

--- core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/control/CtrlNDocNodeBase.java
+++ core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/control/CtrlNDocNodeBase.java
@@ -1,8 +1,8 @@
 package net.thevpc.ndoc.engine.control;
 
-import net.thevpc.ndoc.api.model.node.NDocNodeCtrl;
-import net.thevpc.ndoc.api.resources.NDocResource;
-import net.thevpc.ndoc.spi.base.model.DefaultNDocNode;
+import net.thevpc.ndoc.api.document.node.NDocNodeCtrl;
+import net.thevpc.ndoc.api.parser.NDocResource;
+import net.thevpc.ndoc.api.base.model.DefaultNDocNode;
 
 public abstract class CtrlNDocNodeBase extends DefaultNDocNode implements NDocNodeCtrl {
     public CtrlNDocNodeBase(String nodeType, NDocResource source) {
