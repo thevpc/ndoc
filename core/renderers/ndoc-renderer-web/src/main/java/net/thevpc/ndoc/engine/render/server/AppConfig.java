63edc3d1c0bf03f301dde9240a7dde317394098c
thevpc <taha.bensalah@gmail.com>
2025-07-23 09:53:36 +0100

    [CHANGED] some refactor core/renderers/ndoc-renderer-web/src/main/java/net/thevpc/ndoc/engine/render/server/AppConfig.java

--- core/renderers/ndoc-renderer-web/src/main/java/net/thevpc/ndoc/engine/render/server/AppConfig.java
+++ core/renderers/ndoc-renderer-web/src/main/java/net/thevpc/ndoc/engine/render/server/AppConfig.java
@@ -1,6 +1,6 @@
 package net.thevpc.ndoc.engine.render.server;
 
-import net.thevpc.ndoc.api.NDocEngine;
+import net.thevpc.ndoc.api.engine.NDocEngine;
 import net.thevpc.ndoc.engine.DefaultNDocEngine;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
