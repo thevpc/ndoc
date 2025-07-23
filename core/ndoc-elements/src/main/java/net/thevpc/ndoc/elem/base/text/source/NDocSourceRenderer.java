9565e243e7973615bda09930a62299e1da358da1
thevpc <taha.bensalah@gmail.com>
2025-07-23 09:53:34 +0100

    [CHANGED] some refactor core/ndoc-elements/src/main/java/net/thevpc/ndoc/elem/base/text/source/NDocSourceRenderer.java

--- core/ndoc-elements/src/main/java/net/thevpc/ndoc/elem/base/text/source/NDocSourceRenderer.java
+++ core/ndoc-elements/src/main/java/net/thevpc/ndoc/elem/base/text/source/NDocSourceRenderer.java
@@ -1,18 +1,18 @@
 package net.thevpc.ndoc.elem.base.text.source;
 
-import net.thevpc.ndoc.api.model.node.NDocNodeType;
-import net.thevpc.ndoc.api.model.node.NDocNode;
-import net.thevpc.ndoc.api.style.NDocPropName;
+import net.thevpc.ndoc.api.document.node.NDocNodeType;
+import net.thevpc.ndoc.api.document.node.NDocNode;
+import  net.thevpc.ndoc.api.document.style.NDocPropName;
 import net.thevpc.ndoc.elem.base.text.text.NDocTextRendererBuilderImpl;
 import net.thevpc.ndoc.elem.base.text.text.NutsHighlighterMapper;
-import net.thevpc.ndoc.spi.base.parser.HTextUtils;
-import net.thevpc.ndoc.spi.eval.NDocValueByName;
-import net.thevpc.ndoc.spi.renderer.NDocNodeRendererContext;
-import net.thevpc.ndoc.spi.renderer.text.NDocTextBaseRenderer;
-import net.thevpc.ndoc.spi.renderer.text.NDocTextRendererBuilder;
+import net.thevpc.ndoc.api.base.parser.HTextUtils;
+import net.thevpc.ndoc.api.eval.NDocValueByName;
+import net.thevpc.ndoc.api.renderer.NDocNodeRendererContext;
+import net.thevpc.ndoc.api.renderer.text.NDocTextBaseRenderer;
+import net.thevpc.ndoc.api.renderer.text.NDocTextRendererBuilder;
 import net.thevpc.nuts.util.NStringUtils;
 
-import net.thevpc.ndoc.spi.eval.NDocObjEx;
+import net.thevpc.ndoc.api.eval.NDocObjEx;
 import net.thevpc.nuts.text.NTextCode;
 import net.thevpc.nuts.text.NTexts;
 
