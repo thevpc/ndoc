4ff8c5da3fed124d5f5844e9ce4357f24dc5a847
thevpc <taha.bensalah@gmail.com>
2025-07-23 09:53:34 +0100

    [CHANGED] some refactor core/ndoc-elements/src/main/java/net/thevpc/ndoc/elem/base/filler/NDocVoidParser.java

--- core/ndoc-elements/src/main/java/net/thevpc/ndoc/elem/base/filler/NDocVoidParser.java
+++ core/ndoc-elements/src/main/java/net/thevpc/ndoc/elem/base/filler/NDocVoidParser.java
@@ -4,8 +4,8 @@
  */
 package net.thevpc.ndoc.elem.base.filler;
 
-import net.thevpc.ndoc.api.model.node.NDocNodeType;
-import net.thevpc.ndoc.spi.base.parser.NDocNodeParserBase;
+import net.thevpc.ndoc.api.document.node.NDocNodeType;
+import net.thevpc.ndoc.api.base.parser.NDocNodeParserBase;
 
 /**
  * @author vpc
