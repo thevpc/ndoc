5cdfeec1d691d47b7127a6e70cfa82a20607f518
thevpc <taha.bensalah@gmail.com>
2025-07-23 09:53:35 +0100

    [CHANGED] some refactor core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/renderer/elem3d/NDocCamera3DImpl.java

--- core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/renderer/elem3d/NDocCamera3DImpl.java
+++ core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/renderer/elem3d/NDocCamera3DImpl.java
@@ -1,6 +1,6 @@
 package net.thevpc.ndoc.engine.renderer.elem3d;
 
-import net.thevpc.ndoc.api.model.elem3d.NDocCamera3D;
+import net.thevpc.ndoc.api.document.elem3d.NDocCamera3D;
 
 public class NDocCamera3DImpl implements NDocCamera3D {
 
