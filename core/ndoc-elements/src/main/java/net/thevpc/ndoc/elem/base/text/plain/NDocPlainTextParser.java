11a37162a9e2029b280d3a553c243ecda6152e10
thevpc <taha.bensalah@gmail.com>
2025-07-23 09:53:34 +0100

    [CHANGED] some refactor core/ndoc-elements/src/main/java/net/thevpc/ndoc/elem/base/text/plain/NDocPlainTextParser.java

--- core/ndoc-elements/src/main/java/net/thevpc/ndoc/elem/base/text/plain/NDocPlainTextParser.java
+++ core/ndoc-elements/src/main/java/net/thevpc/ndoc/elem/base/text/plain/NDocPlainTextParser.java
@@ -4,11 +4,11 @@
  */
 package net.thevpc.ndoc.elem.base.text.plain;
 
-import net.thevpc.ndoc.api.model.node.NDocNode;
-import net.thevpc.ndoc.api.model.node.NDocNodeType;
-import net.thevpc.ndoc.api.style.NDocPropName;
-import net.thevpc.ndoc.spi.base.parser.NDocNodeParserBase;
-import net.thevpc.ndoc.spi.base.format.ToElementHelper;
+import net.thevpc.ndoc.api.document.node.NDocNode;
+import net.thevpc.ndoc.api.document.node.NDocNodeType;
+import  net.thevpc.ndoc.api.document.style.NDocPropName;
+import net.thevpc.ndoc.api.base.parser.NDocNodeParserBase;
+import net.thevpc.ndoc.api.base.format.ToElementHelper;
 import net.thevpc.nuts.elem.NElement;
 
 /**
