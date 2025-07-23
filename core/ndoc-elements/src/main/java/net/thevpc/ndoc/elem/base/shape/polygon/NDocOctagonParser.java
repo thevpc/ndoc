e2d43bc04786e6587dc103e73759dd07ddcd238e
thevpc <taha.bensalah@gmail.com>
2025-07-23 09:53:34 +0100

    [CHANGED] some refactor core/ndoc-elements/src/main/java/net/thevpc/ndoc/elem/base/shape/polygon/NDocOctagonParser.java

--- core/ndoc-elements/src/main/java/net/thevpc/ndoc/elem/base/shape/polygon/NDocOctagonParser.java
+++ core/ndoc-elements/src/main/java/net/thevpc/ndoc/elem/base/shape/polygon/NDocOctagonParser.java
@@ -1,7 +1,7 @@
 package net.thevpc.ndoc.elem.base.shape.polygon;
 
-import net.thevpc.ndoc.api.model.node.NDocNodeType;
-import net.thevpc.ndoc.spi.base.parser.NDocNodeParserBase;
+import net.thevpc.ndoc.api.document.node.NDocNodeType;
+import net.thevpc.ndoc.api.base.parser.NDocNodeParserBase;
 
 public class NDocOctagonParser extends NDocNodeParserBase {
 
