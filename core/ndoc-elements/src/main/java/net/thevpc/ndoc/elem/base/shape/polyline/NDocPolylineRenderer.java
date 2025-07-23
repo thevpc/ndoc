4ec607c345203ddaeeefd9e3f9e6dcb1437e56e8
thevpc <taha.bensalah@gmail.com>
2025-07-23 09:53:34 +0100

    [CHANGED] some refactor core/ndoc-elements/src/main/java/net/thevpc/ndoc/elem/base/shape/polyline/NDocPolylineRenderer.java

--- core/ndoc-elements/src/main/java/net/thevpc/ndoc/elem/base/shape/polyline/NDocPolylineRenderer.java
+++ core/ndoc-elements/src/main/java/net/thevpc/ndoc/elem/base/shape/polyline/NDocPolylineRenderer.java
@@ -1,18 +1,18 @@
 package net.thevpc.ndoc.elem.base.shape.polyline;
 
-import net.thevpc.ndoc.api.model.elem2d.NDocBounds2;
-import net.thevpc.ndoc.api.model.elem2d.NDocElement2DFactory;
-import net.thevpc.ndoc.api.model.elem2d.NDocPoint2D;
-import net.thevpc.ndoc.api.model.node.NDocNodeType;
-import net.thevpc.ndoc.api.model.node.NDocNode;
-import net.thevpc.ndoc.api.style.NDocProperties;
-import net.thevpc.ndoc.api.style.NDocPropName;
-import net.thevpc.ndoc.spi.util.NDocNodeRendererUtils;
-import net.thevpc.ndoc.spi.eval.NDocValueByName;
-import net.thevpc.ndoc.spi.renderer.NDocGraphics;
-import net.thevpc.ndoc.spi.renderer.NDocNodeRendererBase;
-import net.thevpc.ndoc.spi.renderer.NDocNodeRendererContext;
-import net.thevpc.ndoc.spi.eval.NDocObjEx;
+import net.thevpc.ndoc.api.document.elem2d.NDocBounds2;
+import net.thevpc.ndoc.api.document.elem2d.NDocElement2DFactory;
+import net.thevpc.ndoc.api.document.elem2d.NDocPoint2D;
+import net.thevpc.ndoc.api.document.node.NDocNodeType;
+import net.thevpc.ndoc.api.document.node.NDocNode;
+import  net.thevpc.ndoc.api.document.style.NDocProperties;
+import  net.thevpc.ndoc.api.document.style.NDocPropName;
+import net.thevpc.ndoc.api.util.NDocNodeRendererUtils;
+import net.thevpc.ndoc.api.eval.NDocValueByName;
+import net.thevpc.ndoc.api.renderer.NDocGraphics;
+import net.thevpc.ndoc.api.renderer.NDocNodeRendererBase;
+import net.thevpc.ndoc.api.renderer.NDocNodeRendererContext;
+import net.thevpc.ndoc.api.eval.NDocObjEx;
 
 import java.awt.*;
 
