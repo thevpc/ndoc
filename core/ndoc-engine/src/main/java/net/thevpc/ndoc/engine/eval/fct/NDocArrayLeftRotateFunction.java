a1da5e3b93e9bf6cad1dc69cdaa661d22329f757
thevpc <taha.bensalah@gmail.com>
2025-07-27 23:11:34 +0100

    Update core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/eval/fct/NDocArrayLeftRotateFunction.java with drop replacement template engine

--- core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/eval/fct/NDocArrayLeftRotateFunction.java
+++ core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/eval/fct/NDocArrayLeftRotateFunction.java
@@ -3,6 +3,7 @@ package net.thevpc.ndoc.engine.eval.fct;
 import net.thevpc.ndoc.api.eval.NDocFunction;
 import net.thevpc.ndoc.api.eval.NDocFunctionArg;
 import net.thevpc.ndoc.api.eval.NDocFunctionContext;
+import net.thevpc.ndoc.api.eval.NDocObjEx;
 import net.thevpc.nuts.elem.NArrayElementBuilder;
 import net.thevpc.nuts.elem.NElement;
 import net.thevpc.nuts.util.NMsg;
@@ -13,7 +14,7 @@ import java.util.List;
 public class NDocArrayLeftRotateFunction implements NDocFunction {
     @Override
     public String name() {
-        return "arrayRightRotate";
+        return "arrayLeftRotate";
     }
 
     @Override
@@ -21,8 +22,9 @@ public class NDocArrayLeftRotateFunction implements NDocFunction {
         if (args.length == 2) {
             NElement u = args[0].eval();
             NElement i = args[1].eval();
-            if (u.isArray() && i.isNumber()) {
-                int iv = i.asNumberValue().get().intValue();
+            NDocObjEx d = NDocObjEx.of(i);
+            if (u.isArray() && d.isNumber()) {
+                int iv = d.asNumber().get().intValue();
                 if (iv == 0) {
                     return u;
                 }
@@ -31,16 +33,16 @@ public class NDocArrayLeftRotateFunction implements NDocFunction {
                 if (children.isEmpty()) {
                     return u;
                 }
-                Collections.rotate(children, iv);
+                Collections.rotate(children, -iv);
                 b.setChildren(children);
                 return b.build();
             }
-            context.messages().log(NMsg.ofC("unable to rotate %s with distance %s", u, i));
+            context.messages().log(NMsg.ofC("unable to left rotate %s with distance %s", u, i));
             return u;
         } else {
             if (args.length > 1) {
                 NElement u = args[0].eval();
-                context.messages().log(NMsg.ofC("arrayRotate: expected 2 arguments, got %s", args.length));
+                context.messages().log(NMsg.ofC("arrayLeftRotate: expected 2 arguments, got %s", args.length));
                 return u;
             }
         }
