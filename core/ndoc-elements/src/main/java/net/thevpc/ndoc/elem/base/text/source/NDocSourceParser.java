f31926bcb5ffe9b8d998f887d1ab8397dd3a88d5
thevpc <taha.bensalah@gmail.com>
2025-07-27 23:11:34 +0100

    Update core/ndoc-elements/src/main/java/net/thevpc/ndoc/elem/base/text/source/NDocSourceParser.java with drop replacement template engine

--- core/ndoc-elements/src/main/java/net/thevpc/ndoc/elem/base/text/source/NDocSourceParser.java
+++ core/ndoc-elements/src/main/java/net/thevpc/ndoc/elem/base/text/source/NDocSourceParser.java
@@ -47,14 +47,15 @@ public class NDocSourceParser extends NDocNodeParserBase {
                             return true;
                         }
                         case "file": {
-                            String path = p.value().asStringValue().get().trim();
-                            NPath nPath = info.context.resolvePath(path);
+                            NElement value = p.value();
+                            //String path = value.asStringValue().get().trim();
+                            NPath nPath = engine().resolvePath(value.asString().get(),info.node);
                             info.context.document().resources().add(nPath);
                             try {
                                 info.node.setProperty(NDocPropName.VALUE, NElement.ofString(nPath.readString().trim()));
                             } catch (Exception ex) {
                                 info.context.messages().log(
-                                        NDocMsg.of(NMsg.ofC("unable to load source file %s as %s", path, nPath).asSevere())
+                                        NMsg.ofC("unable to load source file %s as %s", NDocUtils.snippet(value), nPath).asSevere()
                                 );
                             }
                             return true;
