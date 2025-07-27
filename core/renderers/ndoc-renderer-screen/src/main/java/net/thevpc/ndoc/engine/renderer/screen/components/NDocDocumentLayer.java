864dcfcd67e5347f8efc57cb999fe7adfc1321f9
thevpc <taha.bensalah@gmail.com>
2025-07-27 23:11:35 +0100

    Update core/renderers/ndoc-renderer-screen/src/main/java/net/thevpc/ndoc/engine/renderer/screen/components/NDocDocumentLayer.java with drop replacement template engine

--- core/renderers/ndoc-renderer-screen/src/main/java/net/thevpc/ndoc/engine/renderer/screen/components/NDocDocumentLayer.java
+++ core/renderers/ndoc-renderer-screen/src/main/java/net/thevpc/ndoc/engine/renderer/screen/components/NDocDocumentLayer.java
@@ -2,9 +2,10 @@ package net.thevpc.ndoc.engine.renderer.screen.components;
 
 import net.thevpc.ndoc.engine.renderer.screen.DocumentView;
 import net.thevpc.ndoc.api.renderer.NDocGraphics;
+import net.thevpc.ndoc.engine.renderer.screen.PageView;
 
 import java.awt.*;
 
 public interface NDocDocumentLayer {
-    void draw(DocumentView doc, Dimension size, NDocGraphics g);
+    void draw(DocumentView doc, PageView pageView, Dimension size, NDocGraphics g);
 }
