a211ade5d23216e992b6cf1fdf3e6059c7ae5394
thevpc <taha.bensalah@gmail.com>
2025-07-27 23:11:34 +0100

    Update core/ndoc-api/src/main/java/net/thevpc/ndoc/api/engine/NDocLogger.java with drop replacement template engine

--- core/ndoc-api/src/main/java/net/thevpc/ndoc/api/engine/NDocLogger.java
+++ core/ndoc-api/src/main/java/net/thevpc/ndoc/api/engine/NDocLogger.java
@@ -2,8 +2,12 @@ package net.thevpc.ndoc.api.engine;
 
 
 import net.thevpc.ndoc.api.document.NDocMsg;
+import net.thevpc.ndoc.api.parser.NDocResource;
+import net.thevpc.nuts.util.NMsg;
 
 public interface NDocLogger {
 
     void log(NDocMsg message);
+    void log(NMsg message);
+    void log(NMsg message, NDocResource source);
 }
