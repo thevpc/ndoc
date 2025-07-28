e9cf65433c5fafc5f01e55ad8dfe5fe5f477d0f1
thevpc <taha.bensalah@gmail.com>
2025-07-28 19:19:47 +0100

    Update extensions/ndoc-extension-shapes2d/src/main/java/net/thevpc/ndoc/extension/shapes2d/NDocDonutBuilder.java

--- extensions/ndoc-extension-shapes2d/src/main/java/net/thevpc/ndoc/extension/shapes2d/NDocDonutBuilder.java
+++ extensions/ndoc-extension-shapes2d/src/main/java/net/thevpc/ndoc/extension/shapes2d/NDocDonutBuilder.java
@@ -5,6 +5,7 @@ import net.thevpc.ndoc.api.document.elem2d.NDocPoint2D;
 import net.thevpc.ndoc.api.document.node.NDocNode;
 import net.thevpc.ndoc.api.document.node.NDocNodeType;
 import net.thevpc.ndoc.api.document.style.NDocPropName;
+import net.thevpc.ndoc.api.document.style.NDocProperties;
 import net.thevpc.ndoc.api.eval.NDocObjEx;
 import net.thevpc.ndoc.api.eval.NDocValueByName;
 import net.thevpc.ndoc.api.extension.NDocNodeCustomBuilder;
@@ -14,78 +15,176 @@ import net.thevpc.ndoc.api.renderer.NDocNodeRendererContext;
 import net.thevpc.ndoc.api.util.NDocNodeRendererUtils;
 
 import java.awt.*;
+import java.awt.geom.Arc2D;
+import java.awt.geom.Area;
 import java.awt.geom.GeneralPath;
+import java.lang.reflect.Field;
+import java.util.Arrays;
 
 
 /**
  *
  */
 public class NDocDonutBuilder implements NDocNodeCustomBuilder {
+    NDocProperties defaultStyles = new NDocProperties();
 
     @Override
     public void build(NDocNodeCustomBuilderContext builderContext) {
         builderContext
-                .id(NDocNodeType.ARROW)
-                .parseParamNames(NDocPropName.WIDTH,NDocPropName.HEIGHT,"base","hat")
+                .id(NDocNodeType.DONUT)
+                .parseAsDouble(NDocPropName.INNER_RADIUS,NDocPropName.START_ANGLE,NDocPropName.EXTENT_ANGLE,NDocPropName.DASH)
+                .parseAsInt(NDocPropName.SLICE_COUNT)
+                .parseAsDoubleArray(NDocPropName.SLICES)
+                .parseAsStringArray(NDocPropName.COLORS)
                 .parseDefaultParamNames()
                 .render(this::render)
                 ;
     }
 
 
-    public void render(NDocNode p, NDocNodeRendererContext renderContext,NDocNodeCustomBuilderContext buildContext) {
+    private void render(NDocNode p, NDocNodeRendererContext renderContext,NDocNodeCustomBuilderContext builderContext) {
+        renderDonutOrPie(p, renderContext, builderContext, defaultStyles, true);
+    }
+
+    public static void renderDonutOrPie(NDocNode p, NDocNodeRendererContext renderContext,
+                                  NDocNodeCustomBuilderContext builderContext,NDocProperties defaultStyles,
+                                  boolean isDonut
+                                  ) {
+        renderContext = renderContext.withDefaultStyles(p, defaultStyles);
+
         NDocBounds2 b = NDocValueByName.selfBounds(p, null, null, renderContext);
         double x = b.getX();
         double y = b.getY();
         double width = b.getWidth();
         double height = b.getHeight();
 
-        Paint color = NDocValueByName.getForegroundColor(p,renderContext, true);
-        NDocPoint2D base = NDocObjEx.of(p.getPropertyValue("base")).asHPoint2D().orElse(null);
-        if(base==null){
-            Double base3 = NDocObjEx.of(p.getPropertyValue("base")).asDouble().orElse(null);
-            if(base3==null){
-                base=new NDocPoint2D(80, 20);
-            }else{
-                base=new NDocPoint2D(base3, base3);
+        double outerRadius = Math.min(width, height) / 2;
+        double innerRadius=0;
+        if(isDonut) {
+            innerRadius = NDocObjEx.of(p.getPropertyValue(NDocPropName.INNER_RADIUS)).asDouble().orElse(0.0);
+            if (innerRadius <= 0) {
+                innerRadius = 50;
+            } else if (innerRadius >= 100) {
+                innerRadius = 0;
             }
+            innerRadius = innerRadius / 100 * outerRadius;
         }
-        NDocPoint2D hat = NDocObjEx.of(p.getPropertyValue("hat")).asHPoint2D().orElse(null);
-        if(hat==null){
-            Double base3 = NDocObjEx.of(p.getPropertyValue("hat")).asDouble().orElse(null);
-            if(base3==null){
-                hat=new NDocPoint2D(-1, -1);
-            }else{
-                hat=new NDocPoint2D(base3, base3);
-            }
-        }
-        if(hat.y<0){
-            hat.y=15;
-        }
-        if(hat.x<0){
-            hat.x=0;
-        }
-        double baseH=base.y*height/100.0;
-        double baseW=base.x*width/100.0;
-        double hatH=hat.y*height/100.0;
-        double hatW=hat.x*width/100.0;
 
+        double startAngle = NDocObjEx.of(p.getPropertyValue(NDocPropName.START_ANGLE)).asDouble().orElse(0.0);
+        double extentAngle = NDocObjEx.of(p.getPropertyValue(NDocPropName.EXTENT_ANGLE)).asDouble().orElse(360.0);
+        double dash = NDocObjEx.of(p.getPropertyValue(NDocPropName.DASH)).asDouble().orElse(0.0);
         NDocGraphics g = renderContext.graphics();
-        g.setPaint(color);
-        GeneralPath arrow = new GeneralPath();
-        arrow.moveTo(x, y+height/2);
-        arrow.lineTo(x, y+height/2-baseH/2);
-        arrow.lineTo(x+baseW, y+height/2-baseH/2);
-        arrow.lineTo(x+baseW-hatW, y+height/2-baseH/2-hatH);
-        arrow.lineTo(x+width, y+height/2);
-        arrow.lineTo(x+baseW-hatW, y+height/2+baseH/2+hatH);
-        arrow.lineTo(x+baseW, y+height/2+baseH/2);
-        arrow.lineTo(x, y+height/2+baseH/2);
-        arrow.closePath();
-        g.draw(arrow);
-        g.fill(arrow);
-        NDocNodeRendererUtils.paintDebugBox(p, renderContext, g, b);
 
+        String[] colors = NDocObjEx.of(p.getPropertyValue(NDocPropName.COLORS)).asStringArray().orElse(null);
+        g.setColor(Color.black);
+        if (!renderContext.isDry()) {
+            double finalInnerRadius = innerRadius;
+            NDocNodeRendererUtils.withStroke(p, g, renderContext, () -> {
+                int sliceCount = NDocObjEx.of(p.getPropertyValue(NDocPropName.SLICE_COUNT)).asInt().orElse(-1);
+                //equal slices
+                if (sliceCount > 0) {
+                    double sliceAngle = (extentAngle - sliceCount * dash) / sliceCount;
+                    double currentAngle = startAngle;
+
+                    for (int i = 0; i < sliceCount; i++) {
+                        Color color = getColor(i, colors);
+
+                        Area outerCircle = new Area(new Arc2D.Double(x + (width / 2 - outerRadius), y + (height / 2 - outerRadius), outerRadius * 2, outerRadius * 2, currentAngle, sliceAngle, Arc2D.PIE));
+                        if (finalInnerRadius > 0) {
+                            Area innerCircle = new Area(new Arc2D.Double(x + (width / 2 - finalInnerRadius), y + (height / 2 - finalInnerRadius), finalInnerRadius * 2, finalInnerRadius * 2, currentAngle, sliceAngle, Arc2D.PIE));
+                            outerCircle.subtract(innerCircle);
+                        }
+
+                        g.setColor(color);
+                        g.fill(outerCircle);
+
+                        currentAngle += sliceAngle + dash;
+                    }
+                }
+                //diff sizes
+                else {
+
+                    double[] slicePercentage = NDocObjEx.of(p.getPropertyValue(NDocPropName.SLICES)).asDoubleArray().orElse(getSlicePercentage(p));
+                    slicePercentage = Arrays.stream(slicePercentage).filter(xx -> xx <= 0 || Double.isInfinite(xx) || Double.isNaN(xx)).toArray();
+                    if (slicePercentage.length == 0) {
+                        slicePercentage = new double[]{1, 1, 1};
+                    }
+                    double sum = Arrays.stream(slicePercentage).sum();
+                    slicePercentage = Arrays.stream(slicePercentage).map(xx -> xx / (sum == 0 ? 1 : sum) * 100.0).toArray();
+                    int percentageSliceCount = slicePercentage.length;
+                    double totalDash = dash * percentageSliceCount;
+                    double[] sliceAngles = new double[percentageSliceCount];
+
+                    for (int i = 0; i < percentageSliceCount; i++) {
+                        sliceAngles[i] = (extentAngle - totalDash) * slicePercentage[i] / 100.0;
+                    }
+
+                    double currentAngle = startAngle;
+
+                    for (int i = 0; i < percentageSliceCount; i++) {
+                        Color color = getColor(i, colors);
+
+                        double sliceAngle = sliceAngles[i];
+
+                        Area outerCircle = new Area(new Arc2D.Double(x + (width / 2 - outerRadius), y + (height / 2 - outerRadius), outerRadius * 2, outerRadius * 2, currentAngle, sliceAngle, Arc2D.PIE));
+                        if (finalInnerRadius > 0) {
+                            Area innerCircle = new Area(new Arc2D.Double(x + (width / 2 - finalInnerRadius), y + (height / 2 - finalInnerRadius), finalInnerRadius * 2, finalInnerRadius * 2, currentAngle, sliceAngle, Arc2D.PIE));
+                            outerCircle.subtract(innerCircle);
+                        }
+                        g.setColor(color);
+                        g.fill(outerCircle);
+
+                        currentAngle += sliceAngle + dash;
+                    }
+                }
+            });
+
+
+//            if (NDocNodeRendererUtils.applyForeground(p, g, renderContext, !someBG)) {
+//                NDocNodeRendererUtils.applyStroke(p, g, renderContext);
+//
+//                double centerX = x + width / 2;
+//                double centerY = y + height / 2;
+//
+//                g.drawOval((int) (centerX - outerRadius), (int) (centerY - outerRadius), (int) (outerRadius * 2), (int) (outerRadius * 2));
+//                g.drawOval((int) (centerX - innerRadius), (int) (centerY - innerRadius), (int) (innerRadius * 2), (int) (innerRadius * 2));
+//            }
+
+            NDocNodeRendererUtils.paintDebugBox(p, renderContext, g, b);
+        }
     }
+
+    private static Color getColor(int sliceIndex, String[] colors) {
+        String[] defaultColors = {
+                "#402E7A",
+                "#4C3BCF",
+                "#4B70F5",
+                "#3DC2EC",
+        };
+
+        String colorStr = (colors != null && colors.length > 0)
+                ? colors[sliceIndex % colors.length]
+                : defaultColors[sliceIndex % defaultColors.length];
+
+        return parseColor(colorStr);
+    }
+
+    private static Color parseColor(String colorStr) {
+        if (colorStr.startsWith("#")) {
+            return Color.decode(colorStr);
+        } else {
+            try {
+                Field field = Color.class.getField(colorStr.toLowerCase());
+                return (Color) field.get(null);
+            } catch (Exception e) {
+                throw new IllegalArgumentException("Unvalid color: " + colorStr);
+            }
+        }
+    }
+
+    private static double[] getSlicePercentage(NDocNode node) {
+        return new double[]{};
+    }
+
 
 }
