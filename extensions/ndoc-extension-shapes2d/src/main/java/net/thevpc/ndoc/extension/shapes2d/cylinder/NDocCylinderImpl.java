71416ed6ed624769244a8a16e82700de54ffba42
thevpc <taha.bensalah@gmail.com>
2025-07-28 17:45:20 +0100

    Update extensions/ndoc-extension-shapes2d/src/main/java/net/thevpc/ndoc/extension/shapes2d/cylinder/NDocCylinderImpl.java

--- extensions/ndoc-extension-shapes2d/src/main/java/net/thevpc/ndoc/extension/shapes2d/cylinder/NDocCylinderImpl.java
+++ extensions/ndoc-extension-shapes2d/src/main/java/net/thevpc/ndoc/extension/shapes2d/cylinder/NDocCylinderImpl.java
@@ -1,11 +1,12 @@
 package net.thevpc.ndoc.extension.shapes2d.cylinder;
 
+import net.thevpc.ndoc.api.parser.ParseArgumentInfo;
 import net.thevpc.ndoc.api.document.node.NDocNode;
 import net.thevpc.ndoc.api.document.node.NDocNodeType;
 import  net.thevpc.ndoc.api.document.style.NDocProp;
 import  net.thevpc.ndoc.api.document.style.NDocPropName;
 import net.thevpc.ndoc.api.util.NDocUtils;
-import net.thevpc.ndoc.api.base.format.ToElementHelper;
+import net.thevpc.ndoc.api.util.ToElementHelper;
 import net.thevpc.ndoc.api.base.parser.NDocNodeParserBase;
 import net.thevpc.nuts.elem.NElement;
 import net.thevpc.nuts.elem.NPairElement;
