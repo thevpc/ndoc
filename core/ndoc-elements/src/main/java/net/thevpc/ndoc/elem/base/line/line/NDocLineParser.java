4f7f7965c0ffea89d8bee235420d138701b3b075
thevpc <taha.bensalah@gmail.com>
2025-07-28 17:45:20 +0100

    Update core/ndoc-elements/src/main/java/net/thevpc/ndoc/elem/base/line/line/NDocLineParser.java

--- core/ndoc-elements/src/main/java/net/thevpc/ndoc/elem/base/line/line/NDocLineParser.java
+++ core/ndoc-elements/src/main/java/net/thevpc/ndoc/elem/base/line/line/NDocLineParser.java
@@ -1,11 +1,12 @@
 package net.thevpc.ndoc.elem.base.line.line;
 
+import net.thevpc.ndoc.api.parser.ParseArgumentInfo;
 import net.thevpc.ndoc.api.document.node.NDocNode;
 import net.thevpc.ndoc.api.document.node.NDocNodeType;
 import  net.thevpc.ndoc.api.document.style.NDocPropName;
 import net.thevpc.ndoc.api.util.NDocUtils;
 import net.thevpc.ndoc.api.base.parser.NDocNodeParserBase;
-import net.thevpc.ndoc.api.base.format.ToElementHelper;
+import net.thevpc.ndoc.api.util.ToElementHelper;
 import net.thevpc.ndoc.api.eval.NDocObjEx;
 import net.thevpc.nuts.elem.NElement;
 import net.thevpc.nuts.elem.NPairElement;
