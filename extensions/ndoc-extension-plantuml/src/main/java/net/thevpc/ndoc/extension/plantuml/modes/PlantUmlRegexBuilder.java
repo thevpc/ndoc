65c22616d6a60404ba3c24484ac63cc98eb13f0e
thevpc <taha.bensalah@gmail.com>
2025-07-31 18:34:23 +0100

    Update extensions/ndoc-extension-plantuml/src/main/java/net/thevpc/ndoc/extension/plantuml/modes/PlantUmlRegexBuilder.java

--- extensions/ndoc-extension-plantuml/src/main/java/net/thevpc/ndoc/extension/plantuml/modes/PlantUmlRegexBuilder.java
+++ extensions/ndoc-extension-plantuml/src/main/java/net/thevpc/ndoc/extension/plantuml/modes/PlantUmlRegexBuilder.java
@@ -2,8 +2,8 @@ package net.thevpc.ndoc.extension.plantuml.modes;
 
 import net.thevpc.ndoc.extension.plantuml.PlantUmlBuilderBase;
 
-public class PlantUmlRegexParser extends PlantUmlBuilderBase {
-    public PlantUmlRegexParser() {
+public class PlantUmlRegexBuilder extends PlantUmlBuilderBase {
+    public PlantUmlRegexBuilder() {
         super("regex");
     }
 }
