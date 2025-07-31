2a3e7d860cddfcf47f856aa1d357464afd63b6f6
thevpc <taha.bensalah@gmail.com>
2025-07-31 18:34:22 +0100

    Update core/ndoc-elements/src/main/java/net/thevpc/ndoc/elem/base/text/NDocPlainTextBuilder.java

--- core/ndoc-elements/src/main/java/net/thevpc/ndoc/elem/base/text/NDocPlainTextBuilder.java
+++ core/ndoc-elements/src/main/java/net/thevpc/ndoc/elem/base/text/NDocPlainTextBuilder.java
@@ -2,71 +2,61 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package net.thevpc.ndoc.elem.base.text.plain;
+package net.thevpc.ndoc.elem.base.text;
 
+import net.thevpc.ndoc.api.document.style.NDocProp;
 import net.thevpc.ndoc.api.extension.NDocNodeCustomBuilder;
 import net.thevpc.ndoc.api.extension.NDocNodeCustomBuilderContext;
 import net.thevpc.ndoc.api.parser.NDocArgumentParseInfo;
 import net.thevpc.ndoc.api.document.node.NDocNode;
 import net.thevpc.ndoc.api.document.node.NDocNodeType;
 import  net.thevpc.ndoc.api.document.style.NDocPropName;
-import net.thevpc.ndoc.api.base.parser.NDocNodeParserBase;
-import net.thevpc.ndoc.api.util.ToElementHelper;
+import net.thevpc.ndoc.api.renderer.NDocGraphics;
+import net.thevpc.ndoc.api.renderer.NDocNodeRendererContext;
+import net.thevpc.ndoc.api.renderer.text.NDocRichTextToken;
+import net.thevpc.ndoc.api.renderer.text.NDocRichTextTokenType;
+import net.thevpc.ndoc.api.renderer.text.NDocTextOptions;
+import net.thevpc.ndoc.api.renderer.text.NDocTextRendererBuilder;
 import net.thevpc.nuts.elem.NElement;
+import net.thevpc.nuts.io.NPath;
+import net.thevpc.nuts.util.NStringUtils;
 
 /**
  * @author vpc
  */
-public class NDocPlainTextParser extends NDocNodeCustomBuilder {
+public class NDocPlainTextBuilder implements NDocNodeCustomBuilder {
 
     @Override
     public void build(NDocNodeCustomBuilderContext builderContext) {
-
+        builderContext.id(NDocNodeType.PLAIN)
+                .parseParam().named(NDocPropName.VALUE).then()
+                .parseParam().named(NDocPropName.FILE).set(NDocPropName.VALUE).resolvedAs(new NDocNodeCustomBuilderContext.PropResolver() {
+                    @Override
+                    public NDocProp resolve(String uid, NElement value, NDocArgumentParseInfo info, NDocNodeCustomBuilderContext buildContext) {
+                        NPath nPath = buildContext.engine().resolvePath(value.asString().get(), info.node());
+                        info.getContext().document().resources().add(nPath);
+                        return NDocProp.ofString(uid, nPath.readString().trim());
+                    }
+                }).then()
+                .parseParam().matchesStringOrName().set(NDocPropName.VALUE).then()
+                .renderText(this::renderTextBuildText)
+        ;
     }
 
-    public NDocPlainTextParser() {
-        super(false, NDocNodeType.PLAIN);
-    }
+    private void renderTextBuildText(String text, NDocTextOptions options, NDocNode p, NDocNodeRendererContext renderContext, NDocTextRendererBuilder builder, NDocNodeCustomBuilderContext buildContext) {
+//        Paint fg = NDocValueByName.getForegroundColor(p, renderContext,true);
+        NElement d = p.getPropertyValue(NDocPropName.VALUE).orElse(NElement.ofString(""));
 
-    protected String acceptTypeName(NElement e) {
-        switch (e.type()) {
-            case DOUBLE_QUOTED_STRING:
-            case SINGLE_QUOTED_STRING:
-            case ANTI_QUOTED_STRING:
-            case TRIPLE_DOUBLE_QUOTED_STRING:
-            case TRIPLE_SINGLE_QUOTED_STRING:
-            case TRIPLE_ANTI_QUOTED_STRING:
-            case LINE_STRING:
-            {
-                return id();
-            }
+        String message = NStringUtils.trim(d.asStringValue().get());
+        String[] allLines = message.trim().split("[\n]");
+        for (int i = 0; i < allLines.length; i++) {
+            allLines[i] = allLines[i].trim();
+            NDocRichTextToken c = new NDocRichTextToken(NDocRichTextTokenType.PLAIN, allLines[i]);
+            NDocGraphics g = renderContext.graphics();
+            g.setFont(c.textOptions.font);
+            c.bounds = g.getStringBounds(c.text);
+            builder.nextLine().addToken(c);
         }
-        return super.acceptTypeName(e);
     }
 
-    @Override
-    protected boolean processArgument(NDocArgumentParseInfo info) {
-        switch (info.getCurrentArg().type()) {
-            case DOUBLE_QUOTED_STRING:
-            case SINGLE_QUOTED_STRING:
-            case ANTI_QUOTED_STRING:
-            case TRIPLE_DOUBLE_QUOTED_STRING:
-            case TRIPLE_SINGLE_QUOTED_STRING:
-            case TRIPLE_ANTI_QUOTED_STRING:
-            case LINE_STRING:
-            {
-                info.getNode().setProperty(NDocPropName.VALUE, info.getCurrentArg());
-                return true;
-            }
-        }
-        return super.processArgument(info);
-    }
-
-    @Override
-    public NElement toElem(NDocNode item) {
-        return ToElementHelper
-                .of(item, engine())
-                .inlineStringProp(NDocPropName.VALUE)
-                .build();
-    }
 }
