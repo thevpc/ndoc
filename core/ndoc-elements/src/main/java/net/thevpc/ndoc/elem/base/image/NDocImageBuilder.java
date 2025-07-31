d68d0a37f84ce8e921856e4c18df2958581a36cd
thevpc <taha.bensalah@gmail.com>
2025-07-31 18:34:22 +0100

    Update core/ndoc-elements/src/main/java/net/thevpc/ndoc/elem/base/image/NDocImageBuilder.java

--- core/ndoc-elements/src/main/java/net/thevpc/ndoc/elem/base/image/NDocImageBuilder.java
+++ core/ndoc-elements/src/main/java/net/thevpc/ndoc/elem/base/image/NDocImageBuilder.java
@@ -4,144 +4,147 @@
  */
 package net.thevpc.ndoc.elem.base.image;
 
-import net.thevpc.ndoc.api.base.parser.NDocNodeParserBase;
+import net.thevpc.ndoc.api.document.elem2d.NDocBounds2;
+import net.thevpc.ndoc.api.document.elem2d.NDocImageOptions;
 import net.thevpc.ndoc.api.document.node.NDocNode;
 import net.thevpc.ndoc.api.document.node.NDocNodeType;
 import net.thevpc.ndoc.api.document.style.NDocPropName;
-import net.thevpc.ndoc.api.parser.ParseArgumentInfo;
+import net.thevpc.ndoc.api.document.style.NDocProperties;
+import net.thevpc.ndoc.api.eval.NDocObjEx;
+import net.thevpc.ndoc.api.eval.NDocValueByType;
+import net.thevpc.ndoc.api.extension.NDocNodeCustomBuilder;
+import net.thevpc.ndoc.api.extension.NDocNodeCustomBuilderContext;
+import net.thevpc.ndoc.api.parser.NDocResource;
+import net.thevpc.ndoc.api.renderer.NDocGraphics;
+import net.thevpc.ndoc.api.renderer.NDocNodeRendererContext;
+import net.thevpc.ndoc.api.renderer.text.NDocTextOptions;
+import net.thevpc.ndoc.api.util.NDocNodeRendererUtils;
 import net.thevpc.ndoc.api.util.NDocUtils;
-import net.thevpc.ndoc.api.util.ToElementHelper;
 import net.thevpc.nuts.elem.NElement;
-import net.thevpc.nuts.elem.NPairElement;
+import net.thevpc.nuts.io.NPath;
+import net.thevpc.nuts.util.NMsg;
+import net.thevpc.nuts.util.NOptional;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.awt.*;
 
 
 /**
  * @author vpc
  */
-public class NDocImageBuilder extends NDocNodeParserBase {
-
-    public NDocImageBuilder() {
-        super(false, NDocNodeType.IMAGE);
-    }
+public class NDocImageBuilder implements NDocNodeCustomBuilder {
+    NDocProperties defaultStyles = new NDocProperties();
 
     @Override
-    protected boolean processArguments(ParseArgumentInfo info) {
-        Set<Integer> processed = new HashSet<>();
-        boolean found = false;
-        for (int i = 0; i < info.getArguments().length; i++) {
-            NElement currentArg = info.getArguments()[i];
-            switch (currentArg.type()) {
-                case DOUBLE_QUOTED_STRING:
-                case SINGLE_QUOTED_STRING:
-                case ANTI_QUOTED_STRING:
-                case TRIPLE_DOUBLE_QUOTED_STRING:
-                case TRIPLE_SINGLE_QUOTED_STRING:
-                case TRIPLE_ANTI_QUOTED_STRING:
-                case LINE_STRING:
-                {
-                    info.getNode().setProperty(NDocPropName.VALUE, NDocUtils.addCompilerDeclarationPath(currentArg, info.source()));
-                    processed.add(i);
-                    found = true;
-                    break;
-                }
-                case PAIR: {
-                    if (currentArg.isSimplePair()) {
-                        NPairElement p = currentArg.asPair().get();
-                        String sid = NDocUtils.uid(p.key().asStringValue().get());
-                        switch (sid) {
-                            case NDocPropName.VALUE:
-                            case NDocPropName.FILE: {
-                                info.getNode().setProperty(sid, info.getContext().asPathRef(p.value()));
-                                processed.add(i);
-                                found = true;
-                                break;
-                            }
-                            case "content":
-                            case "src": {
-                                info.getNode().setProperty(NDocPropName.VALUE, info.getContext().asPathRef(p.value()));
-                                processed.add(i);
-                                found = true;
-                                break;
-                            }
-                        }
-                    }
-                    break;
-                }
-            }
-        }
-        if (!found) {
-            for (int i = 0; i < info.getArguments().length; i++) {
-                if (!processed.contains(i)) {
-                    NElement currentArg = info.getArguments()[i];
-                    switch (currentArg.type()) {
-                        case NAME: {
-                            info.getNode().setProperty(NDocPropName.VALUE, info.getContext().asPathRef(currentArg));
-                            processed.add(i);
-                            found = true;
-                            break;
-                        }
-                    }
-                }
-            }
-        }
-        for (int i = 0; i < info.getArguments().length; i++) {
-            if (!processed.contains(i)) {
-                info.setCurrentArg(info.getArguments()[i]);
-                info.setCurrentArgIndex(i);
-                if (!processArgument(info)) {
-                    return processArgumentAsCommonStyleProperty(info);
-                }
-            }
-        }
-        return true;
+    public void build(NDocNodeCustomBuilderContext builderContext) {
+        builderContext
+                .id(NDocNodeType.IMAGE)
+                .parseParam().named(NDocPropName.TRANSPARENT_COLOR).then()
+                .parseParam().named(NDocPropName.VALUE, NDocPropName.FILE, "content", "src").set(NDocPropName.VALUE).asString().then()
+                .parseParam().matchesStringOrName().set(NDocPropName.VALUE).ignoreDuplicates(true).asString().then()
+                .renderComponent(this::renderMain)
+        ;
     }
 
-    @Override
-    protected boolean processArgument(ParseArgumentInfo info) {
+    public void renderMain(NDocNode p, NDocNodeRendererContext rendererContext,NDocNodeCustomBuilderContext builderContext) {
+        rendererContext = rendererContext.withDefaultStyles(p, defaultStyles);
+        NDocBounds2 b = rendererContext.selfBounds(p);
+        int w = NDocUtils.intOf(b.getWidth());
+        int h = NDocUtils.intOf(b.getHeight());
+        if (w <= 0 || h <= 0) {
+            return;
+        }
 
-        switch (info.getCurrentArg().type()) {
-            case DOUBLE_QUOTED_STRING:
-            case SINGLE_QUOTED_STRING:
-            case ANTI_QUOTED_STRING:
-            case TRIPLE_DOUBLE_QUOTED_STRING:
-            case TRIPLE_SINGLE_QUOTED_STRING:
-            case TRIPLE_ANTI_QUOTED_STRING:
-            case LINE_STRING:
-            case NAME: {
-                break;
+        Color transparentColor = NDocValueByType.getColor(p, rendererContext, NDocPropName.TRANSPARENT_COLOR).orNull();
+        NDocImageOptions options = new NDocImageOptions();
+        options.setTransparentColor(transparentColor);
+        options.setDisableAnimation(!rendererContext.isAnimate());
+        NDocNodeRendererContext finalCtx = rendererContext;
+        options.setAsyncLoad(() -> finalCtx.repaint());
+        options.setImageObserver(rendererContext.imageObserver());
+        options.setSize(new Dimension(b.getWidth().intValue(), b.getHeight().intValue()));
+
+        Object img = p.getPropertyValue(NDocPropName.VALUE).orNull();
+        NOptional<NElement> imgStr = NDocObjEx.of(img).asTsonStringOrName();
+        if (imgStr.isPresent()) {
+            img = rendererContext.resolvePath(imgStr.get(), p);
+        }
+
+        NDocGraphics g = rendererContext.graphics();
+
+        double x = b.getX();
+        double y = b.getY();
+
+        if (!rendererContext.isDry()) {
+            if (NDocNodeRendererUtils.applyBackgroundColor(p, g, rendererContext)) {
+                g.fillRect((int) x, (int) y, NDocUtils.intOf(b.getWidth()), NDocUtils.intOf(b.getHeight()));
             }
-            case PAIR: {
-                if (info.getCurrentArg().isSimplePair()) {
-                    NPairElement p = info.getCurrentArg().asPair().get();
-                    String sid = NDocUtils.uid(p.key().asStringValue().get());
-                    switch (sid) {
-                        case NDocPropName.TRANSPARENT_COLOR: {
-                            info.getNode().setProperty(sid, p.value());
-                            return true;
-                        }
-                        case NDocPropName.VALUE:
-                        case NDocPropName.FILE:
-                        case "content":
-                        case "src": {
-                            return true;
-                        }
+
+            NDocNodeRendererUtils.applyForeground(p, g, rendererContext, false);
+            if (img instanceof NPath) {
+                NPath imgPath = (NPath) img;
+                NPath vp = resolveImagePath(imgPath);
+                if (vp != null) {
+                    try {
+                        g.drawImage(vp, x, y, options);
+                    } catch (Exception ex) {
+                        NDocResource src = NDocUtils.sourceOf(p);
+                        rendererContext.log().log(NMsg.ofC("[%s] [ERROR] error loading image : %s (%s)",
+                                src == null ? null : src.shortName(),
+                                vp, ex).asSevere(), src);
+
                     }
+                } else {
+                    int descent = g.getFontMetrics().getAscent();
+                    g.drawString("Image not found "+imgPath, x, y+descent,new NDocTextOptions().setForegroundColor(Color.YELLOW).setBackgroundColor(Color.RED).setFontSize(8.0f));
+                    NDocResource src = NDocUtils.sourceOf(p);
+                    rendererContext.log().log(NMsg.ofC("[%s] [ERROR] image not found : %s",
+                            src == null ? null : src.shortName(),
+                            img).asSevere(), src);
                 }
-                break;
             }
         }
-        return super.processArgument(info);
+        NDocNodeRendererUtils.paintDebugBox(p, rendererContext, g, b);
+    }
+
+    private NPath resolveImagePath(NPath p) {
+        if (p.isRegularFile()) {
+            return p;
+        }
+        for (String ext : new String[]{"png", "jpg", "gif", "jpeg","svg"}) {
+            String n = p.getName();
+            String nLowered = n.toLowerCase();
+            if (nLowered.endsWith(".")) {
+                NPath p2 = p.resolveSibling(n + ext);
+                if (p2.isRegularFile()) {
+                    return p2;
+                }
+                p2 = p.resolveSibling(n + ext.toUpperCase());
+                if (p2.isRegularFile()) {
+                    return p2;
+                }
+            } else if (nLowered.endsWith(".*")) {
+                NPath p2 = p.resolveSibling(n.substring(0, n.length() - 1) + ext);
+                if (p2.isRegularFile()) {
+                    return p2;
+                }
+                p2 = p.resolveSibling(n.substring(0, n.length() - 1) + ext.toUpperCase());
+                if (p2.isRegularFile()) {
+                    return p2;
+                }
+            } else {
+                NPath p2 = p.resolveSibling(n + "." + ext);
+                if (p2.isRegularFile()) {
+                    return p2;
+                }
+                p2 = p.resolveSibling(n + "." + ext.toUpperCase());
+                if (p2.isRegularFile()) {
+                    return p2;
+                }
+            }
+        }
+        return null;
+
     }
 
 
-    @Override
-    public NElement toElem(NDocNode item) {
-        return ToElementHelper
-                .of(item, engine())
-                .inlineStringProp(NDocPropName.VALUE)
-                .build();
-    }
 }
