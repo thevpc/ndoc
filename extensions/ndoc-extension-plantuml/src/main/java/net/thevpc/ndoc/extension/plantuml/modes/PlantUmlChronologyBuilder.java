fac2dd72d5770b87c98638b1f6df3155febabd92
thevpc <taha.bensalah@gmail.com>
2025-07-31 18:34:23 +0100

    Update extensions/ndoc-extension-plantuml/src/main/java/net/thevpc/ndoc/extension/plantuml/modes/PlantUmlChronologyBuilder.java

--- extensions/ndoc-extension-plantuml/src/main/java/net/thevpc/ndoc/extension/plantuml/modes/PlantUmlChronologyBuilder.java
+++ extensions/ndoc-extension-plantuml/src/main/java/net/thevpc/ndoc/extension/plantuml/modes/PlantUmlChronologyBuilder.java
@@ -1,10 +1,9 @@
 package net.thevpc.ndoc.extension.plantuml.modes;
 
 import net.thevpc.ndoc.extension.plantuml.PlantUmlBuilderBase;
-import net.thevpc.ndoc.extension.plantuml.PlantUmlBuilderBase;
 
-public class PlantUmlChronologyParser extends PlantUmlBuilderBase {
-    public PlantUmlChronologyParser() {
+public class PlantUmlChronologyBuilder extends PlantUmlBuilderBase {
+    public PlantUmlChronologyBuilder() {
         super("chronology");
     }
 }
