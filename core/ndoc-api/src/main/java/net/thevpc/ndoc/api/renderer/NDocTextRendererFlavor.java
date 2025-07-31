063cb990e7522222de10db9d5f778f6fd8240b75
thevpc <taha.bensalah@gmail.com>
2025-07-31 18:34:21 +0100

    Update core/ndoc-api/src/main/java/net/thevpc/ndoc/api/renderer/NDocTextRendererFlavor.java

--- core/ndoc-api/src/main/java/net/thevpc/ndoc/api/renderer/NDocTextRendererFlavor.java
+++ core/ndoc-api/src/main/java/net/thevpc/ndoc/api/renderer/NDocTextRendererFlavor.java
@@ -3,8 +3,12 @@ package net.thevpc.ndoc.api.renderer;
 import net.thevpc.ndoc.api.document.node.NDocNode;
 import net.thevpc.ndoc.api.renderer.text.NDocTextOptions;
 import net.thevpc.ndoc.api.renderer.text.NDocTextRendererBuilder;
+import net.thevpc.nuts.reserved.util.NReservedSimpleCharQueue;
+
+import java.util.List;
 
 public interface NDocTextRendererFlavor {
     String type();
     void buildText(String text, NDocTextOptions options, NDocNode p, NDocNodeRendererContext ctx, NDocTextRendererBuilder builder) ;
+    List<NDocTextToken> parseImmediate(NReservedSimpleCharQueue queue, NDocNodeRendererContext ctx);
 }
