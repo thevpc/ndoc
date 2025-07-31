4f3c320c4cd060101bda7360b4a5c5efa76fb31f
thevpc <taha.bensalah@gmail.com>
2025-07-31 18:34:21 +0100

    Update core/ndoc-api/src/main/java/net/thevpc/ndoc/api/parser/NDocArgumentParseInfo.java

--- core/ndoc-api/src/main/java/net/thevpc/ndoc/api/parser/NDocArgumentParseInfo.java
+++ core/ndoc-api/src/main/java/net/thevpc/ndoc/api/parser/NDocArgumentParseInfo.java
@@ -3,13 +3,13 @@ package net.thevpc.ndoc.api.parser;
 import net.thevpc.ndoc.api.document.NDocDocumentFactory;
 import net.thevpc.ndoc.api.document.node.NDocNode;
 import net.thevpc.nuts.elem.NElement;
+import net.thevpc.nuts.elem.NPairElement;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Predicate;
 
-public interface ParseArgumentInfo {
+public interface NDocArgumentParseInfo {
     NDocResource source();
 
     String getId();
@@ -18,9 +18,7 @@ public interface ParseArgumentInfo {
 
     NElement getTsonElement();
 
-    NDocNode getNode();
-
-    NElement getCurrentArg();
+    NDocNode node();
 
     int getRemainingArgumentsCount();
 
@@ -38,9 +36,9 @@ public interface ParseArgumentInfo {
 
     NElement peek(Predicate<NElement> e);
 
-    NElement peekNamedPair();
+    NPairElement peekNamedPair();
 
-    NElement readNamedPair();
+    NPairElement readNamedPair();
 
     int getCurrentArgIndex();
 
