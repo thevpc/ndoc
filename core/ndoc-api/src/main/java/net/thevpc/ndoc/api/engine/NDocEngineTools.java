753f754fe80e5fc5e2d845cafc445e988fb6aacf
thevpc <taha.bensalah@gmail.com>
2025-07-31 18:34:21 +0100

    Update core/ndoc-api/src/main/java/net/thevpc/ndoc/api/engine/NDocEngineTools.java

--- core/ndoc-api/src/main/java/net/thevpc/ndoc/api/engine/NDocEngineTools.java
+++ core/ndoc-api/src/main/java/net/thevpc/ndoc/api/engine/NDocEngineTools.java
@@ -1,4 +1,18 @@
 package net.thevpc.ndoc.api.engine;
 
+import net.thevpc.ndoc.api.renderer.NDocDocumentStreamRendererConfig;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
 public interface NDocEngineTools {
+    BufferedImage toBufferedImage(Image originalImage);
+
+    Image resizeImage(Image originalImage, int targetWidth, int targetHeight);
+
+    BufferedImage resizeBufferedImage(BufferedImage img, Dimension size);
+
+    BufferedImage resizeBufferedImage(BufferedImage img, int newW, int newH);
+
+    NDocDocumentStreamRendererConfig validateDocumentStreamRendererConfig(NDocDocumentStreamRendererConfig config);
 }
