3c1ca3af44658b3ab74afd9050a71a840c53b5a2
thevpc <taha.bensalah@gmail.com>
2025-07-31 18:34:23 +0100

    Update extensions/ndoc-extension-presenters/src/main/java/net/thevpc/ndoc/extension/presenters/ScoreBoardBuilder.java

--- extensions/ndoc-extension-presenters/src/main/java/net/thevpc/ndoc/extension/presenters/ScoreBoardBuilder.java
+++ extensions/ndoc-extension-presenters/src/main/java/net/thevpc/ndoc/extension/presenters/ScoreBoardBuilder.java
@@ -4,18 +4,10 @@ import net.thevpc.ndoc.api.document.elem2d.NDocBounds2;
 import net.thevpc.ndoc.api.eval.NDocValueByName;
 import net.thevpc.ndoc.api.extension.NDocNodeCustomBuilder;
 import net.thevpc.ndoc.api.extension.NDocNodeCustomBuilderContext;
-import net.thevpc.ndoc.api.parser.NDocArgumentParseInfo;
 import net.thevpc.ndoc.api.document.node.NDocNode;
-import  net.thevpc.ndoc.api.document.style.NDocProp;
-import  net.thevpc.ndoc.api.document.style.NDocPropName;
 import net.thevpc.ndoc.api.renderer.NDocGraphics;
 import net.thevpc.ndoc.api.renderer.NDocNodeRendererContext;
 import net.thevpc.ndoc.api.util.NDocNodeRendererUtils;
-import net.thevpc.ndoc.api.util.ToElementHelper;
-import net.thevpc.ndoc.api.base.parser.NDocNodeParserBase;
-import net.thevpc.ndoc.api.eval.NDocObjEx;
-import net.thevpc.nuts.elem.NElement;
-import net.thevpc.nuts.util.NOptional;
 
 import java.awt.*;
 
@@ -23,17 +15,17 @@ import java.awt.*;
 /**
  *
  */
-public class ScoreBoardImpl implements NDocNodeCustomBuilder {
+public class ScoreBoardBuilder implements NDocNodeCustomBuilder {
 
     @Override
     public void build(NDocNodeCustomBuilderContext builderContext) {
-            builderContext.id("scoreboard")
-                    .renderComponent(this::renderMain)
-                    ;
+        builderContext.id("scoreboard")
+                .renderComponent(this::renderMain)
+        ;
     }
 
 
-    public void renderMain(NDocNode p, NDocNodeRendererContext ctx,NDocNodeCustomBuilderContext builderContext) {
+    public void renderMain(NDocNode p, NDocNodeRendererContext ctx, NDocNodeCustomBuilderContext builderContext) {
         NDocBounds2 b = NDocValueByName.selfBounds(p, null, null, ctx);
 
         Paint color = NDocValueByName.getForegroundColor(p, ctx, true);
