43ad61ef9ad1508fe947e4dff88ece2959cfef78
thevpc <taha.bensalah@gmail.com>
2025-07-31 18:34:23 +0100

    Update extensions/ndoc-extension-plantuml/src/main/java/net/thevpc/ndoc/extension/plantuml/modes/PlantUmlDitaaBuilder.java

--- extensions/ndoc-extension-plantuml/src/main/java/net/thevpc/ndoc/extension/plantuml/modes/PlantUmlDitaaBuilder.java
+++ extensions/ndoc-extension-plantuml/src/main/java/net/thevpc/ndoc/extension/plantuml/modes/PlantUmlDitaaBuilder.java
@@ -2,8 +2,8 @@ package net.thevpc.ndoc.extension.plantuml.modes;
 
 import net.thevpc.ndoc.extension.plantuml.PlantUmlBuilderBase;
 
-public class PlantUmlDitaaParser extends PlantUmlBuilderBase {
-    public PlantUmlDitaaParser() {
+public class PlantUmlDitaaBuilder extends PlantUmlBuilderBase {
+    public PlantUmlDitaaBuilder() {
         super("ditaa");
     }
 }
