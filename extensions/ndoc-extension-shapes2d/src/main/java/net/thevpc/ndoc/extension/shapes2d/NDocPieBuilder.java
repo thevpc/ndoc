c5771d1d97e3f108639591a567b0a8c1215c3afd
thevpc <taha.bensalah@gmail.com>
2025-07-31 18:34:23 +0100

    Update extensions/ndoc-extension-shapes2d/src/main/java/net/thevpc/ndoc/extension/shapes2d/NDocPieBuilder.java

--- extensions/ndoc-extension-shapes2d/src/main/java/net/thevpc/ndoc/extension/shapes2d/NDocPieBuilder.java
+++ extensions/ndoc-extension-shapes2d/src/main/java/net/thevpc/ndoc/extension/shapes2d/NDocPieBuilder.java
@@ -1,23 +1,12 @@
 package net.thevpc.ndoc.extension.shapes2d;
 
-import net.thevpc.ndoc.api.document.elem2d.NDocBounds2;
 import net.thevpc.ndoc.api.document.node.NDocNode;
 import net.thevpc.ndoc.api.document.node.NDocNodeType;
 import net.thevpc.ndoc.api.document.style.NDocPropName;
 import net.thevpc.ndoc.api.document.style.NDocProperties;
-import net.thevpc.ndoc.api.eval.NDocObjEx;
-import net.thevpc.ndoc.api.eval.NDocValueByName;
 import net.thevpc.ndoc.api.extension.NDocNodeCustomBuilder;
 import net.thevpc.ndoc.api.extension.NDocNodeCustomBuilderContext;
-import net.thevpc.ndoc.api.renderer.NDocGraphics;
 import net.thevpc.ndoc.api.renderer.NDocNodeRendererContext;
-import net.thevpc.ndoc.api.util.NDocNodeRendererUtils;
-
-import java.awt.*;
-import java.awt.geom.Arc2D;
-import java.awt.geom.Area;
-import java.lang.reflect.Field;
-import java.util.Arrays;
 
 
 /**
@@ -30,12 +19,11 @@ public class NDocPieBuilder implements NDocNodeCustomBuilder {
     public void build(NDocNodeCustomBuilderContext builderContext) {
         builderContext
                 .id(NDocNodeType.PIE)
-                .parseAsDouble(NDocPropName.START_ANGLE,NDocPropName.EXTENT_ANGLE,NDocPropName.DASH)
-                .parseAsInt(NDocPropName.SLICE_COUNT)
-                .parseAsDoubleArray(NDocPropName.SLICES)
-                .parseAsStringArray(NDocPropName.COLORS)
-                .parseDefaultParamNames()
-                .render(this::render)
+                .parseParam().named(NDocPropName.INNER_RADIUS,NDocPropName.START_ANGLE,NDocPropName.EXTENT_ANGLE,NDocPropName.DASH).asDouble().then()
+                .parseParam().named(NDocPropName.SLICE_COUNT).asInt().then()
+                .parseParam().named(NDocPropName.SLICES).asDoubleArray().then()
+                .parseParam().named(NDocPropName.COLORS).asStringArray().then()
+                .renderComponent(this::render)
                 ;
     }
 
