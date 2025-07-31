a71217958ba646b3916a7b2535a099413e205be2
thevpc <taha.bensalah@gmail.com>
2025-07-31 18:34:23 +0100

    Update extensions/ndoc-extension-plantuml/src/main/java/net/thevpc/ndoc/extension/plantuml/modes/PlantUmlChenBuilder.java

--- extensions/ndoc-extension-plantuml/src/main/java/net/thevpc/ndoc/extension/plantuml/modes/PlantUmlChenBuilder.java
+++ extensions/ndoc-extension-plantuml/src/main/java/net/thevpc/ndoc/extension/plantuml/modes/PlantUmlChenBuilder.java
@@ -3,8 +3,8 @@ package net.thevpc.ndoc.extension.plantuml.modes;
 
 import net.thevpc.ndoc.extension.plantuml.PlantUmlBuilderBase;
 
-public class PlantUmlChenParser extends PlantUmlBuilderBase {
-    public PlantUmlChenParser() {
+public class PlantUmlChenBuilder extends PlantUmlBuilderBase {
+    public PlantUmlChenBuilder() {
         super("chen");
     }
 }
