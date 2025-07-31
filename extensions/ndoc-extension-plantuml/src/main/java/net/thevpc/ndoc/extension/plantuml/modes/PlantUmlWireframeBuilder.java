231120ec346ec0cf6ada068b89d6ae372f89d8ce
thevpc <taha.bensalah@gmail.com>
2025-07-31 18:34:23 +0100

    Update extensions/ndoc-extension-plantuml/src/main/java/net/thevpc/ndoc/extension/plantuml/modes/PlantUmlWireframeBuilder.java

--- extensions/ndoc-extension-plantuml/src/main/java/net/thevpc/ndoc/extension/plantuml/modes/PlantUmlWireframeBuilder.java
+++ extensions/ndoc-extension-plantuml/src/main/java/net/thevpc/ndoc/extension/plantuml/modes/PlantUmlWireframeBuilder.java
@@ -2,8 +2,8 @@ package net.thevpc.ndoc.extension.plantuml.modes;
 
 import net.thevpc.ndoc.extension.plantuml.PlantUmlBuilderBase;
 
-public class PlantUmlWireframeParser extends PlantUmlBuilderBase {
-    public PlantUmlWireframeParser() {
+public class PlantUmlWireframeBuilder extends PlantUmlBuilderBase {
+    public PlantUmlWireframeBuilder() {
         super("wireframe");
     }
 }
