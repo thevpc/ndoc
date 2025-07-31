ab7e2546e1a51cb78daa6ac5649a1e4dfb4623f4
thevpc <taha.bensalah@gmail.com>
2025-07-31 18:34:22 +0100

    Update core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/parser/nodeparsers/IncludeSpecialParser.java

--- core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/parser/nodeparsers/IncludeSpecialParser.java
+++ core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/parser/nodeparsers/IncludeSpecialParser.java
@@ -1,6 +1,6 @@
 package net.thevpc.ndoc.engine.parser.nodeparsers;
 
-import net.thevpc.ndoc.api.base.parser.NDocNodeParserBase;
+import net.thevpc.ndoc.engine.parser.NDocNodeParserBase;
 import net.thevpc.ndoc.api.document.node.NDocItem;
 import net.thevpc.ndoc.api.document.node.NDocNodeType;
 import net.thevpc.ndoc.api.parser.NDocNodeFactoryParseContext;
