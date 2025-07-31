d04b1d30c5acb831f6756b69f6118101aa717a0d
thevpc <taha.bensalah@gmail.com>
2025-07-31 18:34:22 +0100

    Update core/ndoc-elements/src/main/java/net/thevpc/ndoc/elem/base/line/NDocPolylineBuilder.java

--- core/ndoc-elements/src/main/java/net/thevpc/ndoc/elem/base/line/NDocPolylineBuilder.java
+++ core/ndoc-elements/src/main/java/net/thevpc/ndoc/elem/base/line/NDocPolylineBuilder.java
@@ -1,4 +1,4 @@
-package net.thevpc.ndoc.elem.base.shape.polyline;
+package net.thevpc.ndoc.elem.base.line;
 
 import net.thevpc.ndoc.api.document.elem2d.NDocBounds2;
 import net.thevpc.ndoc.api.document.elem2d.NDocElement2DFactory;
