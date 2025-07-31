d9431306a0142067081edd0eb2f3ecc2d66e4e35
thevpc <taha.bensalah@gmail.com>
2025-07-31 18:34:23 +0100

    Update extensions/ndoc-extension-plantuml/src/main/java/net/thevpc/ndoc/extension/plantuml/modes/PlantUmlNwdiagBuilder.java

--- extensions/ndoc-extension-plantuml/src/main/java/net/thevpc/ndoc/extension/plantuml/modes/PlantUmlNwdiagBuilder.java
+++ extensions/ndoc-extension-plantuml/src/main/java/net/thevpc/ndoc/extension/plantuml/modes/PlantUmlNwdiagBuilder.java
@@ -2,8 +2,8 @@ package net.thevpc.ndoc.extension.plantuml.modes;
 
 import net.thevpc.ndoc.extension.plantuml.PlantUmlBuilderBase;
 
-public class PlantUmlNwdiagParser extends PlantUmlBuilderBase {
-    public PlantUmlNwdiagParser() {
+public class PlantUmlNwdiagBuilder extends PlantUmlBuilderBase {
+    public PlantUmlNwdiagBuilder() {
         super("nwdiag");
     }
 }
