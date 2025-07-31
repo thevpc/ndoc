949bcf4502ceacbe75c36c3ddc94fdd2147e7472
thevpc <taha.bensalah@gmail.com>
2025-07-31 18:34:22 +0100

    Update core/ndoc-elements/src/main/java/net/thevpc/ndoc/elem/base/image/BufferedImageDrawer.java

--- core/ndoc-elements/src/main/java/net/thevpc/ndoc/elem/base/image/BufferedImageDrawer.java
+++ core/ndoc-elements/src/main/java/net/thevpc/ndoc/elem/base/image/BufferedImageDrawer.java
@@ -1,22 +1,21 @@
-package net.thevpc.ndoc.elem.base.image;
-
-import net.thevpc.ndoc.api.document.elem2d.NDocImageOptions;
-import net.thevpc.ndoc.api.base.renderer.HImageUtils;
-import net.thevpc.ndoc.api.renderer.NDocGraphics;
-import net.thevpc.ndoc.api.renderer.NDocGraphicsImageDrawer;
-
-import java.awt.image.BufferedImage;
-
-class BufferedImageDrawer implements NDocGraphicsImageDrawer {
-    private final BufferedImage img;
-
-    public BufferedImageDrawer(BufferedImage img) {
-        this.img = img;
-    }
-
-    @Override
-    public void drawImage(double x, double y, NDocImageOptions options, NDocGraphics g) {
-        BufferedImage image = HImageUtils.resize(img, options.getSize());
-        g.drawImage(image, (int) x, (int) y, options.getImageObserver());
-    }
-}
+//package net.thevpc.ndoc.elem.base.image;
+//
+//import net.thevpc.ndoc.api.document.elem2d.NDocImageOptions;
+//import net.thevpc.ndoc.api.renderer.NDocGraphics;
+//import net.thevpc.ndoc.api.renderer.NDocGraphicsImageDrawer;
+//
+//import java.awt.image.BufferedImage;
+//
+//class BufferedImageDrawer implements NDocGraphicsImageDrawer {
+//    private final BufferedImage img;
+//
+//    public BufferedImageDrawer(BufferedImage img) {
+//        this.img = img;
+//    }
+//
+//    @Override
+//    public void drawImage(double x, double y, NDocImageOptions options, NDocGraphics g) {
+//        BufferedImage image = HImageUtils.resize(img, options.getSize());
+//        g.drawImage(image, (int) x, (int) y, options.getImageObserver());
+//    }
+//}
