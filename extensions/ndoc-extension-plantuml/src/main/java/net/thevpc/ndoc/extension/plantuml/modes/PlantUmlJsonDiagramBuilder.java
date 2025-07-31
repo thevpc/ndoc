1c6f9a79a5b2a8359e434653bffb08bf85578053
thevpc <taha.bensalah@gmail.com>
2025-07-31 18:34:23 +0100

    Update extensions/ndoc-extension-plantuml/src/main/java/net/thevpc/ndoc/extension/plantuml/modes/PlantUmlJsonDiagramBuilder.java

--- extensions/ndoc-extension-plantuml/src/main/java/net/thevpc/ndoc/extension/plantuml/modes/PlantUmlJsonDiagramBuilder.java
+++ extensions/ndoc-extension-plantuml/src/main/java/net/thevpc/ndoc/extension/plantuml/modes/PlantUmlJsonDiagramBuilder.java
@@ -2,8 +2,8 @@ package net.thevpc.ndoc.extension.plantuml.modes;
 
 import net.thevpc.ndoc.extension.plantuml.PlantUmlBuilderBase;
 
-public class PlantUmlJsonDiagramParser extends PlantUmlBuilderBase {
-    public PlantUmlJsonDiagramParser() {
+public class PlantUmlJsonDiagramBuilder extends PlantUmlBuilderBase {
+    public PlantUmlJsonDiagramBuilder() {
         super("plantuml-json","json-diagram");
     }
 }
