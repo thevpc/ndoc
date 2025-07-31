2d71644525724624f720cf0d807cf9f7a0aef436
thevpc <taha.bensalah@gmail.com>
2025-07-31 18:34:23 +0100

    Update extensions/ndoc-extension-plantuml/src/main/java/net/thevpc/ndoc/extension/plantuml/modes/PlantUmlMindmapBuilder.java

--- extensions/ndoc-extension-plantuml/src/main/java/net/thevpc/ndoc/extension/plantuml/modes/PlantUmlMindmapBuilder.java
+++ extensions/ndoc-extension-plantuml/src/main/java/net/thevpc/ndoc/extension/plantuml/modes/PlantUmlMindmapBuilder.java
@@ -2,8 +2,8 @@ package net.thevpc.ndoc.extension.plantuml.modes;
 
 import net.thevpc.ndoc.extension.plantuml.PlantUmlBuilderBase;
 
-public class PlantUmlMindmapParser extends PlantUmlBuilderBase {
-    public PlantUmlMindmapParser() {
+public class PlantUmlMindmapBuilder extends PlantUmlBuilderBase {
+    public PlantUmlMindmapBuilder() {
         super("mindmap");
     }
 }
