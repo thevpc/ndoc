c5b1ca10b170e5f283dec31095ab10ac3e2b4b93
thevpc <taha.bensalah@gmail.com>
2025-07-31 18:34:22 +0100

    Update core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/renderer/ConvertedNDocNodeRenderer.java

--- core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/renderer/ConvertedNDocNodeRenderer.java
+++ core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/renderer/ConvertedNDocNodeRenderer.java
@@ -1,4 +1,4 @@
-package net.thevpc.ndoc.api.base.renderer;
+package net.thevpc.ndoc.engine.renderer;
 
 import net.thevpc.ndoc.api.document.node.NDocNode;
 import net.thevpc.ndoc.api.renderer.NDocNodeRendererBase;
