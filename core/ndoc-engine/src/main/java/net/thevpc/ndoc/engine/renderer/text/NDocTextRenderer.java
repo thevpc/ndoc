dbda0a4c230fd0c1133098fc3899194d60cdae79
thevpc <taha.bensalah@gmail.com>
2025-07-31 18:34:22 +0100

    Update core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/renderer/text/NDocTextRenderer.java

--- core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/renderer/text/NDocTextRenderer.java
+++ core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/renderer/text/NDocTextRenderer.java
@@ -1,4 +1,4 @@
-package net.thevpc.ndoc.elem.base.text.text;
+package net.thevpc.ndoc.engine.renderer.text;
 
 import net.thevpc.ndoc.api.document.node.NDocNodeType;
 import net.thevpc.ndoc.api.document.node.NDocNode;
@@ -10,41 +10,22 @@ import net.thevpc.ndoc.api.renderer.text.NDocTextBaseRenderer;
 import net.thevpc.ndoc.api.renderer.text.NDocTextOptions;
 import net.thevpc.ndoc.api.renderer.text.NDocTextRendererBuilder;
 import net.thevpc.ndoc.api.renderer.NDocTextRendererFlavor;
-import net.thevpc.nuts.util.NStringUtils;
 
 import java.awt.*;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.ServiceLoader;
 
 public class NDocTextRenderer extends NDocTextBaseRenderer {
-    private Map<String, NDocTextRendererFlavor> flavors = new HashMap<>();
-    private NDocTextRendererFlavor defaultFlavor = new NDocTextRendererFlavorDefault();
 
     public NDocTextRenderer() {
         super(NDocNodeType.TEXT);
-        ServiceLoader<NDocTextRendererFlavor> all=ServiceLoader.load(NDocTextRendererFlavor.class);
-        for (NDocTextRendererFlavor h : all) {
-            String t = h.type();
-            if(flavors.containsKey(t)){
-                throw new IllegalArgumentException("already registered text flavor "+t);
-            }
-            flavors.put(t, h);
-        }
-        flavors.put("ntf", new NDocTextRendererFlavorNTF());
     }
 
     public NDocTextRendererBuilder createRichTextHelper(NDocNode p, NDocNodeRendererContext ctx) {
         ctx = ctx.withDefaultStyles(p, defaultStyles);
 //        List<NDocNode> all=new ArrayList<>();
-        String lang = NStringUtils.trim(NDocObjEx.of(p.getPropertyValue(NDocPropName.LANG).orNull()).asStringOrName().orElse(""));
         String text = NDocObjEx.of(p.getPropertyValue(NDocPropName.VALUE).orNull()).asStringOrName().orElse("");
-        NDocTextRendererFlavor f = flavors.get(lang);
-        if (f == null) {
-            f = defaultFlavor;
-        }
+        NDocTextRendererFlavor f = ctx.engine().textRendererFlavor("").get();
         Paint fg = NDocValueByName.getForegroundColor(p, ctx,true);
-        NDocTextRendererBuilderImpl builder = new NDocTextRendererBuilderImpl(flavors,fg);
+        NDocTextRendererBuilderImpl builder = new NDocTextRendererBuilderImpl(ctx.engine(),fg);
         f.buildText(text, new NDocTextOptions(), p, ctx, builder);
         return builder;
     }
