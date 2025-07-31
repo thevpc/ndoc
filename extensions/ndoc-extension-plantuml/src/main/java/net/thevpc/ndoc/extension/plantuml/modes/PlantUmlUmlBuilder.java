e19b14643bbbfc1dfcafd08e22f5916fc55f40a5
thevpc <taha.bensalah@gmail.com>
2025-07-31 18:34:23 +0100

    Update extensions/ndoc-extension-plantuml/src/main/java/net/thevpc/ndoc/extension/plantuml/modes/PlantUmlUmlBuilder.java

--- extensions/ndoc-extension-plantuml/src/main/java/net/thevpc/ndoc/extension/plantuml/modes/PlantUmlUmlBuilder.java
+++ extensions/ndoc-extension-plantuml/src/main/java/net/thevpc/ndoc/extension/plantuml/modes/PlantUmlUmlBuilder.java
@@ -2,8 +2,8 @@ package net.thevpc.ndoc.extension.plantuml.modes;
 
 import net.thevpc.ndoc.extension.plantuml.PlantUmlBuilderBase;
 
-public class PlantUmlUmlParser extends PlantUmlBuilderBase {
-    public PlantUmlUmlParser() {
+public class PlantUmlUmlBuilder extends PlantUmlBuilderBase {
+    public PlantUmlUmlBuilder() {
         super("uml");
     }
 }
