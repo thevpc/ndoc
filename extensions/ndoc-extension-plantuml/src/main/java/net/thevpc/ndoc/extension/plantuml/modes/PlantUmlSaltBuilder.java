37e18f6c8327d07530f164e7af5b7f188a7aac08
thevpc <taha.bensalah@gmail.com>
2025-07-31 18:34:23 +0100

    Update extensions/ndoc-extension-plantuml/src/main/java/net/thevpc/ndoc/extension/plantuml/modes/PlantUmlSaltBuilder.java

--- extensions/ndoc-extension-plantuml/src/main/java/net/thevpc/ndoc/extension/plantuml/modes/PlantUmlSaltBuilder.java
+++ extensions/ndoc-extension-plantuml/src/main/java/net/thevpc/ndoc/extension/plantuml/modes/PlantUmlSaltBuilder.java
@@ -2,8 +2,8 @@ package net.thevpc.ndoc.extension.plantuml.modes;
 
 import net.thevpc.ndoc.extension.plantuml.PlantUmlBuilderBase;
 
-public class PlantUmlSaltParser extends PlantUmlBuilderBase {
-    public PlantUmlSaltParser() {
+public class PlantUmlSaltBuilder extends PlantUmlBuilderBase {
+    public PlantUmlSaltBuilder() {
         super("salt");
     }
 }
