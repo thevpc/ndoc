baa10848f00e124405a6f87b8ba8e7fbbbb9a17e
thevpc <taha.bensalah@gmail.com>
2025-07-31 18:34:22 +0100

    Update core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/parser/nodeparsers/NDocScene3DParser.java

--- core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/parser/nodeparsers/NDocScene3DParser.java
+++ core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/parser/nodeparsers/NDocScene3DParser.java
@@ -1,10 +1,11 @@
 package net.thevpc.ndoc.engine.parser.nodeparsers;
 
-import net.thevpc.ndoc.api.parser.ParseArgumentInfo;
+import net.thevpc.ndoc.api.parser.NDocArgumentParseInfo;
 import net.thevpc.ndoc.api.document.elem3d.NDocPoint3D;
 import net.thevpc.ndoc.api.document.node.NDocNodeType;
-import net.thevpc.ndoc.api.base.parser.NDocNodeParserBase;
+import net.thevpc.ndoc.engine.parser.NDocNodeParserBase;
 import net.thevpc.ndoc.api.eval.NDocObjEx;
+import net.thevpc.nuts.elem.NElement;
 import net.thevpc.nuts.util.NOptional;
 
 public class NDocScene3DParser extends NDocNodeParserBase {
@@ -13,10 +14,11 @@ public class NDocScene3DParser extends NDocNodeParserBase {
     }
 
     @Override
-    protected boolean processArgument(ParseArgumentInfo info) {
-        switch (info.currentArg.type()) {
+    protected boolean processArgument(NDocArgumentParseInfo info) {
+        NElement currentArg = info.peek();
+        switch (currentArg.type()) {
             case PAIR: {
-                NOptional<NDocObjEx.SimplePair> sp = NDocObjEx.of(info.currentArg).asSimplePair();
+                NOptional<NDocObjEx.SimplePair> sp = NDocObjEx.of(currentArg).asSimplePair();
                 if (sp.isPresent()) {
                     NDocObjEx.SimplePair spp = sp.get();
                     NDocObjEx v = spp.getValue();
@@ -25,7 +27,8 @@ public class NDocScene3DParser extends NDocNodeParserBase {
                         case "light-orientation": {
                             NOptional<NDocPoint3D> p2d = v.asHPoint3D();
                             if (p2d.isPresent()) {
-                                info.node.setProperty(spp.getNameId(), p2d.get());
+                                info.node().setProperty(spp.getNameId(), p2d.get());
+                                info.read();
                                 return true;
                             } else {
                                 return false;
