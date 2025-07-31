5b68de69f45afa392e09dc700c8446ad92dcdcc9
thevpc <taha.bensalah@gmail.com>
2025-07-31 18:34:21 +0100

    Update core/ndoc-api/src/main/java/net/thevpc/ndoc/api/renderer/NDocTextTokenBase.java

--- core/ndoc-api/src/main/java/net/thevpc/ndoc/api/renderer/NDocTextTokenBase.java
+++ core/ndoc-api/src/main/java/net/thevpc/ndoc/api/renderer/NDocTextTokenBase.java
@@ -2,17 +2,8 @@ package net.thevpc.ndoc.api.renderer;
 
 import net.thevpc.ndoc.api.renderer.text.NDocTextOptions;
 
-public class NDocTextTokenSimple implements NDocTextToken {
-    private String value;
+public abstract class NDocTextTokenBase implements NDocTextToken {
     private NDocTextOptions options = new NDocTextOptions();
-
-    public NDocTextTokenSimple(String value) {
-        this.value = value;
-    }
-
-    public String value() {
-        return value;
-    }
 
     public NDocTextOptions options() {
         return options;
