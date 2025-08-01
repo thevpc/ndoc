3da10c1d2525e43ac8c74333b303c16ca1ada6ea
thevpc <taha.bensalah@gmail.com>
2025-08-01 02:13:49 +0100

    Update core/ndoc-api/src/main/java/net/thevpc/ndoc/api/eval/NDocFunctionContext.java, simplified API

--- core/ndoc-api/src/main/java/net/thevpc/ndoc/api/eval/NDocFunctionContext.java
+++ core/ndoc-api/src/main/java/net/thevpc/ndoc/api/eval/NDocFunctionContext.java
@@ -2,7 +2,7 @@ package net.thevpc.ndoc.api.eval;
 
 
 import net.thevpc.ndoc.api.engine.NDocEngine;
-import net.thevpc.ndoc.api.engine.NDocLogger;
+import net.thevpc.ndoc.api.log.NDocLogger;
 
 public interface NDocFunctionContext {
     NDocLogger messages();
