31a14cda9b5bfefe24128eb0826c3c70109edf60
thevpc <taha.bensalah@gmail.com>
2025-08-01 02:13:50 +0100

    Update extensions/ndoc-extension-presenters/src/main/java/net/thevpc/ndoc/extension/presenters/ScoreBoardBuilder.java, simplified API

--- extensions/ndoc-extension-presenters/src/main/java/net/thevpc/ndoc/extension/presenters/ScoreBoardBuilder.java
+++ extensions/ndoc-extension-presenters/src/main/java/net/thevpc/ndoc/extension/presenters/ScoreBoardBuilder.java
@@ -1,13 +1,11 @@
 package net.thevpc.ndoc.extension.presenters;
 
 import net.thevpc.ndoc.api.document.elem2d.NDocBounds2;
-import net.thevpc.ndoc.api.eval.NDocValueByName;
 import net.thevpc.ndoc.api.extension.NDocNodeCustomBuilder;
 import net.thevpc.ndoc.api.extension.NDocNodeCustomBuilderContext;
 import net.thevpc.ndoc.api.document.node.NDocNode;
 import net.thevpc.ndoc.api.renderer.NDocGraphics;
 import net.thevpc.ndoc.api.renderer.NDocNodeRendererContext;
-import net.thevpc.ndoc.api.util.NDocNodeRendererUtils;
 
 import java.awt.*;
 
@@ -26,16 +24,16 @@ public class ScoreBoardBuilder implements NDocNodeCustomBuilder {
 
 
     public void renderMain(NDocNode p, NDocNodeRendererContext ctx, NDocNodeCustomBuilderContext builderContext) {
-        NDocBounds2 b = NDocValueByName.selfBounds(p, null, null, ctx);
+        NDocBounds2 b = ctx.selfBounds(p, null, null);
 
-        Paint color = NDocValueByName.getForegroundColor(p, ctx, true);
+        Paint color = ctx.getForegroundColor(p, true);
 
         NDocGraphics g = ctx.graphics();
         if (!ctx.isDry()) {
             g.setPaint(color);
 
 
-            NDocNodeRendererUtils.paintDebugBox(p, ctx, g, b);
+            ctx.paintDebugBox(p , b);
         }
     }
 
