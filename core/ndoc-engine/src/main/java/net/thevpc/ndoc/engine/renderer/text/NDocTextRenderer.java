bf36366743c2677a7b113602b69a2151cba42ed0
thevpc <taha.bensalah@gmail.com>
2025-08-01 02:13:50 +0100

    Update core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/renderer/text/NDocTextRenderer.java, simplified API

--- core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/renderer/text/NDocTextRenderer.java
+++ core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/renderer/text/NDocTextRenderer.java
@@ -6,10 +6,9 @@ import net.thevpc.ndoc.api.document.style.NDocPropName;
 import net.thevpc.ndoc.api.eval.NDocValueByName;
 import net.thevpc.ndoc.api.renderer.NDocNodeRendererContext;
 import net.thevpc.ndoc.api.eval.NDocObjEx;
-import net.thevpc.ndoc.api.renderer.text.NDocTextBaseRenderer;
 import net.thevpc.ndoc.api.renderer.text.NDocTextOptions;
 import net.thevpc.ndoc.api.renderer.text.NDocTextRendererBuilder;
-import net.thevpc.ndoc.api.renderer.NDocTextRendererFlavor;
+import net.thevpc.ndoc.api.renderer.text.NDocTextRendererFlavor;
 
 import java.awt.*;
 
