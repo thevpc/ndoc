bb271276b99a0a7f79e8368a7a0e44eb49820f52
thevpc <taha.bensalah@gmail.com>
2025-08-01 02:13:50 +0100

    Update core/ndoc-elements/src/main/java/net/thevpc/ndoc/elem/base/container/NDocGroupBuilder.java, simplified API

--- core/ndoc-elements/src/main/java/net/thevpc/ndoc/elem/base/container/NDocGroupBuilder.java
+++ core/ndoc-elements/src/main/java/net/thevpc/ndoc/elem/base/container/NDocGroupBuilder.java
@@ -8,12 +8,10 @@ import net.thevpc.ndoc.api.document.elem2d.NDocBounds2;
 import net.thevpc.ndoc.api.document.node.NDocNode;
 import net.thevpc.ndoc.api.document.node.NDocNodeType;
 import net.thevpc.ndoc.api.document.style.NDocProperties;
-import net.thevpc.ndoc.api.eval.NDocValueByName;
 import net.thevpc.ndoc.api.extension.NDocNodeCustomBuilder;
 import net.thevpc.ndoc.api.extension.NDocNodeCustomBuilderContext;
 import net.thevpc.ndoc.api.renderer.NDocGraphics;
 import net.thevpc.ndoc.api.renderer.NDocNodeRendererContext;
-import net.thevpc.ndoc.api.util.NDocNodeRendererUtils;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -37,16 +35,16 @@ public class NDocGroupBuilder implements NDocNodeCustomBuilder {
         NDocGraphics g = ctx.graphics();
         NDocNodeRendererContext ctx2 = ctx.withBounds(p, selfBounds);
         if (!ctx2.isDry()) {
-            NDocNodeRendererUtils.paintBackground(p, ctx2, g, selfBounds);
+            ctx2.paintBackground(p, selfBounds);
         }
         NDocNodeRendererContext finalCtx = ctx;
         List<NDocNode> texts = p.children()
-                .stream().filter(x -> NDocValueByName.isVisible(x, finalCtx)).collect(Collectors.toList());
+                .stream().filter(x -> finalCtx.isVisible(x)).collect(Collectors.toList());
         for (NDocNode text : texts) {
             ctx2.render(text);
         }
         if (!ctx2.isDry()) {
-            NDocNodeRendererUtils.paintBorderLine(p, ctx2, g, selfBounds);
+            ctx2.paintBorderLine(p, selfBounds);
         }
     }
 
