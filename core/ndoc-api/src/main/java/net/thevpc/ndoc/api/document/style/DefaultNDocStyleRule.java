51db60e0c8ff90ed0e1e055d29b6a0b7d8ecba29
thevpc <taha.bensalah@gmail.com>
2025-08-01 02:13:49 +0100

    Update core/ndoc-api/src/main/java/net/thevpc/ndoc/api/document/style/DefaultNDocStyleRule.java, simplified API

--- core/ndoc-api/src/main/java/net/thevpc/ndoc/api/document/style/DefaultNDocStyleRule.java
+++ core/ndoc-api/src/main/java/net/thevpc/ndoc/api/document/style/DefaultNDocStyleRule.java
@@ -1,7 +1,7 @@
 package net.thevpc.ndoc.api.document.style;
 
 import net.thevpc.ndoc.api.document.node.NDocNode;
-import net.thevpc.ndoc.api.parser.NDocResource;
+import net.thevpc.ndoc.api.source.NDocResource;
 import net.thevpc.nuts.elem.NElement;
 
 import java.util.*;
