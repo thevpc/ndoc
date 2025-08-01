a5d7522cca9571138307403e1612a8ec90281598
thevpc <taha.bensalah@gmail.com>
2025-08-01 02:13:49 +0100

    Update core/ndoc-api/src/main/java/net/thevpc/ndoc/api/document/elem3d/NDocElement3D.java, simplified API

--- core/ndoc-api/src/main/java/net/thevpc/ndoc/api/document/elem3d/NDocElement3D.java
+++ core/ndoc-api/src/main/java/net/thevpc/ndoc/api/document/elem3d/NDocElement3D.java
@@ -1,6 +1,6 @@
 package net.thevpc.ndoc.api.document.elem3d;
 
-import net.thevpc.ndoc.api.model.HElement;
+import net.thevpc.ndoc.api.document.NDocElement;
 
-public interface NDocElement3D extends HElement {
+public interface NDocElement3D extends NDocElement {
 }
