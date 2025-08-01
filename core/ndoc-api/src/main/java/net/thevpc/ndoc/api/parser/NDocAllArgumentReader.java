f7f5adc8a79d9b3a5e3589aba19128d18e289b32
thevpc <taha.bensalah@gmail.com>
2025-08-01 02:13:49 +0100

    Update core/ndoc-api/src/main/java/net/thevpc/ndoc/api/parser/NDocAllArgumentReader.java, simplified API

--- core/ndoc-api/src/main/java/net/thevpc/ndoc/api/parser/NDocAllArgumentReader.java
+++ core/ndoc-api/src/main/java/net/thevpc/ndoc/api/parser/NDocAllArgumentReader.java
@@ -2,9 +2,9 @@ package net.thevpc.ndoc.api.parser;
 
 import net.thevpc.ndoc.api.document.NDocDocumentFactory;
 import net.thevpc.ndoc.api.document.node.NDocNode;
+import net.thevpc.ndoc.api.source.NDocResource;
 import net.thevpc.nuts.elem.NElement;
 
-import java.util.List;
 import java.util.Map;
 
 public interface NDocAllArgumentReader {
@@ -18,17 +18,7 @@ public interface NDocAllArgumentReader {
 
     NDocNode node();
 
-    int availableCount();
-
-    List<NElement> availableArguments();
-
     NElement[] allArguments();
-
-    boolean isEmpty();
-
-    NElement peek();
-
-    NElement read();
 
     NDocDocumentFactory documentFactory();
 
@@ -36,5 +26,4 @@ public interface NDocAllArgumentReader {
 
     Map<String, Object> props();
 
-    int currentIndex();
 }
