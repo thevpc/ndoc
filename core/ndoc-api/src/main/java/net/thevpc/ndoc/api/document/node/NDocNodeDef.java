9c0d12b14b245e0f0b713812f70a8d88bf280357
thevpc <taha.bensalah@gmail.com>
2025-08-01 02:13:49 +0100

    Update core/ndoc-api/src/main/java/net/thevpc/ndoc/api/document/node/NDocNodeDef.java, simplified API

--- core/ndoc-api/src/main/java/net/thevpc/ndoc/api/document/node/NDocNodeDef.java
+++ core/ndoc-api/src/main/java/net/thevpc/ndoc/api/document/node/NDocNodeDef.java
@@ -1,6 +1,6 @@
 package net.thevpc.ndoc.api.document.node;
 
-import net.thevpc.ndoc.api.parser.NDocResource;
+import net.thevpc.ndoc.api.source.NDocResource;
 
 public interface NDocNodeDef extends NDocItem {
     String name();
