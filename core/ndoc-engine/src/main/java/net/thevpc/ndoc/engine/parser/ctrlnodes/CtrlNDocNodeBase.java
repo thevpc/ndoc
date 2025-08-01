a89e180e2ff4143ae8693aa9e51e72a8607d4fbb
thevpc <taha.bensalah@gmail.com>
2025-08-01 02:13:50 +0100

    Update core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/parser/ctrlnodes/CtrlNDocNodeBase.java, simplified API

--- core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/parser/ctrlnodes/CtrlNDocNodeBase.java
+++ core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/parser/ctrlnodes/CtrlNDocNodeBase.java
@@ -1,7 +1,7 @@
 package net.thevpc.ndoc.engine.parser.ctrlnodes;
 
 import net.thevpc.ndoc.api.document.node.NDocNodeCtrl;
-import net.thevpc.ndoc.api.parser.NDocResource;
+import net.thevpc.ndoc.api.source.NDocResource;
 import net.thevpc.ndoc.engine.document.DefaultNDocNode;
 
 public abstract class CtrlNDocNodeBase extends DefaultNDocNode implements NDocNodeCtrl {
