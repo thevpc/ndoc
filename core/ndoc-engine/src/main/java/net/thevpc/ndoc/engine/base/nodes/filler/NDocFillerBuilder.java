596ee6f454c0536fc253130ab6f2e5b48ce0ff2d
thevpc <taha.bensalah@gmail.com>
2025-08-02 19:42:34 +0100

    Update core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/base/nodes/filler/NDocFillerBuilder.java

--- core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/base/nodes/filler/NDocFillerBuilder.java
+++ core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/base/nodes/filler/NDocFillerBuilder.java
@@ -2,20 +2,20 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package net.thevpc.ndoc.elem.base.filler;
+package net.thevpc.ndoc.engine.base.nodes.filler;
 
 import net.thevpc.ndoc.api.document.elem2d.NDocBounds2;
 import net.thevpc.ndoc.api.document.node.NDocNode;
 import net.thevpc.ndoc.api.document.node.NDocNodeType;
-import net.thevpc.ndoc.api.extension.NDocNodeCustomBuilder;
-import net.thevpc.ndoc.api.extension.NDocNodeCustomBuilderContext;
+import net.thevpc.ndoc.api.extension.NDocNodeBuilder;
+import net.thevpc.ndoc.api.engine.NDocNodeCustomBuilderContext;
 import net.thevpc.ndoc.api.renderer.NDocGraphics;
 import net.thevpc.ndoc.api.renderer.NDocNodeRendererContext;
 
 /**
  * @author vpc
  */
-public class NDocFillerBuilder implements NDocNodeCustomBuilder {
+public class NDocFillerBuilder implements NDocNodeBuilder {
 
     @Override
     public void build(NDocNodeCustomBuilderContext builderContext) {
