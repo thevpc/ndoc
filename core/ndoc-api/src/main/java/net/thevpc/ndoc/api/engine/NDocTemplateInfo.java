3bfc5b740632be1eb5278a8c236c48a581dcf772
thevpc <taha.bensalah@gmail.com>
2025-08-02 19:42:34 +0100

    Update core/ndoc-api/src/main/java/net/thevpc/ndoc/api/engine/NDocTemplateInfo.java

--- core/ndoc-api/src/main/java/net/thevpc/ndoc/api/engine/NDocTemplateInfo.java
+++ core/ndoc-api/src/main/java/net/thevpc/ndoc/api/engine/NDocTemplateInfo.java
@@ -1,4 +1,15 @@
 package net.thevpc.ndoc.api.engine;
 
 public interface NDocTemplateInfo {
+    String repoUrl();
+
+    String repoName();
+
+    String name();
+
+    String localPath();
+
+    String url();
+
+    boolean recommended();
 }
