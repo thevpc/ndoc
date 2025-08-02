3454441f59aafe9a1e13f5c9a5d26697efb24485
thevpc <taha.bensalah@gmail.com>
2025-08-02 19:42:34 +0100

    Update core/ndoc-api/src/main/java/net/thevpc/ndoc/api/eval/NDocFunctionArgs.java

--- core/ndoc-api/src/main/java/net/thevpc/ndoc/api/eval/NDocFunctionArgs.java
+++ core/ndoc-api/src/main/java/net/thevpc/ndoc/api/eval/NDocFunctionArgs.java
@@ -2,6 +2,15 @@ package net.thevpc.ndoc.api.eval;
 
 import net.thevpc.nuts.elem.NElement;
 
-public interface NDocFunctionArg {
-    NElement eval();
+public interface NDocFunctionArgs {
+    int size();
+
+    NDocFunctionArg[] args();
+
+    NDocFunctionArg arg(int index);
+    NElement src(int index);
+
+    NElement[] eval();
+
+    NElement eval(int index);
 }
