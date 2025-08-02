83f173997cf4881b2d1881b7b22ae108d4b83bc6
thevpc <taha.bensalah@gmail.com>
2025-08-02 19:42:35 +0100

    Update extensions/ndoc-extension-shapes2d/src/main/java/net/thevpc/ndoc/extension/shapes2d/NDocArrowBuilder.java

--- extensions/ndoc-extension-shapes2d/src/main/java/net/thevpc/ndoc/extension/shapes2d/NDocArrowBuilder.java
+++ extensions/ndoc-extension-shapes2d/src/main/java/net/thevpc/ndoc/extension/shapes2d/NDocArrowBuilder.java
@@ -5,9 +5,9 @@ import net.thevpc.ndoc.api.document.elem2d.NDocPoint2D;
 import net.thevpc.ndoc.api.document.node.NDocNode;
 import net.thevpc.ndoc.api.document.node.NDocNodeType;
 import net.thevpc.ndoc.api.document.style.NDocPropName;
-import net.thevpc.ndoc.api.eval.NDocObjEx;
-import net.thevpc.ndoc.api.extension.NDocNodeCustomBuilder;
-import net.thevpc.ndoc.api.extension.NDocNodeCustomBuilderContext;
+import net.thevpc.ndoc.api.eval.NDocValue;
+import net.thevpc.ndoc.api.extension.NDocNodeBuilder;
+import net.thevpc.ndoc.api.engine.NDocNodeCustomBuilderContext;
 import net.thevpc.ndoc.api.renderer.NDocGraphics;
 import net.thevpc.ndoc.api.renderer.NDocNodeRendererContext;
 
@@ -18,7 +18,7 @@ import java.awt.geom.GeneralPath;
 /**
  *
  */
-public class NDocArrowBuilder implements NDocNodeCustomBuilder {
+public class NDocArrowBuilder implements NDocNodeBuilder {
 
     @Override
     public void build(NDocNodeCustomBuilderContext builderContext) {
@@ -38,18 +38,18 @@ public class NDocArrowBuilder implements NDocNodeCustomBuilder {
         double height = b.getHeight();
 
         Paint color = renderContext.getForegroundColor(p, true);
-        NDocPoint2D base = NDocObjEx.of(p.getPropertyValue("base")).asHPoint2D().orElse(null);
+        NDocPoint2D base = NDocValue.of(p.getPropertyValue("base")).asHPoint2D().orElse(null);
         if(base==null){
-            Double base3 = NDocObjEx.of(p.getPropertyValue("base")).asDouble().orElse(null);
+            Double base3 = NDocValue.of(p.getPropertyValue("base")).asDouble().orElse(null);
             if(base3==null){
                 base=new NDocPoint2D(80, 20);
             }else{
                 base=new NDocPoint2D(base3, base3);
             }
         }
-        NDocPoint2D hat = NDocObjEx.of(p.getPropertyValue("hat")).asHPoint2D().orElse(null);
+        NDocPoint2D hat = NDocValue.of(p.getPropertyValue("hat")).asHPoint2D().orElse(null);
         if(hat==null){
-            Double base3 = NDocObjEx.of(p.getPropertyValue("hat")).asDouble().orElse(null);
+            Double base3 = NDocValue.of(p.getPropertyValue("hat")).asDouble().orElse(null);
             if(base3==null){
                 hat=new NDocPoint2D(-1, -1);
             }else{
