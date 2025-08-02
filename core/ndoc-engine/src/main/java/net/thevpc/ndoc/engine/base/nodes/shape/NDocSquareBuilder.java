f3031c5511b71d9f9c210cac59d47ef7d914e7da
thevpc <taha.bensalah@gmail.com>
2025-08-02 19:42:34 +0100

    Update core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/base/nodes/shape/NDocSquareBuilder.java

--- core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/base/nodes/shape/NDocSquareBuilder.java
+++ core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/base/nodes/shape/NDocSquareBuilder.java
@@ -1,4 +1,4 @@
-package net.thevpc.ndoc.elem.base.shape;
+package net.thevpc.ndoc.engine.base.nodes.shape;
 
 import net.thevpc.ndoc.api.document.elem2d.NDocBounds2;
 import net.thevpc.ndoc.api.document.elem2d.NDocDouble2;
@@ -6,13 +6,13 @@ import net.thevpc.ndoc.api.document.node.NDocNode;
 import net.thevpc.ndoc.api.document.node.NDocNodeType;
 import net.thevpc.ndoc.api.document.style.NDocPropName;
 import net.thevpc.ndoc.api.document.style.NDocProperties;
-import net.thevpc.ndoc.api.extension.NDocNodeCustomBuilder;
-import net.thevpc.ndoc.api.extension.NDocNodeCustomBuilderContext;
+import net.thevpc.ndoc.api.extension.NDocNodeBuilder;
+import net.thevpc.ndoc.api.engine.NDocNodeCustomBuilderContext;
 import net.thevpc.ndoc.api.renderer.NDocGraphics;
 import net.thevpc.ndoc.api.renderer.NDocNodeRendererContext;
 import net.thevpc.ndoc.api.util.NDocUtils;
 
-public class NDocSquareBuilder implements NDocNodeCustomBuilder {
+public class NDocSquareBuilder implements NDocNodeBuilder {
     private NDocProperties defaultStyles = new NDocProperties();
 
     @Override
