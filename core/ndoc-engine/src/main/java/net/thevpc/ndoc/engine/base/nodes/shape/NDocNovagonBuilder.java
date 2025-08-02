d659d68e0eba4191d6f7c8f1737815df8e0876c0
thevpc <taha.bensalah@gmail.com>
2025-08-02 19:42:34 +0100

    Update core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/base/nodes/shape/NDocNovagonBuilder.java

--- core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/base/nodes/shape/NDocNovagonBuilder.java
+++ core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/base/nodes/shape/NDocNovagonBuilder.java
@@ -1,13 +1,13 @@
-package net.thevpc.ndoc.elem.base.shape;
+package net.thevpc.ndoc.engine.base.nodes.shape;
 
 import net.thevpc.ndoc.api.document.node.NDocNode;
 import net.thevpc.ndoc.api.document.node.NDocNodeType;
 import net.thevpc.ndoc.api.document.style.NDocProperties;
-import net.thevpc.ndoc.api.extension.NDocNodeCustomBuilder;
-import net.thevpc.ndoc.api.extension.NDocNodeCustomBuilderContext;
+import net.thevpc.ndoc.api.extension.NDocNodeBuilder;
+import net.thevpc.ndoc.api.engine.NDocNodeCustomBuilderContext;
 import net.thevpc.ndoc.api.renderer.NDocNodeRendererContext;
 
-public class NDocNovagonBuilder implements NDocNodeCustomBuilder {
+public class NDocNovagonBuilder implements NDocNodeBuilder {
 
     NDocProperties defaultStyles = new NDocProperties();
 
