72320262b48f44df59e5aada9a1262dbe364b875
thevpc <taha.bensalah@gmail.com>
2025-08-02 19:42:35 +0100

    Update extensions/ndoc-extension-latex/src/main/java/net/thevpc/ndoc/extension/latex/eq/NDocTextRendererFlavorLatexEquation.java

--- extensions/ndoc-extension-latex/src/main/java/net/thevpc/ndoc/extension/latex/eq/NDocTextRendererFlavorLatexEquation.java
+++ extensions/ndoc-extension-latex/src/main/java/net/thevpc/ndoc/extension/latex/eq/NDocTextRendererFlavorLatexEquation.java
@@ -8,6 +8,7 @@ import net.thevpc.ndoc.api.util.Colors;
 import net.thevpc.ndoc.api.util.NDocUtils;
 import net.thevpc.ndoc.api.eval.NDocValueByName;
 import net.thevpc.nuts.reserved.util.NReservedSimpleCharQueue;
+import net.thevpc.nuts.util.NMsg;
 import org.scilab.forge.jlatexmath.TeXConstants;
 import org.scilab.forge.jlatexmath.TeXFormula;
 import org.scilab.forge.jlatexmath.TeXIcon;
@@ -32,7 +33,7 @@ public class NDocTextRendererFlavorLatexEquation implements NDocTextRendererFlav
                     text.toString()
             );
             double fontSize = ctx.getFontSize(p);
-            r.imagePainter = this.createLatex(text, fontSize,options,p,ctx);
+            r.imagePainter = this.createLatex(text, fontSize, options, p, ctx);
             NDocDouble2 size = r.imagePainter.size();
             r.bounds = new Rectangle2D.Double(0, 0, size.getX(), size.getX());
             builder.currRow().addToken(r);
@@ -42,35 +43,36 @@ public class NDocTextRendererFlavorLatexEquation implements NDocTextRendererFlav
     @Override
     public List<NDocTextToken> parseImmediate(NReservedSimpleCharQueue queue, NDocNodeRendererContext ctx) {
         String end;
-        if(queue.peek(2).equals("\\(")) {
+        if (queue.peek(2).equals("\\(")) {
             end = "\\)";
-        }else if(queue.peek(5).equals("[[eq:")){
+            queue.read(2);
+        } else if (queue.peek(5).equals("[[eq:")) {
             end = "]]";
-        }else{
+            queue.read(5);
+        } else {
             return null;
         }
-        NExtendedLatexMathBuilder sb=new NExtendedLatexMathBuilder();
+        NExtendedLatexMathBuilder sb = new NExtendedLatexMathBuilder();
         while (queue.hasNext()) {
             String u = queue.peek(3);
-            if(u.equals("\\"+end)){
+            if (u.equals("\\" + end)) {
                 sb.append(queue.read(3));
-            }else if(queue.peek(2).equals(end)){
+            } else if (queue.peek(2).equals(end)) {
                 queue.read(2);
                 sb.flush();
                 return Arrays.asList(
-                        new NDocTextTokenFlavored(type(),sb.toString().trim())
+                        new NDocTextTokenFlavored(type(), sb.toString().trim())
                 );
-            }else{
+            } else {
                 char c = queue.read();
                 sb.append(c);
             }
         }
         sb.flush();
         return Arrays.asList(
-                new NDocTextTokenFlavored(type(),sb.toString().trim())
+                new NDocTextTokenFlavored(type(), sb.toString().trim())
         );
     }
-
 
 
     public NDocTextRendererBuilder.ImagePainter createLatex(String tex, double fontSize, NDocTextOptions options, NDocNode p, NDocNodeRendererContext ctx) {
@@ -81,7 +83,7 @@ public class NDocTextRendererFlavorLatexEquation implements NDocTextRendererFlav
         } catch (Exception ex) {
             error = true;
             formula = new TeXFormula("?error?");
-            ex.printStackTrace();
+            ctx.engine().log().log(NMsg.ofC("error evaluating latex formula %s : %s", tex, ex),NDocUtils.sourceOf(p));
         }
         float size = (float) (fontSize / 2.0);
         TeXIcon icon = formula.createTeXIcon(TeXConstants.STYLE_DISPLAY, size);
@@ -91,17 +93,17 @@ public class NDocTextRendererFlavorLatexEquation implements NDocTextRendererFlav
 //        if (error) {
 //            return null;
 //        }
-        Color foregroundColor=null;
-        if(options.foregroundColorIndex!=null){
-            foregroundColor=Colors.resolveDefaultColorByIndex(options.foregroundColorIndex,null);
-        }else if(options.foregroundColor instanceof Color){
-            foregroundColor=(Color) options.foregroundColor;
+        Color foregroundColor = null;
+        if (options.foregroundColorIndex != null) {
+            foregroundColor = Colors.resolveDefaultColorByIndex(options.foregroundColorIndex, null);
+        } else if (options.foregroundColor instanceof Color) {
+            foregroundColor = (Color) options.foregroundColor;
         }
-        Color fg= NDocUtils.paintAsColor(NDocUtils.resolveForegroundColor(options));
-        if(fg==null) {
+        Color fg = NDocUtils.paintAsColor(NDocUtils.resolveForegroundColor(options));
+        if (fg == null) {
             fg = NDocUtils.paintAsColor(ctx.getForegroundColor(p, true));
         }
-        if(fg==null) {
+        if (fg == null) {
             fg = Color.BLACK;
         }
         Color finalForegroundColor = fg;
