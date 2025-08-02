286bd6d83297824e0bc32c35e9755e3a72949d4a
thevpc <taha.bensalah@gmail.com>
2025-08-02 19:42:34 +0100

    Update core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/renderer/elem2d/ShapeFactory.java

--- core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/renderer/elem2d/ShapeFactory.java
+++ core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/renderer/elem2d/ShapeFactory.java
@@ -1,7 +1,7 @@
 package net.thevpc.ndoc.engine.renderer.elem2d;
 
 import net.thevpc.ndoc.api.util.NDocUtils;
-import net.thevpc.ndoc.api.eval.NDocObjEx;
+import net.thevpc.ndoc.api.eval.NDocValue;
 import net.thevpc.nuts.util.NOptional;
 import net.thevpc.nuts.elem.NElement;
 
@@ -10,15 +10,15 @@ import java.awt.geom.Ellipse2D;
 
 public class ShapeFactory {
     public static Shape createShape(NElement e) {
-        NDocObjEx o = NDocObjEx.of(e);
+        NDocValue o = NDocValue.of(e);
         if (o.name() != null) {
             switch (o.name()) {
                 case "star": {
                     double radius = 5;
                     for (NElement arg : o.args()) {
-                        NOptional<NDocObjEx.SimplePair> sp = NDocObjEx.of(arg).asSimplePair();
+                        NOptional<NDocValue.SimplePair> sp = NDocValue.of(arg).asSimplePair();
                         if (sp.isPresent()) {
-                            NDocObjEx.SimplePair ke = sp.get();
+                            NDocValue.SimplePair ke = sp.get();
                             switch (NDocUtils.uid(ke.getName())) {
                                 case "radius": {
                                     radius = ke.getValue().asDouble().orElse(radius);
