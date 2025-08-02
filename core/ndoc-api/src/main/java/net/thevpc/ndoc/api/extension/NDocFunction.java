7cdf292c423d6844978c35eff3e155e9963f2936
thevpc <taha.bensalah@gmail.com>
2025-08-02 19:42:34 +0100

    Update core/ndoc-api/src/main/java/net/thevpc/ndoc/api/extension/NDocFunction.java

--- core/ndoc-api/src/main/java/net/thevpc/ndoc/api/extension/NDocFunction.java
+++ core/ndoc-api/src/main/java/net/thevpc/ndoc/api/extension/NDocFunction.java
@@ -1,5 +1,7 @@
-package net.thevpc.ndoc.api.eval;
+package net.thevpc.ndoc.api.extension;
 
+import net.thevpc.ndoc.api.eval.NDocFunctionArgs;
+import net.thevpc.ndoc.api.eval.NDocFunctionContext;
 import net.thevpc.nuts.elem.NElement;
 
 public interface NDocFunction {
