9179963ef29a97e12ea05df132ad220852090802
thevpc <taha.bensalah@gmail.com>
2025-08-02 19:42:34 +0100

    Update core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/base/nodes/container/NDocRowBuilder.java

--- core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/base/nodes/container/NDocRowBuilder.java
+++ core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/base/nodes/container/NDocRowBuilder.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package net.thevpc.ndoc.elem.base.container;
+package net.thevpc.ndoc.engine.base.nodes.container;
 
 import net.thevpc.ndoc.api.document.elem2d.NDocBounds2;
 import net.thevpc.ndoc.api.document.node.NDocNode;
@@ -10,15 +10,15 @@ import net.thevpc.ndoc.api.document.node.NDocNodeType;
 import net.thevpc.ndoc.api.document.style.NDocProp;
 import net.thevpc.ndoc.api.document.style.NDocPropName;
 import net.thevpc.ndoc.api.document.style.NDocProperties;
-import net.thevpc.ndoc.api.extension.NDocNodeCustomBuilder;
-import net.thevpc.ndoc.api.extension.NDocNodeCustomBuilderContext;
+import net.thevpc.ndoc.api.extension.NDocNodeBuilder;
+import net.thevpc.ndoc.api.engine.NDocNodeCustomBuilderContext;
 import net.thevpc.ndoc.api.renderer.NDocNodeRendererContext;
 import net.thevpc.nuts.elem.NElement;
 
 /**
  * @author vpc
  */
-public class NDocRowBuilder implements NDocNodeCustomBuilder {
+public class NDocRowBuilder implements NDocNodeBuilder {
     NDocProperties defaultStyles = new NDocProperties();
 
     @Override
