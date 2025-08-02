ac6cc4de071ce0a2851a4ce989cc6630f0c27238
thevpc <taha.bensalah@gmail.com>
2025-08-02 19:42:34 +0100

    Update core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/base/nodes/container/NDocFlowContainerBuilder.java

--- core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/base/nodes/container/NDocFlowContainerBuilder.java
+++ core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/base/nodes/container/NDocFlowContainerBuilder.java
@@ -2,15 +2,15 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package net.thevpc.ndoc.elem.base.container;
+package net.thevpc.ndoc.engine.base.nodes.container;
 
 import net.thevpc.ndoc.api.document.elem2d.NDocBounds2;
 import net.thevpc.ndoc.api.document.elem2d.NDocDouble2;
 import net.thevpc.ndoc.api.document.node.NDocNode;
 import net.thevpc.ndoc.api.document.node.NDocNodeType;
 import net.thevpc.ndoc.api.document.style.NDocProperties;
-import net.thevpc.ndoc.api.extension.NDocNodeCustomBuilder;
-import net.thevpc.ndoc.api.extension.NDocNodeCustomBuilderContext;
+import net.thevpc.ndoc.api.extension.NDocNodeBuilder;
+import net.thevpc.ndoc.api.engine.NDocNodeCustomBuilderContext;
 import net.thevpc.ndoc.api.document.NDocSizeRequirements;
 import net.thevpc.ndoc.api.renderer.NDocGraphics;
 import net.thevpc.ndoc.api.renderer.NDocNodeRendererContext;
@@ -21,7 +21,7 @@ import java.util.stream.Collectors;
 /**
  * @author vpc
  */
-public class NDocFlowContainerBuilder implements NDocNodeCustomBuilder {
+public class NDocFlowContainerBuilder implements NDocNodeBuilder {
     NDocProperties defaultStyles = new NDocProperties();
     @Override
     public void build(NDocNodeCustomBuilderContext builderContext) {
