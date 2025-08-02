cc88f561862174e871937ff4154ef49a91d419d6
thevpc <taha.bensalah@gmail.com>
2025-08-02 19:42:35 +0100

    Update core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/tools/MyNDocEngineTools.java

--- core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/tools/MyNDocEngineTools.java
+++ core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/tools/MyNDocEngineTools.java
@@ -1,25 +1,17 @@
 package net.thevpc.ndoc.engine.tools;
 
 import net.thevpc.ndoc.api.document.NDocument;
-import net.thevpc.ndoc.api.document.elem2d.NDocBounds2;
-import net.thevpc.ndoc.api.document.elem2d.NDocDouble2;
 import net.thevpc.ndoc.api.document.elem2d.NDocPoint2D;
-import net.thevpc.ndoc.api.document.elem2d.SizeD;
 import net.thevpc.ndoc.api.document.elem3d.NDocPoint3D;
 import net.thevpc.ndoc.api.document.node.NDocNode;
-import net.thevpc.ndoc.api.document.style.NDocPropName;
 import net.thevpc.ndoc.api.engine.NDocEngine;
 import net.thevpc.ndoc.api.engine.NDocEngineTools;
-import net.thevpc.ndoc.api.eval.NDocObjEx;
-import net.thevpc.ndoc.api.eval.NDocValueByName;
 import net.thevpc.ndoc.api.renderer.NDocDocumentStreamRendererConfig;
-import net.thevpc.ndoc.api.renderer.NDocGraphics;
-import net.thevpc.ndoc.api.renderer.NDocNodeRendererContext;
-import net.thevpc.ndoc.api.util.NDocUtils;
-import net.thevpc.ndoc.engine.tools.util.*;
 import net.thevpc.ndoc.engine.renderer.HSpiUtils;
-import net.thevpc.nuts.elem.NElement;
-import net.thevpc.nuts.util.NOptional;
+import net.thevpc.ndoc.engine.util.NDocUtilsImages;
+import net.thevpc.ndoc.engine.util.NDocUtilsPages;
+import net.thevpc.ndoc.engine.util.NDocUtilsPoints;
+import net.thevpc.ndoc.engine.util.NDocUtilsText;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
