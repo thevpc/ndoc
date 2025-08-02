071539c2d737cf43a5633b0e3e3afd97531924ec
thevpc <taha.bensalah@gmail.com>
2025-08-02 19:42:35 +0100

    Update core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/renderer/text/NDocTextRendererBase.java

--- core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/renderer/text/NDocTextRendererBase.java
+++ core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/renderer/text/NDocTextRendererBase.java
@@ -2,7 +2,7 @@ package net.thevpc.ndoc.engine.renderer.text;
 
 import net.thevpc.ndoc.api.document.node.NDocNode;
 import net.thevpc.ndoc.api.document.style.NDocPropName;
-import net.thevpc.ndoc.api.eval.NDocObjEx;
+import net.thevpc.ndoc.api.eval.NDocValue;
 import net.thevpc.ndoc.api.eval.NDocValueByName;
 import net.thevpc.ndoc.api.renderer.NDocNodeRendererContext;
 import net.thevpc.ndoc.api.renderer.text.NDocTextRendererFlavor;
@@ -28,7 +28,7 @@ public class NDocTextRendererBase extends NDocTextBaseRenderer {
         }
         ctx = ctx.withDefaultStyles(p, defaultStyles);
 //        List<NDocNode> all=new ArrayList<>();
-        String text = NDocObjEx.of(p.getPropertyValue(NDocPropName.VALUE).orNull()).asStringOrName().orElse("");
+        String text = NDocValue.of(p.getPropertyValue(NDocPropName.VALUE).orNull()).asStringOrName().orElse("");
         Paint fg = NDocValueByName.getForegroundColor(p, ctx,true);
         NDocTextRendererBuilderImpl builder = new NDocTextRendererBuilderImpl(ctx.engine(),fg);
         f.buildText(text, new NDocTextOptions(), p, ctx, builder);
