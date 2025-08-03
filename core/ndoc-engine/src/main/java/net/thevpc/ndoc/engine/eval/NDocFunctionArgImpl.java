b9de4fd40594ed3735c16d9e036caccd0b14bf5c
thevpc <taha.bensalah@gmail.com>
2025-08-03 15:23:56 +0100

    Update core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/eval/NDocFunctionArgImpl.java

--- core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/eval/NDocFunctionArgImpl.java
+++ core/ndoc-engine/src/main/java/net/thevpc/ndoc/engine/eval/NDocFunctionArgImpl.java
@@ -3,17 +3,20 @@ package net.thevpc.ndoc.engine.eval;
 import net.thevpc.ndoc.api.document.node.NDocNode;
 import net.thevpc.ndoc.api.engine.NDocEngine;
 import net.thevpc.ndoc.api.eval.NDocFunctionArg;
+import net.thevpc.ndoc.api.eval.NDocVarProvider;
 import net.thevpc.nuts.elem.NElement;
 
 public class NDocFunctionArgImpl implements NDocFunctionArg {
     private final NDocNode node;
     private final NElement expression;
     private final NDocEngine engine;
+    private final NDocVarProvider varProvider;
 
-    public NDocFunctionArgImpl(NElement expression, NDocNode node, NDocEngine engine) {
+    public NDocFunctionArgImpl(NElement expression, NDocNode node, NDocEngine engine, NDocVarProvider varProvider) {
         this.node = node;
         this.expression = expression;
         this.engine = engine;
+        this.varProvider = varProvider;
     }
 
     public NElement src() {
@@ -22,8 +25,8 @@ public class NDocFunctionArgImpl implements NDocFunctionArg {
 
     @Override
     public NElement eval() {
-        NElement u = engine.evalExpression(expression, node);
-        NElement u2 = engine.evalExpression(u, node);
+        NElement u = engine.evalExpression(expression, node, varProvider);
+        NElement u2 = engine.evalExpression(u, node, varProvider);
         return u2;
     }
 
