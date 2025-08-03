5f829ecf6e957bc55fb06e6f67aa36b894e71ea1
thevpc <taha.bensalah@gmail.com>
2025-08-03 15:23:55 +0100

    Update core/ndoc-api/src/main/java/net/thevpc/ndoc/api/eval/NDocFunctionContext.java

--- core/ndoc-api/src/main/java/net/thevpc/ndoc/api/eval/NDocFunctionContext.java
+++ core/ndoc-api/src/main/java/net/thevpc/ndoc/api/eval/NDocFunctionContext.java
@@ -1,10 +1,16 @@
 package net.thevpc.ndoc.api.eval;
 
 
+import net.thevpc.ndoc.api.document.node.NDocNode;
 import net.thevpc.ndoc.api.engine.NDocEngine;
 import net.thevpc.ndoc.api.log.NDocLogger;
+import net.thevpc.nuts.elem.NElement;
+import net.thevpc.nuts.util.NOptional;
 
 public interface NDocFunctionContext {
     NDocLogger messages();
     NDocEngine engine();
+    NDocNode node();
+    NElement eval(NElement expr);
+    NOptional<NDocVar> findVar(String varName);
 }
