56c2eee721cb360adccb7edeb6ce72fef26d0b49
thevpc <taha.bensalah@gmail.com>
2025-08-03 15:23:55 +0100

    Update core/ndoc-api/src/main/java/net/thevpc/ndoc/api/eval/NDocVarProvider.java

--- core/ndoc-api/src/main/java/net/thevpc/ndoc/api/eval/NDocVarProvider.java
+++ core/ndoc-api/src/main/java/net/thevpc/ndoc/api/eval/NDocVarProvider.java
@@ -1,4 +1,8 @@
 package net.thevpc.ndoc.api.eval;
 
+import net.thevpc.ndoc.api.document.node.NDocNode;
+import net.thevpc.nuts.util.NOptional;
+
 public interface NDocVarProvider {
+    NOptional<NDocVar> findVar(String varName, NDocNode node);
 }
