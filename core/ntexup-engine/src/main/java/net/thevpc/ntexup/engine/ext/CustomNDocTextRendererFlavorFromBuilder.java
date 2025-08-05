0e55289c32798612f800809eaf2ec3961804d30a
thevpc <taha.bensalah@gmail.com>
2025-08-05 13:55:26 +0100

    Update CustomNDocTextRendererFlavorFromBuilder.java : rename ndoc to ntexup

---
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/ext/CustomNDocTextRendererFlavorFromBuilder.java
@@ -0,0 +1,74 @@
+package net.thevpc.ntexup.engine.ext;
+
+import net.thevpc.ntexup.api.document.node.NTxNode;
+import net.thevpc.ntexup.api.renderer.NDocNodeRendererContext;
+import net.thevpc.ntexup.api.renderer.text.NDocTextRendererFlavor;
+import net.thevpc.ntexup.api.renderer.text.NDocTextToken;
+import net.thevpc.ntexup.api.renderer.text.NDocTextTokenFlavored;
+import net.thevpc.ntexup.api.renderer.text.NDocTextOptions;
+import net.thevpc.ntexup.api.renderer.text.NDocTextRendererBuilder;
+import net.thevpc.ntexup.api.util.NDocUtils;
+import net.thevpc.nuts.reserved.util.NReservedSimpleCharQueue;
+
+import java.util.Arrays;
+import java.util.List;
+
+class CustomNDocTextRendererFlavorFromBuilder implements NDocTextRendererFlavor {
+    private final NDocNodeCustomBuilderContextImpl myNDocNodeCustomBuilderContext;
+
+    public CustomNDocTextRendererFlavorFromBuilder(NDocNodeCustomBuilderContextImpl myNDocNodeCustomBuilderContext) {
+        this.myNDocNodeCustomBuilderContext = myNDocNodeCustomBuilderContext;
+    }
+
+    @Override
+    public String type() {
+        return myNDocNodeCustomBuilderContext.id;
+    }
+
+    @Override
+    public void buildText(String text, NDocTextOptions options, NTxNode p, NDocNodeRendererContext ctx, NDocTextRendererBuilder builder) {
+        myNDocNodeCustomBuilderContext.renderTextAction.buildText(text, options, p, ctx, builder, myNDocNodeCustomBuilderContext);
+    }
+
+    @Override
+    public List<NDocTextToken> parseImmediate(NReservedSimpleCharQueue queue, NDocNodeRendererContext ctx) {
+        if (myNDocNodeCustomBuilderContext.renderEmbeddedTextAction != null) {
+            List<NDocTextToken> u = myNDocNodeCustomBuilderContext.renderEmbeddedTextAction.parseImmediate(queue, ctx, myNDocNodeCustomBuilderContext);
+            if (u != null) {
+                return u;
+            }
+        }
+        String flavor = NDocUtils.uid(myNDocNodeCustomBuilderContext.id);
+
+        String end;
+        if (queue.peek(3 + flavor.length()).equals("[[" + flavor + ":")) {
+            end = "]]";
+            queue.read(3 + flavor.length());
+        } else {
+            return null;
+        }
+        StringBuffer sb = new StringBuffer();
+        while (queue.hasNext()) {
+            String u = queue.peek(3);
+            if (u.equals("\\" + end)) {
+                sb.append(queue.read(3));
+            } else if (queue.peek(2).equals(end)) {
+                queue.read(2);
+                return Arrays.asList(
+                        new NDocTextTokenFlavored(type(), sb.toString().trim())
+                );
+            } else {
+                char c = queue.read();
+                sb.append(c);
+            }
+        }
+        return Arrays.asList(
+                new NDocTextTokenFlavored(type(), sb.toString().trim())
+        );
+    }
+
+    @Override
+    public String toString() {
+        return "CustomFlavorFromBuilder(" + myNDocNodeCustomBuilderContext.id() + ")";
+    }
+}
