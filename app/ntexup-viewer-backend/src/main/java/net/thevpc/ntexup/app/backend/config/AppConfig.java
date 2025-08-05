95e0c7c5b438f8522f090c3f9afaf7e63055e109
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:08:11 +0100

    Update AppConfig.java : rename ndoc to ntexup

---
+++ app/ntexup-viewer-backend/src/main/java/net/thevpc/ntexup/app/backend/config/AppConfig.java
@@ -0,0 +1,15 @@
+package net.thevpc.ntexup.app.backend.config;
+
+import net.thevpc.ntexup.api.engine.NTxEngine;
+import net.thevpc.ntexup.engine.DefaultNTxEngine;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public NTxEngine hEngine() {
+        return new DefaultNTxEngine();
+    }
+
+}
