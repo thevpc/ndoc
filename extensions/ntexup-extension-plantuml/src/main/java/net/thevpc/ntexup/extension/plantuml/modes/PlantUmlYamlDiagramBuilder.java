97f6e53c3ae0d2e9f85768eb1cb923b69cc81856
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:41:31 +0100

    Update PlantUmlYamlDiagramBuilder.java : rename ntx

---
+++ extensions/ntexup-extension-plantuml/src/main/java/net/thevpc/ntexup/extension/plantuml/modes/PlantUmlYamlDiagramBuilder.java
@@ -0,0 +1,9 @@
+package net.thevpc.ntexup.extension.plantuml.modes;
+
+import net.thevpc.ntexup.extension.plantuml.PlantUmlBuilderBase;
+
+public class PlantUmlYamlDiagramBuilder extends PlantUmlBuilderBase {
+    public PlantUmlYamlDiagramBuilder() {
+        super("plantuml-yaml","yaml-diagram");
+    }
+}
