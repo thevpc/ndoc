a5708190c809e869b78f96e2b75fd8f8492cc933
thevpc <taha.bensalah@gmail.com>
2025-08-05 13:55:26 +0100

    Update NDocTrapezoidBuilder.java : rename ndoc to ntexup

---
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/base/nodes/shape/NDocTrapezoidBuilder.java
@@ -0,0 +1,33 @@
+package net.thevpc.ntexup.engine.base.nodes.shape;
+
+import net.thevpc.ntexup.api.document.elem2d.NDocPoint2D;
+import net.thevpc.ntexup.api.document.node.NTxNode;
+import net.thevpc.ntexup.api.document.node.NDocNodeType;
+import net.thevpc.ntexup.api.document.style.NDocProperties;
+import net.thevpc.ntexup.api.extension.NDocNodeBuilder;
+import net.thevpc.ntexup.api.engine.NDocNodeCustomBuilderContext;
+import net.thevpc.ntexup.api.renderer.NDocNodeRendererContext;
+
+public class NDocTrapezoidBuilder implements NDocNodeBuilder {
+    NDocProperties defaultStyles = new NDocProperties();
+
+    @Override
+    public void build(NDocNodeCustomBuilderContext builderContext) {
+        builderContext.id(NDocNodeType.TRAPEZOID)
+                .renderComponent(this::renderMain)
+        ;
+    }
+
+    public void renderMain(NTxNode p, NDocNodeRendererContext rendererContext, NDocNodeCustomBuilderContext builderContext) {
+        rendererContext = rendererContext.withDefaultStyles(p, defaultStyles);
+        double x1 = 20;
+        double x2 = 20;
+        NDocPoint2D[] points = new NDocPoint2D[]{
+                new NDocPoint2D(0, 100),
+                new NDocPoint2D(100, 100),
+                new NDocPoint2D(100 - x2, 0),
+                new NDocPoint2D(x1, 0),
+        };
+        NDocPolygonHelper.renderPoints(p, points, rendererContext);
+    }
+}
