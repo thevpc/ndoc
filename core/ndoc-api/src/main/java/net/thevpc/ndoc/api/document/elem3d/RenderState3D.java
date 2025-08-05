3d75b4f92ac00b41552a84f1f8ce01a0775aea0b
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:40:51 +0100

    Update RenderState3D.java : rename ntx

--- core/ndoc-api/src/main/java/net/thevpc/ndoc/api/document/elem3d/RenderState3D.java
+++
@@ -1,7 +0,0 @@
-package net.thevpc.ndoc.api.document.elem3d;
-
-public interface RenderState3D {
-    NDocVector3D lightOrientation();
-
-    NDocElement3DPrimitive[] toPrimitives(NDocElement3D e);
-}
