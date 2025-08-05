192a5ef7cfff1bda0198c4c786c0912619c689e7
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:41:29 +0100

    Update NTxNodeBuilder.java : rename ntx

---
+++ core/ntexup-api/src/main/java/net/thevpc/ntexup/api/extension/NTxNodeBuilder.java
@@ -0,0 +1,9 @@
+package net.thevpc.ntexup.api.extension;
+
+import net.thevpc.ntexup.api.engine.NTxNodeCustomBuilderContext;
+
+public interface NTxNodeBuilder {
+
+    void build(NTxNodeCustomBuilderContext builderContext);
+
+}
