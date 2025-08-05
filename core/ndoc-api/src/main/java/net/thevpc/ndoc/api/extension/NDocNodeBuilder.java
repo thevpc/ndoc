f09b2694ace18af357e65142ad9298d6bad8a275
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:40:51 +0100

    Update NDocNodeBuilder.java : rename ntx

--- core/ndoc-api/src/main/java/net/thevpc/ndoc/api/extension/NDocNodeBuilder.java
+++
@@ -1,9 +0,0 @@
-package net.thevpc.ndoc.api.extension;
-
-import net.thevpc.ndoc.api.engine.NDocNodeCustomBuilderContext;
-
-public interface NDocNodeBuilder {
-
-    void build(NDocNodeCustomBuilderContext builderContext);
-
-}
