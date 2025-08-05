21cba93f9e433f8cddeb5d9aeeab33ca61ad3c49
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:40:56 +0100

    Rename PlantUmlSaltBuilder.java → PlantUmlSaltBuilder.java : rename ntx

--- extensions/ndoc-extension-plantuml/src/main/java/net/thevpc/ndoc/extension/plantuml/modes/PlantUmlSaltBuilder.java
+++
@@ -1,9 +0,0 @@
-package net.thevpc.ndoc.extension.plantuml.modes;
-
-import net.thevpc.ndoc.extension.plantuml.PlantUmlBuilderBase;
-
-public class PlantUmlSaltBuilder extends PlantUmlBuilderBase {
-    public PlantUmlSaltBuilder() {
-        super("salt");
-    }
-}
