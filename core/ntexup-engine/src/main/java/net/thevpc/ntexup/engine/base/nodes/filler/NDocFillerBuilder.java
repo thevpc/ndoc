a13ce74895c4173516e126df2ad92f9e97dd2a7b
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:08:13 +0100

    Update NDocFillerBuilder.java : rename ndoc to ntexup

--- core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/base/nodes/filler/NDocFillerBuilder.java
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/base/nodes/filler/NDocFillerBuilder.java
@@ -4,11 +4,11 @@
  */
 package net.thevpc.ntexup.engine.base.nodes.filler;
 
-import net.thevpc.ntexup.api.document.elem2d.NDocBounds2;
+import net.thevpc.ntexup.api.document.elem2d.NTxBounds2;
 import net.thevpc.ntexup.api.document.node.NTxNode;
-import net.thevpc.ntexup.api.document.node.NDocNodeType;
+import net.thevpc.ntexup.api.document.node.NTxNodeType;
 import net.thevpc.ntexup.api.extension.NDocNodeBuilder;
-import net.thevpc.ntexup.api.engine.NDocNodeCustomBuilderContext;
+import net.thevpc.ntexup.api.engine.NTxNodeCustomBuilderContext;
 import net.thevpc.ntexup.api.renderer.NDocGraphics;
 import net.thevpc.ntexup.api.renderer.NDocNodeRendererContext;
 
@@ -18,15 +18,15 @@ import net.thevpc.ntexup.api.renderer.NDocNodeRendererContext;
 public class NDocFillerBuilder implements NDocNodeBuilder {
 
     @Override
-    public void build(NDocNodeCustomBuilderContext builderContext) {
-        builderContext.id(NDocNodeType.FILLER)
+    public void build(NTxNodeCustomBuilderContext builderContext) {
+        builderContext.id(NTxNodeType.FILLER)
                 .renderComponent(this::renderMain);
     }
 
-    public void renderMain(NTxNode p, NDocNodeRendererContext ctx, NDocNodeCustomBuilderContext builderContext) {
+    public void renderMain(NTxNode p, NDocNodeRendererContext ctx, NTxNodeCustomBuilderContext builderContext) {
         NDocGraphics g = ctx.graphics();
-        NDocBounds2 bounds = ctx.getBounds();
-        NDocBounds2 b = new NDocBounds2(
+        NTxBounds2 bounds = ctx.getBounds();
+        NTxBounds2 b = new NTxBounds2(
                 bounds.getMinX(),
                 bounds.getMinY(),
                 bounds.getWidth(),
