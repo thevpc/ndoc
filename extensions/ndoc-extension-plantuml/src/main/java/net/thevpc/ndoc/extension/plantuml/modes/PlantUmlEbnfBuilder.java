985654bbdb449ee3da5e61c0ee0d8b394444d40f
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:40:56 +0100

    Rename PlantUmlEbnfBuilder.java → PlantUmlEbnfBuilder.java : rename ntx

--- extensions/ndoc-extension-plantuml/src/main/java/net/thevpc/ndoc/extension/plantuml/modes/PlantUmlEbnfBuilder.java
+++
@@ -1,9 +0,0 @@
-package net.thevpc.ndoc.extension.plantuml.modes;
-
-import net.thevpc.ndoc.extension.plantuml.PlantUmlBuilderBase;
-
-public class PlantUmlEbnfBuilder extends PlantUmlBuilderBase {
-    public PlantUmlEbnfBuilder() {
-        super("ebnf");
-    }
-}
