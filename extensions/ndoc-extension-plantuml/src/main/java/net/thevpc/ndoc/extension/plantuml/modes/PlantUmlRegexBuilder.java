425ab803b9b801b23c4e3e6dd2a7ed69cb56e135
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:40:56 +0100

    Rename PlantUmlRegexBuilder.java → PlantUmlRegexBuilder.java : rename ntx

--- extensions/ndoc-extension-plantuml/src/main/java/net/thevpc/ndoc/extension/plantuml/modes/PlantUmlRegexBuilder.java
+++
@@ -1,9 +0,0 @@
-package net.thevpc.ndoc.extension.plantuml.modes;
-
-import net.thevpc.ndoc.extension.plantuml.PlantUmlBuilderBase;
-
-public class PlantUmlRegexBuilder extends PlantUmlBuilderBase {
-    public PlantUmlRegexBuilder() {
-        super("regex");
-    }
-}
