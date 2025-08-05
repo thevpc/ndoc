a3f434e83aafcd6fb687204a42db3f48221bca47
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:08:11 +0100

    Update NTxCmdLineParser.java : rename ndoc to ntexup

---
+++ app/ntexup/src/main/java/net/thevpc/ntexup/cmdline/NTxCmdLineParser.java
@@ -0,0 +1,28 @@
+package net.thevpc.ntexup.cmdline;
+
+import net.thevpc.nuts.NSession;
+import net.thevpc.nuts.cmdline.NCmdLine;
+import net.thevpc.nuts.io.NPath;
+
+public class NTxCmdLineParser {
+    public void parse(Options options,NCmdLine cmdLine){
+        while (!cmdLine.isEmpty()) {
+            cmdLine.matcher()
+                    .with("--reopen").matchTrueFlag(a -> options.reopen = true)
+                    .with("--list-templates").matchFlag(a -> options.action = net.thevpc.ntexup.cmdline.Action.LIST_TEMPLATES)
+                    .with("--open").matchEntry(a -> options.paths.add(NPath.of(a.stringValue())))
+                    .with("--new").matchTrueFlag(a -> options.action = Action.NEW)
+                    .with("--view").matchTrueFlag(a -> options.requireViewer())
+                    .with("--view-log").matchTrueFlag(a -> options.requireViewer().showLogs = true)
+                    .withNonOption().matchAny(a -> options.paths.add(NPath.of(a.image())))
+                    .requireWithDefault();
+        }
+        if (!options.viewer && !options.console) {
+            options.viewer = NSession.of().isGui();
+            options.console = !options.viewer;
+        } else if (options.viewer && options.console) {
+            options.viewer = NSession.of().isGui();
+            options.console = !options.viewer;
+        }
+    }
+}
