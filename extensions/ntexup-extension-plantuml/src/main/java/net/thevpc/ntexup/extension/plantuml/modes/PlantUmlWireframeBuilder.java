9023ff083d5b631337883d7b6f92be75b68e27c6
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:41:31 +0100

    Update PlantUmlWireframeBuilder.java : rename ntx

---
+++ extensions/ntexup-extension-plantuml/src/main/java/net/thevpc/ntexup/extension/plantuml/modes/PlantUmlWireframeBuilder.java
@@ -0,0 +1,9 @@
+package net.thevpc.ntexup.extension.plantuml.modes;
+
+import net.thevpc.ntexup.extension.plantuml.PlantUmlBuilderBase;
+
+public class PlantUmlWireframeBuilder extends PlantUmlBuilderBase {
+    public PlantUmlWireframeBuilder() {
+        super("wireframe");
+    }
+}
