d692aa7917791f429d94988d8ce6af52982cfb4b
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:41:31 +0100

    Update PlantUmlMindmapBuilder.java : rename ntx

---
+++ extensions/ntexup-extension-plantuml/src/main/java/net/thevpc/ntexup/extension/plantuml/modes/PlantUmlMindmapBuilder.java
@@ -0,0 +1,9 @@
+package net.thevpc.ntexup.extension.plantuml.modes;
+
+import net.thevpc.ntexup.extension.plantuml.PlantUmlBuilderBase;
+
+public class PlantUmlMindmapBuilder extends PlantUmlBuilderBase {
+    public PlantUmlMindmapBuilder() {
+        super("mindmap");
+    }
+}
