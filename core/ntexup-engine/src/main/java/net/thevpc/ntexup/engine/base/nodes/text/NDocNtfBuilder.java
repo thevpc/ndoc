b924bd6791259685637e20c965d106a79ea9394c
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:08:13 +0100

    Update NDocNtfBuilder.java : rename ndoc to ntexup

--- core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/base/nodes/text/NDocNtfBuilder.java
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/base/nodes/text/NDocNtfBuilder.java
@@ -1,10 +1,10 @@
 package net.thevpc.ntexup.engine.base.nodes.text;
 
 import net.thevpc.ntexup.api.document.node.NTxNode;
-import net.thevpc.ntexup.api.document.node.NDocNodeType;
-import net.thevpc.ntexup.api.document.style.NDocPropName;
+import net.thevpc.ntexup.api.document.node.NTxNodeType;
+import net.thevpc.ntexup.api.document.style.NTxPropName;
 import net.thevpc.ntexup.api.extension.NDocNodeBuilder;
-import net.thevpc.ntexup.api.engine.NDocNodeCustomBuilderContext;
+import net.thevpc.ntexup.api.engine.NTxNodeCustomBuilderContext;
 import net.thevpc.ntexup.api.renderer.NDocNodeRendererContext;
 import net.thevpc.ntexup.api.renderer.text.NDocTextOptions;
 import net.thevpc.ntexup.api.renderer.text.NDocTextRendererBuilder;
@@ -13,17 +13,17 @@ import net.thevpc.nuts.text.NTexts;
 public class NDocNtfBuilder implements NDocNodeBuilder {
 
     @Override
-    public void build(NDocNodeCustomBuilderContext builderContext) {
-        builderContext.id(NDocNodeType.NTF)
-                .parseParam().named(NDocPropName.VALUE).resolvedAsTrimmedBloc().then()
-                .parseParam().named(NDocPropName.FILE).store(NDocPropName.VALUE).resolvedAsTrimmedPathTextContent().then()
+    public void build(NTxNodeCustomBuilderContext builderContext) {
+        builderContext.id(NTxNodeType.NTF)
+                .parseParam().named(NTxPropName.VALUE).resolvedAsTrimmedBloc().then()
+                .parseParam().named(NTxPropName.FILE).store(NTxPropName.VALUE).resolvedAsTrimmedPathTextContent().then()
                 .parseDefaultParams()
-                .parseParam().matchesStringOrName().store(NDocPropName.VALUE).resolvedAsTrimmedBloc().then()
+                .parseParam().matchesStringOrName().store(NTxPropName.VALUE).resolvedAsTrimmedBloc().then()
                 .renderText(this::renderTextBuildText)
         ;
     }
 
-    private void renderTextBuildText(String text, NDocTextOptions options, NTxNode p, NDocNodeRendererContext renderContext, NDocTextRendererBuilder builder, NDocNodeCustomBuilderContext buildContext) {
+    private void renderTextBuildText(String text, NDocTextOptions options, NTxNode p, NDocNodeRendererContext renderContext, NDocTextRendererBuilder builder, NTxNodeCustomBuilderContext buildContext) {
         renderContext.highlightNutsText("ntf", text, NTexts.of().of(text), p, builder);
     }
 
