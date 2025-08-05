d43854b4afbc4c0ff9d75e94966fcf70ba28b714
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:41:31 +0100

    Update PlantUmlNwdiagBuilder.java : rename ntx

---
+++ extensions/ntexup-extension-plantuml/src/main/java/net/thevpc/ntexup/extension/plantuml/modes/PlantUmlNwdiagBuilder.java
@@ -0,0 +1,9 @@
+package net.thevpc.ntexup.extension.plantuml.modes;
+
+import net.thevpc.ntexup.extension.plantuml.PlantUmlBuilderBase;
+
+public class PlantUmlNwdiagBuilder extends PlantUmlBuilderBase {
+    public PlantUmlNwdiagBuilder() {
+        super("nwdiag");
+    }
+}
