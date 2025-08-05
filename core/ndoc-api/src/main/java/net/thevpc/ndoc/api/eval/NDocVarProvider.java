d66e54e1fcfd2d3a4e407974c0095901794e81dc
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:40:51 +0100

    Update NDocVarProvider.java : rename ntx

--- core/ndoc-api/src/main/java/net/thevpc/ndoc/api/eval/NDocVarProvider.java
+++
@@ -1,8 +0,0 @@
-package net.thevpc.ndoc.api.eval;
-
-import net.thevpc.ndoc.api.document.node.NDocNode;
-import net.thevpc.nuts.util.NOptional;
-
-public interface NDocVarProvider {
-    NOptional<NDocVar> findVar(String varName, NDocNode node);
-}
