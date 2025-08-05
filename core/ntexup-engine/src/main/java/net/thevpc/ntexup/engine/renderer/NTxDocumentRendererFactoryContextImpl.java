4aafe5cb1b563b1652f7724f0805a40862f79803
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:41:31 +0100

    Update NTxDocumentRendererFactoryContextImpl.java : rename ntx

---
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/renderer/NTxDocumentRendererFactoryContextImpl.java
@@ -0,0 +1,27 @@
+package net.thevpc.ntexup.engine.renderer;
+
+import net.thevpc.ntexup.api.engine.NTxEngine;
+import net.thevpc.ntexup.engine.DefaultNTxEngine;
+import net.thevpc.ntexup.api.renderer.NTxDocumentRendererFactoryContext;
+import net.thevpc.nuts.util.NStringUtils;
+
+public class NTxDocumentRendererFactoryContextImpl implements NTxDocumentRendererFactoryContext {
+    private final DefaultNTxEngine hEngine;
+    private final String type;
+
+    public NTxDocumentRendererFactoryContextImpl(DefaultNTxEngine hEngine, String type) {
+        this.hEngine = hEngine;
+        this.type = NStringUtils.trim(type);
+    }
+
+    @Override
+    public String rendererType() {
+        return type;
+    }
+
+    @Override
+    public NTxEngine engine() {
+        return hEngine;
+    }
+
+}
