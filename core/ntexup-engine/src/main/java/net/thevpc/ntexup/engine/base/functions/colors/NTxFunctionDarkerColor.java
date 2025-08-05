f3de6c60b3214ee7e656da996e04265ec9031645
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:40:54 +0100

    Update NTxFunctionDarkerColor.java : rename ntx

--- core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/base/functions/colors/NTxFunctionDarkerColor.java
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/base/functions/colors/NTxFunctionDarkerColor.java
@@ -1,11 +1,11 @@
 package net.thevpc.ntexup.engine.base.functions.colors;
 
-import net.thevpc.ntexup.api.eval.NDocFunctionArgs;
-import net.thevpc.ntexup.api.eval.NDocFunctionContext;
-import net.thevpc.ntexup.api.eval.NDocValue;
+import net.thevpc.ntexup.api.eval.NTxFunctionArgs;
+import net.thevpc.ntexup.api.eval.NTxFunctionContext;
+import net.thevpc.ntexup.api.eval.NTxValue;
 import net.thevpc.ntexup.api.extension.NTxFunction;
-import net.thevpc.ntexup.engine.util.NDocColorUtils;
-import net.thevpc.ntexup.engine.util.NDocElementUtils;
+import net.thevpc.ntexup.engine.util.NTxColorUtils;
+import net.thevpc.ntexup.engine.util.NTxElementUtils;
 import net.thevpc.nuts.elem.NElement;
 import net.thevpc.nuts.util.NMsg;
 
@@ -18,18 +18,18 @@ public class NTxFunctionDarkerColor implements NTxFunction {
     }
 
     @Override
-    public NElement invoke(NDocFunctionArgs args, NDocFunctionContext context) {
+    public NElement invoke(NTxFunctionArgs args, NTxFunctionContext context) {
         if (args.size() == 0) {
             return NElement.ofNull();
         }
-        Color c = NDocValue.of(args.eval(0)).asColor().get();
+        Color c = NTxValue.of(args.eval(0)).asColor().get();
         if (args.size() == 1) {
-            return NDocElementUtils.toElement(NDocColorUtils.darker(c));
+            return NTxElementUtils.toElement(NTxColorUtils.darker(c));
         }
         if (args.size() > 2) {
             context.messages().log(NMsg.ofC("%s: expected 2 arguments, got %s", name(), args.size()));
         }
-        float degrees = NDocValue.of(args.eval(1)).asFloat().get();
-        return NDocElementUtils.toElement(NDocColorUtils.darker(c, degrees));
+        float degrees = NTxValue.of(args.eval(1)).asFloat().get();
+        return NTxElementUtils.toElement(NTxColorUtils.darker(c, degrees));
     }
 }
