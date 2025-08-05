cbd5096dcb8d2364bf90c0eac26c62e3f6388b0c
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:40:54 +0100

    Update NTxFunctionGrayScaleColor.java : rename ntx

--- core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/base/functions/colors/NTxFunctionGrayScaleColor.java
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/base/functions/colors/NTxFunctionGrayScaleColor.java
@@ -1,11 +1,11 @@
 package net.thevpc.ntexup.engine.base.functions.colors;
 
 import net.thevpc.ntexup.api.extension.NTxFunction;
-import net.thevpc.ntexup.api.eval.NDocFunctionArgs;
-import net.thevpc.ntexup.api.eval.NDocFunctionContext;
-import net.thevpc.ntexup.api.eval.NDocValue;
-import net.thevpc.ntexup.engine.util.NDocColorUtils;
-import net.thevpc.ntexup.engine.util.NDocElementUtils;
+import net.thevpc.ntexup.api.eval.NTxFunctionArgs;
+import net.thevpc.ntexup.api.eval.NTxFunctionContext;
+import net.thevpc.ntexup.api.eval.NTxValue;
+import net.thevpc.ntexup.engine.util.NTxColorUtils;
+import net.thevpc.ntexup.engine.util.NTxElementUtils;
 import net.thevpc.nuts.elem.NElement;
 import net.thevpc.nuts.util.NMsg;
 
@@ -18,14 +18,14 @@ public class NTxFunctionGrayScaleColor implements NTxFunction {
     }
 
     @Override
-    public NElement invoke(NDocFunctionArgs args, NDocFunctionContext context) {
+    public NElement invoke(NTxFunctionArgs args, NTxFunctionContext context) {
         if (args.size() == 0) {
             return NElement.ofNull();
         }
         if (args.size() > 1) {
             context.messages().log(NMsg.ofC("%s: expected 1 argument, got %s",name(), args.size()));
         }
-        Color c = NDocValue.of(args.eval(0)).asColor().get();
-        return NDocElementUtils.toElement(NDocColorUtils.grayscale(c));
+        Color c = NTxValue.of(args.eval(0)).asColor().get();
+        return NTxElementUtils.toElement(NTxColorUtils.grayscale(c));
     }
 }
