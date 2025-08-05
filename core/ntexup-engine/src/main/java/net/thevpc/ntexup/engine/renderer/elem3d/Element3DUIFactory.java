24b3989f966dabb82312c9fc4fcf4f36acddd680
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:40:55 +0100

    Update Element3DUIFactory.java : rename ntx

--- core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/renderer/elem3d/Element3DUIFactory.java
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/renderer/elem3d/Element3DUIFactory.java
@@ -2,30 +2,30 @@ package net.thevpc.ntexup.engine.renderer.elem3d;
 
 import net.thevpc.ntexup.api.document.elem3d.NtxElement3DPrimitive;
 import net.thevpc.ntexup.api.document.elem3d.NtxElement3D;
-import net.thevpc.ntexup.api.document.elem3d.RenderState3D;
-import net.thevpc.ntexup.api.renderer.NDocElement3DRenderer;
+import net.thevpc.ntexup.api.document.elem3d.NTxRenderState3D;
+import net.thevpc.ntexup.api.renderer.NTxElement3DRenderer;
 
 import java.util.*;
 
 public class Element3DUIFactory {
-    private Map<Class, NDocElement3DRenderer> map = new HashMap<>();
+    private Map<Class, NTxElement3DRenderer> map = new HashMap<>();
 
     public Element3DUIFactory() {
-        ServiceLoader<NDocElement3DRenderer> serviceLoader = ServiceLoader.load(NDocElement3DRenderer.class);
-        for (NDocElement3DRenderer element3DPrimitiveBuilder : serviceLoader) {
+        ServiceLoader<NTxElement3DRenderer> serviceLoader = ServiceLoader.load(NTxElement3DRenderer.class);
+        for (NTxElement3DRenderer element3DPrimitiveBuilder : serviceLoader) {
             register(element3DPrimitiveBuilder.forType(), element3DPrimitiveBuilder);
         }
     }
 
-    void register(Class c, NDocElement3DRenderer f) {
+    void register(Class c, NTxElement3DRenderer f) {
         map.put(c, f);
     }
 
-    public NtxElement3DPrimitive[] toPrimitives(NtxElement3D e, RenderState3D renderState) {
+    public NtxElement3DPrimitive[] toPrimitives(NtxElement3D e, NTxRenderState3D renderState) {
         if (e instanceof NtxElement3DPrimitive) {
             return new NtxElement3DPrimitive[]{(NtxElement3DPrimitive) e};
         }
-        NDocElement3DRenderer i = map.get(e.getClass());
+        NTxElement3DRenderer i = map.get(e.getClass());
         if (i != null) {
             return i.toPrimitives(e, renderState);
         }
