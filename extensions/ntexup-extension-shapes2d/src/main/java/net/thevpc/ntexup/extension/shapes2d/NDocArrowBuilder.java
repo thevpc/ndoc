f9da462353c86c2a85ec83372ab1f3f94ac3504d
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:08:14 +0100

    Update NDocArrowBuilder.java : rename ndoc to ntexup

--- extensions/ntexup-extension-shapes2d/src/main/java/net/thevpc/ntexup/extension/shapes2d/NDocArrowBuilder.java
+++ extensions/ntexup-extension-shapes2d/src/main/java/net/thevpc/ntexup/extension/shapes2d/NDocArrowBuilder.java
@@ -1,13 +1,13 @@
 package net.thevpc.ntexup.extension.shapes2d;
 
-import net.thevpc.ntexup.api.document.elem2d.NDocBounds2;
-import net.thevpc.ntexup.api.document.elem2d.NDocPoint2D;
+import net.thevpc.ntexup.api.document.elem2d.NTxBounds2;
+import net.thevpc.ntexup.api.document.elem2d.NTxPoint2D;
 import net.thevpc.ntexup.api.document.node.NTxNode;
-import net.thevpc.ntexup.api.document.node.NDocNodeType;
-import net.thevpc.ntexup.api.document.style.NDocPropName;
+import net.thevpc.ntexup.api.document.node.NTxNodeType;
+import net.thevpc.ntexup.api.document.style.NTxPropName;
 import net.thevpc.ntexup.api.eval.NDocValue;
 import net.thevpc.ntexup.api.extension.NDocNodeBuilder;
-import net.thevpc.ntexup.api.engine.NDocNodeCustomBuilderContext;
+import net.thevpc.ntexup.api.engine.NTxNodeCustomBuilderContext;
 import net.thevpc.ntexup.api.renderer.NDocGraphics;
 import net.thevpc.ntexup.api.renderer.NDocNodeRendererContext;
 
@@ -21,39 +21,39 @@ import java.awt.geom.GeneralPath;
 public class NDocArrowBuilder implements NDocNodeBuilder {
 
     @Override
-    public void build(NDocNodeCustomBuilderContext builderContext) {
+    public void build(NTxNodeCustomBuilderContext builderContext) {
         builderContext
-                .id(NDocNodeType.ARROW)
-                .parseParam().named(NDocPropName.WIDTH,NDocPropName.HEIGHT,"base","hat").end()
+                .id(NTxNodeType.ARROW)
+                .parseParam().named(NTxPropName.WIDTH, NTxPropName.HEIGHT,"base","hat").end()
                 .renderComponent(this::render)
                 ;
     }
 
 
-    public void render(NTxNode p, NDocNodeRendererContext renderContext, NDocNodeCustomBuilderContext buildContext) {
-        NDocBounds2 b = renderContext.selfBounds(p, null, null);
+    public void render(NTxNode p, NDocNodeRendererContext renderContext, NTxNodeCustomBuilderContext buildContext) {
+        NTxBounds2 b = renderContext.selfBounds(p, null, null);
         double x = b.getX();
         double y = b.getY();
         double width = b.getWidth();
         double height = b.getHeight();
 
         Paint color = renderContext.getForegroundColor(p, true);
-        NDocPoint2D base = NDocValue.of(p.getPropertyValue("base")).asHPoint2D().orElse(null);
+        NTxPoint2D base = NDocValue.of(p.getPropertyValue("base")).asHPoint2D().orElse(null);
         if(base==null){
             Double base3 = NDocValue.of(p.getPropertyValue("base")).asDouble().orElse(null);
             if(base3==null){
-                base=new NDocPoint2D(80, 20);
+                base=new NTxPoint2D(80, 20);
             }else{
-                base=new NDocPoint2D(base3, base3);
+                base=new NTxPoint2D(base3, base3);
             }
         }
-        NDocPoint2D hat = NDocValue.of(p.getPropertyValue("hat")).asHPoint2D().orElse(null);
+        NTxPoint2D hat = NDocValue.of(p.getPropertyValue("hat")).asHPoint2D().orElse(null);
         if(hat==null){
             Double base3 = NDocValue.of(p.getPropertyValue("hat")).asDouble().orElse(null);
             if(base3==null){
-                hat=new NDocPoint2D(-1, -1);
+                hat=new NTxPoint2D(-1, -1);
             }else{
-                hat=new NDocPoint2D(base3, base3);
+                hat=new NTxPoint2D(base3, base3);
             }
         }
         if(hat.y<0){
