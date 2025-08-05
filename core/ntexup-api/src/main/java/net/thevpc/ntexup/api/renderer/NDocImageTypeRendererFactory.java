4553f7a0eb81a5fba4d0123504fd377ef2540246
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:40:53 +0100

    Rename NTxImageTypeRendererFactory.java → NDocImageTypeRendererFactory.java : rename ntx

--- core/ntexup-api/src/main/java/net/thevpc/ntexup/api/renderer/NDocImageTypeRendererFactory.java
+++
@@ -1,9 +0,0 @@
-package net.thevpc.ntexup.api.renderer;
-
-import net.thevpc.ntexup.api.document.elem2d.NTxImageOptions;
-import net.thevpc.nuts.NCallableSupport;
-import net.thevpc.nuts.io.NPath;
-
-public interface NDocImageTypeRendererFactory {
-    NCallableSupport<NDocGraphicsImageDrawer> resolveRenderer(NPath path, NTxImageOptions options, NDocGraphics graphics);
-}
