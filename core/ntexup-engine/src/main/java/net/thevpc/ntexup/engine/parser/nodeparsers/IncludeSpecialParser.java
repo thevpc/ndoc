077cf08b4d7c0600a35ec836efa313948e357e15
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:40:55 +0100

    Update IncludeSpecialParser.java : rename ntx

--- core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/parser/nodeparsers/IncludeSpecialParser.java
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/parser/nodeparsers/IncludeSpecialParser.java
@@ -1,24 +1,24 @@
 package net.thevpc.ntexup.engine.parser.nodeparsers;
 
-import net.thevpc.ntexup.engine.parser.NDocNodeParserBase;
+import net.thevpc.ntexup.engine.parser.NTxNodeParserBase;
 import net.thevpc.ntexup.api.document.node.NTxItem;
 import net.thevpc.ntexup.api.document.node.NTxNodeType;
-import net.thevpc.ntexup.api.parser.NDocNodeFactoryParseContext;
-import net.thevpc.ntexup.api.util.NDocUtils;
+import net.thevpc.ntexup.api.parser.NTxNodeFactoryParseContext;
+import net.thevpc.ntexup.api.util.NTxUtils;
 import net.thevpc.ntexup.engine.parser.ctrlnodes.CtrlNTxNodeInclude;
 import net.thevpc.nuts.NCallableSupport;
 import net.thevpc.nuts.elem.NUpletElement;
 import net.thevpc.nuts.util.NMsg;
 import net.thevpc.nuts.elem.NElement;
 
-public class IncludeSpecialParser extends NDocNodeParserBase {
+public class IncludeSpecialParser extends NTxNodeParserBase {
     public IncludeSpecialParser() {
         super(true, NTxNodeType.CTRL_INCLUDE);
     }
 
 
     @Override
-    public NCallableSupport<NTxItem> parseNode(NDocNodeFactoryParseContext context) {
+    public NCallableSupport<NTxItem> parseNode(NTxNodeFactoryParseContext context) {
         NElement tsonElement = context.element();
         switch (tsonElement.type()) {
             case NAMED_UPLET: {
@@ -29,7 +29,7 @@ public class IncludeSpecialParser extends NDocNodeParserBase {
                 break;
             }
         }
-        return NCallableSupport.ofInvalid(NMsg.ofC("missing include from %s", NDocUtils.snippet(tsonElement)).asError());
+        return NCallableSupport.ofInvalid(NMsg.ofC("missing include from %s", NTxUtils.snippet(tsonElement)).asError());
     }
 
 
