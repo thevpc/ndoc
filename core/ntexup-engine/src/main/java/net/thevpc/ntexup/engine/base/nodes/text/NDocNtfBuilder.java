7b8c71e295d64f261c4f2a99d28199c3334e209c
thevpc <taha.bensalah@gmail.com>
2025-08-05 13:55:26 +0100

    Update NDocNtfBuilder.java : rename ndoc to ntexup

---
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/base/nodes/text/NDocNtfBuilder.java
@@ -0,0 +1,30 @@
+package net.thevpc.ntexup.engine.base.nodes.text;
+
+import net.thevpc.ntexup.api.document.node.NTxNode;
+import net.thevpc.ntexup.api.document.node.NDocNodeType;
+import net.thevpc.ntexup.api.document.style.NDocPropName;
+import net.thevpc.ntexup.api.extension.NDocNodeBuilder;
+import net.thevpc.ntexup.api.engine.NDocNodeCustomBuilderContext;
+import net.thevpc.ntexup.api.renderer.NDocNodeRendererContext;
+import net.thevpc.ntexup.api.renderer.text.NDocTextOptions;
+import net.thevpc.ntexup.api.renderer.text.NDocTextRendererBuilder;
+import net.thevpc.nuts.text.NTexts;
+
+public class NDocNtfBuilder implements NDocNodeBuilder {
+
+    @Override
+    public void build(NDocNodeCustomBuilderContext builderContext) {
+        builderContext.id(NDocNodeType.NTF)
+                .parseParam().named(NDocPropName.VALUE).resolvedAsTrimmedBloc().then()
+                .parseParam().named(NDocPropName.FILE).store(NDocPropName.VALUE).resolvedAsTrimmedPathTextContent().then()
+                .parseDefaultParams()
+                .parseParam().matchesStringOrName().store(NDocPropName.VALUE).resolvedAsTrimmedBloc().then()
+                .renderText(this::renderTextBuildText)
+        ;
+    }
+
+    private void renderTextBuildText(String text, NDocTextOptions options, NTxNode p, NDocNodeRendererContext renderContext, NDocTextRendererBuilder builder, NDocNodeCustomBuilderContext buildContext) {
+        renderContext.highlightNutsText("ntf", text, NTexts.of().of(text), p, builder);
+    }
+
+}
