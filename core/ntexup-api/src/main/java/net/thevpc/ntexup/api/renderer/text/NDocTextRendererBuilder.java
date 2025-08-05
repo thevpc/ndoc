272f989bbf2622468a8d0b9dfc9f71b1eca89b70
thevpc <taha.bensalah@gmail.com>
2025-08-05 13:55:25 +0100

    Update NDocTextRendererBuilder.java : rename ndoc to ntexup

---
+++ core/ntexup-api/src/main/java/net/thevpc/ntexup/api/renderer/text/NDocTextRendererBuilder.java
@@ -0,0 +1,40 @@
+package net.thevpc.ntexup.api.renderer.text;
+
+import net.thevpc.ntexup.api.document.elem2d.NDocBounds2;
+import net.thevpc.ntexup.api.document.elem2d.NDocDouble2;
+import net.thevpc.ntexup.api.document.node.NTxNode;
+import net.thevpc.ntexup.api.renderer.NDocGraphics;
+import net.thevpc.ntexup.api.renderer.NDocNodeRendererContext;
+import net.thevpc.nuts.text.NText;
+
+public interface NDocTextRendererBuilder {
+    void appendText(String rawText, NDocTextOptions options, NTxNode node, NDocNodeRendererContext ctx);
+
+    void appendNText(String lang, String rawText, NText text, NTxNode node, NDocNodeRendererContext ctx);
+
+    void appendCustom(String lang, String rawText, NDocTextOptions options, NTxNode node, NDocNodeRendererContext ctx);
+
+    public void appendPlain(String text, NDocNodeRendererContext ctx);
+
+    NDocRichTextRow nextLine();
+
+    NDocRichTextRow currRow();
+
+    NDocBounds2 computeBound(NDocNodeRendererContext ctx);
+
+    public void setLang(String lang);
+
+    public void setCode(String rawText);
+
+    interface ImagePainter {
+        void paint(NDocGraphics g, double x, double y);
+
+        NDocDouble2 size();
+    }
+
+    public void render(NTxNode p, NDocNodeRendererContext ctx, NDocBounds2 bgBounds, NDocBounds2 selfBounds);
+
+    boolean isEmpty();
+
+    void addToken(NDocRichTextToken col);
+}
