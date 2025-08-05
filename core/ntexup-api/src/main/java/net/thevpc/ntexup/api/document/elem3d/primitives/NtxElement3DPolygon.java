663257035f06f82895fa8195854214086aa0ac02
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:08:12 +0100

    Update NtxElement3DPolygon.java : rename ndoc to ntexup

---
+++ core/ntexup-api/src/main/java/net/thevpc/ntexup/api/document/elem3d/primitives/NtxElement3DPolygon.java
@@ -0,0 +1,34 @@
+package net.thevpc.ntexup.api.document.elem3d.primitives;
+
+import net.thevpc.ntexup.api.document.elem3d.AbstractNTxElement3DPrimitive;
+import net.thevpc.ntexup.api.document.elem3d.NDocElement3DPrimitiveType;
+import net.thevpc.ntexup.api.document.elem3d.NTxPoint3D;
+
+public class NtxElement3DPolygon extends AbstractNTxElement3DPrimitive {
+    private NTxPoint3D[] nodes;
+    private boolean fill;
+    private boolean contour;
+
+    public NtxElement3DPolygon(NTxPoint3D[] nodes, boolean fill, boolean contour) {
+        this.nodes = nodes;
+        this.fill = fill;
+        this.contour = contour;
+    }
+
+    public boolean isFill() {
+        return fill;
+    }
+
+    public boolean isContour() {
+        return contour;
+    }
+
+    public NTxPoint3D[] getNodes() {
+        return nodes;
+    }
+
+    @Override
+    public NDocElement3DPrimitiveType type() {
+        return NDocElement3DPrimitiveType.POLYGON;
+    }
+}
