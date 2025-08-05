1da6f12d6f16cdf45da67927fc5933fd28974fc6
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:08:11 +0100

    Update NTxDocument.java : rename ndoc to ntexup

---
+++ core/ntexup-api/src/main/java/net/thevpc/ntexup/api/document/NTxDocument.java
@@ -0,0 +1,36 @@
+package net.thevpc.ntexup.api.document;
+
+import java.util.List;
+import java.util.Properties;
+
+import net.thevpc.ntexup.api.document.node.NTxNode;
+import net.thevpc.ntexup.api.source.NDocResource;
+import net.thevpc.ntexup.api.source.NDocResourceMonitor;
+import net.thevpc.nuts.util.NOptional;
+
+public interface NTxDocument {
+    NDocResource source();
+    NDocResourceMonitor resources();
+
+    NTxDocument add(NTxNode part);
+
+    NDocDocumentClass documentClass();
+
+    NTxNode root();
+
+    NOptional<String> name();
+
+    Properties getProperties();
+
+    NOptional<String> getProperty(String name);
+
+    void setDocumentClass(NDocDocumentClass documentClass);
+
+    NTxDocument setProperty(String name, String value);
+
+    void mergeDocument(NTxDocument other);
+
+    NTxDocument copy();
+
+    List<NTxNode> pages();
+}
