e9f94fb4b174205b197162f4907fc2ade8d493f8
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:08:11 +0100

    Update NTxViewerProcessor.java : rename ndoc to ntexup

---
+++ app/ntexup/src/main/java/net/thevpc/ntexup/cmdline/NTxViewerProcessor.java
@@ -0,0 +1,35 @@
+package net.thevpc.ntexup.cmdline;
+
+import com.formdev.flatlaf.FlatLightLaf;
+import net.thevpc.ntexup.main.MainFrame;
+import net.thevpc.nuts.io.NPath;
+import net.thevpc.nuts.nswing.NSwingUtils;
+
+import javax.swing.*;
+
+public class NTxViewerProcessor {
+    public void runViewer(Options options) {
+        NSwingUtils.setSharedWorkspaceInstance();
+        FlatLightLaf.setup(new com.formdev.flatlaf.FlatDarculaLaf());
+        MainFrame mainFrame = new MainFrame();
+        SwingUtilities.invokeLater(() -> {
+            mainFrame.setVisible(true);
+            if (options.reopen) {
+                NPath p = mainFrame.getService().getLatestProjectPath();
+                if (p != null) {
+                    options.paths.add(p);
+                }
+            }
+            if (options.showLogs) {
+                mainFrame.getService().showDebug();
+            }
+            if (options.paths.isEmpty()) {
+
+            } else {
+                for (NPath path : options.paths) {
+                    mainFrame.getService().openProject(path);
+                }
+            }
+        });
+    }
+}
