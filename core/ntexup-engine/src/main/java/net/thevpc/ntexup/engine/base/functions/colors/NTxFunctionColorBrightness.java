1a90d31b856cf596ca1d86464f83112da8a177ac
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:40:54 +0100

    Update NTxFunctionColorBrightness.java : rename ntx

--- core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/base/functions/colors/NTxFunctionColorBrightness.java
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/base/functions/colors/NTxFunctionColorBrightness.java
@@ -1,11 +1,11 @@
 package net.thevpc.ntexup.engine.base.functions.colors;
 
 import net.thevpc.ntexup.api.extension.NTxFunction;
-import net.thevpc.ntexup.api.eval.NDocFunctionArgs;
-import net.thevpc.ntexup.api.eval.NDocFunctionContext;
-import net.thevpc.ntexup.api.eval.NDocValue;
-import net.thevpc.ntexup.engine.util.NDocColorUtils;
-import net.thevpc.ntexup.engine.util.NDocElementUtils;
+import net.thevpc.ntexup.api.eval.NTxFunctionArgs;
+import net.thevpc.ntexup.api.eval.NTxFunctionContext;
+import net.thevpc.ntexup.api.eval.NTxValue;
+import net.thevpc.ntexup.engine.util.NTxColorUtils;
+import net.thevpc.ntexup.engine.util.NTxElementUtils;
 import net.thevpc.nuts.elem.NElement;
 import net.thevpc.nuts.util.NMsg;
 
@@ -18,7 +18,7 @@ public class NTxFunctionColorBrightness implements NTxFunction {
     }
 
     @Override
-    public NElement invoke(NDocFunctionArgs args, NDocFunctionContext context) {
+    public NElement invoke(NTxFunctionArgs args, NTxFunctionContext context) {
         if (args.size() == 0) {
             return NElement.ofNull();
         }
@@ -28,8 +28,8 @@ public class NTxFunctionColorBrightness implements NTxFunction {
         if (args.size() > 2) {
             context.messages().log(NMsg.ofC("%s: expected 2 arguments, got %s", name(), args.size()));
         }
-        Color c = NDocValue.of(args.eval(0)).asColor().get();
-        float factor = NDocValue.of(args.eval(1)).asFloat().get();
-        return NDocElementUtils.toElement(NDocColorUtils.adjustBrightness(c, factor));
+        Color c = NTxValue.of(args.eval(0)).asColor().get();
+        float factor = NTxValue.of(args.eval(1)).asFloat().get();
+        return NTxElementUtils.toElement(NTxColorUtils.adjustBrightness(c, factor));
     }
 }
