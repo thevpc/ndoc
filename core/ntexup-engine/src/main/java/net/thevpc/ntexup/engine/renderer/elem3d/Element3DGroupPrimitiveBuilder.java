64f993764e316c113c62877381f3e1797a8b28c7
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:40:55 +0100

    Update Element3DGroupPrimitiveBuilder.java : rename ntx

--- core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/renderer/elem3d/Element3DGroupPrimitiveBuilder.java
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/renderer/elem3d/Element3DGroupPrimitiveBuilder.java
@@ -2,22 +2,22 @@ package net.thevpc.ntexup.engine.renderer.elem3d;
 
 import net.thevpc.ntexup.api.document.elem3d.NtxElement3DPrimitive;
 import net.thevpc.ntexup.api.document.elem3d.NtxElement3D;
-import net.thevpc.ntexup.api.document.elem3d.RenderState3D;
+import net.thevpc.ntexup.api.document.elem3d.NTxRenderState3D;
 import net.thevpc.ntexup.api.document.elem3d.composite.NtxElement3DGroup;
-import net.thevpc.ntexup.api.renderer.NDocElement3DRenderer;
+import net.thevpc.ntexup.api.renderer.NTxElement3DRenderer;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class Element3DGroupPrimitiveBuilder implements NDocElement3DRenderer {
+public class Element3DGroupPrimitiveBuilder implements NTxElement3DRenderer {
     @Override
     public Class<? extends NtxElement3D> forType() {
         return NtxElement3DGroup.class;
     }
 
     @Override
-    public NtxElement3DPrimitive[] toPrimitives(NtxElement3D e, RenderState3D renderState) {
+    public NtxElement3DPrimitive[] toPrimitives(NtxElement3D e, NTxRenderState3D renderState) {
         NtxElement3DGroup ee = (NtxElement3DGroup) e;
         List<NtxElement3D> elements = ee.getElements();
         List<NtxElement3DPrimitive> all = new ArrayList<>();
