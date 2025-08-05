281ce5862789f5f79a73151c614c4b386cfacad6
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:41:31 +0100

    Update PlantUmlGanttBuilder.java : rename ntx

---
+++ extensions/ntexup-extension-plantuml/src/main/java/net/thevpc/ntexup/extension/plantuml/modes/PlantUmlGanttBuilder.java
@@ -0,0 +1,9 @@
+package net.thevpc.ntexup.extension.plantuml.modes;
+
+import net.thevpc.ntexup.extension.plantuml.PlantUmlBuilderBase;
+
+public class PlantUmlGanttBuilder extends PlantUmlBuilderBase {
+    public PlantUmlGanttBuilder() {
+        super("gantt");
+    }
+}
