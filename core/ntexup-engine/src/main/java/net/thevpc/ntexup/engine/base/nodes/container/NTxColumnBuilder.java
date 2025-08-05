9c19b8d7d51eaa16aa384c356be24c6dfe1d4d8d
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:41:30 +0100

    Update NTxColumnBuilder.java : rename ntx

---
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/base/nodes/container/NTxColumnBuilder.java
@@ -0,0 +1,86 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package net.thevpc.ntexup.engine.base.nodes.container;
+
+import net.thevpc.ntexup.api.document.elem2d.NTxBounds2;
+import net.thevpc.ntexup.api.document.node.NTxNode;
+import net.thevpc.ntexup.api.document.node.NTxNodeType;
+import net.thevpc.ntexup.api.document.style.NTxProp;
+import net.thevpc.ntexup.api.document.style.NTxPropName;
+import net.thevpc.ntexup.api.document.style.NTxProperties;
+import net.thevpc.ntexup.api.extension.NTxNodeBuilder;
+import net.thevpc.ntexup.api.engine.NTxNodeCustomBuilderContext;
+import net.thevpc.ntexup.api.renderer.NTxNodeRendererContext;
+import net.thevpc.nuts.elem.NElement;
+
+/**
+ * @author vpc
+ */
+public class NTxColumnBuilder implements NTxNodeBuilder {
+    NTxProperties defaultStyles = new NTxProperties();
+
+    @Override
+    public void build(NTxNodeCustomBuilderContext builderContext) {
+        builderContext.id(NTxNodeType.COLUMN)
+                .alias("col")
+                .parseParam().named(NTxPropName.ROWS).then()
+                .parseParam((info, buildContext) -> {
+                    NElement p = info.peek();
+                    if(p.isInt()) {
+                        info.read();
+                        info.node().setProperty(NTxProp.of(NTxPropName.ROWS,p));
+                        return true;
+                    }else{
+                        return false;
+                    }
+                })
+                .selfBounds(this::selfBounds)
+                .renderComponent(this::renderMain)
+                ;
+    }
+
+
+    public NTxBounds2 selfBounds(NTxNode p, NTxNodeRendererContext ctx, NTxNodeCustomBuilderContext builderContext) {
+        ctx = ctx.withDefaultStyles(p, defaultStyles);
+        NTxBounds2 expectedBounds = ctx.defaultSelfBounds(p);
+        NTxGridRendererHelper h = new NTxGridRendererHelper(p.children());
+        return h.computeBound(p, ctx, expectedBounds);
+    }
+
+    public void renderMain(NTxNode p, NTxNodeRendererContext ctx, NTxNodeCustomBuilderContext builderContext) {
+        ctx = ctx.withDefaultStyles(p, defaultStyles);
+        NTxBounds2 expectedBounds = ctx.selfBounds(p);
+        NTxGridRendererHelper h = new NTxGridRendererHelper(p.children());
+        h.render(p, ctx, expectedBounds);
+    }
+
+
+//    @Override
+//    protected void processImplicitStyles(NTxArgumentParseInfo info) {
+//        switch (info.getUid()) {
+//            case "vgrid":
+//            case "column": {
+//                if(info.node().getProperty(NTxPropName.COLUMNS).isNotPresent()) {
+//                    info.node().setProperty(NTxPropName.COLUMNS, NElement.ofInt(1));
+//                }
+//                if(info.node().getProperty(NTxPropName.ROWS).isNotPresent()) {
+//                    info.node().setProperty(NTxPropName.ROWS, NElement.ofInt(-1));
+//                }
+//                break;
+//            }
+//            case "hgrid":
+//            case "row": {
+//                if(info.node().getProperty(NTxPropName.COLUMNS).isNotPresent()) {
+//                    info.node().setProperty(NTxPropName.COLUMNS, NElement.ofInt(-1));
+//                }
+//                if(info.node().getProperty(NTxPropName.ROWS).isNotPresent()) {
+//                    info.node().setProperty(NTxPropName.ROWS, NElement.ofInt(1));
+//                }
+//                break;
+//            }
+//        }
+//    }
+
+}
