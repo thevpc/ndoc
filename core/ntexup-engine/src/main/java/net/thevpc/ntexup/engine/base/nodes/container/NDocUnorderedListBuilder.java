4fdf2271f9a98cb4fc77f919fc6934153a46bb84
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:08:13 +0100

    Update NDocUnorderedListBuilder.java : rename ndoc to ntexup

--- core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/base/nodes/container/NDocUnorderedListBuilder.java
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/base/nodes/container/NDocUnorderedListBuilder.java
@@ -5,14 +5,14 @@
 package net.thevpc.ntexup.engine.base.nodes.container;
 
 import net.thevpc.ntexup.api.document.NTxDocumentFactory;
-import net.thevpc.ntexup.api.document.elem2d.NDocAlign;
+import net.thevpc.ntexup.api.document.elem2d.NTxAlign;
 import net.thevpc.ntexup.api.document.node.NTxNode;
-import net.thevpc.ntexup.api.document.node.NDocNodeType;
+import net.thevpc.ntexup.api.document.node.NTxNodeType;
 import net.thevpc.ntexup.api.document.style.NTxProp;
-import net.thevpc.ntexup.api.document.style.NDocPropName;
-import net.thevpc.ntexup.api.document.style.NDocProperties;
+import net.thevpc.ntexup.api.document.style.NTxPropName;
+import net.thevpc.ntexup.api.document.style.NTxProperties;
 import net.thevpc.ntexup.api.extension.NDocNodeBuilder;
-import net.thevpc.ntexup.api.engine.NDocNodeCustomBuilderContext;
+import net.thevpc.ntexup.api.engine.NTxNodeCustomBuilderContext;
 import net.thevpc.ntexup.api.renderer.NDocNodeRendererContext;
 import net.thevpc.nuts.elem.NElement;
 
@@ -23,23 +23,23 @@ import java.util.List;
  * @author vpc
  */
 public class NDocUnorderedListBuilder implements NDocNodeBuilder {
-    NDocProperties defaultStyles = new NDocProperties();
+    NTxProperties defaultStyles = new NTxProperties();
     @Override
-    public void build(NDocNodeCustomBuilderContext builderContext) {
-        builderContext.id(NDocNodeType.UNORDERED_LIST)
+    public void build(NTxNodeCustomBuilderContext builderContext) {
+        builderContext.id(NTxNodeType.UNORDERED_LIST)
                 .alias("ul")
                 .renderConvert(this::convert)
                 ;
     }
 
-    private NTxNode convert(NTxNode p, NDocNodeRendererContext ctx, NDocNodeCustomBuilderContext buildContext){
+    private NTxNode convert(NTxNode p, NDocNodeRendererContext ctx, NTxNodeCustomBuilderContext buildContext){
         ctx = ctx.withDefaultStyles(p, defaultStyles);
         NTxDocumentFactory f = ctx.documentFactory();
         List<NTxNode> all = new ArrayList<>();
         for (NTxNode child : p.children()) {
             switch (child.type()) {
-                case NDocNodeType.UNORDERED_LIST:
-                case NDocNodeType.ORDERED_LIST: {
+                case NTxNodeType.UNORDERED_LIST:
+                case NTxNodeType.ORDERED_LIST: {
                     all.add(f.ofVoid().addStyleClasses("ul-bullet").setSource(p.source()));
                     break;
                 }
@@ -55,35 +55,35 @@ public class NDocUnorderedListBuilder implements NDocNodeBuilder {
             all.add(child.addStyleClasses("ul-item").setSource(p.source()));
         }
         NTxNode newNode = f.ofGrid().addChildren(all.toArray(new NTxNode[0]))
-                .setProperty(NDocPropName.COLUMNS, NElement.ofInt(2))
-                .setProperty(NDocPropName.ROWS, NElement.ofInt(2))
-                .setProperty(NDocPropName.ORIGIN, NDocAlign.TOP_LEFT)
-                .setProperty(NDocPropName.COLUMNS_WEIGHT, NElement.ofDoubleArray(1, 20))
+                .setProperty(NTxPropName.COLUMNS, NElement.ofInt(2))
+                .setProperty(NTxPropName.ROWS, NElement.ofInt(2))
+                .setProperty(NTxPropName.ORIGIN, NTxAlign.TOP_LEFT)
+                .setProperty(NTxPropName.COLUMNS_WEIGHT, NElement.ofDoubleArray(1, 20))
                 .setProperties(p.props().toArray(new NTxProp[0]));
         for (String s : new String[]{
-                NDocPropName.GRID_COLOR
-                , NDocPropName.ORIGIN
-                , NDocPropName.DRAW_GRID
-                , NDocPropName.DRAW_CONTOUR
-                , NDocPropName.COLUMNS_WEIGHT
-                , NDocPropName.BACKGROUND_COLOR
-                , NDocPropName.FILL_BACKGROUND
-                , NDocPropName.PADDING
-                , NDocPropName.MARGIN
-                , NDocPropName.AT
-                , NDocPropName.STROKE
-                , NDocPropName.CLASS
-                , NDocPropName.DEBUG
-                , NDocPropName.DEBUG_COLOR
-                , NDocPropName.HIDE
-                , NDocPropName.FOREGROUND_COLOR
-                , NDocPropName.FONT_BOLD
-                , NDocPropName.FONT_FAMILY
-                , NDocPropName.FONT_ITALIC
-                , NDocPropName.FONT_SIZE
-                , NDocPropName.FONT_UNDERLINED
-                , NDocPropName.FONT_STRIKE
-                , NDocPropName.FONT_STRIKE
+                NTxPropName.GRID_COLOR
+                , NTxPropName.ORIGIN
+                , NTxPropName.DRAW_GRID
+                , NTxPropName.DRAW_CONTOUR
+                , NTxPropName.COLUMNS_WEIGHT
+                , NTxPropName.BACKGROUND_COLOR
+                , NTxPropName.FILL_BACKGROUND
+                , NTxPropName.PADDING
+                , NTxPropName.MARGIN
+                , NTxPropName.AT
+                , NTxPropName.STROKE
+                , NTxPropName.CLASS
+                , NTxPropName.DEBUG
+                , NTxPropName.DEBUG_COLOR
+                , NTxPropName.HIDE
+                , NTxPropName.FOREGROUND_COLOR
+                , NTxPropName.FONT_BOLD
+                , NTxPropName.FONT_FAMILY
+                , NTxPropName.FONT_ITALIC
+                , NTxPropName.FONT_SIZE
+                , NTxPropName.FONT_UNDERLINED
+                , NTxPropName.FONT_STRIKE
+                , NTxPropName.FONT_STRIKE
         }) {
             NElement v = ctx.computePropertyValue(p, s).orNull();
             if (v != null) {
@@ -91,9 +91,9 @@ public class NDocUnorderedListBuilder implements NDocNodeBuilder {
             }
         }
         if(ctx.isDebug(p)){
-            Object v = ctx.computePropertyValue(p, NDocPropName.DRAW_GRID).orNull();
+            Object v = ctx.computePropertyValue(p, NTxPropName.DRAW_GRID).orNull();
             if(v==null){
-                newNode.setProperty(NDocPropName.DRAW_GRID, NElement.ofBoolean(true));
+                newNode.setProperty(NTxPropName.DRAW_GRID, NElement.ofBoolean(true));
             }
         }
         return newNode;
