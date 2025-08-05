e65cc040f36f03e0a312b4838c96ba7f45e8c764
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:40:56 +0100

    Rename PlantUmlChronologyBuilder.java → PlantUmlChronologyBuilder.java : rename ntx

--- extensions/ndoc-extension-plantuml/src/main/java/net/thevpc/ndoc/extension/plantuml/modes/PlantUmlChronologyBuilder.java
+++
@@ -1,9 +0,0 @@
-package net.thevpc.ndoc.extension.plantuml.modes;
-
-import net.thevpc.ndoc.extension.plantuml.PlantUmlBuilderBase;
-
-public class PlantUmlChronologyBuilder extends PlantUmlBuilderBase {
-    public PlantUmlChronologyBuilder() {
-        super("chronology");
-    }
-}
