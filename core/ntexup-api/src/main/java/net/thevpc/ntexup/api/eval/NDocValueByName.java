b204936966d07bfc9daebc2d7cb4a74b7e6e4b2c
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:08:12 +0100

    Update NDocValueByName.java : rename ndoc to ntexup

--- core/ntexup-api/src/main/java/net/thevpc/ntexup/api/eval/NDocValueByName.java
+++ core/ntexup-api/src/main/java/net/thevpc/ntexup/api/eval/NDocValueByName.java
@@ -1,7 +1,7 @@
 package net.thevpc.ntexup.api.eval;
 
-import net.thevpc.ntexup.api.document.elem2d.NDocBounds2;
-import net.thevpc.ntexup.api.document.elem2d.NDocDouble2;
+import net.thevpc.ntexup.api.document.elem2d.NTxBounds2;
+import net.thevpc.ntexup.api.document.elem2d.NTxDouble2;
 import net.thevpc.ntexup.api.document.elem2d.*;
 import net.thevpc.ntexup.api.document.node.*;
 import net.thevpc.ntexup.api.document.style.*;
@@ -22,19 +22,19 @@ public class NDocValueByName {
     }
 
     protected static boolean isPreserveShapeRatio(NTxNode t, NDocNodeRendererContext ctx) {
-        Boolean b = NDocValue.of(ctx.computePropertyValue(t, NDocPropName.PRESERVE_ASPECT_RATIO).orNull()).asBoolean().orElse(false);
+        Boolean b = NDocValue.of(ctx.computePropertyValue(t, NTxPropName.PRESERVE_ASPECT_RATIO).orNull()).asBoolean().orElse(false);
         return b == null ? false : b;
     }
 
-    private static NDocDouble2 getSize(NTxNode t, NDocDouble2 minSize, NDocNodeRendererContext ctx, NDocSizeRef hSizeRef) {
+    private static NTxDouble2 getSize(NTxNode t, NTxDouble2 minSize, NDocNodeRendererContext ctx, NDocSizeRef hSizeRef) {
         if(hSizeRef==null) {
             hSizeRef = ctx.sizeRef();
         }
-        NDocElemNumber2 double2OrHAlign = NDocValueByType.getNNumberElement2Or1OrHAlign(t, ctx, NDocPropName.SIZE).orElse(
-                new NDocElemNumber2((NNumberElement) NElement.ofDouble(100.0), (NNumberElement) NElement.ofDouble(100.0))
+        NTxElemNumber2 double2OrHAlign = NDocValueByType.getNNumberElement2Or1OrHAlign(t, ctx, NTxPropName.SIZE).orElse(
+                new NTxElemNumber2((NNumberElement) NElement.ofDouble(100.0), (NNumberElement) NElement.ofDouble(100.0))
         );
 
-        NDocDouble2 size = new NDocDouble2(
+        NTxDouble2 size = new NTxDouble2(
                 hSizeRef.x(double2OrHAlign.getX()).get(),
                 hSizeRef.y(double2OrHAlign.getY()).get()
         );
@@ -54,48 +54,48 @@ public class NDocValueByName {
             if (sy > sx) {
                 sy = sx;
             }
-            return new NDocDouble2(
+            return new NTxDouble2(
                     sx,
                     sy
             );
         }
-        return new NDocDouble2(
+        return new NTxDouble2(
                 sx,
                 sy
         );
     }
 
-    public static NDocDouble2 getOrigin(NTxNode t, NDocNodeRendererContext ctx, NDocDouble2 a) {
-        NDocElemNumber2 double2OrHAlign = NDocValueByType.getNNumberElement2Or1OrHAlign(t, ctx, NDocPropName.ORIGIN)
-                .orElseUse(() -> NDocValueByType.getNNumberElement2Or1OrHAlign(t, ctx, NDocPropName.AT))
-                .orElse(new NDocElemNumber2(NElement.ofDouble(0), NElement.ofDouble(0)));
+    public static NTxDouble2 getOrigin(NTxNode t, NDocNodeRendererContext ctx, NTxDouble2 a) {
+        NTxElemNumber2 double2OrHAlign = NDocValueByType.getNNumberElement2Or1OrHAlign(t, ctx, NTxPropName.ORIGIN)
+                .orElseUse(() -> NDocValueByType.getNNumberElement2Or1OrHAlign(t, ctx, NTxPropName.AT))
+                .orElse(new NTxElemNumber2(NElement.ofDouble(0), NElement.ofDouble(0)));
         NDocSizeRef sr = new NDocSizeRef(a.getX(), a.getY(), ctx.getGlobalBounds().getWidth(), ctx.getGlobalBounds().getHeight());
-        return new NDocDouble2(
+        return new NTxDouble2(
                 sr.x(double2OrHAlign.getX()).get(),
                 sr.y(double2OrHAlign.getY()).get()
         );
     }
 
-    public static NDocDouble2 getPosition(NTxNode t, NDocNodeRendererContext ctx, NDocDouble2 a) {
-        NDocElemNumber2 double2OrHAlign = NDocValueByType.getNNumberElement2Or1OrHAlign(t, ctx, NDocPropName.POSITION)
-                .orElseUse(() -> NDocValueByType.getNNumberElement2Or1OrHAlign(t, ctx, NDocPropName.AT))
-                .orElse(new NDocElemNumber2(NElement.ofDouble(0), NElement.ofDouble(0)));
+    public static NTxDouble2 getPosition(NTxNode t, NDocNodeRendererContext ctx, NTxDouble2 a) {
+        NTxElemNumber2 double2OrHAlign = NDocValueByType.getNNumberElement2Or1OrHAlign(t, ctx, NTxPropName.POSITION)
+                .orElseUse(() -> NDocValueByType.getNNumberElement2Or1OrHAlign(t, ctx, NTxPropName.AT))
+                .orElse(new NTxElemNumber2(NElement.ofDouble(0), NElement.ofDouble(0)));
         NDocSizeRef sr = new NDocSizeRef(a.getX(), a.getY(), ctx.getGlobalBounds().getWidth(), ctx.getGlobalBounds().getHeight());
-        return new NDocDouble2(
+        return new NTxDouble2(
                 sr.x(double2OrHAlign.getX()).get(),
                 sr.y(double2OrHAlign.getY()).get()
         );
     }
 
     public static NElement getStroke(NTxNode t, NDocNodeRendererContext ctx) {
-        return NDocValueByType.getElement(t, ctx, NDocPropName.STROKE).orNull();
+        return NDocValueByType.getElement(t, ctx, NTxPropName.STROKE).orNull();
     }
 
-    public static NDocBounds2 selfBounds(NTxNode t, NDocDouble2 selfSize, NDocDouble2 minSize, NDocNodeRendererContext ctx) {
-        NDocBounds2 parentBounds = ctx.getBounds();
+    public static NTxBounds2 selfBounds(NTxNode t, NTxDouble2 selfSize, NTxDouble2 minSize, NDocNodeRendererContext ctx) {
+        NTxBounds2 parentBounds = ctx.getBounds();
         double pw = parentBounds.getWidth();
         double ph = parentBounds.getHeight();
-        Padding padding = NDocValueByType.getPadding(t, ctx, NDocPropName.MARGIN).orElse(Padding.of(0))
+        NTxPadding padding = NDocValueByType.getPadding(t, ctx, NTxPropName.MARGIN).orElse(NTxPadding.of(0))
                 .mul(pw / 100, ph / 100);
         NDocSizeRef hSizeRef = new NDocSizeRef(
                 Math.max(pw - padding.getLeft() - padding.getRight(), 0),
@@ -107,15 +107,15 @@ public class NDocValueByName {
             selfSize = getSize(t, minSize, ctx, hSizeRef);
         }
 
-        NDocDouble2 pos = getPosition(t, ctx, new NDocDouble2(hSizeRef.getParentWidth(), hSizeRef.getParentHeight()));
+        NTxDouble2 pos = getPosition(t, ctx, new NTxDouble2(hSizeRef.getParentWidth(), hSizeRef.getParentHeight()));
 
-        NDocDouble2 origin = getOrigin(t, ctx, selfSize);
+        NTxDouble2 origin = getOrigin(t, ctx, selfSize);
 
         double x = pos.getX() - origin.getX() + parentBounds.getX();
         double y = pos.getY() - origin.getY() + parentBounds.getY();
 
 
-        return new NDocBounds2(
+        return new NTxBounds2(
                 x + padding.getLeft(),
                 y + padding.getTop(),
                 selfSize.getX(),
@@ -124,7 +124,7 @@ public class NDocValueByName {
     }
 
     public static boolean isVisible(NTxNode t, NDocNodeRendererContext ctx) {
-        NOptional<Boolean> b = NDocValueByType.getBoolean(t, ctx, NDocPropName.HIDE);
+        NOptional<Boolean> b = NDocValueByType.getBoolean(t, ctx, NTxPropName.HIDE);
         if (b.isPresent()) {
             return !b.get();
         }
@@ -140,7 +140,7 @@ public class NDocValueByName {
     }
 
     public static double getFontSize(NTxNode t, NDocNodeRendererContext ctx) {
-        NElement e = NDocValueByType.getElement(t, ctx, NDocPropName.FONT_SIZE).orNull();
+        NElement e = NDocValueByType.getElement(t, ctx, NTxPropName.FONT_SIZE).orNull();
         NDocSizeRef sr = ctx.sizeRef();
         NOptional<Double> srpx = sr.x(e);
         NOptional<Double> srpy = sr.y(e);
@@ -148,23 +148,23 @@ public class NDocValueByName {
     }
 
     public static String getFontFamily(NTxNode t, NDocNodeRendererContext ctx) {
-        return NDocValueByType.getStringOrName(t, ctx, NDocPropName.FONT_FAMILY).orElse("Serif");
+        return NDocValueByType.getStringOrName(t, ctx, NTxPropName.FONT_FAMILY).orElse("Serif");
     }
 
     public static boolean isFontUnderlined(NTxNode t, NDocNodeRendererContext ctx) {
-        return NDocValueByType.getBoolean(t, ctx, NDocPropName.FONT_UNDERLINED, "underlined").orElse(false);
+        return NDocValueByType.getBoolean(t, ctx, NTxPropName.FONT_UNDERLINED, "underlined").orElse(false);
     }
 
     public static boolean isFontStrike(NTxNode t, NDocNodeRendererContext ctx) {
-        return NDocValueByType.getBoolean(t, ctx, NDocPropName.FONT_STRIKE, "strike").orElse(false);
+        return NDocValueByType.getBoolean(t, ctx, NTxPropName.FONT_STRIKE, "strike").orElse(false);
     }
 
     public static boolean isFontBold(NTxNode t, NDocNodeRendererContext ctx) {
-        return NDocValueByType.getBoolean(t, ctx, NDocPropName.FONT_BOLD, "bold").orElse(false);
+        return NDocValueByType.getBoolean(t, ctx, NTxPropName.FONT_BOLD, "bold").orElse(false);
     }
 
     public static boolean isFontItalic(NTxNode t, NDocNodeRendererContext ctx) {
-        return NDocValueByType.getBoolean(t, ctx, NDocPropName.FONT_ITALIC, "italic").orElse(false);
+        return NDocValueByType.getBoolean(t, ctx, NTxPropName.FONT_ITALIC, "italic").orElse(false);
     }
 
     public static Font getFont(NTxNode t, NDocNodeRendererContext ctx) {
@@ -178,12 +178,12 @@ public class NDocValueByName {
         );
     }
 
-    public static NDocDouble2 getRoundCornerArcs(NTxNode t, NDocNodeRendererContext ctx) {
-        return NDocValue.of(ctx.computePropertyValue(t, NDocPropName.ROUND_CORNER).orNull()).asDouble2OrDouble().orNull();
+    public static NTxDouble2 getRoundCornerArcs(NTxNode t, NDocNodeRendererContext ctx) {
+        return NDocValue.of(ctx.computePropertyValue(t, NTxPropName.ROUND_CORNER).orNull()).asDouble2OrDouble().orNull();
     }
 
     public static int getColSpan(NTxNode t, NDocNodeRendererContext ctx) {
-        Integer i = NDocValueByType.getInt(t, ctx, NDocPropName.COLSPAN).orElse(1);
+        Integer i = NDocValueByType.getInt(t, ctx, NTxPropName.COLSPAN).orElse(1);
         if (i == null) {
             return 1;
         }
@@ -194,7 +194,7 @@ public class NDocValueByName {
     }
 
     public static int getRowSpan(NTxNode t, NDocNodeRendererContext ctx) {
-        Integer i = NDocValueByType.getInt(t, ctx, NDocPropName.ROWSPAN).orElse(1);
+        Integer i = NDocValueByType.getInt(t, ctx, NTxPropName.ROWSPAN).orElse(1);
         if (i == null) {
             return 1;
         }
@@ -205,30 +205,30 @@ public class NDocValueByName {
     }
 
     public static Boolean get3D(NTxNode t, NDocNodeRendererContext ctx) {
-        return NDocValueByType.getBoolean(t, ctx, NDocPropName.THEED).orElse(false);
+        return NDocValueByType.getBoolean(t, ctx, NTxPropName.THEED).orElse(false);
     }
 
     public static Boolean getRaised(NTxNode t, NDocNodeRendererContext ctx) {
-        return NDocValueByType.getBoolean(t, ctx, NDocPropName.RAISED).orElse(false);
+        return NDocValueByType.getBoolean(t, ctx, NTxPropName.RAISED).orElse(false);
     }
 
-    public static NOptional<Shadow> readStyleAsShadow(NTxNode t, String s, NDocNodeRendererContext ctx) {
+    public static NOptional<NTxShadow> readStyleAsShadow(NTxNode t, String s, NDocNodeRendererContext ctx) {
         Object sv = ctx.computePropertyValue(t, s).orNull();
         NDocValue o = NDocValue.of(sv);
-        NOptional<NDocPoint2D> r = getStyleAsShadowDistance(sv, ctx);
+        NOptional<NTxPoint2D> r = getStyleAsShadowDistance(sv, ctx);
         if (r.isPresent()) {
-            Shadow ss = new Shadow();
+            NTxShadow ss = new NTxShadow();
             ss.setTranslation(r.get());
             return NOptional.of(ss);
         }
         if (sv instanceof NElement && ((NElement) sv).isListContainer()) {
-            Shadow shadow = new Shadow();
+            NTxShadow shadow = new NTxShadow();
             for (Map.Entry<String, NDocValue> e : o.argsOrBodyMap().entrySet()) {
                 switch (e.getKey()) {
                     case "distance":
                     case "shift":
                     case "origin": {
-                        NOptional<NDocPoint2D> d = getStyleAsShadowDistance(e.getValue(), ctx);
+                        NOptional<NTxPoint2D> d = getStyleAsShadowDistance(e.getValue(), ctx);
                         if (d.isPresent()) {
                             shadow.setTranslation(d.get());
                         } else {
@@ -237,7 +237,7 @@ public class NDocValueByName {
                         break;
                     }
                     case "shear": {
-                        NOptional<NDocPoint2D> d = getStyleAsShadowDistance(e.getValue(), ctx);
+                        NOptional<NTxPoint2D> d = getStyleAsShadowDistance(e.getValue(), ctx);
                         if (d.isPresent()) {
                             shadow.setShear(d.get());
                         } else {
@@ -264,15 +264,15 @@ public class NDocValueByName {
         NOptional<Boolean> rb = NDocValue.of(sv).asBoolean();
         if (rb.isPresent()) {
             if (rb.get()) {
-                Shadow ss = new Shadow();
-                ss.setTranslation(new NDocPoint2D(1, 1));
+                NTxShadow ss = new NTxShadow();
+                ss.setTranslation(new NTxPoint2D(1, 1));
                 return NOptional.of(ss);
             }
         }
         NOptional<Double> rd = NDocValue.of(sv).asDouble();
         if (r.isPresent()) {
-            Shadow ss = new Shadow();
-            ss.setTranslation(new NDocPoint2D(rd.get(), rd.get()));
+            NTxShadow ss = new NTxShadow();
+            ss.setTranslation(new NTxPoint2D(rd.get(), rd.get()));
             return NOptional.of(ss);
         }
         return NOptional.ofNamedEmpty("shadow");
@@ -282,14 +282,14 @@ public class NDocValueByName {
         if (ctx.isDry()) {
             return null;
         }
-        return NDocValueByType.getPaint(t, ctx, NDocPropName.FOREGROUND_COLOR, "foreground", "color", "fg").orElse(force ? Color.BLACK : null);
+        return NDocValueByType.getPaint(t, ctx, NTxPropName.FOREGROUND_COLOR, "foreground", "color", "fg").orElse(force ? Color.BLACK : null);
     }
 
     public static Paint resolveGridColor(NTxNode t, NDocNodeRendererContext ctx) {
         if (ctx.isDry()) {
             return null;
         }
-        return NDocValueByType.getPaint(t, ctx, NDocPropName.GRID_COLOR).orElse(Color.BLACK);
+        return NDocValueByType.getPaint(t, ctx, NTxPropName.GRID_COLOR).orElse(Color.BLACK);
     }
 
 //    public static Paint resolveLineColor(NDocNode t, NDocNodeRendererContext ctx) {
@@ -303,20 +303,20 @@ public class NDocValueByName {
         if (ctx.isDry()) {
             return null;
         }
-        Paint color = NDocValueByType.getPaint(t, ctx, NDocPropName.BACKGROUND_COLOR, "background", "bg").orNull();
+        Paint color = NDocValueByType.getPaint(t, ctx, NTxPropName.BACKGROUND_COLOR, "background", "bg").orNull();
         return color;
     }
 
     public static boolean isDrawContour(NTxNode node, NDocNodeRendererContext ctx) {
-        return NDocValueByType.getBoolean(node, ctx, NDocPropName.DRAW_CONTOUR, "contour").orElse(false);
+        return NDocValueByType.getBoolean(node, ctx, NTxPropName.DRAW_CONTOUR, "contour").orElse(false);
     }
 
     public static boolean requireDrawGrid(NTxNode t, NDocNodeRendererContext ctx) {
-        return NDocValueByType.getBoolean(t, ctx, NDocPropName.DRAW_GRID, "grid").orElse(false);
+        return NDocValueByType.getBoolean(t, ctx, NTxPropName.DRAW_GRID, "grid").orElse(false);
     }
 
     public static boolean requireFillBackground(NTxNode t, NDocNodeRendererContext ctx) {
-        return NDocValueByType.getBoolean(t, ctx, NDocPropName.FILL_BACKGROUND, "fill").orElse(false);
+        return NDocValueByType.getBoolean(t, ctx, NTxPropName.FILL_BACKGROUND, "fill").orElse(false);
     }
 
 //    public static Paint resolveLineColor(NDocNode t, NDocNodeRendererContext ctx, boolean force) {
@@ -339,11 +339,11 @@ public class NDocValueByName {
 //    }
 
     public static int getColumns(NTxNode node, NDocNodeRendererContext ctx) {
-        return NDocValueByType.getInt(node, ctx, NDocPropName.COLUMNS, "cols").orElse(-1);
+        return NDocValueByType.getInt(node, ctx, NTxPropName.COLUMNS, "cols").orElse(-1);
     }
 
     public static int getRows(NTxNode node, NDocNodeRendererContext ctx) {
-        return NDocValueByType.getInt(node, ctx, NDocPropName.ROWS, "rows").orElse(-1);
+        return NDocValueByType.getInt(node, ctx, NTxPropName.ROWS, "rows").orElse(-1);
     }
 
     public static boolean isDebug(NTxNode p, NDocNodeRendererContext ctx) {
@@ -351,29 +351,29 @@ public class NDocValueByName {
     }
 
     public static int getDebugLevel(NTxNode p, NDocNodeRendererContext ctx) {
-        return NDocValueByType.getIntOrBoolean(p, ctx, NDocPropName.DEBUG).orElse(0);
+        return NDocValueByType.getIntOrBoolean(p, ctx, NTxPropName.DEBUG).orElse(0);
     }
 
     public static Color getDebugColor(NTxNode t, NDocNodeRendererContext ctx) {
-        return (Color) NDocValueByType.getPaint(t, ctx, NDocPropName.DEBUG_COLOR).orElse(Color.GRAY);
+        return (Color) NDocValueByType.getPaint(t, ctx, NTxPropName.DEBUG_COLOR).orElse(Color.GRAY);
     }
 
-    public static NOptional<NDocPoint2D> getStyleAsShadowDistance(Object sv, NDocNodeRendererContext ctx) {
+    public static NOptional<NTxPoint2D> getStyleAsShadowDistance(Object sv, NDocNodeRendererContext ctx) {
         NDocValue o = NDocValue.of(sv);
         double ww = ctx.getBounds().getWidth();
         double hh = ctx.getBounds().getHeight();
         if (o.isNumber()) {
             NOptional<Number> n = o.asNumber();
             if (n.isPresent()) {
-                return NOptional.of(new NDocPoint2D(
+                return NOptional.of(new NTxPoint2D(
                         n.get().doubleValue() / 100.0 * ww,
                         n.get().doubleValue() / 100.0 * hh
                 ));
             }
         } else {
-            NOptional<NDocPoint2D> n = o.asHPoint2D();
+            NOptional<NTxPoint2D> n = o.asHPoint2D();
             if (n.isPresent()) {
-                return NOptional.of(new NDocPoint2D(
+                return NOptional.of(new NTxPoint2D(
                         n.get().getX() / 100.0 * ww,
                         n.get().getY() / 100.0 * hh
                 ));
