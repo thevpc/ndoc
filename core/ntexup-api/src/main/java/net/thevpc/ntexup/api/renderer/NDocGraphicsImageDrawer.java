6440b479e2a11b248342aa711fba9cc8c16bdff5
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:40:53 +0100

    Rename NTxGraphicsImageDrawer.java → NDocGraphicsImageDrawer.java : rename ntx

--- core/ntexup-api/src/main/java/net/thevpc/ntexup/api/renderer/NDocGraphicsImageDrawer.java
+++
@@ -1,7 +0,0 @@
-package net.thevpc.ntexup.api.renderer;
-
-import net.thevpc.ntexup.api.document.elem2d.NTxImageOptions;
-
-public interface NDocGraphicsImageDrawer {
-    void drawImage(double x, double y, NTxImageOptions options, NDocGraphics graphics);
-}
