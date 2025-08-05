2684afb5f69bf2c4e82e7c6773f543eca06a368e
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:41:30 +0100

    Update CustomNTxNodeParserFromBuilder.java : rename ntx

---
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/ext/CustomNTxNodeParserFromBuilder.java
@@ -0,0 +1,147 @@
+package net.thevpc.ntexup.engine.ext;
+
+import net.thevpc.ntexup.api.document.node.NTxItem;
+import net.thevpc.ntexup.api.parser.*;
+import net.thevpc.ntexup.api.source.NTxSource;
+import net.thevpc.ntexup.api.util.NTxUtils;
+import net.thevpc.ntexup.engine.parser.NTxArgumentReaderImpl;
+import net.thevpc.ntexup.engine.parser.NTxNodeParserBase;
+import net.thevpc.ntexup.api.document.node.NTxNode;
+import net.thevpc.ntexup.api.engine.NTxNodeCustomBuilderContext;
+import net.thevpc.ntexup.engine.util.ToElementHelper;
+import net.thevpc.ntexup.engine.parser.NTxParseHelper;
+import net.thevpc.nuts.NCallableSupport;
+import net.thevpc.nuts.elem.NElement;
+import net.thevpc.nuts.util.NMsg;
+import net.thevpc.nuts.util.NOptional;
+
+class CustomNTxNodeParserFromBuilder extends NTxNodeParserBase {
+    private final NTxNodeCustomBuilderContextImpl ctx;
+
+    public CustomNTxNodeParserFromBuilder(NTxNodeCustomBuilderContextImpl ctx) {
+        super(true, ctx.id, ctx.aliases);
+        this.ctx = ctx;
+    }
+
+    @Override
+    public NElement toElem(NTxNode item) {
+        if (ctx.toElem != null) {
+            NElement u = ctx.toElem.toElem(item, ctx);
+            if (u != null) {
+                return u;
+            }
+        }
+        if (ctx.knownArgNames != null) {
+            return ToElementHelper.of(
+                            item,
+                            engine()
+                    )
+                    .addChildrenByName(ctx.knownArgNames.toArray(new String[0]))
+                    .build();
+        }
+        return super.toElem(item);
+    }
+
+    @Override
+    protected String acceptTypeName(NElement e) {
+        String s = super.acceptTypeName(e);
+        if (s != null) {
+            return s;
+        }
+        return s;
+    }
+
+    protected void processChildren(NTxAllArgumentReader info) {
+        if (ctx.processChildren != null) {
+            ctx.processChildren.processNode(info, ctx);
+            return;
+        }
+        super.processChildren(info);
+    }
+
+    @Override
+    public NCallableSupport<NTxItem> parseNode(NTxNodeFactoryParseContext context) {
+        NElement e = context.element();
+        String s = acceptTypeName(e);
+        if (s != null) {
+            return NCallableSupport.ofValid(
+                    () -> {
+                        NOptional<NTxItem> o = parseItem(s, e, context);
+                        if (!o.isPresent()) {
+                            return parseItem(s, e, context).get();
+                        }
+                        return o.get();
+                    }
+            );
+        }
+        NTxNodeCustomBuilderContext.NTxItemSpecialParser pp = ctx.extraElementSupport;
+        if (pp == null && ctx.extraElementSupportByPredicate != null) {
+            if (ctx.extraElementSupportByPredicate.test(e)) {
+                pp = new NTxNodeCustomBuilderContext.NTxItemSpecialParser() {
+                    @Override
+                    public NCallableSupport<NTxItem> parseElement(String id, NElement element, NTxNodeFactoryParseContext context) {
+                        NTxNode p = context.documentFactory().of(resolveEffectiveId(id));
+                        String compilerDeclarationPath = NTxUtils.getCompilerDeclarationPath(element);
+                        NTxSource source = context.source();
+                        if (compilerDeclarationPath != null) {
+                            NTxSource s = context.document().resources().find(compilerDeclarationPath).orNull();
+                            if (s != null) {
+                                source = s;
+                            }
+                        }
+                        p.setSource(source);
+                        NTxNodeFactoryParseContext context2 = context.push(p);
+                        onStartParsingItem(id, p, element, context);
+                        NTxParseHelper.fillAnnotations(element, p);
+                        NTxArgumentReaderImpl info = new NTxArgumentReaderImpl();
+                        info.setContext(context2);
+                        info.setId(id);
+                        info.setUid(NTxUtils.uid(id));
+                        info.setElement(NTxUtils.addCompilerDeclarationPath(element, info.parseContext().source()));
+                        info.setNode(p);
+                        info.setArguments(new NElement[]{element});
+                        info.setF(context.documentFactory());
+                        processArguments(info);
+                        processImplicitStyles(info);
+                        onFinishParsingItem(info);
+                        return NCallableSupport.ofValid(ctx.extraElementSupportByPredicateSupport, p);
+                    }
+                };
+            }
+        }
+        if (pp != null) {
+            NCallableSupport<NTxItem> y = pp.parseElement(id(), e, context);
+            if (y != null) {
+                return y;
+            }
+        }
+        //context.messages().addError(NMsg.ofC("invalid %s : %s", id(), e), context.source());
+        return NCallableSupport.ofInvalid(() -> NMsg.ofC("invalid %s : %s", id(), e));
+    }
+
+    @Override
+    protected boolean processArgument(NTxArgumentReader info) {
+        if (ctx.processSingleArgumentList != null) {
+            for (NTxNodeCustomBuilderContext.ProcessParamAction a : ctx.processSingleArgumentList) {
+                if (a.processParam(info, ctx)) {
+                    return true;
+                }
+            }
+            if (ctx.createdParser.defaultProcessArgument(info)) {
+                return true;
+            }
+            return false;
+        }
+        return super.processArgument(info);
+    }
+
+    @Override
+    public void onFinishParsingItem(NTxAllArgumentReader info) {
+        super.onFinishParsingItem(info);
+        if (ctx.afterProcessAllArgumentsList != null) {
+            for (NTxNodeCustomBuilderContext.ProcessNodeAction a : ctx.afterProcessAllArgumentsList) {
+                a.processNode(info, ctx);
+            }
+        }
+    }
+}
