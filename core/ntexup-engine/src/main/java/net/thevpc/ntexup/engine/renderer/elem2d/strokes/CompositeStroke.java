f8f6291718c48d513603664234f420a3e1dcc248
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:41:31 +0100

    Update CompositeStroke.java : rename ntx

---
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/renderer/elem2d/strokes/CompositeStroke.java
@@ -0,0 +1,39 @@
+package net.thevpc.ntexup.engine.renderer.elem2d.strokes;
+
+import net.thevpc.ntexup.api.eval.NTxValue;
+import net.thevpc.ntexup.api.renderer.NTxGraphics;
+import net.thevpc.nuts.elem.NElement;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CompositeStroke implements Stroke {
+    private Stroke[] strokes;
+
+    public static Stroke of(NElement e, NTxGraphics g) {
+        NTxValue o = NTxValue.of(e);
+        List<NElement> args = new ArrayList<>();
+        args.addAll(o.args());
+        args.addAll(o.body());
+        return new CompositeStroke(
+                args.stream().map(g::createStroke).toArray(Stroke[]::new)
+        );
+    }
+
+    public static CompositeStroke of(Stroke... strokes) {
+        return new CompositeStroke(strokes);
+    }
+
+    public CompositeStroke(Stroke... strokes) {
+        this.strokes = strokes;
+    }
+
+    public Shape createStrokedShape(Shape shape) {
+        Shape s = shape;
+        for (int i = 0; i < strokes.length; i++) {
+            s = strokes[i].createStrokedShape(s);
+        }
+        return s;
+    }
+}
