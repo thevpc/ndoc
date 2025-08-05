582e7ed0592d3a82cbe6a59e5c0ac1ade653974b
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:08:13 +0100

    Update NDocTriangleBuilder.java : rename ndoc to ntexup

--- core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/base/nodes/shape/NDocTriangleBuilder.java
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/base/nodes/shape/NDocTriangleBuilder.java
@@ -1,29 +1,29 @@
 package net.thevpc.ntexup.engine.base.nodes.shape;
 
-import net.thevpc.ntexup.api.document.elem2d.NDocPoint2D;
+import net.thevpc.ntexup.api.document.elem2d.NTxPoint2D;
 import net.thevpc.ntexup.api.document.node.NTxNode;
-import net.thevpc.ntexup.api.document.node.NDocNodeType;
-import net.thevpc.ntexup.api.document.style.NDocProperties;
+import net.thevpc.ntexup.api.document.node.NTxNodeType;
+import net.thevpc.ntexup.api.document.style.NTxProperties;
 import net.thevpc.ntexup.api.extension.NDocNodeBuilder;
-import net.thevpc.ntexup.api.engine.NDocNodeCustomBuilderContext;
+import net.thevpc.ntexup.api.engine.NTxNodeCustomBuilderContext;
 import net.thevpc.ntexup.api.renderer.NDocNodeRendererContext;
 
 public class NDocTriangleBuilder implements NDocNodeBuilder {
-    NDocProperties defaultStyles = new NDocProperties();
+    NTxProperties defaultStyles = new NTxProperties();
 
     @Override
-    public void build(NDocNodeCustomBuilderContext builderContext) {
-        builderContext.id(NDocNodeType.TRIANGLE)
+    public void build(NTxNodeCustomBuilderContext builderContext) {
+        builderContext.id(NTxNodeType.TRIANGLE)
                 .renderComponent(this::renderMain)
         ;
     }
 
-    public void renderMain(NTxNode p, NDocNodeRendererContext rendererContext, NDocNodeCustomBuilderContext builderContext) {
+    public void renderMain(NTxNode p, NDocNodeRendererContext rendererContext, NTxNodeCustomBuilderContext builderContext) {
         rendererContext = rendererContext.withDefaultStyles(p, defaultStyles);
-        NDocPoint2D[] points = new NDocPoint2D[]{
-                new NDocPoint2D(0, 100),
-                new NDocPoint2D(100, 100),
-                new NDocPoint2D(50, 0),
+        NTxPoint2D[] points = new NTxPoint2D[]{
+                new NTxPoint2D(0, 100),
+                new NTxPoint2D(100, 100),
+                new NTxPoint2D(50, 0),
         };
         NDocPolygonHelper.renderPoints(p, points, rendererContext);
     }
