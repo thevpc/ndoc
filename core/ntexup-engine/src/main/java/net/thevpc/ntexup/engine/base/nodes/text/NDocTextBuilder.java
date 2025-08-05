80df6e8513739be9aa156495937bbf91ffc43f44
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:08:13 +0100

    Update NDocTextBuilder.java : rename ndoc to ntexup

--- core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/base/nodes/text/NDocTextBuilder.java
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/base/nodes/text/NDocTextBuilder.java
@@ -5,10 +5,10 @@
 package net.thevpc.ntexup.engine.base.nodes.text;
 
 import net.thevpc.ntexup.api.extension.NDocNodeBuilder;
-import net.thevpc.ntexup.api.engine.NDocNodeCustomBuilderContext;
+import net.thevpc.ntexup.api.engine.NTxNodeCustomBuilderContext;
 import net.thevpc.ntexup.api.document.node.NTxNode;
-import net.thevpc.ntexup.api.document.node.NDocNodeType;
-import  net.thevpc.ntexup.api.document.style.NDocPropName;
+import net.thevpc.ntexup.api.document.node.NTxNodeType;
+import net.thevpc.ntexup.api.document.style.NTxPropName;
 import net.thevpc.ntexup.api.renderer.*;
 import net.thevpc.ntexup.api.renderer.text.*;
 import net.thevpc.nuts.elem.NElement;
@@ -22,18 +22,18 @@ import java.util.*;
 public class NDocTextBuilder implements NDocNodeBuilder {
 
     @Override
-    public void build(NDocNodeCustomBuilderContext builderContext) {
-        builderContext.id(NDocNodeType.TEXT)
+    public void build(NTxNodeCustomBuilderContext builderContext) {
+        builderContext.id(NTxNodeType.TEXT)
                 .parseAny(x->true)
-                .parseParam().named(NDocPropName.VALUE).resolvedAsTrimmedBloc().then()
-                .parseParam().named(NDocPropName.FILE).store(NDocPropName.VALUE).resolvedAsTrimmedPathTextContent().then()
+                .parseParam().named(NTxPropName.VALUE).resolvedAsTrimmedBloc().then()
+                .parseParam().named(NTxPropName.FILE).store(NTxPropName.VALUE).resolvedAsTrimmedPathTextContent().then()
                 .parseDefaultParams()
-                .parseParam().matchesStringOrName().store(NDocPropName.VALUE).resolvedAsTrimmedBloc().then()
+                .parseParam().matchesStringOrName().store(NTxPropName.VALUE).resolvedAsTrimmedBloc().then()
                 .renderText(this::buildText,this::parseImmediate)
         ;
     }
 
-    public void buildText(String text, NDocTextOptions options, NTxNode p, NDocNodeRendererContext ctx, NDocTextRendererBuilder builder, NDocNodeCustomBuilderContext builderContext) {
+    public void buildText(String text, NDocTextOptions options, NTxNode p, NDocNodeRendererContext ctx, NDocTextRendererBuilder builder, NTxNodeCustomBuilderContext builderContext) {
         NDocTextTokenParseHelper aa = new NDocTextTokenParseHelper(ctx, new NReservedSimpleCharQueue(ctx.engine().tools().trimBloc(text).toCharArray()),builderContext);
         List<NDocTextToken> all=aa.parse();
         for (NDocTextToken a : all) {
@@ -57,7 +57,7 @@ public class NDocTextBuilder implements NDocNodeBuilder {
         }
     }
 
-    public List<NDocTextToken> parseImmediate(NReservedSimpleCharQueue queue, NDocNodeRendererContext ctx,NDocNodeCustomBuilderContext builderContext) {
+    public List<NDocTextToken> parseImmediate(NReservedSimpleCharQueue queue, NDocNodeRendererContext ctx, NTxNodeCustomBuilderContext builderContext) {
         NDocTextTokenParseHelper aa = new NDocTextTokenParseHelper(ctx, queue,builderContext);
         return aa.parse();
     }
