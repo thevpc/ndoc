7d81f28b756881942dcb089512ed6ff5beb8fde3
thevpc <taha.bensalah@gmail.com>
2025-08-05 13:55:26 +0100

    Update NTxFunctionArgImpl.java : rename ndoc to ntexup

---
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/eval/NTxFunctionArgImpl.java
@@ -0,0 +1,37 @@
+package net.thevpc.ntexup.engine.eval;
+
+import net.thevpc.ntexup.api.document.node.NTxNode;
+import net.thevpc.ntexup.api.engine.NDocEngine;
+import net.thevpc.ntexup.api.eval.NTxFunctionArg;
+import net.thevpc.ntexup.api.eval.NDocVarProvider;
+import net.thevpc.nuts.elem.NElement;
+
+public class NTxFunctionArgImpl implements NTxFunctionArg {
+    private final NTxNode node;
+    private final NElement expression;
+    private final NDocEngine engine;
+    private final NDocVarProvider varProvider;
+
+    public NTxFunctionArgImpl(NElement expression, NTxNode node, NDocEngine engine, NDocVarProvider varProvider) {
+        this.node = node;
+        this.expression = expression;
+        this.engine = engine;
+        this.varProvider = varProvider;
+    }
+
+    public NElement src() {
+        return expression;
+    }
+
+    @Override
+    public NElement eval() {
+        NElement u = engine.evalExpression(expression, node, varProvider);
+        NElement u2 = engine.evalExpression(u, node, varProvider);
+        return u2;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(expression);
+    }
+}
