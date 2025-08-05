6580147f93cd93203f367f0de0b893430a7c786c
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:41:30 +0100

    Update NTxTriangleBuilder.java : rename ntx

---
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/base/nodes/shape/NTxTriangleBuilder.java
@@ -0,0 +1,31 @@
+package net.thevpc.ntexup.engine.base.nodes.shape;
+
+import net.thevpc.ntexup.api.document.elem2d.NTxPoint2D;
+import net.thevpc.ntexup.api.document.node.NTxNode;
+import net.thevpc.ntexup.api.document.node.NTxNodeType;
+import net.thevpc.ntexup.api.document.style.NTxProperties;
+import net.thevpc.ntexup.api.extension.NTxNodeBuilder;
+import net.thevpc.ntexup.api.engine.NTxNodeCustomBuilderContext;
+import net.thevpc.ntexup.api.renderer.NTxNodeRendererContext;
+
+public class NTxTriangleBuilder implements NTxNodeBuilder {
+    NTxProperties defaultStyles = new NTxProperties();
+
+    @Override
+    public void build(NTxNodeCustomBuilderContext builderContext) {
+        builderContext.id(NTxNodeType.TRIANGLE)
+                .renderComponent(this::renderMain)
+        ;
+    }
+
+    public void renderMain(NTxNode p, NTxNodeRendererContext rendererContext, NTxNodeCustomBuilderContext builderContext) {
+        rendererContext = rendererContext.withDefaultStyles(p, defaultStyles);
+        NTxPoint2D[] points = new NTxPoint2D[]{
+                new NTxPoint2D(0, 100),
+                new NTxPoint2D(100, 100),
+                new NTxPoint2D(50, 0),
+        };
+        NTxPolygonHelper.renderPoints(p, points, rendererContext);
+    }
+
+}
