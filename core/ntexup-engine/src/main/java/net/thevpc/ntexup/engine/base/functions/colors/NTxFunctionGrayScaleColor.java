61c45ea5aee1fb20db04fca4186e1633a036b63d
thevpc <taha.bensalah@gmail.com>
2025-08-05 13:55:25 +0100

    Update NTxFunctionGrayScaleColor.java : rename ndoc to ntexup

---
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/base/functions/colors/NTxFunctionGrayScaleColor.java
@@ -0,0 +1,31 @@
+package net.thevpc.ntexup.engine.base.functions.colors;
+
+import net.thevpc.ntexup.api.extension.NTxFunction;
+import net.thevpc.ntexup.api.eval.NDocFunctionArgs;
+import net.thevpc.ntexup.api.eval.NDocFunctionContext;
+import net.thevpc.ntexup.api.eval.NDocValue;
+import net.thevpc.ntexup.engine.util.NDocColorUtils;
+import net.thevpc.ntexup.engine.util.NDocElementUtils;
+import net.thevpc.nuts.elem.NElement;
+import net.thevpc.nuts.util.NMsg;
+
+import java.awt.*;
+
+public class NTxFunctionGrayScaleColor implements NTxFunction {
+    @Override
+    public String name() {
+        return "grayScaleColor";
+    }
+
+    @Override
+    public NElement invoke(NDocFunctionArgs args, NDocFunctionContext context) {
+        if (args.size() == 0) {
+            return NElement.ofNull();
+        }
+        if (args.size() > 1) {
+            context.messages().log(NMsg.ofC("%s: expected 1 argument, got %s",name(), args.size()));
+        }
+        Color c = NDocValue.of(args.eval(0)).asColor().get();
+        return NDocElementUtils.toElement(NDocColorUtils.grayscale(c));
+    }
+}
