d54fc41455f99d4d4b6f1ec2f15f15c2fb44ceee
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:40:50 +0100

    Update NDocDocumentPanel.java : rename ntx

--- app/ntexup/src/main/java/net/thevpc/ntexup/debug/NDocDocumentPanel.java
+++ app/ntexup/src/main/java/net/thevpc/ntexup/debug/NDocDocumentPanel.java
@@ -1,7 +1,7 @@
 package net.thevpc.ntexup.debug;
 
 import net.thevpc.ntexup.api.engine.NTxEngine;
-import net.thevpc.ntexup.api.log.NDocLogger;
+import net.thevpc.ntexup.api.log.NTxLogger;
 import net.thevpc.ntexup.api.document.NTxDocument;
 import net.thevpc.nuts.elem.NElement;
 
@@ -10,9 +10,9 @@ import java.util.function.Supplier;
 public class NDocDocumentPanel extends TsonPanel {
     private Supplier<NTxDocument> model;
     private NTxEngine engine;
-    private NDocLogger logger;
+    private NTxLogger logger;
 
-    public NDocDocumentPanel(NTxEngine engine, Supplier<NTxDocument> model, NDocLogger logger) {
+    public NDocDocumentPanel(NTxEngine engine, Supplier<NTxDocument> model, NTxLogger logger) {
         this.model = model;
         this.engine = engine;
         this.logger = logger;
