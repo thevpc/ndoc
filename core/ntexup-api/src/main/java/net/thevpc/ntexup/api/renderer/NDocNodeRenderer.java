11ddc54acabc8315f44980a4246f55e60396f3a3
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:08:12 +0100

    Update NDocNodeRenderer.java : rename ndoc to ntexup

--- core/ntexup-api/src/main/java/net/thevpc/ntexup/api/renderer/NDocNodeRenderer.java
+++ core/ntexup-api/src/main/java/net/thevpc/ntexup/api/renderer/NDocNodeRenderer.java
@@ -1,7 +1,7 @@
 package net.thevpc.ntexup.api.renderer;
 
 import net.thevpc.ntexup.api.document.NDocSizeRequirements;
-import net.thevpc.ntexup.api.document.elem2d.NDocBounds2;
+import net.thevpc.ntexup.api.document.elem2d.NTxBounds2;
 import net.thevpc.ntexup.api.document.node.*;
 
 
@@ -9,7 +9,7 @@ public interface NDocNodeRenderer {
 
     NDocSizeRequirements sizeRequirements(NTxNode p, NDocNodeRendererContext ctx);
 
-    NDocBounds2 selfBounds(NTxNode p, NDocNodeRendererContext ctx);
+    NTxBounds2 selfBounds(NTxNode p, NDocNodeRendererContext ctx);
 
     void render(NTxNode p, NDocNodeRendererContext ctx);
 
