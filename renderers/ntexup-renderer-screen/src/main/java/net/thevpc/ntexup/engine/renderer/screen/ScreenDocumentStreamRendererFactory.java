4ca84cda3714351a5d29a4848df0ec38a13dc247
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:40:57 +0100

    Update ScreenDocumentStreamRendererFactory.java : rename ntx

--- renderers/ntexup-renderer-screen/src/main/java/net/thevpc/ntexup/engine/renderer/screen/ScreenDocumentStreamRendererFactory.java
+++ renderers/ntexup-renderer-screen/src/main/java/net/thevpc/ntexup/engine/renderer/screen/ScreenDocumentStreamRendererFactory.java
@@ -5,18 +5,18 @@
 package net.thevpc.ntexup.engine.renderer.screen;
 
 import net.thevpc.ntexup.api.renderer.NTxDocumentRenderer;
-import net.thevpc.ntexup.api.renderer.NDocDocumentRendererFactory;
-import net.thevpc.ntexup.api.renderer.NDocDocumentRendererFactoryContext;
+import net.thevpc.ntexup.api.renderer.NTxDocumentRendererFactory;
+import net.thevpc.ntexup.api.renderer.NTxDocumentRendererFactoryContext;
 import net.thevpc.nuts.NCallableSupport;
 import net.thevpc.nuts.util.NMsg;
 
 /**
  * @author vpc
  */
-public class ScreenDocumentStreamRendererFactory implements NDocDocumentRendererFactory {
+public class ScreenDocumentStreamRendererFactory implements NTxDocumentRendererFactory {
 
     @Override
-    public NCallableSupport<NTxDocumentRenderer> createDocumentRenderer(NDocDocumentRendererFactoryContext context) {
+    public NCallableSupport<NTxDocumentRenderer> createDocumentRenderer(NTxDocumentRendererFactoryContext context) {
         switch (String.valueOf(context.rendererType()).toLowerCase()) {
             case "screen":
                 return NCallableSupport.ofValid( () -> new ScreenDocumentRenderer(context.engine()));
