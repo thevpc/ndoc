3a5ab7cbfb4e0880f500c1480c437382f2898771
thevpc <taha.bensalah@gmail.com>
2025-08-05 13:55:26 +0100

    Update DocumentController.java : rename ndoc to ntexup

---
+++ renderers/ntexup-renderer-web/src/main/java/net/thevpc/ntexup/engine/render/server/DocumentController.java
@@ -0,0 +1,66 @@
+package net.thevpc.ntexup.engine.render.server;
+
+import net.thevpc.ntexup.api.engine.NDocEngine;
+import net.thevpc.ntexup.api.document.NDocument;
+import net.thevpc.ntexup.api.document.node.NTxNode;
+import net.thevpc.ntexup.engine.DefaultNDocEngine;
+import net.thevpc.ntexup.api.renderer.NDocNodeRendererConfig;
+import net.thevpc.nuts.io.NPath;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/document")
+@CrossOrigin(origins = "http://localhost:4200")
+public class DocumentController {
+
+    private final NDocEngine engine;
+
+    @Autowired
+    public DocumentController(NDocEngine engine) {
+        this.engine = engine;
+    }
+
+    @GetMapping(value = "/images", produces = MediaType.IMAGE_PNG_VALUE)
+    public ResponseEntity<byte[]> getDocumentImages(@RequestParam(name = "pageNumber", defaultValue = "1") int pageNumber) {
+        try {
+            NPath file = NPath.of("/home/mohamed/Desktop/stage/ndoc/documentation/tson-doc/main.ndoc")
+                    .toAbsolute()
+                    .normalize();
+
+            int sizeWidth = 1200;
+            int sizeHeight = 1000;
+            NDocEngine e = new DefaultNDocEngine();
+            NDocument doc = e.loadDocument(file).get();
+
+            List<NTxNode> pages = doc.pages();
+
+            if (pageNumber >= 0 && pageNumber < pages.size()) {
+                byte[] imageData = engine.renderManager().renderImageBytes(
+                        pages.get(pageNumber),
+                        new NDocNodeRendererConfig(sizeWidth, sizeHeight)
+                                .withAnimate(false)
+                );
+                return ResponseEntity
+                        .ok()
+                        .contentType(MediaType.IMAGE_PNG)
+                        .body(imageData);
+            } else {
+                return ResponseEntity
+                        .status(HttpStatus.NOT_FOUND)
+                        .body(null);
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
+}
