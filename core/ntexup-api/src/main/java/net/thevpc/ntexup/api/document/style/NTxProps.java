f240c8c103089db05fa55b85458252ea8a90bfd0
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:40:53 +0100

    Update NTxProps.java : rename ntx

--- core/ntexup-api/src/main/java/net/thevpc/ntexup/api/document/style/NTxProps.java
+++ core/ntexup-api/src/main/java/net/thevpc/ntexup/api/document/style/NTxProps.java
@@ -5,7 +5,7 @@ import java.awt.*;
 import net.thevpc.ntexup.api.document.elem2d.NTxDouble2;
 import net.thevpc.ntexup.api.document.elem2d.NTxAlign;
 import net.thevpc.ntexup.api.document.elem2d.NTxSize;
-import net.thevpc.ntexup.api.util.NDocUtils;
+import net.thevpc.ntexup.api.util.NTxUtils;
 import net.thevpc.nuts.elem.NElement;
 
 /**
@@ -14,11 +14,11 @@ import net.thevpc.nuts.elem.NElement;
 public class NTxProps {
 
     public static NTxProp fontSize(Number value) {
-        return NTxProp.of(NTxPropName.FONT_SIZE, NDocUtils.addCompilerDeclarationPathDummy(NElement.ofNumber(value == null ? null : value.doubleValue())));
+        return NTxProp.of(NTxPropName.FONT_SIZE, NTxUtils.addCompilerDeclarationPathDummy(NElement.ofNumber(value == null ? null : value.doubleValue())));
     }
 
     public static NTxProp fontFamily(String value) {
-        return NTxProp.of(NTxPropName.FONT_FAMILY, NDocUtils.addCompilerDeclarationPathDummy(NElement.ofString(value)));
+        return NTxProp.of(NTxPropName.FONT_FAMILY, NTxUtils.addCompilerDeclarationPathDummy(NElement.ofString(value)));
     }
 
     public static NTxProp width(NTxSize size) {
@@ -34,15 +34,15 @@ public class NTxProps {
 //    }
 
     public static NTxProp foregroundColor(int color) {
-        return NTxProp.of(NTxPropName.FOREGROUND_COLOR, NDocUtils.addCompilerDeclarationPathDummy(NElement.ofInt(color)));
+        return NTxProp.of(NTxPropName.FOREGROUND_COLOR, NTxUtils.addCompilerDeclarationPathDummy(NElement.ofInt(color)));
     }
 
     public static NTxProp backgroundColor(int rgb) {
-        return NTxProp.of(NTxPropName.BACKGROUND_COLOR, NDocUtils.addCompilerDeclarationPathDummy(NElement.ofInt(rgb)));
+        return NTxProp.of(NTxPropName.BACKGROUND_COLOR, NTxUtils.addCompilerDeclarationPathDummy(NElement.ofInt(rgb)));
     }
 
     public static NTxProp backgroundColor(Color color) {
-        return NTxProp.of(NTxPropName.BACKGROUND_COLOR, color == null ? null : NDocUtils.addCompilerDeclarationPathDummy(NElement.ofInt(color.getRGB())));
+        return NTxProp.of(NTxPropName.BACKGROUND_COLOR, color == null ? null : NTxUtils.addCompilerDeclarationPathDummy(NElement.ofInt(color.getRGB())));
     }
 
 //    public static HProp lineColor(Paint color) {
@@ -50,7 +50,7 @@ public class NTxProps {
 //    }
 
     public static NTxProp gridColor(Color color) {
-        return NTxProp.of(NTxPropName.GRID_COLOR, color == null ? null : NDocUtils.addCompilerDeclarationPathDummy(NElement.ofInt(color.getRGB())));
+        return NTxProp.of(NTxPropName.GRID_COLOR, color == null ? null : NTxUtils.addCompilerDeclarationPathDummy(NElement.ofInt(color.getRGB())));
     }
 
 //    public static HProp lineColor(int color) {
@@ -66,35 +66,35 @@ public class NTxProps {
     }
 
     public static NTxProp preserveShapeRatio(boolean value) {
-        return NTxProp.of(NTxPropName.PRESERVE_ASPECT_RATIO, NDocUtils.addCompilerDeclarationPathDummy((NElement.ofBoolean(value))));
+        return NTxProp.of(NTxPropName.PRESERVE_ASPECT_RATIO, NTxUtils.addCompilerDeclarationPathDummy((NElement.ofBoolean(value))));
     }
 
     public static NTxProp drawContour(boolean value) {
-        return NTxProp.of(NTxPropName.DRAW_CONTOUR, NDocUtils.addCompilerDeclarationPathDummy(NElement.ofBoolean(value)));
+        return NTxProp.of(NTxPropName.DRAW_CONTOUR, NTxUtils.addCompilerDeclarationPathDummy(NElement.ofBoolean(value)));
     }
 
     public static NTxProp drawGrid(boolean value) {
-        return NTxProp.of(NTxPropName.DRAW_GRID, NDocUtils.addCompilerDeclarationPathDummy(NElement.ofBoolean(value)));
+        return NTxProp.of(NTxPropName.DRAW_GRID, NTxUtils.addCompilerDeclarationPathDummy(NElement.ofBoolean(value)));
     }
 
     public static NTxProp fillBackground(boolean value) {
-        return NTxProp.of(NTxPropName.FILL_BACKGROUND, NDocUtils.addCompilerDeclarationPathDummy(NElement.ofBoolean(value)));
+        return NTxProp.of(NTxPropName.FILL_BACKGROUND, NTxUtils.addCompilerDeclarationPathDummy(NElement.ofBoolean(value)));
     }
 
     public static NTxProp fontBold(boolean value) {
-        return NTxProp.of(NTxPropName.FONT_BOLD, NDocUtils.addCompilerDeclarationPathDummy(NElement.ofBoolean(value)));
+        return NTxProp.of(NTxPropName.FONT_BOLD, NTxUtils.addCompilerDeclarationPathDummy(NElement.ofBoolean(value)));
     }
 
     public static NTxProp fontItalic(boolean value) {
-        return NTxProp.of(NTxPropName.FONT_ITALIC, NDocUtils.addCompilerDeclarationPathDummy(NElement.ofBoolean(value)));
+        return NTxProp.of(NTxPropName.FONT_ITALIC, NTxUtils.addCompilerDeclarationPathDummy(NElement.ofBoolean(value)));
     }
 
     public static NTxProp fontUnderlined(boolean value) {
-        return NTxProp.of(NTxPropName.FONT_UNDERLINED, NDocUtils.addCompilerDeclarationPathDummy(NElement.ofBoolean(value)));
+        return NTxProp.of(NTxPropName.FONT_UNDERLINED, NTxUtils.addCompilerDeclarationPathDummy(NElement.ofBoolean(value)));
     }
 
     public static NTxProp fontStrike(boolean value) {
-        return NTxProp.of(NTxPropName.FONT_STRIKE, NDocUtils.addCompilerDeclarationPathDummy(NElement.ofBoolean(value)));
+        return NTxProp.of(NTxPropName.FONT_STRIKE, NTxUtils.addCompilerDeclarationPathDummy(NElement.ofBoolean(value)));
     }
 
     public static NTxProp position(NTxDouble2 p) {
@@ -138,23 +138,23 @@ public class NTxProps {
     }
 
     public static NTxProp roundCorner(double x, double y) {
-        return NTxProp.of(NTxPropName.ROUND_CORNER, NDocUtils.addCompilerDeclarationPathDummy(new NTxDouble2(x, y).toElement()));
+        return NTxProp.of(NTxPropName.ROUND_CORNER, NTxUtils.addCompilerDeclarationPathDummy(new NTxDouble2(x, y).toElement()));
     }
 
     public static NTxProp threeD(Boolean b) {
-        return NTxProp.of(NTxPropName.THEED, NDocUtils.addCompilerDeclarationPathDummy(NElement.ofBoolean(b)));
+        return NTxProp.of(NTxPropName.THEED, NTxUtils.addCompilerDeclarationPathDummy(NElement.ofBoolean(b)));
     }
 
     public static NTxProp raised(Boolean b) {
-        return NTxProp.of(NTxPropName.RAISED, NDocUtils.addCompilerDeclarationPathDummy(NElement.ofBoolean(b)));
+        return NTxProp.of(NTxPropName.RAISED, NTxUtils.addCompilerDeclarationPathDummy(NElement.ofBoolean(b)));
     }
 
     public static NTxProp columns(int o) {
-        return NTxProp.of(NTxPropName.COLUMNS, NDocUtils.addCompilerDeclarationPathDummy(NElement.ofInt(o)));
+        return NTxProp.of(NTxPropName.COLUMNS, NTxUtils.addCompilerDeclarationPathDummy(NElement.ofInt(o)));
     }
 
     public static NTxProp rows(int o) {
-        return NTxProp.of(NTxPropName.ROWS, NDocUtils.addCompilerDeclarationPathDummy(NElement.ofInt(o)));
+        return NTxProp.of(NTxPropName.ROWS, NTxUtils.addCompilerDeclarationPathDummy(NElement.ofInt(o)));
     }
 
     public static NTxProp rowsWeight(double... w) {
@@ -162,31 +162,31 @@ public class NTxProps {
     }
 
     public static NTxProp colspan(int c) {
-        return NTxProp.of(NTxPropName.COLSPAN, NDocUtils.addCompilerDeclarationPathDummy(NElement.ofInt(c)));
+        return NTxProp.of(NTxPropName.COLSPAN, NTxUtils.addCompilerDeclarationPathDummy(NElement.ofInt(c)));
     }
 
     public static NTxProp rowspan(int r) {
-        return NTxProp.of(NTxPropName.ROWSPAN, NDocUtils.addCompilerDeclarationPathDummy(NElement.ofInt(r)));
+        return NTxProp.of(NTxPropName.ROWSPAN, NTxUtils.addCompilerDeclarationPathDummy(NElement.ofInt(r)));
     }
 
     public static NTxProp styleClasses(String... array) {
-        return NTxProp.of(NTxPropName.CLASS, NDocUtils.addCompilerDeclarationPathDummy(NElement.ofStringArray(array)));
+        return NTxProp.of(NTxPropName.CLASS, NTxUtils.addCompilerDeclarationPathDummy(NElement.ofStringArray(array)));
     }
 
     public static NTxProp disabled(boolean b) {
-        return NTxProp.of(NTxPropName.HIDE, NDocUtils.addCompilerDeclarationPathDummy(NElement.ofBoolean(b)));
+        return NTxProp.of(NTxPropName.HIDE, NTxUtils.addCompilerDeclarationPathDummy(NElement.ofBoolean(b)));
     }
 
     public static NTxProp name(String name) {
-        return NTxProp.of(NTxPropName.NAME, NDocUtils.addCompilerDeclarationPathDummy(NElement.ofString(name)));
+        return NTxProp.of(NTxPropName.NAME, NTxUtils.addCompilerDeclarationPathDummy(NElement.ofString(name)));
     }
 
     public static NTxProp maxX(double x) {
-        return NTxProp.of(NTxPropName.MAX_X, NDocUtils.addCompilerDeclarationPathDummy(NElement.ofDouble(x)));
+        return NTxProp.of(NTxPropName.MAX_X, NTxUtils.addCompilerDeclarationPathDummy(NElement.ofDouble(x)));
     }
 
     public static NTxProp maxY(double x) {
-        return NTxProp.of(NTxPropName.MAX_Y, NDocUtils.addCompilerDeclarationPathDummy(NElement.ofDouble(x)));
+        return NTxProp.of(NTxPropName.MAX_Y, NTxUtils.addCompilerDeclarationPathDummy(NElement.ofDouble(x)));
     }
 
 }
