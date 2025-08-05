3a4bc9389311a31d46f24de9f1a34e909c3cf52f
thevpc <taha.bensalah@gmail.com>
2025-08-05 13:55:25 +0100

    Update NDocEllipseBuilder.java : rename ndoc to ntexup

---
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/base/nodes/shape/NDocEllipseBuilder.java
@@ -0,0 +1,43 @@
+package net.thevpc.ntexup.engine.base.nodes.shape;
+
+import net.thevpc.ntexup.api.document.elem2d.NDocBounds2;
+import net.thevpc.ntexup.api.document.node.NTxNode;
+import net.thevpc.ntexup.api.document.node.NDocNodeType;
+import net.thevpc.ntexup.api.document.style.NDocProperties;
+import net.thevpc.ntexup.api.extension.NDocNodeBuilder;
+import net.thevpc.ntexup.api.engine.NDocNodeCustomBuilderContext;
+import net.thevpc.ntexup.api.renderer.NDocGraphics;
+import net.thevpc.ntexup.api.renderer.NDocNodeRendererContext;
+import net.thevpc.ntexup.api.util.NDocUtils;
+
+public class NDocEllipseBuilder implements NDocNodeBuilder {
+    NDocProperties defaultStyles = new NDocProperties();
+
+    @Override
+    public void build(NDocNodeCustomBuilderContext builderContext) {
+        builderContext
+                .id(NDocNodeType.ELLIPSE)
+                .renderComponent(this::renderMain)
+                ;
+    }
+
+    public void renderMain(NTxNode p, NDocNodeRendererContext ctx, NDocNodeCustomBuilderContext builderContext) {
+        ctx = ctx.withDefaultStyles(p, defaultStyles);
+        NDocBounds2 b = ctx.selfBounds(p, null, null);
+        double x = b.getX();
+        double y = b.getY();
+        NDocGraphics g = ctx.graphics();
+        boolean someBG = false;
+        if (!ctx.isDry()) {
+            if (someBG = ctx.applyBackgroundColor(p)) {
+                g.fillOval((int) x, (int) y, NDocUtils.intOf(b.getWidth()), NDocUtils.intOf(b.getHeight()));
+            }
+            if (ctx.applyForeground(p, !someBG)) {
+                ctx.withStroke(p,()->{
+                    g.drawOval((int) x, (int) y, NDocUtils.intOf(b.getWidth()), NDocUtils.intOf(b.getHeight()));
+                });
+            }
+        }
+        ctx.paintDebugBox(p, b);
+    }
+}
