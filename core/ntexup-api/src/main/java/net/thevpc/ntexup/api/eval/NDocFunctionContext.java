e9b96495ff7cf2fd82b16548bb51e089fe763d4d
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:08:12 +0100

    Update NDocFunctionContext.java : rename ndoc to ntexup

--- core/ntexup-api/src/main/java/net/thevpc/ntexup/api/eval/NDocFunctionContext.java
+++ core/ntexup-api/src/main/java/net/thevpc/ntexup/api/eval/NDocFunctionContext.java
@@ -2,14 +2,14 @@ package net.thevpc.ntexup.api.eval;
 
 
 import net.thevpc.ntexup.api.document.node.NTxNode;
-import net.thevpc.ntexup.api.engine.NDocEngine;
+import net.thevpc.ntexup.api.engine.NTxEngine;
 import net.thevpc.ntexup.api.log.NDocLogger;
 import net.thevpc.nuts.elem.NElement;
 import net.thevpc.nuts.util.NOptional;
 
 public interface NDocFunctionContext {
     NDocLogger messages();
-    NDocEngine engine();
+    NTxEngine engine();
     NTxNode node();
     NElement eval(NElement expr);
     NOptional<NDocVar> findVar(String varName);
