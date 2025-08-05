dd254751bee7c1d571c283a7c7d8ce002f355e82
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:41:30 +0100

    Update NTxTextPartStyle.java : rename ntx

---
+++ core/ntexup-api/src/main/java/net/thevpc/ntexup/api/renderer/text/NTxTextPartStyle.java
@@ -0,0 +1,14 @@
+package net.thevpc.ntexup.api.renderer.text;
+
+import java.awt.*;
+
+public class NTxTextPartStyle {
+
+    public boolean bold;
+    public boolean italic;
+    public boolean undelined;
+    public String fontFamily;
+    public double fontSize;
+    public Color foreground;
+    public Color background;
+}
