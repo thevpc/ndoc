12188f4ec0844c1997c10936cc9da1ebb96b3eca
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:41:31 +0100

    Update PlantUmlWbsBuilder.java : rename ntx

---
+++ extensions/ntexup-extension-plantuml/src/main/java/net/thevpc/ntexup/extension/plantuml/modes/PlantUmlWbsBuilder.java
@@ -0,0 +1,9 @@
+package net.thevpc.ntexup.extension.plantuml.modes;
+
+import net.thevpc.ntexup.extension.plantuml.PlantUmlBuilderBase;
+
+public class PlantUmlWbsBuilder extends PlantUmlBuilderBase {
+    public PlantUmlWbsBuilder() {
+        super("wbs");
+    }
+}
