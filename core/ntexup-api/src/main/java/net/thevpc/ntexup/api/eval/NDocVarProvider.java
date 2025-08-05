9b7e65bdba51c034c114e8a87de603440e439670
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:40:53 +0100

    Rename NTxVarProvider.java → NDocVarProvider.java : rename ntx

--- core/ntexup-api/src/main/java/net/thevpc/ntexup/api/eval/NDocVarProvider.java
+++
@@ -1,8 +0,0 @@
-package net.thevpc.ntexup.api.eval;
-
-import net.thevpc.ntexup.api.document.node.NTxNode;
-import net.thevpc.nuts.util.NOptional;
-
-public interface NDocVarProvider {
-    NOptional<NDocVar> findVar(String varName, NTxNode node);
-}
