3081fe75f96721beba5bb614859156c784c29c73
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:40:56 +0100

    Rename PlantUmlWbsBuilder.java → PlantUmlWbsBuilder.java : rename ntx

--- extensions/ndoc-extension-plantuml/src/main/java/net/thevpc/ndoc/extension/plantuml/modes/PlantUmlWbsBuilder.java
+++
@@ -1,9 +0,0 @@
-package net.thevpc.ndoc.extension.plantuml.modes;
-
-import net.thevpc.ndoc.extension.plantuml.PlantUmlBuilderBase;
-
-public class PlantUmlWbsBuilder extends PlantUmlBuilderBase {
-    public PlantUmlWbsBuilder() {
-        super("wbs");
-    }
-}
