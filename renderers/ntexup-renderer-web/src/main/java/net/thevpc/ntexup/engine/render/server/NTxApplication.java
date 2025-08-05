24dec632c360172aca3cd97bf83808004aaf3a6e
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:41:32 +0100

    Update NTxApplication.java : rename ntx

---
+++ renderers/ntexup-renderer-web/src/main/java/net/thevpc/ntexup/engine/render/server/NTxApplication.java
@@ -0,0 +1,12 @@
+package net.thevpc.ntexup.engine.render.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NTxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NTxApplication.class, args);
+    }
+}
