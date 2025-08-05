e63d0f70fe67243b057dbed1e40d3860681e9d07
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:40:53 +0100

    Rename NTxNodeBuilder.java → NDocNodeBuilder.java : rename ntx

--- core/ntexup-api/src/main/java/net/thevpc/ntexup/api/extension/NDocNodeBuilder.java
+++
@@ -1,9 +0,0 @@
-package net.thevpc.ntexup.api.extension;
-
-import net.thevpc.ntexup.api.engine.NTxNodeCustomBuilderContext;
-
-public interface NDocNodeBuilder {
-
-    void build(NTxNodeCustomBuilderContext builderContext);
-
-}
