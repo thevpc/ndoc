b2e9d6d90609f2270ce382ad45cb6731baaff68e
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:41:30 +0100

    Update NTxHeptagonBuilder.java : rename ntx

---
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/base/nodes/shape/NTxHeptagonBuilder.java
@@ -0,0 +1,23 @@
+package net.thevpc.ntexup.engine.base.nodes.shape;
+
+import net.thevpc.ntexup.api.document.node.NTxNode;
+import net.thevpc.ntexup.api.document.node.NTxNodeType;
+import net.thevpc.ntexup.api.document.style.NTxProperties;
+import net.thevpc.ntexup.api.extension.NTxNodeBuilder;
+import net.thevpc.ntexup.api.engine.NTxNodeCustomBuilderContext;
+import net.thevpc.ntexup.api.renderer.NTxNodeRendererContext;
+
+public class NTxHeptagonBuilder implements NTxNodeBuilder {
+    NTxProperties defaultStyles = new NTxProperties();
+
+    @Override
+    public void build(NTxNodeCustomBuilderContext builderContext) {
+        builderContext.id(NTxNodeType.HEPTAGON)
+                .renderComponent(this::render);
+    }
+
+    public void render(NTxNode p, NTxNodeRendererContext renderContext, NTxNodeCustomBuilderContext buildContext) {
+        NTxPolygonHelper.renderPointsCount(7, p, renderContext, defaultStyles);
+    }
+
+}
