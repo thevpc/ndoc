de16dfc70660b36535fb6b6963c4551bcee547fe
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:41:29 +0100

    Update NTxArrowType.java : rename ntx

---
+++ core/ntexup-api/src/main/java/net/thevpc/ntexup/api/document/NTxArrowType.java
@@ -0,0 +1,14 @@
+package net.thevpc.ntexup.api.document;
+
+public enum NTxArrowType {
+    DEFAULT,
+    SIMPLE,
+    TRIANGLE,
+    TRIANGLE_FULL,
+    OVAL,
+    OVAL_FULL,
+    RECTANGLE,
+    RECTANGLE_FULL,
+    DIAMOND,
+    DIAMOND_FULL
+}
