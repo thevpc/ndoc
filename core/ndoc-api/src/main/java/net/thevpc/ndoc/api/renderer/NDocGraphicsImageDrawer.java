da738b958ac3c71d079afa62f7d684d62713d0c9
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:40:51 +0100

    Update NDocGraphicsImageDrawer.java : rename ntx

--- core/ndoc-api/src/main/java/net/thevpc/ndoc/api/renderer/NDocGraphicsImageDrawer.java
+++
@@ -1,7 +0,0 @@
-package net.thevpc.ndoc.api.renderer;
-
-import net.thevpc.ndoc.api.document.elem2d.NDocImageOptions;
-
-public interface NDocGraphicsImageDrawer {
-    void drawImage(double x, double y, NDocImageOptions options, NDocGraphics graphics);
-}
