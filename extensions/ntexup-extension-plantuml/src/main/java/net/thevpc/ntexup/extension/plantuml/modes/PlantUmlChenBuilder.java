d9f64915d4539e1ebee01d18d34dbabbe85d814f
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:41:31 +0100

    Update PlantUmlChenBuilder.java : rename ntx

---
+++ extensions/ntexup-extension-plantuml/src/main/java/net/thevpc/ntexup/extension/plantuml/modes/PlantUmlChenBuilder.java
@@ -0,0 +1,10 @@
+package net.thevpc.ntexup.extension.plantuml.modes;
+
+
+import net.thevpc.ntexup.extension.plantuml.PlantUmlBuilderBase;
+
+public class PlantUmlChenBuilder extends PlantUmlBuilderBase {
+    public PlantUmlChenBuilder() {
+        super("chen");
+    }
+}
