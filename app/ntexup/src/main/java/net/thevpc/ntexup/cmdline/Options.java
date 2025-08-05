14a1de9950ff78b60335189259ff37b0b583da1c
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:41:29 +0100

    Update Options.java : rename ntx

---
+++ app/ntexup/src/main/java/net/thevpc/ntexup/cmdline/Options.java
@@ -0,0 +1,21 @@
+package net.thevpc.ntexup.cmdline;
+
+import net.thevpc.nuts.io.NPath;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Options {
+    public List<NPath> paths = new ArrayList<>();
+    public NPath toPdf;
+    public Action action = Action.OPEN;
+    public boolean viewer;
+    public boolean console;
+    public boolean showLogs;
+    public boolean reopen;
+
+    public Options requireViewer() {
+        this.viewer = true;
+        return this;
+    }
+}
