286e1d9366d8672b6bd535d3fef22ee9160ef2a0
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:08:12 +0100

    Update NtxAbstractElement2D.java : rename ndoc to ntexup

---
+++ core/ntexup-api/src/main/java/net/thevpc/ntexup/api/document/elem2d/primitives/NtxAbstractElement2D.java
@@ -0,0 +1,6 @@
+package net.thevpc.ntexup.api.document.elem2d.primitives;
+
+import net.thevpc.ntexup.api.document.elem2d.NtxElement2D;
+
+public abstract class NtxAbstractElement2D implements NtxElement2D {
+}
