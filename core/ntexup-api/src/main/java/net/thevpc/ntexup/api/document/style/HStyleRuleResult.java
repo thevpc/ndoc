6e3ef03664d34cfb99ccb172d3d94fcb93616baa
thevpc <taha.bensalah@gmail.com>
2025-08-05 13:55:25 +0100

    Update HStyleRuleResult.java : rename ndoc to ntexup

---
+++ core/ntexup-api/src/main/java/net/thevpc/ntexup/api/document/style/HStyleRuleResult.java
@@ -0,0 +1,27 @@
+package net.thevpc.ntexup.api.document.style;
+
+import java.util.Set;
+
+public interface HStyleRuleResult /*extends Comparable<HStyleRuleResult>*/ {
+    boolean isValid();
+
+    HStyleMagnitude magnitude();
+
+    Set<NTxProp> value();
+
+//    @Override
+//    default int compareTo(HStyleRuleResult o) {
+//        if (this.isValid() && o.isValid()) {
+//            HStyleMagnitude m1 = this.magnitude();
+//            HStyleMagnitude m2 = o.magnitude();
+//            return m1.compareTo(m2);
+//        } else if (!this.isValid() && !o.isValid()) {
+//            return 0;
+//        } else if (this.isValid()) {
+//            return -1;
+//        } else if (o.isValid()) {
+//            return 1;
+//        }
+//        return 0;
+//    }
+}
