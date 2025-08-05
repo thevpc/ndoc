4fc538e7e0d269b0bf93c42d76b0126744d1d744
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:41:31 +0100

    Update PlantUmlJsonDiagramBuilder.java : rename ntx

---
+++ extensions/ntexup-extension-plantuml/src/main/java/net/thevpc/ntexup/extension/plantuml/modes/PlantUmlJsonDiagramBuilder.java
@@ -0,0 +1,9 @@
+package net.thevpc.ntexup.extension.plantuml.modes;
+
+import net.thevpc.ntexup.extension.plantuml.PlantUmlBuilderBase;
+
+public class PlantUmlJsonDiagramBuilder extends PlantUmlBuilderBase {
+    public PlantUmlJsonDiagramBuilder() {
+        super("plantuml-json","json-diagram");
+    }
+}
