44525dc70e0a21377f92a2b3e34d8fb64e991686
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:41:32 +0100

    Update NTxExampleFromFolder.java : rename ntx

---
+++ test/ntexup-examples/src/main/java/net/thevpc/ntexup/examples/NTxExampleFromFolder.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package net.thevpc.ntexup.examples;
+
+import net.thevpc.ntexup.api.engine.NTxEngine;
+import net.thevpc.ntexup.engine.DefaultNTxEngine;
+import net.thevpc.nuts.Nuts;
+import net.thevpc.nuts.io.NPath;
+
+/**
+ * @author vpc
+ */
+public class NTxExampleFromFolder {
+
+    public static void main(String[] args) {
+        Nuts.openWorkspace().share();
+        NTxEngine e = new DefaultNTxEngine();
+        NPath file = NPath.of("C:\\Users\\ibtih\\IdeaProjects\\ndoc\\test\\ndoc-examples\\src\\ndoc\\ibtihel").toAbsolute().normalize();
+        e.newScreenRenderer().get().renderPath(file);
+    }
+}
