04f9b89254408fb359479a5f3f2fad3f4bd870fd
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:08:14 +0100

    Update NDocExampleTestGrid.java : rename ndoc to ntexup

--- test/ntexup-examples/src/main/java/net/thevpc/ntexup/examples/NDocExampleTestGrid.java
+++ test/ntexup-examples/src/main/java/net/thevpc/ntexup/examples/NDocExampleTestGrid.java
@@ -5,12 +5,12 @@
 package net.thevpc.ntexup.examples;
 
 import net.thevpc.ntexup.api.document.NTxDocumentFactory;
-import  net.thevpc.ntexup.api.document.style.NDocPropName;
-import  net.thevpc.ntexup.api.document.style.NDocProps;
-import net.thevpc.ntexup.api.engine.NDocEngine;
-import net.thevpc.ntexup.api.document.NDocument;
-import net.thevpc.ntexup.engine.DefaultNDocEngine;
-import net.thevpc.ntexup.api.renderer.NDocDocumentRenderer;
+import net.thevpc.ntexup.api.document.style.NTxPropName;
+import net.thevpc.ntexup.api.document.style.NTxProps;
+import net.thevpc.ntexup.api.engine.NTxEngine;
+import net.thevpc.ntexup.api.document.NTxDocument;
+import net.thevpc.ntexup.engine.DefaultNTxEngine;
+import net.thevpc.ntexup.api.renderer.NTxDocumentRenderer;
 import net.thevpc.nuts.Nuts;
 import net.thevpc.nuts.elem.NElement;
 
@@ -24,31 +24,31 @@ public class NDocExampleTestGrid {
 
     public static void main(String[] args) {
         Nuts.openWorkspace().share();
-        NDocEngine e = new DefaultNDocEngine();
+        NTxEngine e = new DefaultNTxEngine();
         NTxDocumentFactory f = e.documentFactory();
-        NDocument d = f.ofDocument(null)
+        NTxDocument d = f.ofDocument(null)
                 .add(f.ofPage()
                         .addChild(
                                 f.ofGrid(3, 2)
-                                        .setProperty(NDocPropName.COLUMNS_WEIGHT, NElement.ofDoubleArray(1, 2, 3, 4, 5, 6))
+                                        .setProperty(NTxPropName.COLUMNS_WEIGHT, NElement.ofDoubleArray(1, 2, 3, 4, 5, 6))
                                         .addChild(f.ofRectangle()
-                                                .setProperty(NDocProps.backgroundColor(Color.BLUE))
-                                                .setProperty(NDocProps.colspan(4))
+                                                .setProperty(NTxProps.backgroundColor(Color.BLUE))
+                                                .setProperty(NTxProps.colspan(4))
                                         )
                                         .addChild(
-                                                f.ofRectangle().setProperty(NDocProps.backgroundColor(Color.RED))
-                                                        .setProperty(NDocProps.rowspan(4))
+                                                f.ofRectangle().setProperty(NTxProps.backgroundColor(Color.RED))
+                                                        .setProperty(NTxProps.rowspan(4))
                                         )
                                         .addChild(f.ofGrid(4, 4)
-                                                .setProperty(NDocProps.colspan(3))
-                                                .setProperty(NDocProps.rowspan(2))
-                                                .addChild(f.ofRectangle().setProperty(NDocProps.backgroundColor(Color.BLUE))
-                                                        .setProperty(NDocProps.colspan(5))
-                                                        .setProperty(NDocProps.rowspan(2))
+                                                .setProperty(NTxProps.colspan(3))
+                                                .setProperty(NTxProps.rowspan(2))
+                                                .addChild(f.ofRectangle().setProperty(NTxProps.backgroundColor(Color.BLUE))
+                                                        .setProperty(NTxProps.colspan(5))
+                                                        .setProperty(NTxProps.rowspan(2))
                                                 )
-                                                .addChild(f.ofRectangle().setProperty(NDocProps.backgroundColor(Color.RED)))
-                                                .addChild(f.ofRectangle().setProperty(NDocProps.backgroundColor(Color.GREEN)))
-                                                .addChild(f.ofRectangle().setProperty(NDocProps.backgroundColor(Color.YELLOW))))
+                                                .addChild(f.ofRectangle().setProperty(NTxProps.backgroundColor(Color.RED)))
+                                                .addChild(f.ofRectangle().setProperty(NTxProps.backgroundColor(Color.GREEN)))
+                                                .addChild(f.ofRectangle().setProperty(NTxProps.backgroundColor(Color.YELLOW))))
                         )
                 )
 //                .add(f.page()
@@ -57,7 +57,7 @@ public class NDocExampleTestGrid {
 //                                )
 //                )
                 ;
-        NDocDocumentRenderer renderer = e.newScreenRenderer().get();
+        NTxDocumentRenderer renderer = e.newScreenRenderer().get();
         renderer.render(d);
     }
 }
