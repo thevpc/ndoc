6c625cc3157193a3e5bc68f745e3b0fdd20ea60e
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:08:14 +0100

    Update SourceNameSimpleLayer.java : rename ndoc to ntexup

---
+++ renderers/ntexup-renderer-screen/src/main/java/net/thevpc/ntexup/engine/renderer/screen/components/SourceNameSimpleLayer.java
@@ -0,0 +1,15 @@
+package net.thevpc.ntexup.engine.renderer.screen.components;
+
+import net.thevpc.ntexup.api.document.elem2d.NTxAlign;
+import net.thevpc.ntexup.engine.renderer.screen.DocumentView;
+import net.thevpc.ntexup.api.renderer.NDocGraphics;
+import net.thevpc.ntexup.engine.renderer.screen.PageView;
+
+import java.awt.*;
+
+public class SourceNameSimpleLayer extends SimpleLayer {
+    public void draw(DocumentView doc, PageView pageView, Dimension size, NDocGraphics g) {
+        g.setFont(new Font("Arial", 0, 20));
+        drawStr(doc.getPageSourceName(), NTxAlign.LEFT, size, g);
+    }
+}
