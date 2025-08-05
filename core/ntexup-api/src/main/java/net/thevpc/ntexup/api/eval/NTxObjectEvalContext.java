e7a304a8a37033c7c2c3af77e0958670cb98a8d0
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:41:29 +0100

    Update NTxObjectEvalContext.java : rename ntx

---
+++ core/ntexup-api/src/main/java/net/thevpc/ntexup/api/eval/NTxObjectEvalContext.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package net.thevpc.ntexup.api.eval;
+
+import net.thevpc.ntexup.api.document.node.NTxNode;
+import net.thevpc.nuts.elem.NElement;
+import net.thevpc.nuts.util.NOptional;
+
+/**
+ * @author vpc
+ */
+public interface NTxObjectEvalContext {
+
+    NOptional<NTxVar> findVar(String varName, NTxNode node);
+
+    NElement eval(NElement elementExpr, NTxNode node);
+
+}
