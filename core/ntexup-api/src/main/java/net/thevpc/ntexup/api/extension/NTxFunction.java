6f6678237013576e8068d98351abd17787afa83e
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:40:53 +0100

    Update NTxFunction.java : rename ntx

--- core/ntexup-api/src/main/java/net/thevpc/ntexup/api/extension/NTxFunction.java
+++ core/ntexup-api/src/main/java/net/thevpc/ntexup/api/extension/NTxFunction.java
@@ -1,11 +1,11 @@
 package net.thevpc.ntexup.api.extension;
 
-import net.thevpc.ntexup.api.eval.NDocFunctionArgs;
-import net.thevpc.ntexup.api.eval.NDocFunctionContext;
+import net.thevpc.ntexup.api.eval.NTxFunctionArgs;
+import net.thevpc.ntexup.api.eval.NTxFunctionContext;
 import net.thevpc.nuts.elem.NElement;
 
 public interface NTxFunction {
     String name();
 
-    NElement invoke(NDocFunctionArgs args, NDocFunctionContext context);
+    NElement invoke(NTxFunctionArgs args, NTxFunctionContext context);
 }
