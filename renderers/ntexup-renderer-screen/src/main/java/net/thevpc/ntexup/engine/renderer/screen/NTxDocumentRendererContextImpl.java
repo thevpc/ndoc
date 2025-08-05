c9bf404a42a6c170b5dc642d6a293d545246bb27
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:08:14 +0100

    Update NTxDocumentRendererContextImpl.java : rename ndoc to ntexup

---
+++ renderers/ntexup-renderer-screen/src/main/java/net/thevpc/ntexup/engine/renderer/screen/NTxDocumentRendererContextImpl.java
@@ -0,0 +1,10 @@
+package net.thevpc.ntexup.engine.renderer.screen;
+
+import net.thevpc.ntexup.api.renderer.NTxDocumentRendererContext;
+
+class NTxDocumentRendererContextImpl implements NTxDocumentRendererContext {
+
+    public NTxDocumentRendererContextImpl() {
+    }
+
+}
