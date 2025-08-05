a26b11afdf85dc939eac30cf881651bb4101b02f
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:41:31 +0100

    Update PlantUmlTextBuilder.java : rename ntx

---
+++ extensions/ntexup-extension-plantuml/src/main/java/net/thevpc/ntexup/extension/plantuml/modes/PlantUmlTextBuilder.java
@@ -0,0 +1,10 @@
+package net.thevpc.ntexup.extension.plantuml.modes;
+
+
+import net.thevpc.ntexup.extension.plantuml.PlantUmlBuilderBase;
+
+public class PlantUmlTextBuilder extends PlantUmlBuilderBase {
+    public PlantUmlTextBuilder() {
+        super("plantuml","",new String[0]);
+    }
+}
