90609ad0fc38a6c5a126c17f1210fdcfd704122c
thevpc <taha.bensalah@gmail.com>
2025-08-05 13:55:25 +0100

    Update NDocNodePanel.java : rename ndoc to ntexup

---
+++ app/ntexup/src/main/java/net/thevpc/ntexup/debug/NDocNodePanel.java
@@ -0,0 +1,29 @@
+package net.thevpc.ntexup.debug;
+
+import net.thevpc.ntexup.api.engine.NDocEngine;
+import net.thevpc.ntexup.api.document.node.NTxNode;
+import net.thevpc.ntexup.api.util.NDocUtils;
+import net.thevpc.nuts.elem.NElement;
+
+import java.util.function.Supplier;
+
+public class NDocNodePanel extends TsonPanel {
+    private Supplier<NTxNode> model;
+    private NDocEngine engine;
+
+    public NDocNodePanel(NDocEngine engine, Supplier<NTxNode> model) {
+        this.model = model;
+        this.engine = engine;
+    }
+
+    @Override
+    public NElement createTson() {
+        NTxNode m = null;
+        m = model.get();
+        if (m != null) {
+            return NDocUtils.removeCompilerDeclarationPathAnnotations(engine.toElement(m));
+        }
+        return null;
+    }
+
+}
