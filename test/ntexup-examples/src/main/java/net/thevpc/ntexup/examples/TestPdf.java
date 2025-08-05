feab3b031c991b7d52b8af6baa036a7368d8e773
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:08:14 +0100

    Update TestPdf.java : rename ndoc to ntexup

---
+++ test/ntexup-examples/src/main/java/net/thevpc/ntexup/examples/TestPdf.java
@@ -0,0 +1,20 @@
+package net.thevpc.ntexup.examples;
+
+import net.thevpc.ntexup.api.engine.NTxEngine;
+import net.thevpc.ntexup.api.document.NTxDocument;
+import net.thevpc.ntexup.engine.DefaultNTxEngine;
+import net.thevpc.ntexup.api.renderer.NTxDocumentStreamRenderer;
+import net.thevpc.nuts.Nuts;
+import net.thevpc.nuts.io.NPath;
+
+public class TestPdf {
+    public static void main(String[] args) {
+        Nuts.openWorkspace().share();
+        NTxEngine e = new DefaultNTxEngine();
+        NPath file = NPath.of("documentation/ndoc-doc").toAbsolute().normalize();
+        NTxDocument doc = e.loadDocument(file).get();
+        NTxDocumentStreamRenderer renderer = e.newPdfRenderer().get();
+        renderer.setOutput(file.resolve("output.pdf"));
+        renderer.render(doc);
+    }
+}
