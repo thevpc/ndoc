0fb45c6bbd334b3fbd6a7292aa41f825677e81df
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:08:12 +0100

    Update NTxDocumentStreamRendererConfig.java : rename ndoc to ntexup

---
+++ core/ntexup-api/src/main/java/net/thevpc/ntexup/api/renderer/NTxDocumentStreamRendererConfig.java
@@ -0,0 +1,134 @@
+package net.thevpc.ntexup.api.renderer;
+
+import java.io.Serializable;
+
+public class NTxDocumentStreamRendererConfig implements Serializable, Cloneable {
+    private NDocPageOrientation orientation;
+    private int gridX;
+    private int gridY;
+    private int pageWidth;
+    private int pageHeight;
+    private boolean showPageNumber;
+    private boolean showFileName;
+    private boolean showDate;
+    private float marginTop;
+    private float marginBottom;
+    private float marginLeft;
+    private float marginRight;
+
+    public NDocPageOrientation getOrientation() {
+        return orientation;
+    }
+
+    public NTxDocumentStreamRendererConfig setOrientation(NDocPageOrientation orientation) {
+        this.orientation = orientation;
+        return this;
+    }
+
+    public int getGridX() {
+        return gridX;
+    }
+
+    public NTxDocumentStreamRendererConfig setGridX(int gridX) {
+        this.gridX = gridX;
+        return this;
+    }
+
+    public int getGridY() {
+        return gridY;
+    }
+
+    public NTxDocumentStreamRendererConfig setGridY(int gridY) {
+        this.gridY = gridY;
+        return this;
+    }
+
+    public int getPageWidth() {
+        return pageWidth;
+    }
+
+    public NTxDocumentStreamRendererConfig setPageWidth(int pageWidth) {
+        this.pageWidth = pageWidth;
+        return this;
+    }
+
+    public int getPageHeight() {
+        return pageHeight;
+    }
+
+    public NTxDocumentStreamRendererConfig setPageHeight(int pageHeight) {
+        this.pageHeight = pageHeight;
+        return this;
+    }
+
+    public boolean isShowPageNumber() {
+        return showPageNumber;
+    }
+
+    public NTxDocumentStreamRendererConfig setShowPageNumber(boolean showPageNumber) {
+        this.showPageNumber = showPageNumber;
+        return this;
+    }
+
+    public boolean isShowFileName() {
+        return showFileName;
+    }
+
+    public NTxDocumentStreamRendererConfig setShowFileName(boolean showFileName) {
+        this.showFileName = showFileName;
+        return this;
+    }
+
+    public boolean isShowDate() {
+        return showDate;
+    }
+
+    public NTxDocumentStreamRendererConfig setShowDate(boolean showDate) {
+        this.showDate = showDate;
+        return this;
+    }
+
+    public float getMarginTop() {
+        return marginTop;
+    }
+
+    public NTxDocumentStreamRendererConfig setMarginTop(float marginTop) {
+        this.marginTop = marginTop;
+        return this;
+    }
+
+    public float getMarginBottom() {
+        return marginBottom;
+    }
+
+    public NTxDocumentStreamRendererConfig setMarginBottom(float marginBottom) {
+        this.marginBottom = marginBottom;
+        return this;
+    }
+
+    public float getMarginLeft() {
+        return marginLeft;
+    }
+
+    public NTxDocumentStreamRendererConfig setMarginLeft(float marginLeft) {
+        this.marginLeft = marginLeft;
+        return this;
+    }
+
+    public float getMarginRight() {
+        return marginRight;
+    }
+
+    public NTxDocumentStreamRendererConfig setMarginRight(float marginRight) {
+        this.marginRight = marginRight;
+        return this;
+    }
+
+    public NTxDocumentStreamRendererConfig copy() {
+        try {
+            return (NTxDocumentStreamRendererConfig) clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
