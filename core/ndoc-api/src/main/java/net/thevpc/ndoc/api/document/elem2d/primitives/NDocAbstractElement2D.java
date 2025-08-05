f1f8516d74f8111463599eb562ff19a03c304c10
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:40:51 +0100

    Update NDocAbstractElement2D.java : rename ntx

--- core/ndoc-api/src/main/java/net/thevpc/ndoc/api/document/elem2d/primitives/NDocAbstractElement2D.java
+++
@@ -1,6 +0,0 @@
-package net.thevpc.ndoc.api.document.elem2d.primitives;
-
-import net.thevpc.ndoc.api.document.elem2d.NDocElement2D;
-
-public abstract class NDocAbstractElement2D implements NDocElement2D {
-}
