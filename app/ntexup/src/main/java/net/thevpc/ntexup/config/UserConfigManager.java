1546da71b4db02720a68d7707aa6ef7148b6f5fd
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:41:29 +0100

    Update UserConfigManager.java : rename ntx

---
+++ app/ntexup/src/main/java/net/thevpc/ntexup/config/UserConfigManager.java
@@ -0,0 +1,74 @@
+package net.thevpc.ntexup.config;
+
+import net.thevpc.nuts.*;
+import net.thevpc.nuts.elem.NElementParser;
+import net.thevpc.nuts.elem.NElementWriter;
+import net.thevpc.nuts.io.NPath;
+
+public class UserConfigManager {
+    private NPath userConfigFile;
+
+    public UserConfigManager() {
+        this.userConfigFile = NWorkspace.of().getStoreLocation(NId.of("net.thevpc.nuts:nuts"), NStoreType.CONF).resolve("user-config.tson");
+    }
+
+
+    public UserConfigs validate(UserConfigs config) {
+        return (config == null ? new UserConfigs() : config).validate();
+    }
+
+    public UserConfig validate(UserConfig config) {
+        return (config == null ? new UserConfig() : config).validate();
+    }
+
+    public UserConfigs updateUser(UserConfigs config, UserConfig user) {
+        if (user != null) {
+            validate(user);
+            validate(config);
+            config.updateUser(user);
+        }
+        return config;
+    }
+
+    public void saveUserConfig(UserConfig user) {
+        UserConfigs userConfigs = loadUserConfigs();
+        updateUser(userConfigs, user);
+        saveUserConfigs(userConfigs);
+        userConfigFile.mkParentDirs();
+        NElementWriter.ofTson().write(user,userConfigFile.mkParentDirs());
+    }
+
+    public void saveUserConfigs(UserConfigs config) {
+        config=validate(config);
+        NElementWriter.ofTson().write(config,userConfigFile.mkParentDirs());
+    }
+
+    public UserConfig loadUserConfig(String id) {
+        return loadUserConfigs().findUser(id);
+    }
+
+    public UserConfigs loadUserConfigs() {
+        UserConfigs config = null;
+        if (userConfigFile.isRegularFile()) {
+            config = NElementParser.ofTson().parse(userConfigFile, UserConfigs.class);
+        }
+        config=validate(config);
+        return config;
+    }
+
+    private int compare(Object a, Object b) {
+        if (a == null && b == null) {
+            return 0;
+        }
+        if (a == null && b != null) {
+            return -1;
+        }
+        if (a != null && b == null) {
+            return 1;
+        }
+        if (a instanceof Comparable) {
+            return ((Comparable) a).compareTo(b);
+        }
+        throw new IllegalArgumentException("invalid comparison");
+    }
+}
