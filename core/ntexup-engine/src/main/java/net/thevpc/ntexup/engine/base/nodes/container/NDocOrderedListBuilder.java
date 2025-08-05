ee621cae6fcc40de5fd647c86a829f6805871a07
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:08:13 +0100

    Update NDocOrderedListBuilder.java : rename ndoc to ntexup

--- core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/base/nodes/container/NDocOrderedListBuilder.java
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/base/nodes/container/NDocOrderedListBuilder.java
@@ -6,12 +6,12 @@ package net.thevpc.ntexup.engine.base.nodes.container;
 
 import net.thevpc.ntexup.api.document.NTxDocumentFactory;
 import net.thevpc.ntexup.api.document.node.NTxNode;
-import net.thevpc.ntexup.api.document.node.NDocNodeType;
+import net.thevpc.ntexup.api.document.node.NTxNodeType;
 import net.thevpc.ntexup.api.document.style.NTxProp;
-import net.thevpc.ntexup.api.document.style.NDocPropName;
-import net.thevpc.ntexup.api.document.style.NDocProperties;
+import net.thevpc.ntexup.api.document.style.NTxPropName;
+import net.thevpc.ntexup.api.document.style.NTxProperties;
 import net.thevpc.ntexup.api.extension.NDocNodeBuilder;
-import net.thevpc.ntexup.api.engine.NDocNodeCustomBuilderContext;
+import net.thevpc.ntexup.api.engine.NTxNodeCustomBuilderContext;
 import net.thevpc.ntexup.api.renderer.NDocNodeRendererContext;
 import net.thevpc.nuts.elem.NElement;
 
@@ -22,27 +22,27 @@ import java.util.List;
  * @author vpc
  */
 public class NDocOrderedListBuilder implements NDocNodeBuilder {
-    NDocProperties defaultStyles = new NDocProperties();
+    NTxProperties defaultStyles = new NTxProperties();
     @Override
-    public void build(NDocNodeCustomBuilderContext builderContext) {
-        builderContext.id(NDocNodeType.ORDERED_LIST)
+    public void build(NTxNodeCustomBuilderContext builderContext) {
+        builderContext.id(NTxNodeType.ORDERED_LIST)
                 .alias("ol")
                 .renderConvert(this::convert)
                 ;
     }
 
-    private NTxNode convert(NTxNode p, NDocNodeRendererContext ctx, NDocNodeCustomBuilderContext buildContext){
+    private NTxNode convert(NTxNode p, NDocNodeRendererContext ctx, NTxNodeCustomBuilderContext buildContext){
         ctx = ctx.withDefaultStyles(p, defaultStyles);
         NTxDocumentFactory f = ctx.documentFactory();
         List<NTxNode> all = new ArrayList<>();
         for (NTxNode child : p.children()) {
-            all.add(f.of(NDocNodeType.SPHERE).addStyleClasses("ol-bullet"));
+            all.add(f.of(NTxNodeType.SPHERE).addStyleClasses("ol-bullet"));
             all.add(child.addStyleClasses("ol-item"));
         }
         return f.ofGrid().addChildren(all.toArray(new NTxNode[0]))
-                .setProperty(NDocPropName.COLUMNS, NElement.ofInt(2))
-                .setProperty(NDocPropName.ROWS, NElement.ofInt(2))
-                .setProperty(NDocPropName.COLUMNS_WEIGHT, NElement.ofDoubleArray(1, 20))
+                .setProperty(NTxPropName.COLUMNS, NElement.ofInt(2))
+                .setProperty(NTxPropName.ROWS, NElement.ofInt(2))
+                .setProperty(NTxPropName.COLUMNS_WEIGHT, NElement.ofDoubleArray(1, 20))
                 .setProperties(p.props().toArray(new NTxProp[0]))
                 ;
     }
