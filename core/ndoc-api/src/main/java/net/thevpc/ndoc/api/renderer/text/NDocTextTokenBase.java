d2c93a542fc728e6db016886be3c164c047d391c
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:40:53 +0100

    Rename NTxTextTokenBase.java → NDocTextTokenBase.java : rename ntx

--- core/ndoc-api/src/main/java/net/thevpc/ndoc/api/renderer/text/NDocTextTokenBase.java
+++
@@ -1,9 +0,0 @@
-package net.thevpc.ndoc.api.renderer.text;
-
-public abstract class NDocTextTokenBase implements NDocTextToken {
-    private NDocTextOptions options = new NDocTextOptions();
-
-    public NDocTextOptions options() {
-        return options;
-    }
-}
