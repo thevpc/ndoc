a83b4643e6e5367c741021595f24476f3603d4ea
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:08:14 +0100

    Update ScoreBoardBuilder.java : rename ndoc to ntexup

--- extensions/ntexup-extension-presenters/src/main/java/net/thevpc/ntexup/extension/presenters/ScoreBoardBuilder.java
+++ extensions/ntexup-extension-presenters/src/main/java/net/thevpc/ntexup/extension/presenters/ScoreBoardBuilder.java
@@ -1,8 +1,8 @@
 package net.thevpc.ntexup.extension.presenters;
 
-import net.thevpc.ntexup.api.document.elem2d.NDocBounds2;
+import net.thevpc.ntexup.api.document.elem2d.NTxBounds2;
 import net.thevpc.ntexup.api.extension.NDocNodeBuilder;
-import net.thevpc.ntexup.api.engine.NDocNodeCustomBuilderContext;
+import net.thevpc.ntexup.api.engine.NTxNodeCustomBuilderContext;
 import net.thevpc.ntexup.api.document.node.NTxNode;
 import net.thevpc.ntexup.api.renderer.NDocGraphics;
 import net.thevpc.ntexup.api.renderer.NDocNodeRendererContext;
@@ -16,15 +16,15 @@ import java.awt.*;
 public class ScoreBoardBuilder implements NDocNodeBuilder {
 
     @Override
-    public void build(NDocNodeCustomBuilderContext builderContext) {
+    public void build(NTxNodeCustomBuilderContext builderContext) {
         builderContext.id("scoreboard")
                 .renderComponent(this::renderMain)
         ;
     }
 
 
-    public void renderMain(NTxNode p, NDocNodeRendererContext ctx, NDocNodeCustomBuilderContext builderContext) {
-        NDocBounds2 b = ctx.selfBounds(p, null, null);
+    public void renderMain(NTxNode p, NDocNodeRendererContext ctx, NTxNodeCustomBuilderContext builderContext) {
+        NTxBounds2 b = ctx.selfBounds(p, null, null);
 
         Paint color = ctx.getForegroundColor(p, true);
 
