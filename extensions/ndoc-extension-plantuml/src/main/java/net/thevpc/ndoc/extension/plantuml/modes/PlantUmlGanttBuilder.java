49a40417b5dfef9e6c2286a554b848b1463ff7cb
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:40:56 +0100

    Rename PlantUmlGanttBuilder.java → PlantUmlGanttBuilder.java : rename ntx

--- extensions/ndoc-extension-plantuml/src/main/java/net/thevpc/ndoc/extension/plantuml/modes/PlantUmlGanttBuilder.java
+++
@@ -1,9 +0,0 @@
-package net.thevpc.ndoc.extension.plantuml.modes;
-
-import net.thevpc.ndoc.extension.plantuml.PlantUmlBuilderBase;
-
-public class PlantUmlGanttBuilder extends PlantUmlBuilderBase {
-    public PlantUmlGanttBuilder() {
-        super("gantt");
-    }
-}
