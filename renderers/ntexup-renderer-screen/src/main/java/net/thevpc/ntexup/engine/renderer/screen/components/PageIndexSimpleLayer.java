e42c8cbde3dd9e28b183006a3801bbd69c5e336a
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:40:57 +0100

    Update PageIndexSimpleLayer.java : rename ntx

--- renderers/ntexup-renderer-screen/src/main/java/net/thevpc/ntexup/engine/renderer/screen/components/PageIndexSimpleLayer.java
+++ renderers/ntexup-renderer-screen/src/main/java/net/thevpc/ntexup/engine/renderer/screen/components/PageIndexSimpleLayer.java
@@ -2,13 +2,13 @@ package net.thevpc.ntexup.engine.renderer.screen.components;
 
 import net.thevpc.ntexup.api.document.elem2d.NTxAlign;
 import net.thevpc.ntexup.engine.renderer.screen.DocumentView;
-import net.thevpc.ntexup.api.renderer.NDocGraphics;
+import net.thevpc.ntexup.api.renderer.NTxGraphics;
 import net.thevpc.ntexup.engine.renderer.screen.PageView;
 
 import java.awt.*;
 
 public class PageIndexSimpleLayer extends SimpleLayer {
-    public void draw(DocumentView doc, PageView pageView, Dimension size, NDocGraphics g) {
+    public void draw(DocumentView doc, PageView pageView, Dimension size, NTxGraphics g) {
         g.setFont(new Font("Arial", 0, 20));
         drawStr(doc.getPageUserIndex() + "/" + doc.getPagesCount(), NTxAlign.RIGHT, size, g);
     }
