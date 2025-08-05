0e224c1130b43cc7e0382709ff36df60cf5a11d2
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:41:31 +0100

    Update PlantUmlEbnfBuilder.java : rename ntx

---
+++ extensions/ntexup-extension-plantuml/src/main/java/net/thevpc/ntexup/extension/plantuml/modes/PlantUmlEbnfBuilder.java
@@ -0,0 +1,9 @@
+package net.thevpc.ntexup.extension.plantuml.modes;
+
+import net.thevpc.ntexup.extension.plantuml.PlantUmlBuilderBase;
+
+public class PlantUmlEbnfBuilder extends PlantUmlBuilderBase {
+    public PlantUmlEbnfBuilder() {
+        super("ebnf");
+    }
+}
