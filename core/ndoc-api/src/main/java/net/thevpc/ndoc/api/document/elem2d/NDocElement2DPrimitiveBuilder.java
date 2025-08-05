84d9ac7a75be585859f5dbdec43b1ac447ae74b5
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:40:50 +0100

    Update NDocElement2DPrimitiveBuilder.java : rename ntx

--- core/ndoc-api/src/main/java/net/thevpc/ndoc/api/document/elem2d/NDocElement2DPrimitiveBuilder.java
+++
@@ -1,8 +0,0 @@
-package net.thevpc.ndoc.api.document.elem2d;
-
-import net.thevpc.ndoc.api.document.elem2d.NDocElement2DPrimitive;
-import net.thevpc.ndoc.api.document.elem2d.NDocElement2D;
-
-public interface NDocElement2DPrimitiveBuilder {
-    net.thevpc.ndoc.api.document.elem2d.NDocElement2DPrimitive[] toPrimitives(net.thevpc.ndoc.api.document.elem2d.NDocElement2D e);
-}
