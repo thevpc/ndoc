4b5b3c2c4df63b29f0e9f696c57f275047026749
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:08:14 +0100

    Update NTxHtmlDocumentRenderer.java : rename ndoc to ntexup

---
+++ renderers/ntexup-renderer-html/src/main/java/net/thevpc/ntexup/engine/renderer/html/NTxHtmlDocumentRenderer.java
@@ -0,0 +1,108 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package net.thevpc.ntexup.engine.renderer.html;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+import net.thevpc.ntexup.api.engine.NTxEngine;
+import net.thevpc.ntexup.api.document.NTxDocument;
+import net.thevpc.ntexup.api.document.node.NTxNodeType;
+import net.thevpc.ntexup.api.document.node.NTxNode;
+import net.thevpc.ntexup.api.renderer.NTxDocumentStreamRendererBase;
+import net.thevpc.ntexup.api.renderer.NTxDocumentRendererContext;
+import net.thevpc.ntexup.api.renderer.NTxDocumentRendererSupplier;
+import net.thevpc.ntexup.api.renderer.NTxDocumentStreamRenderer;
+import net.thevpc.nuts.io.NIOException;
+import net.thevpc.nuts.io.NPath;
+
+/**
+ * @author vpc
+ */
+public class NTxHtmlDocumentRenderer extends NTxDocumentStreamRendererBase implements NTxDocumentStreamRenderer {
+
+    private NTxDocumentRendererContext rendererContext = new NTxDocumentRendererContextImpl();
+
+    public NTxHtmlDocumentRenderer(NTxEngine engine) {
+        super(engine);
+    }
+
+    protected void renderStream(NTxDocument document, OutputStream os) {
+        document = engine.compileDocument(document).get();
+        PrintStream out = new PrintStream(os);
+        out.println("<html>");
+        out.println("<body>");
+        renderNode(document.root(), out);
+        out.println("</body>");
+        out.println("</html>");
+    }
+
+    @Override
+    public void renderSupplier(NTxDocumentRendererSupplier document) {
+        NTxDocument d = document.get(rendererContext);
+        Object o = output;
+        if (o == null) {
+            o = NPath.of("document.pdf");
+        }
+        if (o instanceof NPath) {
+            try (OutputStream os = ((NPath) o).getOutputStream()) {
+                renderStream(d, os);
+            } catch (IOException ex) {
+                throw new NIOException(ex);
+            }
+        } else if (o instanceof OutputStream) {
+            renderStream(d, (OutputStream) o);
+        }
+    }
+
+    public PrintStream psOf(OutputStream out) {
+        if (out instanceof PrintStream) {
+            return (PrintStream) out;
+        }
+        return new PrintStream(out);
+    }
+
+    @Override
+    public NTxDocumentStreamRenderer renderNode(NTxNode part, OutputStream out) {
+        switch (part.type()) {
+            case NTxNodeType.PAGE_GROUP:
+                break;
+            case NTxNodeType.PAGE: {
+                PrintStream o = psOf(out);
+                for (NTxNode pp : part.children()) {
+                    renderNode(pp, o);
+                }
+                o.flush();
+                break;
+            }
+            default:
+                throw new IllegalArgumentException("invalid type " + part);
+        }
+        return this;
+    }
+
+//    public void render(HDocumentPart part, PrintStream out) {
+//        switch (part.type()) {
+//            case PAGE_GROUP:
+//                break;
+//            case PAGE:
+//                break;
+//            case PARAGRAPH:
+//                break;
+//            case PHRASE:
+//                break;
+//            default:
+//                throw new IllegalArgumentException("invalid type " + part);
+//        }
+//    }
+
+    private class NTxDocumentRendererContextImpl implements NTxDocumentRendererContext {
+
+        public NTxDocumentRendererContextImpl() {
+        }
+    }
+
+}
