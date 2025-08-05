855d981c672046c531f3df6d3cdef8b20b55d41b
thevpc <taha.bensalah@gmail.com>
2025-08-05 10:00:32 +0100

    Update extensions/ndoc-extension-plot2d/src/main/java/net/thevpc/ndoc/extension/plot2d/GlobalPlotData.java

---
+++ extensions/ndoc-extension-plot2d/src/main/java/net/thevpc/ndoc/extension/plot2d/GlobalPlotData.java
@@ -0,0 +1,18 @@
+package net.thevpc.ndoc.extension.plot2d;
+
+import java.awt.*;
+
+public class GlobalPlotData {
+    double xmin;
+    double xmax;
+    Double ymin;
+    Double ymax;
+    double majorGridSpacing;
+    boolean showMajorGrid;
+    Color majorGridColor;
+    Stroke majorGridStroke;
+    double minorGridSpacing;
+    boolean showMinorGrid;
+    Color minorGridColor;
+    Stroke minorGridStroke;
+}
