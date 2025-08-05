634322e3f67af22964adbd1c7dc1181a8637a138
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:08:13 +0100

    Update NDocHexagonBuilder.java : rename ndoc to ntexup

--- core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/base/nodes/shape/NDocHexagonBuilder.java
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/base/nodes/shape/NDocHexagonBuilder.java
@@ -1,22 +1,22 @@
 package net.thevpc.ntexup.engine.base.nodes.shape;
 
 import net.thevpc.ntexup.api.document.node.NTxNode;
-import net.thevpc.ntexup.api.document.node.NDocNodeType;
-import net.thevpc.ntexup.api.document.style.NDocProperties;
+import net.thevpc.ntexup.api.document.node.NTxNodeType;
+import net.thevpc.ntexup.api.document.style.NTxProperties;
 import net.thevpc.ntexup.api.extension.NDocNodeBuilder;
-import net.thevpc.ntexup.api.engine.NDocNodeCustomBuilderContext;
+import net.thevpc.ntexup.api.engine.NTxNodeCustomBuilderContext;
 import net.thevpc.ntexup.api.renderer.NDocNodeRendererContext;
 
 public class NDocHexagonBuilder implements NDocNodeBuilder {
-    NDocProperties defaultStyles = new NDocProperties();
+    NTxProperties defaultStyles = new NTxProperties();
 
     @Override
-    public void build(NDocNodeCustomBuilderContext builderContext) {
-        builderContext.id(NDocNodeType.HEXAGON)
+    public void build(NTxNodeCustomBuilderContext builderContext) {
+        builderContext.id(NTxNodeType.HEXAGON)
                 .renderComponent(this::render);
     }
 
-    public void render(NTxNode p, NDocNodeRendererContext renderContext, NDocNodeCustomBuilderContext buildContext) {
+    public void render(NTxNode p, NDocNodeRendererContext renderContext, NTxNodeCustomBuilderContext buildContext) {
         NDocPolygonHelper.renderPointsCount(6, p, renderContext, defaultStyles);
     }
 
