d7a91fbf26566dfacceec951f99a56a238653416
thevpc <taha.bensalah@gmail.com>
2025-08-05 13:55:25 +0100

    Update NTxFunctionRotateHueColor.java : rename ndoc to ntexup

---
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/base/functions/colors/NTxFunctionRotateHueColor.java
@@ -0,0 +1,35 @@
+package net.thevpc.ntexup.engine.base.functions.colors;
+
+import net.thevpc.ntexup.api.extension.NTxFunction;
+import net.thevpc.ntexup.api.eval.NDocFunctionArgs;
+import net.thevpc.ntexup.api.eval.NDocFunctionContext;
+import net.thevpc.ntexup.api.eval.NDocValue;
+import net.thevpc.ntexup.engine.util.NDocColorUtils;
+import net.thevpc.ntexup.engine.util.NDocElementUtils;
+import net.thevpc.nuts.elem.NElement;
+import net.thevpc.nuts.util.NMsg;
+
+import java.awt.*;
+
+public class NTxFunctionRotateHueColor implements NTxFunction {
+    @Override
+    public String name() {
+        return "rotateHueColor";
+    }
+
+    @Override
+    public NElement invoke(NDocFunctionArgs args, NDocFunctionContext context) {
+        if (args.size() == 0) {
+            return NElement.ofNull();
+        }
+        if (args.size() == 1) {
+            return args.eval(0);
+        }
+        if (args.size() > 2) {
+            context.messages().log(NMsg.ofC("%s: expected 2 arguments, got %s", name(), args.size()));
+        }
+        Color c = NDocValue.of(args.eval(0)).asColor().get();
+        float degrees = NDocValue.of(args.eval(1)).asFloat().get();
+        return NDocElementUtils.toElement(NDocColorUtils.rotateHue(c, degrees));
+    }
+}
