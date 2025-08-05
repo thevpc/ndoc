811c64b891ea4c007c87a6bf85b139793744720c
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:08:13 +0100

    Update CtrlNTxNodeName.java : rename ndoc to ntexup

---
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/parser/ctrlnodes/CtrlNTxNodeName.java
@@ -0,0 +1,45 @@
+package net.thevpc.ntexup.engine.parser.ctrlnodes;
+
+import net.thevpc.ntexup.api.document.node.NTxNode;
+import net.thevpc.ntexup.api.document.node.NTxNodeType;
+import net.thevpc.ntexup.api.source.NDocResource;
+import net.thevpc.nuts.elem.NElement;
+
+public class CtrlNTxNodeName extends CtrlNTxNodeBase {
+    private NElement __varName;
+
+    private CtrlNTxNodeName(NDocResource source) {
+        super(NTxNodeType.CTRL_NAME,source);
+    }
+
+    public CtrlNTxNodeName(NDocResource source, NElement __varName) {
+        super(NTxNodeType.CTRL_NAME,source);
+        this.__varName=__varName;
+    }
+
+    public NElement getVarName() {
+        return __varName;
+    }
+
+    @Override
+    public NTxNode copy() {
+        CtrlNTxNodeName c = new CtrlNTxNodeName(source());
+        copyTo(c);
+        return c;
+    }
+
+    @Override
+    public NTxNode copyTo(NTxNode other) {
+        super.copyTo(other);
+        if (other instanceof CtrlNTxNodeName) {
+            CtrlNTxNodeName oc = (CtrlNTxNodeName) other;
+            oc.__varName = __varName;
+        }
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "Name("+__varName+')';
+    }
+}
