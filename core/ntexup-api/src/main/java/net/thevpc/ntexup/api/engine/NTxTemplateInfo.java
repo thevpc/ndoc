59782fce6703e4c3d109756e95c93a53bb2d9ffe
thevpc <taha.bensalah@gmail.com>
2025-08-05 14:08:12 +0100

    Update NTxTemplateInfo.java : rename ndoc to ntexup

---
+++ core/ntexup-api/src/main/java/net/thevpc/ntexup/api/engine/NTxTemplateInfo.java
@@ -0,0 +1,15 @@
+package net.thevpc.ntexup.api.engine;
+
+public interface NTxTemplateInfo {
+    String repoUrl();
+
+    String repoName();
+
+    String name();
+
+    String localPath();
+
+    String url();
+
+    boolean recommended();
+}
