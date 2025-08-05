8d142842637e648e748ad49857575027f44b906c
thevpc <taha.bensalah@gmail.com>
2025-08-05 13:55:26 +0100

    Update DefaultNDocFunctionContext.java : rename ndoc to ntexup

---
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/eval/DefaultNDocFunctionContext.java
@@ -0,0 +1,44 @@
+package net.thevpc.ntexup.engine.eval;
+
+import net.thevpc.ntexup.api.document.node.NTxNode;
+import net.thevpc.ntexup.api.engine.NDocEngine;
+import net.thevpc.ntexup.api.eval.NDocVar;
+import net.thevpc.ntexup.api.eval.NDocVarProvider;
+import net.thevpc.ntexup.api.log.NDocLogger;
+import net.thevpc.ntexup.api.eval.NDocFunctionContext;
+import net.thevpc.nuts.elem.NElement;
+import net.thevpc.nuts.util.NOptional;
+
+public class DefaultNDocFunctionContext implements NDocFunctionContext {
+    private NDocEngine engine;
+    private NTxNode node;
+    private NDocVarProvider varProvider;
+
+    public DefaultNDocFunctionContext(NDocEngine engine, NTxNode node, NDocVarProvider varProvider) {
+        this.engine = engine;
+        this.node = node;
+        this.varProvider = varProvider;
+    }
+
+    public NElement eval(NElement expr) {
+        return engine.evalExpression(expr, node, varProvider);
+    }
+
+    @Override
+    public NOptional<NDocVar> findVar(String varName) {
+        return engine.findVar(varName, node, varProvider);
+    }
+
+    @Override
+    public NTxNode node() {
+        return node;
+    }
+
+    public NDocEngine engine() {
+        return engine;
+    }
+
+    public NDocLogger messages() {
+        return engine.log();
+    }
+}
