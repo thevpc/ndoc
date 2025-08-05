f66e608ab2b48091ecf0237825fe237c21d7d049
thevpc <taha.bensalah@gmail.com>
2025-08-05 13:55:25 +0100

    Update NDocRowBuilder.java : rename ndoc to ntexup

---
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/base/nodes/container/NDocRowBuilder.java
@@ -0,0 +1,88 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package net.thevpc.ntexup.engine.base.nodes.container;
+
+import net.thevpc.ntexup.api.document.elem2d.NDocBounds2;
+import net.thevpc.ntexup.api.document.node.NTxNode;
+import net.thevpc.ntexup.api.document.node.NDocNodeType;
+import net.thevpc.ntexup.api.document.style.NTxProp;
+import net.thevpc.ntexup.api.document.style.NDocPropName;
+import net.thevpc.ntexup.api.document.style.NDocProperties;
+import net.thevpc.ntexup.api.extension.NDocNodeBuilder;
+import net.thevpc.ntexup.api.engine.NDocNodeCustomBuilderContext;
+import net.thevpc.ntexup.api.renderer.NDocNodeRendererContext;
+import net.thevpc.nuts.elem.NElement;
+
+/**
+ * @author vpc
+ */
+public class NDocRowBuilder implements NDocNodeBuilder {
+    NDocProperties defaultStyles = new NDocProperties();
+
+    @Override
+    public void build(NDocNodeCustomBuilderContext builderContext) {
+        builderContext.id(NDocNodeType.ROW)
+                .parseParam().named(NDocPropName.COLUMNS).then()
+                .parseParam((info, buildContext) -> {
+                    NElement p = info.peek();
+                    if(p.isInt()) {
+                        info.read();
+                        info.node().setProperty(NTxProp.of(NDocPropName.COLUMNS,p));
+                        return true;
+                    }else{
+                        return false;
+                    }
+                })
+                .selfBounds(this::selfBounds)
+                .renderComponent(this::renderMain)
+                ;
+    }
+
+
+    public NDocBounds2 selfBounds(NTxNode p, NDocNodeRendererContext ctx, NDocNodeCustomBuilderContext builderContext) {
+        ctx = ctx.withDefaultStyles(p, defaultStyles);
+        NDocBounds2 expectedBounds = ctx.defaultSelfBounds(p);
+//        HGraphics g = ctx.graphics();
+//        g.setColor(Color.RED);
+//        g.drawRect(expectedBounds);
+        NDocGridRendererHelper h = new NDocGridRendererHelper(p.children());
+        return h.computeBound(p, ctx, expectedBounds);
+    }
+
+    public void renderMain(NTxNode p, NDocNodeRendererContext ctx, NDocNodeCustomBuilderContext builderContext) {
+        ctx = ctx.withDefaultStyles(p, defaultStyles);
+        NDocBounds2 expectedBounds = ctx.selfBounds(p);
+        NDocGridRendererHelper h = new NDocGridRendererHelper(p.children());
+        h.render(p, ctx, expectedBounds);
+    }
+
+
+//    @Override
+//    protected void processImplicitStyles(NDocArgumentParseInfo info) {
+//        switch (info.getUid()) {
+//            case "vgrid":
+//            case "column": {
+//                if(info.node().getProperty(NDocPropName.COLUMNS).isNotPresent()) {
+//                    info.node().setProperty(NDocPropName.COLUMNS, NElement.ofInt(1));
+//                }
+//                if(info.node().getProperty(NDocPropName.ROWS).isNotPresent()) {
+//                    info.node().setProperty(NDocPropName.ROWS, NElement.ofInt(-1));
+//                }
+//                break;
+//            }
+//            case "hgrid":
+//            case "row": {
+//                if(info.node().getProperty(NDocPropName.COLUMNS).isNotPresent()) {
+//                    info.node().setProperty(NDocPropName.COLUMNS, NElement.ofInt(-1));
+//                }
+//                if(info.node().getProperty(NDocPropName.ROWS).isNotPresent()) {
+//                    info.node().setProperty(NDocPropName.ROWS, NElement.ofInt(1));
+//                }
+//                break;
+//            }
+//        }
+//    }
+
+}
