7872755f855ca4f4cc840960607e26c50f6e6a1d
thevpc <taha.bensalah@gmail.com>
2025-08-06 19:24:30 +0100

    Update NTxNtfBuilder.java : fixing flavor issue

--- core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/base/nodes/text/NTxNtfBuilder.java
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/base/nodes/text/NTxNtfBuilder.java
@@ -19,7 +19,7 @@ public class NTxNtfBuilder implements NTxNodeBuilder {
                 .parseParam().named(NTxPropName.FILE).store(NTxPropName.VALUE).resolvedAsTrimmedPathTextContent().then()
                 .parseDefaultParams()
                 .parseParam().matchesStringOrName().store(NTxPropName.VALUE).resolvedAsTrimmedBloc().then()
-                .renderText(this::renderTextBuildText)
+                .renderText().buildText(this::renderTextBuildText)
         ;
     }
 
