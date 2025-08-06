643d00514f66bd40e0e0f4bffa3c7341725fb75b
thevpc <taha.bensalah@gmail.com>
2025-08-06 19:24:30 +0100

    Update NTxTextRendererFlavor.java : fixing flavor issue

--- core/ntexup-api/src/main/java/net/thevpc/ntexup/api/renderer/text/NTxTextRendererFlavor.java
+++ core/ntexup-api/src/main/java/net/thevpc/ntexup/api/renderer/text/NTxTextRendererFlavor.java
@@ -2,12 +2,14 @@ package net.thevpc.ntexup.api.renderer.text;
 
 import net.thevpc.ntexup.api.document.node.NTxNode;
 import net.thevpc.ntexup.api.renderer.NTxNodeRendererContext;
-import net.thevpc.nuts.reserved.util.NReservedSimpleCharQueue;
 
 import java.util.List;
 
 public interface NTxTextRendererFlavor {
     String type();
-    void buildText(String text, NTxTextOptions options, NTxNode p, NTxNodeRendererContext ctx, NTxTextRendererBuilder builder) ;
-    List<NTxTextToken> parseImmediate(NReservedSimpleCharQueue queue, NTxNodeRendererContext ctx);
+
+    void buildText(String text, NTxTextOptions options, NTxNode p, NTxNodeRendererContext ctx, NTxTextRendererBuilder builder);
+
+    List<String> getParsePrefixes();
+    List<NTxTextToken> parseTokens(NTxTextRendererFlavorParseContext ctx);
 }
