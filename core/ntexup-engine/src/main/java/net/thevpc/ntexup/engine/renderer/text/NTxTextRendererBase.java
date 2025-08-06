45186d3359082bfa9f7e1c4fba06333cf8b3f249
thevpc <taha.bensalah@gmail.com>
2025-08-06 23:00:34 +0100

    Update NTxTextRendererBase.java : fixing flavor issue

--- core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/renderer/text/NTxTextRendererBase.java
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/renderer/text/NTxTextRendererBase.java
@@ -8,6 +8,8 @@ import net.thevpc.ntexup.api.renderer.NTxNodeRendererContext;
 import net.thevpc.ntexup.api.renderer.text.NTxTextRendererFlavor;
 import net.thevpc.ntexup.api.renderer.text.NTxTextOptions;
 import net.thevpc.ntexup.api.renderer.text.NTxTextRendererBuilder;
+import net.thevpc.ntexup.api.util.NtxFontInfo;
+import net.thevpc.nuts.util.NBlankable;
 import net.thevpc.nuts.util.NMsg;
 
 import java.awt.*;
@@ -17,19 +19,23 @@ public class NTxTextRendererBase extends NTxTextBaseRenderer {
 
     public NTxTextRendererBase(String type, String flavor) {
         super(type);
-        this.flavor=flavor;
+        this.flavor = flavor;
     }
 
     public NTxTextRendererBuilder createRichTextHelper(NTxNode p, NTxNodeRendererContext ctx) {
         NTxTextRendererFlavor f = ctx.engine().textRendererFlavor(flavor).orNull();
-        if(f==null){
-            ctx.engine().log().log(NMsg.ofC("TextRendererFlavor not found %s",flavor));
-            f=ctx.engine().textRendererFlavor("text").get();
+        if (f == null) {
+            ctx.engine().log().log(NMsg.ofC("TextRendererFlavor not found %s", flavor));
+            f = ctx.engine().textRendererFlavor("text").get();
         }
         ctx = ctx.withDefaultStyles(p, defaultStyles);
         String text = NTxValue.of(p.getPropertyValue(NTxPropName.VALUE).orNull()).asStringOrName().orElse("");
-        Paint fg = NTxValueByName.getForegroundColor(p, ctx,true);
-        NTxTextRendererBuilderImpl builder = new NTxTextRendererBuilderImpl(ctx.engine(),fg);
+        Paint fg = NTxValueByName.getForegroundColor(p, ctx, true);
+        NtxFontInfo font = NTxValueByName.getFontInfo(p, ctx);
+        if (font.baseFont == null && NBlankable.isBlank(font.family)) {
+            font.baseFont = ctx.graphics().getFont();
+        }
+        NTxTextRendererBuilderImpl builder = new NTxTextRendererBuilderImpl(ctx.engine(), fg, font);
         f.buildText(text, new NTxTextOptions(), p, ctx, builder);
         return builder;
     }
