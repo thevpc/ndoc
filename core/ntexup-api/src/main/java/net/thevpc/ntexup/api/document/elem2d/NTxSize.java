2467ad09c89400e70b2d725add68a2803c6f11d6
thevpc <taha.bensalah@gmail.com>
2025-08-06 23:00:34 +0100

    Update NTxSize.java : fixing flavor issue

--- core/ntexup-api/src/main/java/net/thevpc/ntexup/api/document/elem2d/NTxSize.java
+++ core/ntexup-api/src/main/java/net/thevpc/ntexup/api/document/elem2d/NTxSize.java
@@ -5,31 +5,165 @@
 package net.thevpc.ntexup.api.document.elem2d;
 
 import net.thevpc.nuts.elem.NElement;
+import net.thevpc.nuts.elem.NNumberElement;
 import net.thevpc.nuts.elem.NToElement;
+import net.thevpc.nuts.elem.NUpletElement;
+
+import java.util.Objects;
 
 /**
  * @author vpc
  */
 public class NTxSize implements NToElement {
 
-    private double value;
-    private boolean percent;
+    Type type;
+    double value;
+    double value2;
 
-    public static NTxSize ofPercent(double value) {
-        return new NTxSize(value, true);
+    public enum Type {
+        PARENT,
+        PAGE,
+        BOUNDS,
+        PX,
+        REM
     }
 
-    public static NTxSize of(double value) {
-        return new NTxSize(value, false);
+    public static NTxSize ofElement(NElement b) {
+        if(b==null) {
+            return null;
+        }
+        if (b.isNumber()) {
+            Number n = b.asNumberValue().get();
+            String u = ((NNumberElement) b).numberSuffix();
+            if (u == null) {
+                u = "";
+            } else {
+                u = u.toLowerCase().trim();
+            }
+            switch (u) {
+                case "%p":
+                case "%P": {
+                    return ofPage(n.doubleValue());
+                }
+                case "px": {
+                    return ofPx(n.doubleValue());
+                }
+                case "rem": {
+                    return ofRem(n.doubleValue());
+                }
+                case "":
+                case "%":
+                default: {
+                    return ofParent(n.doubleValue());
+                }
+            }
+        } else if(b.isUplet()){
+            NUpletElement u = b.asUplet().get();
+            return ofBounds(
+                    u.get(0).get().asNumberValue().get().doubleValue(),
+                    u.get(1).get().asNumberValue().get().doubleValue()
+            );
+        }
+        throw new IllegalStateException("not a size");
     }
 
-    public NTxSize(double value, boolean percent) {
+    public static NTxSize ofParent(double percent) {
+        return new NTxSize(Type.PARENT, percent, 0);
+    }
+
+    public static NTxSize ofPage(double percent) {
+        return new NTxSize(Type.PAGE, percent, 0);
+    }
+
+    public static NTxSize ofPx(double value) {
+        return new NTxSize(Type.PX, value, 0);
+    }
+
+    public static NTxSize ofRem(double value) {
+        return new NTxSize(Type.REM, value, 0);
+    }
+
+    public static NTxSize ofBounds(double width, double height) {
+        return new NTxSize(Type.PAGE, width, height);
+    }
+
+    public static NTxSize ofJava(double size) {
+        return new NTxSize(Type.PAGE, size, 0);
+    }
+
+    private NTxSize(Type type, double value, double value2) {
+        this.type = type;
         this.value = value;
-        this.percent = percent;
+        this.value2 = value2;
+    }
+
+    public Type type() {
+        return type;
+    }
+
+    public double value() {
+        return value;
+    }
+    public double percent() {
+        switch (type) {
+            case PARENT:
+            case PAGE:
+                return value;
+        }
+        throw new IllegalStateException("not a percent");
+    }
+
+    public double size() {
+        switch (type) {
+            case PX:
+            case REM:
+                return value;
+        }
+        throw new IllegalStateException("not a size");
+    }
+
+    public double width() {
+        switch (type) {
+            case BOUNDS:
+                return value;
+        }
+        throw new IllegalStateException("not a width");
+    }
+
+    public double height() {
+        switch (type) {
+            case BOUNDS:
+                return value2;
+        }
+        throw new IllegalStateException("not a height");
     }
 
     @Override
     public NElement toElement() {
-        return NElement.ofNumber(value + "%");
+        switch (type) {
+            case PARENT:
+                return NElement.ofNumber(value + "%");
+            case PAGE:
+                return NElement.ofNumber(value + "%P");
+            case REM:
+                return NElement.ofNumber(value + "rem");
+            case BOUNDS:
+                return NElement.ofUplet(NElement.ofDouble(value), NElement.ofDouble(value2));
+            case PX:
+                return NElement.ofDouble(value, "px");
+        }
+        throw new IllegalStateException("not a size");
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object == null || getClass() != object.getClass()) return false;
+        NTxSize nTxSize = (NTxSize) object;
+        return Double.compare(value, nTxSize.value) == 0 && Double.compare(value2, nTxSize.value2) == 0 && type == nTxSize.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(type, value, value2);
     }
 }
