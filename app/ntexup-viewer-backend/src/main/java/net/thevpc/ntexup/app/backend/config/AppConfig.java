0e3c04af8287a54119a99aa5899e6e0c819c7c57
thevpc <taha.bensalah@gmail.com>
2025-08-08 23:36:29 +0100

    Update AppConfig.java : update docs

--- app/ntexup-viewer-backend/src/main/java/net/thevpc/ntexup/app/backend/config/AppConfig.java
+++ app/ntexup-viewer-backend/src/main/java/net/thevpc/ntexup/app/backend/config/AppConfig.java
@@ -1,7 +1,7 @@
 package net.thevpc.ntexup.app.backend.config;
 
 import net.thevpc.ntexup.api.engine.NTxEngine;
-import net.thevpc.ntexup.engine.DefaultNTxEngine;
+import net.thevpc.ntexup.engine.impl.DefaultNTxEngine;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
