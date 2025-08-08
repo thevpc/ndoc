0b30d92c86179ce72600e7624f41d0adf35b9766
thevpc <taha.bensalah@gmail.com>
2025-08-08 23:36:30 +0100

    Update NTxExampleFromFile2.java : update docs

--- test/ntexup-examples/src/main/java/net/thevpc/ntexup/examples/NTxExampleFromFile2.java
+++ test/ntexup-examples/src/main/java/net/thevpc/ntexup/examples/NTxExampleFromFile2.java
@@ -6,7 +6,7 @@ package net.thevpc.ntexup.examples;
 
 import net.thevpc.ntexup.api.engine.NTxEngine;
 import net.thevpc.ntexup.api.document.NTxDocument;
-import net.thevpc.ntexup.engine.DefaultNTxEngine;
+import net.thevpc.ntexup.engine.impl.DefaultNTxEngine;
 import net.thevpc.nuts.Nuts;
 import net.thevpc.nuts.io.NPath;
 
