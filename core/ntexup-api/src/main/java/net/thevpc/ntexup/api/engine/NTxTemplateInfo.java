ff69ddceeed7c86e753e06dc6003450a572f6cca
thevpc <taha.bensalah@gmail.com>
2025-08-08 23:36:29 +0100

    Update NTxTemplateInfo.java : update docs

--- core/ntexup-api/src/main/java/net/thevpc/ntexup/api/engine/NTxTemplateInfo.java
+++ core/ntexup-api/src/main/java/net/thevpc/ntexup/api/engine/NTxTemplateInfo.java
@@ -1,6 +1,24 @@
 package net.thevpc.ntexup.api.engine;
 
+import net.thevpc.nuts.elem.NElement;
+
+import java.util.List;
+
 public interface NTxTemplateInfo {
+    NTxTemplateInfo withName(String name);
+
+    NTxTemplateInfo withRecommended(boolean recommended);
+
+    NTxTemplateInfo withBinaryVersions(String[] binaryVersions);
+
+    NTxTemplateInfo withLocalPath(String localPath);
+
+    NTxTemplateInfo withUrl(String url);
+
+    NTxTemplateInfo withRepoUrl(String repoUrl);
+
+    NTxTemplateInfo withRepoName(String repoName);
+
     String repoUrl();
 
     String repoName();
@@ -11,5 +29,7 @@ public interface NTxTemplateInfo {
 
     String url();
 
+    List<String> binaryVersions();
     boolean recommended();
+    NElement toElement();
 }
