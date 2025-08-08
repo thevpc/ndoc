dc4a4a2be2453909238d1596baa3994c81d06713
thevpc <taha.bensalah@gmail.com>
2025-08-08 23:36:30 +0100

    Update AppConfig.java : update docs

--- renderers/ntexup-renderer-web/src/main/java/net/thevpc/ntexup/engine/render/server/AppConfig.java
+++ renderers/ntexup-renderer-web/src/main/java/net/thevpc/ntexup/engine/render/server/AppConfig.java
@@ -1,7 +1,7 @@
 package net.thevpc.ntexup.engine.render.server;
 
 import net.thevpc.ntexup.api.engine.NTxEngine;
-import net.thevpc.ntexup.engine.DefaultNTxEngine;
+import net.thevpc.ntexup.engine.impl.DefaultNTxEngine;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
