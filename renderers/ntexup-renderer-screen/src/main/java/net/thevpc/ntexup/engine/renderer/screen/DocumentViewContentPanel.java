91d27e2162f1c3aab728d3c3f5d8ff17aea8b908
thevpc <taha.bensalah@gmail.com>
2025-08-09 01:08:16 +0100

    Update DocumentViewContentPanel.java : update rendering

--- renderers/ntexup-renderer-screen/src/main/java/net/thevpc/ntexup/engine/renderer/screen/DocumentViewContentPanel.java
+++ renderers/ntexup-renderer-screen/src/main/java/net/thevpc/ntexup/engine/renderer/screen/DocumentViewContentPanel.java
@@ -32,9 +32,15 @@ public class DocumentViewContentPanel extends JPanel {
 
         Graphics2D g2d = (Graphics2D) g;
         NTxGraphics hg = documentView.engine.createGraphics(g2d);
-        g2d.setRenderingHint(
-                RenderingHints.KEY_ANTIALIASING,
-                RenderingHints.VALUE_ANTIALIAS_ON);
+
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+        g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+        g2d.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
+        g2d.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY);
+        g2d.setRenderingHint(RenderingHints.KEY_DITHERING, RenderingHints.VALUE_DITHER_ENABLE);
+        g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+
         Dimension size = getSize();
         for (NTxDocumentLayer filter : layers) {
             filter.draw(documentView, documentView.currentShowingPage, size, hg);
