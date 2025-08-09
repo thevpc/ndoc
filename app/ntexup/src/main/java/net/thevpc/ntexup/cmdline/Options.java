cb798729b928411d3e2ac65c73d57b8e98ac68f5
thevpc <taha.bensalah@gmail.com>
2025-08-09 01:08:15 +0100

    Update Options.java : update rendering

--- app/ntexup/src/main/java/net/thevpc/ntexup/cmdline/Options.java
+++ app/ntexup/src/main/java/net/thevpc/ntexup/cmdline/Options.java
@@ -3,17 +3,23 @@ package net.thevpc.ntexup.cmdline;
 import net.thevpc.nuts.io.NPath;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Options {
     public List<NPath> paths = new ArrayList<>();
-    public NPath toPdf;
-    public NPath buildRepoPath;
     public Action action = Action.OPEN;
+    public OutputFormat outputFormat = OutputFormat.PDF;
+    public NPath output;
+    public boolean documentation;
+    public boolean dump;
+    public String templateUrl;
     public boolean viewer;
     public boolean console;
     public boolean showLogs;
     public boolean reopen;
+    public Map<String,String> vars=new LinkedHashMap<>();
 
     public Options requireViewer() {
         this.viewer = true;
