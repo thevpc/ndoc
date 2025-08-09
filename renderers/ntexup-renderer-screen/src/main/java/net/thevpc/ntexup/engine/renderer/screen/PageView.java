f13282aa02f818bcb7b34de4e8d30bbaf8e7c571
thevpc <taha.bensalah@gmail.com>
2025-08-09 01:08:16 +0100

    Update PageView.java : update rendering

--- renderers/ntexup-renderer-screen/src/main/java/net/thevpc/ntexup/engine/renderer/screen/PageView.java
+++ renderers/ntexup-renderer-screen/src/main/java/net/thevpc/ntexup/engine/renderer/screen/PageView.java
@@ -72,9 +72,14 @@ public class PageView extends JComponent {
         super.paintComponent(g);
         Dimension size = getSize();
         Graphics2D g2d = (Graphics2D) g;
-        g2d.setRenderingHint(
-                RenderingHints.KEY_ANTIALIASING,
-                RenderingHints.VALUE_ANTIALIAS_ON);
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+        g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+        g2d.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
+        g2d.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY);
+        g2d.setRenderingHint(RenderingHints.KEY_DITHERING, RenderingHints.VALUE_DITHER_ENABLE);
+        g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+
         NTxNodeRendererContext ctx = new ScreenNTxPartRendererContext(this,
                 engine.createGraphics(g2d)
                 , size, pageStartTime);
