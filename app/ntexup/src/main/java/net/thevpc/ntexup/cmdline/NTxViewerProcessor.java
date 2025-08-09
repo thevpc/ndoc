34bc4aa5d999b2d12aa9696c6ef41e5fcbf8167b
thevpc <taha.bensalah@gmail.com>
2025-08-09 01:08:15 +0100

    Update NTxViewerProcessor.java : update rendering

--- app/ntexup/src/main/java/net/thevpc/ntexup/cmdline/NTxViewerProcessor.java
+++ app/ntexup/src/main/java/net/thevpc/ntexup/cmdline/NTxViewerProcessor.java
@@ -2,23 +2,36 @@ package net.thevpc.ntexup.cmdline;
 
 import com.formdev.flatlaf.FlatLightLaf;
 import net.thevpc.ntexup.main.MainFrame;
+import net.thevpc.nuts.NSession;
+import net.thevpc.nuts.cmdline.NCmdLine;
 import net.thevpc.nuts.io.NPath;
 import net.thevpc.nuts.nswing.NSwingUtils;
 
 import javax.swing.*;
 
 public class NTxViewerProcessor {
-    public void runViewer(Options options) {
+    public void runViewer(NCmdLine cmdLine,Options options) {
+        while (!cmdLine.isEmpty()) {
+            cmdLine.matcher()
+                    .with("--reopen").matchTrueFlag(a -> options.reopen = true)
+                    .with("--list-templates").matchFlag(a -> options.action = net.thevpc.ntexup.cmdline.Action.LIST_TEMPLATES)
+                    .with("--documentation").matchFlag(a -> options.documentation = true)
+                    .with("--open").matchEntry(a -> options.paths.add(NPath.of(a.stringValue())))
+                    .with("--new").matchTrueFlag(a -> options.action = Action.NEW)
+                    .with("--view").matchTrueFlag(a -> options.requireViewer())
+                    .with("--view-log").matchTrueFlag(a -> options.requireViewer().showLogs = true)
+                    .withNonOption().matchAny(a -> options.paths.add(NPath.of(a.image())))
+                    .requireWithDefault();
+        }
+        if (options.documentation) {
+            options.paths.add(NPath.of("github://thevpc/ntexup-doc-slides/"));
+        }
         NSwingUtils.setSharedWorkspaceInstance();
         FlatLightLaf.setup(new com.formdev.flatlaf.FlatDarculaLaf());
         MainFrame mainFrame = new MainFrame();
         SwingUtilities.invokeLater(() -> {
             mainFrame.setVisible(true);
-            if (options.action==Action.DUMP) {
-
-            }else if (options.action==Action.DOC) {
-                options.paths.add(NPath.of("github://thevpc/ntexup-doc-slides/"));
-            }else if (options.reopen) {
+            if (options.reopen) {
                 NPath p = mainFrame.getService().getLatestProjectPath();
                 if (p != null) {
                     options.paths.add(p);
