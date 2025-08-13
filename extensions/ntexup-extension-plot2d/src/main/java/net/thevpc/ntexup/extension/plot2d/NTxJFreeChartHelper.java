98d3a14a269cab5305e4f9d53b8e8a7b23c35474
thevpc <taha.bensalah@gmail.com>
2025-08-13 15:47:30 +0100

    Update NTxJFreeChartHelper.java : add jfreechar support

---
+++ extensions/ntexup-extension-plot2d/src/main/java/net/thevpc/ntexup/extension/plot2d/NTxJFreeChartHelper.java
@@ -0,0 +1,108 @@
+package net.thevpc.ntexup.extension.plot2d;
+
+import net.thevpc.ntexup.api.document.elem2d.NTxBounds2;
+import net.thevpc.ntexup.api.document.node.NTxNode;
+import net.thevpc.ntexup.api.renderer.NTxNodeRendererContext;
+import net.thevpc.ntexup.extension.plot2d.model.NTxDrawContext;
+import net.thevpc.ntexup.extension.plot2d.model.NTxPlot2DData;
+import net.thevpc.ntexup.extension.plot2d.model.NTxPlotType;
+import net.thevpc.nuts.util.NStringUtils;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.plot.DrawingSupplier;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.*;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
+import java.awt.geom.Rectangle2D;
+
+public class NTxJFreeChartHelper {
+    static void drawCurves(NTxNode p,NTxNodeRendererContext renderContext, NTxDrawContext drawContext){
+        NTxBounds2 bounds = renderContext.getBounds();
+        XYSeriesCollection dataset = new XYSeriesCollection();
+        for (int j = 0; j < drawContext.allData.size(); j++) {
+            NTxPlot2DData pd = drawContext.allData.get(j);
+            XYSeries series = new XYSeries(NStringUtils.firstNonBlank(pd.title, "Curve " + (j+1)));
+            double[] yy2 = pd.animatedYY(renderContext);
+            for (int i = 0; i < pd.xx.length; i++) {
+                series.add(pd.xx[i], yy2[i]);
+            }
+            dataset.addSeries(series);
+        }
+
+
+        NumberAxis xAxis = new NumberAxis(NStringUtils.firstNonBlank(drawContext.xLabel,"X"));
+        xAxis.setAutoRangeIncludesZero(false);
+        NumberAxis yAxis = new NumberAxis(NStringUtils.firstNonBlank(drawContext.yLabel,"Y"));
+        yAxis.setAutoRange(false); // disable auto-scaling
+        yAxis.setRange(drawContext.gridMinY, drawContext.gridMaxY); // fixed min/max
+        XYPlot plot = new XYPlot();
+        plot.setRangeAxis(yAxis);
+        plot.setDomainAxis(xAxis);
+
+
+        DrawingSupplier supplier = plot.getDrawingSupplier();
+
+        plot.setBackgroundPaint(null);       // plot area
+        plot.setOutlinePaint(null);          // remove plot border
+        for (int i = 0; i < drawContext.allData.size(); i++) {
+            NTxPlot2DData pd = drawContext.allData.get(i);
+            if(pd.pld.plotType== NTxPlotType.CURVE) {
+                XYSeriesCollection dataset2 = new XYSeriesCollection();
+                XYSeries series = new XYSeries(NStringUtils.firstNonBlank(pd.title, "Curve " + (i+1)));
+                double[] yy2 = pd.animatedYY(renderContext);
+                for (int jx = 0; jx < pd.xx.length; jx++) {
+                    series.add(pd.xx[jx], yy2[jx]);
+                }
+                dataset2.addSeries(series);
+                plot.setDataset(i, dataset2);
+                plot.setRenderer(i, new XYLineAndShapeRenderer(true, pd.lineShapes));
+            }else if(pd.pld.plotType== NTxPlotType.BAR){
+                XYSeriesCollection dataset2 = new XYSeriesCollection();
+                XYSeries series = new XYSeries(NStringUtils.firstNonBlank(pd.title, "Bar " + (i+1)));
+                double[] yy2 = pd.animatedYY(renderContext);
+                for (int jx = 0; jx < pd.xx.length; jx++) {
+                    series.add(pd.xx[jx], yy2[jx]);
+                }
+                dataset2.addSeries(series);
+                plot.setDataset(i, dataset2);
+                plot.setRenderer(i, new XYBarRenderer());
+            }else if(pd.pld.plotType== NTxPlotType.AREA){
+                XYSeriesCollection dataset2 = new XYSeriesCollection();
+                XYSeries series = new XYSeries(NStringUtils.firstNonBlank(pd.title, "Area " + (i+1)));
+                double[] yy2 = pd.animatedYY(renderContext);
+                for (int jx = 0; jx < pd.xx.length; jx++) {
+                    series.add(pd.xx[jx], yy2[jx]);
+                }
+                dataset2.addSeries(series);
+                plot.setDataset(i, dataset2);
+                plot.setRenderer(i, new XYAreaRenderer());
+            }
+
+            plot.getRenderer().setSeriesPaint(i, supplier.getNextPaint());
+        }
+        // Create chart
+
+        JFreeChart chart = new JFreeChart(NStringUtils.firstNonBlank(drawContext.title,null),
+                JFreeChart.DEFAULT_TITLE_FONT,
+                plot,
+                drawContext.legend);
+
+//        JFreeChart chart = ChartFactory.createXYLineChart(
+//                null,
+//                "X",
+//                "Y",
+//                plot,
+//                PlotOrientation.VERTICAL,
+//                true,
+//                true,
+//                false
+//        );
+        chart.setBackgroundPaint(null);      // chart area
+
+        chart.draw(renderContext.graphics().graphics2D(), new Rectangle2D.Double(bounds.getX(),bounds.getY(),bounds.getWidth(),bounds.getHeight()));
+    }
+}
