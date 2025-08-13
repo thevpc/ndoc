eb3e850ae2c7b7b6ff5d62062c04b594b3490b60
thevpc <taha.bensalah@gmail.com>
2025-08-13 15:47:31 +0100

    Update NTxPlotSource.java : add jfreechar support

---
+++ extensions/ntexup-extension-plot2d/src/main/java/net/thevpc/ntexup/extension/plot2d/model/NTxPlotSource.java
@@ -0,0 +1,12 @@
+package net.thevpc.ntexup.extension.plot2d.model;
+
+public enum NTxPlotSource {
+    FUNCTION_X,
+    FUNCTION_XY,
+    FUNCTION_XYZ,
+    FUNCTION_XYZT,
+    VALUE_X,
+    VALUE_XY,
+    VALUE_XYZ,
+    VALUE_XYZT,
+}
