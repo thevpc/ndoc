93b98a2c767f754f001df43aa8136d29474b76b1
thevpc <taha.bensalah@gmail.com>
2025-08-13 15:47:34 +0100

    Update NTxGlobalPlotData.java : add jfreechar support

---
+++ extensions/ntexup-extension-plot2d/src/main/java/net/thevpc/ntexup/extension/plot2d/model/NTxGlobalPlotData.java
@@ -0,0 +1,18 @@
+package net.thevpc.ntexup.extension.plot2d.model;
+
+import java.awt.*;
+
+public class NTxGlobalPlotData {
+    public double xmin;
+    public double xmax;
+    public Double ymin;
+    public Double ymax;
+    public double majorGridSpacing;
+    public boolean showMajorGrid;
+    public Color majorGridColor;
+    public Stroke majorGridStroke;
+    public double minorGridSpacing;
+    public boolean showMinorGrid;
+    public Color minorGridColor;
+    public Stroke minorGridStroke;
+}
