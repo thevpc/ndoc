9f6ad9ec8794f53cb835788469acb8d7410b32ec
thevpc <taha.bensalah@gmail.com>
2025-08-13 15:47:34 +0100

    Update NTxFunctionPlotInfo.java : add jfreechar support

---
+++ extensions/ntexup-extension-plot2d/src/main/java/net/thevpc/ntexup/extension/plot2d/model/NTxFunctionPlotInfo.java
@@ -0,0 +1,24 @@
+package net.thevpc.ntexup.extension.plot2d.model;
+
+import net.thevpc.nuts.elem.NElement;
+import net.thevpc.nuts.util.NDoubleFunction;
+
+public class NTxFunctionPlotInfo {
+    public NTxPlotType plotType;
+    public NTxPlotSource source;
+    public NElement fexpr;
+    public NElement fval;
+    public String var1;
+    public String var2;
+    public String var3;
+    public String var4;
+    public int args;
+    public NElement title;
+    public NElement color;
+    public NElement stroke;
+    public NElement x;
+    public NElement y;
+    public NElement z;
+    public NElement t;
+    public NDoubleFunction f;
+}
