1ae42a5a26d2f44eb8d9dec713b5b0e8414585e5
thevpc <taha.bensalah@gmail.com>
2025-08-18 00:02:28 +0100

    Update NTxSquareBuilder.java : added cache

--- core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/base/nodes/shape/NTxSquareBuilder.java
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/base/nodes/shape/NTxSquareBuilder.java
@@ -6,8 +6,8 @@ import net.thevpc.ntexup.api.document.node.NTxNode;
 import net.thevpc.ntexup.api.document.node.NTxNodeType;
 import net.thevpc.ntexup.api.document.style.NTxPropName;
 import net.thevpc.ntexup.api.document.style.NTxProperties;
+import net.thevpc.ntexup.api.engine.NTxNodeBuilderContext;
 import net.thevpc.ntexup.api.extension.NTxNodeBuilder;
-import net.thevpc.ntexup.api.engine.NTxNodeCustomBuilderContext;
 import net.thevpc.ntexup.api.renderer.NTxGraphics;
 import net.thevpc.ntexup.api.renderer.NTxNodeRendererContext;
 import net.thevpc.ntexup.api.util.NTxUtils;
@@ -16,30 +16,30 @@ public class NTxSquareBuilder implements NTxNodeBuilder {
     private NTxProperties defaultStyles = new NTxProperties();
 
     @Override
-    public void build(NTxNodeCustomBuilderContext builderContext) {
+    public void build(NTxNodeBuilderContext builderContext) {
         builderContext
                 .id(NTxNodeType.SQUARE)
-                .parseParam().named(NTxPropName.ROUND_CORNER, NTxPropName.THEED, NTxPropName.RAISED).asFlags().then()
-                .renderComponent(this::renderMain)
+                .parseParam().matchesNamedPair(NTxPropName.ROUND_CORNER, NTxPropName.THEED, NTxPropName.RAISED).asFlags().then()
+                .renderComponent((rendererContext, builderContext1) -> renderMain(rendererContext, builderContext1))
         ;
     }
 
 
-    public void renderMain(NTxNode p, NTxNodeRendererContext rendererContext, NTxNodeCustomBuilderContext builderContext) {
-        rendererContext = rendererContext.withDefaultStyles(p, defaultStyles);
-
-        NTxBounds2 b = rendererContext.selfBounds(p);
+    public void renderMain(NTxNodeRendererContext rendererContext, NTxNodeBuilderContext builderContext) {
+        rendererContext = rendererContext.withDefaultStyles(defaultStyles);
+        NTxNode node = rendererContext.node();
+        NTxBounds2 b = rendererContext.selfBounds();
         double x = b.getX();
         double y = b.getY();
         NTxGraphics g = rendererContext.graphics();
-        Boolean threeD = rendererContext.get3D(p);
-        Boolean raised = rendererContext.getRaised(p);
+        Boolean threeD = rendererContext.get3D(node);
+        Boolean raised = rendererContext.getRaised(node);
         if (raised != null) {
             if (threeD == null) {
                 threeD = true;
             }
         }
-        NTxDouble2 roundCorners = rendererContext.getRoundCornerArcs(p);
+        NTxDouble2 roundCorners = rendererContext.getRoundCornerArcs(node);
         boolean round = roundCorners != null;
         boolean d3 = threeD == null ? false : threeD;
         if (!rendererContext.isDry()) {
@@ -51,11 +51,11 @@ public class NTxSquareBuilder implements NTxNodeBuilder {
             int finalHh = hh;
             if (!round && !d3) {
                 boolean someBG = false;
-                if (someBG = rendererContext.applyBackgroundColor(p)) {
+                if (someBG = rendererContext.applyBackgroundColor(node)) {
                     g.fillRect((int) x, (int) y, ww, hh);
                 }
-                if (rendererContext.applyForeground(p, !someBG)) {
-                    rendererContext.withStroke(p, () -> {
+                if (rendererContext.applyForeground(node, !someBG)) {
+                    rendererContext.withStroke(node, () -> {
                         g.drawRect((int) x, (int) y, finalWw, finalHh);
                     });
                 }
@@ -63,27 +63,27 @@ public class NTxSquareBuilder implements NTxNodeBuilder {
                 double cx = NTxUtils.doubleOf(roundCorners.getX()) / 100 * rendererContext.getGlobalBounds().getWidth();
                 double cy = NTxUtils.doubleOf(roundCorners.getY()) / 100 * rendererContext.getGlobalBounds().getHeight();
                 boolean someBG = false;
-                if (someBG = rendererContext.applyBackgroundColor(p)) {
+                if (someBG = rendererContext.applyBackgroundColor(node)) {
                     g.fillRoundRect((int) x, (int) y, ww, hh, (int) cx, (int) cy);
                 }
-                if (rendererContext.applyForeground(p, !someBG)) {
-                    rendererContext.withStroke(p, () -> {
+                if (rendererContext.applyForeground(node, !someBG)) {
+                    rendererContext.withStroke(node, () -> {
                         g.drawRoundRect((int) x, (int) y, finalWw, finalHh, (int) cx, (int) cy);
                     });
                 }
             } else if (threeD) {
                 boolean someBG = false;
-                if (someBG = rendererContext.applyBackgroundColor(p)) {
+                if (someBG = rendererContext.applyBackgroundColor(node)) {
                     g.fill3DRect((int) x, (int) y, ww, hh, raised != null && raised);
                 }
-                if (rendererContext.applyForeground(p, !someBG)) {
-                    rendererContext.withStroke(p, () -> {
+                if (rendererContext.applyForeground(node, !someBG)) {
+                    rendererContext.withStroke(node, () -> {
                         g.draw3DRect((int) x, (int) y, finalWw, finalHh, raised != null && raised);
                     });
                 }
             }
         }
-        rendererContext.paintDebugBox(p, b);
+        rendererContext.drawContour();
     }
 
 }
