ed040c949312307f9c9f45ab189195318d0fdbe6
thevpc <taha.bensalah@gmail.com>
2025-08-18 00:02:28 +0100

    Update NTxPentagonBuilder.java : added cache

--- core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/base/nodes/shape/NTxPentagonBuilder.java
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/base/nodes/shape/NTxPentagonBuilder.java
@@ -1,10 +1,9 @@
 package net.thevpc.ntexup.engine.base.nodes.shape;
 
-import net.thevpc.ntexup.api.document.node.NTxNode;
 import net.thevpc.ntexup.api.document.node.NTxNodeType;
 import net.thevpc.ntexup.api.document.style.NTxProperties;
 import net.thevpc.ntexup.api.extension.NTxNodeBuilder;
-import net.thevpc.ntexup.api.engine.NTxNodeCustomBuilderContext;
+import net.thevpc.ntexup.api.engine.NTxNodeBuilderContext;
 import net.thevpc.ntexup.api.renderer.NTxNodeRendererContext;
 
 public class NTxPentagonBuilder implements NTxNodeBuilder {
@@ -12,13 +11,13 @@ public class NTxPentagonBuilder implements NTxNodeBuilder {
     NTxProperties defaultStyles = new NTxProperties();
 
     @Override
-    public void build(NTxNodeCustomBuilderContext builderContext) {
+    public void build(NTxNodeBuilderContext builderContext) {
         builderContext.id(NTxNodeType.PENTAGON)
                 .renderComponent(this::render);
     }
 
-    public void render(NTxNode p, NTxNodeRendererContext renderContext, NTxNodeCustomBuilderContext buildContext) {
-        NTxPolygonHelper.renderPointsCount(5, p, renderContext, defaultStyles);
+    public void render(NTxNodeRendererContext rendererContext, NTxNodeBuilderContext buildContext) {
+        NTxPolygonHelper.renderPointsCount(5, rendererContext, defaultStyles);
     }
 
 }
