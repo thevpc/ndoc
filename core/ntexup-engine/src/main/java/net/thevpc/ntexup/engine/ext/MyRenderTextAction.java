f3d01c50eb4715ebbcd8a97c484f9f60aa154186
thevpc <taha.bensalah@gmail.com>
2025-08-18 00:02:28 +0100

    Update MyRenderTextAction.java : added cache

--- core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/ext/MyRenderTextAction.java
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/ext/MyRenderTextAction.java
@@ -1,42 +1,42 @@
 package net.thevpc.ntexup.engine.ext;
 
-import net.thevpc.ntexup.api.engine.NTxNodeCustomBuilderContext;
+import net.thevpc.ntexup.api.engine.NTxNodeBuilderContext;
 
-class MyRenderTextAction implements NTxNodeCustomBuilderContext.RenderTextAction {
-    public NTxNodeCustomBuilderContextImpl base;
-    public NTxNodeCustomBuilderContext.RenderTextBuildAction buildAction;
-    public NTxNodeCustomBuilderContext.RenderTextParseTokensAction parseTokensAction;
+class MyRenderTextAction implements NTxNodeBuilderContext.RenderTextAction {
+    public NTxNodeBuilderContextImpl base;
+    public NTxNodeBuilderContext.RenderTextBuildAction buildAction;
+    public NTxNodeBuilderContext.RenderTextParseTokensAction parseTokensAction;
     public String[] renderEmbeddedTextStartSeparators;
 
-    public MyRenderTextAction(NTxNodeCustomBuilderContextImpl base) {
+    public MyRenderTextAction(NTxNodeBuilderContextImpl base) {
         this.base = base;
     }
 
     @Override
-    public NTxNodeCustomBuilderContext.RenderTextAction buildText(NTxNodeCustomBuilderContext.RenderTextBuildAction a) {
+    public NTxNodeBuilderContext.RenderTextAction buildText(NTxNodeBuilderContext.RenderTextBuildAction a) {
         this.buildAction = a;
         return this;
     }
 
     @Override
-    public NTxNodeCustomBuilderContext.RenderTextAction parseTokens(NTxNodeCustomBuilderContext.RenderTextParseTokensAction embeddedTextAction) {
+    public NTxNodeBuilderContext.RenderTextAction parseTokens(NTxNodeBuilderContext.RenderTextParseTokensAction embeddedTextAction) {
         this.parseTokensAction = embeddedTextAction;
         return this;
     }
 
     @Override
-    public NTxNodeCustomBuilderContext.RenderTextAction startSeparators(String... startSeparators) {
+    public NTxNodeBuilderContext.RenderTextAction startSeparators(String... startSeparators) {
         this.renderEmbeddedTextStartSeparators = startSeparators;
         return this;
     }
 
     @Override
-    public NTxNodeCustomBuilderContext then() {
+    public NTxNodeBuilderContext then() {
         return end();
     }
 
     @Override
-    public NTxNodeCustomBuilderContext end() {
+    public NTxNodeBuilderContext end() {
         return base;
     }
 }
