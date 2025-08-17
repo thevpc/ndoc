b47c987f54876e1703268541fedf9fb1b4d9a16e
thevpc <taha.bensalah@gmail.com>
2025-08-18 00:02:29 +0100

    Update NTxMyShapeBuilder.java : added cache

--- tutorials/ntexup-extension-tutorial-myshape/src/main/java/net/thevpc/ntexup/extension/tutorials/myshape/NTxMyShapeBuilder.java
+++ tutorials/ntexup-extension-tutorial-myshape/src/main/java/net/thevpc/ntexup/extension/tutorials/myshape/NTxMyShapeBuilder.java
@@ -2,9 +2,9 @@ package net.thevpc.ntexup.extension.tutorials.myshape;
 
 import net.thevpc.ntexup.api.document.elem2d.NTxBounds2;
 import net.thevpc.ntexup.api.document.elem2d.NTxPoint2D;
+import net.thevpc.ntexup.api.engine.NTxNodeBuilderContext;
 import net.thevpc.ntexup.api.eval.NTxValue;
 import net.thevpc.ntexup.api.extension.NTxNodeBuilder;
-import net.thevpc.ntexup.api.engine.NTxNodeCustomBuilderContext;
 import net.thevpc.ntexup.api.document.node.NTxNode;
 import net.thevpc.ntexup.api.document.style.NTxPropName;
 import net.thevpc.ntexup.api.renderer.NTxGraphics;
@@ -19,39 +19,30 @@ import java.awt.geom.GeneralPath;
  */
 public class NTxMyShapeBuilder implements NTxNodeBuilder {
     @Override
-    public void build(NTxNodeCustomBuilderContext builderContext) {
+    public void build(NTxNodeBuilderContext builderContext) {
         builderContext
                 .id("my-shape")
-                .parseParam().named(NTxPropName.WIDTH, NTxPropName.HEIGHT,"base","hat").end()
-                .renderComponent(this::render)
+                .parseParam().matchesNamedPair(NTxPropName.WIDTH, NTxPropName.HEIGHT,"base","hat").end()
+                .renderComponent((rendererContext, buildContext) -> render(rendererContext, buildContext))
         ;
     }
 
-    public void render(NTxNode p, NTxNodeRendererContext renderContext, NTxNodeCustomBuilderContext buildContext) {
-        NTxBounds2 b = renderContext.selfBounds(p, null, null);
+    public void render(NTxNodeRendererContext rendererContext, NTxNodeBuilderContext buildContext) {
+        NTxNode node=rendererContext.node();
+        NTxBounds2 b = rendererContext.selfBounds(node, null, null);
         double x = b.getX();
         double y = b.getY();
         double width = b.getWidth();
         double height = b.getHeight();
 
-        Paint color = renderContext.getForegroundColor(p, true);
-        NTxPoint2D base = NTxValue.of(p.getPropertyValue("base")).asHPoint2D().orNull();
+        Paint color = rendererContext.getForegroundColor(node, true);
+        NTxPoint2D base = NTxValue.of(node.getPropertyValue("base")).asPoint2DOrDouble().orNull();
         if (base == null) {
-            Double base3 = NTxValue.of(p.getPropertyValue("base")).asDouble().orNull();
-            if (base3 == null) {
-                base = new NTxPoint2D(80, 20);
-            } else {
-                base = new NTxPoint2D(base3, base3);
-            }
+            base = new NTxPoint2D(80, 20);
         }
-        NTxPoint2D hat = NTxValue.of(p.getPropertyValue("hat")).asHPoint2D().orNull();
+        NTxPoint2D hat = NTxValue.of(node.getPropertyValue("hat")).asPoint2DOrDouble().orNull();
         if (hat == null) {
-            Double base3 = NTxValue.of(p.getPropertyValue("hat")).asDouble().orNull();
-            if (base3 == null) {
-                hat = new NTxPoint2D(-1, -1);
-            } else {
-                hat = new NTxPoint2D(base3, base3);
-            }
+            hat = new NTxPoint2D(-1, -1);
         }
         if (hat.y < 0) {
             hat.y = 15;
@@ -64,7 +55,7 @@ public class NTxMyShapeBuilder implements NTxNodeBuilder {
         double hatH = hat.y * height / 100.0;
         double hatW = hat.x * width / 100.0;
 
-        NTxGraphics g = renderContext.graphics();
+        NTxGraphics g = rendererContext.graphics();
         g.setPaint(color);
         GeneralPath arrow = new GeneralPath();
         arrow.moveTo(x, y + height / 2);
@@ -78,7 +69,7 @@ public class NTxMyShapeBuilder implements NTxNodeBuilder {
         arrow.closePath();
         g.draw(arrow);
         g.fill(arrow);
-        renderContext.paintDebugBox(p, b);
+        rendererContext.drawContour();
 
     }
 }
