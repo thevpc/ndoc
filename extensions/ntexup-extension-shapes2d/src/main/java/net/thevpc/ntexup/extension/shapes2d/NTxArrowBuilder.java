199e19edd5c7a14911e2e54a8ab43f66adf53570
thevpc <taha.bensalah@gmail.com>
2025-08-18 00:02:28 +0100

    Update NTxArrowBuilder.java : added cache

--- extensions/ntexup-extension-shapes2d/src/main/java/net/thevpc/ntexup/extension/shapes2d/NTxArrowBuilder.java
+++ extensions/ntexup-extension-shapes2d/src/main/java/net/thevpc/ntexup/extension/shapes2d/NTxArrowBuilder.java
@@ -5,9 +5,9 @@ import net.thevpc.ntexup.api.document.elem2d.NTxPoint2D;
 import net.thevpc.ntexup.api.document.node.NTxNode;
 import net.thevpc.ntexup.api.document.node.NTxNodeType;
 import net.thevpc.ntexup.api.document.style.NTxPropName;
+import net.thevpc.ntexup.api.engine.NTxNodeBuilderContext;
 import net.thevpc.ntexup.api.eval.NTxValue;
 import net.thevpc.ntexup.api.extension.NTxNodeBuilder;
-import net.thevpc.ntexup.api.engine.NTxNodeCustomBuilderContext;
 import net.thevpc.ntexup.api.renderer.NTxGraphics;
 import net.thevpc.ntexup.api.renderer.NTxNodeRendererContext;
 
@@ -21,35 +21,36 @@ import java.awt.geom.GeneralPath;
 public class NTxArrowBuilder implements NTxNodeBuilder {
 
     @Override
-    public void build(NTxNodeCustomBuilderContext builderContext) {
+    public void build(NTxNodeBuilderContext builderContext) {
         builderContext
                 .id(NTxNodeType.ARROW)
-                .parseParam().named(NTxPropName.WIDTH, NTxPropName.HEIGHT,"base","hat").end()
+                .parseParam().matchesNamedPair(NTxPropName.WIDTH, NTxPropName.HEIGHT,"base","hat").end()
                 .renderComponent(this::render)
                 ;
     }
 
 
-    public void render(NTxNode p, NTxNodeRendererContext renderContext, NTxNodeCustomBuilderContext buildContext) {
-        NTxBounds2 b = renderContext.selfBounds(p, null, null);
+    public void render(NTxNodeRendererContext rendererContext, NTxNodeBuilderContext buildContext) {
+        NTxNode node=rendererContext.node();
+        NTxBounds2 b = rendererContext.selfBounds(node, null, null);
         double x = b.getX();
         double y = b.getY();
         double width = b.getWidth();
         double height = b.getHeight();
 
-        Paint color = renderContext.getForegroundColor(p, true);
-        NTxPoint2D base = NTxValue.of(p.getPropertyValue("base")).asHPoint2D().orElse(null);
+        Paint color = rendererContext.getForegroundColor(node, true);
+        NTxPoint2D base = NTxValue.of(node.getPropertyValue("base")).asPoint2D().orElse(null);
         if(base==null){
-            Double base3 = NTxValue.of(p.getPropertyValue("base")).asDouble().orElse(null);
+            Double base3 = NTxValue.of(node.getPropertyValue("base")).asDouble().orElse(null);
             if(base3==null){
                 base=new NTxPoint2D(80, 20);
             }else{
                 base=new NTxPoint2D(base3, base3);
             }
         }
-        NTxPoint2D hat = NTxValue.of(p.getPropertyValue("hat")).asHPoint2D().orElse(null);
+        NTxPoint2D hat = NTxValue.of(node.getPropertyValue("hat")).asPoint2D().orElse(null);
         if(hat==null){
-            Double base3 = NTxValue.of(p.getPropertyValue("hat")).asDouble().orElse(null);
+            Double base3 = NTxValue.of(node.getPropertyValue("hat")).asDouble().orElse(null);
             if(base3==null){
                 hat=new NTxPoint2D(-1, -1);
             }else{
@@ -67,7 +68,7 @@ public class NTxArrowBuilder implements NTxNodeBuilder {
         double hatH=hat.y*height/100.0;
         double hatW=hat.x*width/100.0;
 
-        NTxGraphics g = renderContext.graphics();
+        NTxGraphics g = rendererContext.graphics();
         g.setPaint(color);
         GeneralPath arrow = new GeneralPath();
         arrow.moveTo(x, y+height/2);
@@ -81,8 +82,7 @@ public class NTxArrowBuilder implements NTxNodeBuilder {
         arrow.closePath();
         g.draw(arrow);
         g.fill(arrow);
-        renderContext.paintDebugBox(p, b);
-
+        rendererContext.drawContour();
     }
 
 }
