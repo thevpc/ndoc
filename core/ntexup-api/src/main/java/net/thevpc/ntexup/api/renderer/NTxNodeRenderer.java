9660bdc0942f944cfbd41a8672a69d2e30662887
thevpc <taha.bensalah@gmail.com>
2025-08-18 00:02:28 +0100

    Update NTxNodeRenderer.java : added cache

--- core/ntexup-api/src/main/java/net/thevpc/ntexup/api/renderer/NTxNodeRenderer.java
+++ core/ntexup-api/src/main/java/net/thevpc/ntexup/api/renderer/NTxNodeRenderer.java
@@ -2,16 +2,15 @@ package net.thevpc.ntexup.api.renderer;
 
 import net.thevpc.ntexup.api.document.NTxSizeRequirements;
 import net.thevpc.ntexup.api.document.elem2d.NTxBounds2;
-import net.thevpc.ntexup.api.document.node.*;
 
 
 public interface NTxNodeRenderer {
 
-    NTxSizeRequirements sizeRequirements(NTxNode p, NTxNodeRendererContext ctx);
+    NTxSizeRequirements sizeRequirements(NTxNodeRendererContext ctx);
 
-    NTxBounds2 selfBounds(NTxNode p, NTxNodeRendererContext ctx);
+    NTxBounds2 selfBounds(NTxNodeRendererContext ctx);
 
-    void render(NTxNode p, NTxNodeRendererContext ctx);
+    void render(NTxNodeRendererContext rendererContext);
 
     String[] types();
 }
