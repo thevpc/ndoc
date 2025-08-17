63a952d8412affeb14628d488beaba2e10d68265
thevpc <taha.bensalah@gmail.com>
2025-08-18 00:02:28 +0100

    Update NTxPieBuilder.java : added cache

--- extensions/ntexup-extension-shapes2d/src/main/java/net/thevpc/ntexup/extension/shapes2d/NTxPieBuilder.java
+++ extensions/ntexup-extension-shapes2d/src/main/java/net/thevpc/ntexup/extension/shapes2d/NTxPieBuilder.java
@@ -1,11 +1,10 @@
 package net.thevpc.ntexup.extension.shapes2d;
 
-import net.thevpc.ntexup.api.document.node.NTxNode;
 import net.thevpc.ntexup.api.document.node.NTxNodeType;
 import net.thevpc.ntexup.api.document.style.NTxPropName;
 import net.thevpc.ntexup.api.document.style.NTxProperties;
+import net.thevpc.ntexup.api.engine.NTxNodeBuilderContext;
 import net.thevpc.ntexup.api.extension.NTxNodeBuilder;
-import net.thevpc.ntexup.api.engine.NTxNodeCustomBuilderContext;
 import net.thevpc.ntexup.api.renderer.NTxNodeRendererContext;
 
 
@@ -16,20 +15,20 @@ public class NTxPieBuilder implements NTxNodeBuilder {
     NTxProperties defaultStyles = new NTxProperties();
 
     @Override
-    public void build(NTxNodeCustomBuilderContext builderContext) {
+    public void build(NTxNodeBuilderContext builderContext) {
         builderContext
                 .id(NTxNodeType.PIE)
-                .parseParam().named(NTxPropName.START_ANGLE, NTxPropName.EXTENT_ANGLE, NTxPropName.DASH).then()
-                .parseParam().named(NTxPropName.SLICE_COUNT).then()
-                .parseParam().named(NTxPropName.SLICES).then()
-                .parseParam().named(NTxPropName.COLORS).then()
+                .parseParam().matchesNamedPair(NTxPropName.START_ANGLE, NTxPropName.EXTENT_ANGLE, NTxPropName.DASH).then()
+                .parseParam().matchesNamedPair(NTxPropName.SLICE_COUNT).then()
+                .parseParam().matchesNamedPair(NTxPropName.SLICES).then()
+                .parseParam().matchesNamedPair(NTxPropName.COLORS).then()
                 .renderComponent(this::render)
                 ;
     }
 
 
-    private void render(NTxNode p, NTxNodeRendererContext renderContext, NTxNodeCustomBuilderContext builderContext) {
-        NTxDonutBuilder.renderDonutOrPie(p, renderContext, builderContext, defaultStyles, false);
+    private void render(NTxNodeRendererContext rendererContext, NTxNodeBuilderContext builderContext) {
+        NTxDonutBuilder.renderDonutOrPie(rendererContext, builderContext, defaultStyles, false);
     }
 
 
