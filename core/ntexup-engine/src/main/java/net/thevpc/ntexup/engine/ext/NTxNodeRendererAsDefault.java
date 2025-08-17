de5035576df87ebc4ba68f25d39338ee0dd3845c
thevpc <taha.bensalah@gmail.com>
2025-08-18 00:02:28 +0100

    Update NTxNodeRendererAsDefault.java : added cache

--- core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/ext/NTxNodeRendererAsDefault.java
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/ext/NTxNodeRendererAsDefault.java
@@ -1,45 +1,44 @@
 package net.thevpc.ntexup.engine.ext;
 
 import net.thevpc.ntexup.api.document.elem2d.NTxBounds2;
-import net.thevpc.ntexup.api.document.node.NTxNode;
 import net.thevpc.ntexup.api.document.NTxSizeRequirements;
 import net.thevpc.ntexup.engine.renderer.NTxNodeRendererBase;
 import net.thevpc.ntexup.api.renderer.NTxNodeRendererContext;
 
 class NTxNodeRendererAsDefault extends NTxNodeRendererBase {
-    private final NTxNodeCustomBuilderContextImpl ctx;
+    private final NTxNodeBuilderContextImpl ctx;
 
-    public NTxNodeRendererAsDefault(NTxNodeCustomBuilderContextImpl ctx) {
+    public NTxNodeRendererAsDefault(NTxNodeBuilderContextImpl ctx) {
         super(ctx.id);
         this.ctx = ctx;
     }
 
     @Override
-    public void renderMain(NTxNode p, NTxNodeRendererContext ctx) {
+    public void renderMain(NTxNodeRendererContext ctx) {
         if (this.ctx.renderMainAction != null) {
-            this.ctx.renderMainAction.renderMain(p, ctx, this.ctx);
+            this.ctx.renderMainAction.renderMain(ctx, this.ctx);
         }
     }
 
     @Override
-    public NTxSizeRequirements sizeRequirements(NTxNode p, NTxNodeRendererContext ctx) {
+    public NTxSizeRequirements sizeRequirements(NTxNodeRendererContext ctx) {
         if (this.ctx.sizeRequirementsAction != null) {
-            NTxSizeRequirements u = this.ctx.sizeRequirementsAction.sizeRequirements(p, ctx, this.ctx);
+            NTxSizeRequirements u = this.ctx.sizeRequirementsAction.sizeRequirements(ctx, this.ctx);
             if (u != null) {
                 return u;
             }
         }
-        return super.sizeRequirements(p, ctx);
+        return super.sizeRequirements(ctx);
     }
 
     @Override
-    public NTxBounds2 selfBounds(NTxNode t, NTxNodeRendererContext ctx) {
+    public NTxBounds2 selfBounds(NTxNodeRendererContext ctx) {
         if (this.ctx.selfBoundsAction != null) {
-            NTxBounds2 u = this.ctx.selfBoundsAction.selfBounds(t, ctx, this.ctx);
+            NTxBounds2 u = this.ctx.selfBoundsAction.selfBounds(ctx, this.ctx);
             if (u != null) {
                 return u;
             }
         }
-        return super.selfBounds(t, ctx);
+        return super.selfBounds(ctx);
     }
 }
