222fd0000cef9ca662b267ec8750556be838344d
thevpc <taha.bensalah@gmail.com>
2025-08-18 00:02:28 +0100

    Update NTxImportRenderer.java : added cache

--- core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/renderer/special/NTxImportRenderer.java
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/renderer/special/NTxImportRenderer.java
@@ -1,6 +1,5 @@
 package net.thevpc.ntexup.engine.renderer.special;
 
-import net.thevpc.ntexup.api.document.node.NTxNode;
 import net.thevpc.ntexup.api.document.node.NTxNodeType;
 import net.thevpc.ntexup.api.renderer.NTxNodeRendererContext;
 import net.thevpc.ntexup.engine.renderer.NTxNodeRendererBase;
@@ -11,6 +10,6 @@ public class NTxImportRenderer extends NTxNodeRendererBase {
     }
 
     @Override
-    public void renderMain(NTxNode p, NTxNodeRendererContext ctx) {
+    public void renderMain(NTxNodeRendererContext ctx) {
     }
 }
