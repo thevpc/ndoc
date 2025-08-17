840863222f1720cb467164277deac841d6f8ea8c
thevpc <taha.bensalah@gmail.com>
2025-08-18 00:02:28 +0100

    Update NTxNodeRendererManager.java : added cache

--- core/ntexup-api/src/main/java/net/thevpc/ntexup/api/renderer/NTxNodeRendererManager.java
+++ core/ntexup-api/src/main/java/net/thevpc/ntexup/api/renderer/NTxNodeRendererManager.java
@@ -1,15 +1,15 @@
-package net.thevpc.ntexup.api.renderer;
-
-import net.thevpc.ntexup.api.document.node.NTxNode;
-import net.thevpc.nuts.util.NOptional;
-
-import java.awt.image.BufferedImage;
-
-public interface NTxNodeRendererManager {
-
-    NOptional<NTxNodeRenderer> getRenderer(String type);
-
-    BufferedImage renderImage(NTxNode node, NTxNodeRendererConfig config);
-
-    byte[] renderImageBytes(NTxNode node, NTxNodeRendererConfig config);
-}
+//package net.thevpc.ntexup.api.renderer;
+//
+//import net.thevpc.ntexup.api.document.node.NTxNode;
+//import net.thevpc.nuts.util.NOptional;
+//
+//import java.awt.image.BufferedImage;
+//
+//public interface NTxNodeRendererManager {
+//
+//    NOptional<NTxNodeRenderer> getRenderer(String type);
+//
+//    BufferedImage renderImage(NTxNode node, NTxNodeRendererConfig config);
+//
+//    byte[] renderImageBytes(NTxNode node, NTxNodeRendererConfig config);
+//}
