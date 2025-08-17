c6a85c7f206e33eb617337a00d333e80e5cc5aae
thevpc <taha.bensalah@gmail.com>
2025-08-18 00:02:28 +0100

    Update NTxNodeRendererBase.java : added cache

--- core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/renderer/NTxNodeRendererBase.java
+++ core/ntexup-engine/src/main/java/net/thevpc/ntexup/engine/renderer/NTxNodeRendererBase.java
@@ -2,14 +2,13 @@ package net.thevpc.ntexup.engine.renderer;
 
 import net.thevpc.ntexup.api.document.elem2d.*;
 import net.thevpc.ntexup.api.document.node.*;
-import net.thevpc.ntexup.api.document.style.NTxPropName;
 import net.thevpc.ntexup.api.eval.NTxValueByName;
-import net.thevpc.ntexup.api.eval.NTxValueByType;
 import net.thevpc.ntexup.api.document.NTxSizeRequirements;
 import net.thevpc.ntexup.api.eval.NTxValue;
 import net.thevpc.ntexup.api.renderer.NTxGraphics;
 import net.thevpc.ntexup.api.renderer.NTxNodeRenderer;
 import net.thevpc.ntexup.api.renderer.NTxNodeRendererContext;
+import net.thevpc.ntexup.engine.util.NTxNodeRendererUtils;
 
 public abstract class NTxNodeRendererBase implements NTxNodeRenderer {
 
@@ -20,8 +19,8 @@ public abstract class NTxNodeRendererBase implements NTxNodeRenderer {
     }
 
     @Override
-    public NTxSizeRequirements sizeRequirements(NTxNode p, NTxNodeRendererContext ctx) {
-        NTxBounds2 bounds = ctx.getBounds();
+    public NTxSizeRequirements sizeRequirements(NTxNodeRendererContext ctx) {
+        NTxBounds2 bounds = ctx.selfBounds();
         return new NTxSizeRequirements(
                 0,
                 bounds.getWidth(),
@@ -37,29 +36,30 @@ public abstract class NTxNodeRendererBase implements NTxNodeRenderer {
         return types;
     }
 
-    public void render(NTxNode p, NTxNodeRendererContext ctx) {
-        boolean v = NTxValueByName.isVisible(p, ctx);
+    public void render(NTxNodeRendererContext rendererContext) {
+        NTxNode node=rendererContext.node();
+        boolean v = NTxValueByName.isVisible(node, rendererContext);
         if (!v) {
             return;
         }
-        NTxBounds2 selfBounds = selfBounds(p, ctx);
+
+        NTxBounds2 selfBounds = rendererContext.selfBounds();
         NTxGraphics nv = null;
         try {
-            if (!ctx.isDry()) {
-            //if (!ctx.isDry()) {
-                NTxRotation rotation = NTxValueByType.getRotation(p, ctx, NTxPropName.ROTATE).orNull();
+            if (!rendererContext.isDry()) {
+                NTxRotation rotation = NTxValueByName.getRotation(node, rendererContext);
                 if (rotation != null) {
                     double angle = NTxValue.of(rotation.getAngle()).asDouble().orElse(0.0);
                     if (angle != 0) {
                         angle = angle / 180.0 * Math.PI;
                         if (angle != 0) {
-                            NTxGraphics g = ctx.graphics();
+                            NTxGraphics g = rendererContext.graphics();
                             nv = g.copy();
                             ///HSizeRef sr=new HSizeRef();
                             double rotX = NTxValue.of(rotation.getX()).asDouble().get() / 100.0 * selfBounds.getWidth() + selfBounds.getX();
                             double rotY = NTxValue.of(rotation.getY()).asDouble().get() / 100.0 * selfBounds.getHeight() + selfBounds.getY();
-                            if (ctx.isDebug(p)) {
-                                g.setColor(NTxValueByName.getDebugColor(p, ctx));
+                            if (rendererContext.isDebug(node)) {
+                                g.setColor(NTxValueByName.getDebugColor(node, rendererContext));
                                 g.drawRect(selfBounds);
                                 g.fillRect(rotX - 3, rotY - 3, 6, 6);
 //                            g.drawString(rotX + "," + rotY+" : "+p, 100, 100);
@@ -69,12 +69,12 @@ public abstract class NTxNodeRendererBase implements NTxNodeRenderer {
                                     rotX,
                                     rotY
                             );
-                            ctx = ctx.withGraphics(nv);
+                            rendererContext = rendererContext.withGraphics(nv);
                         }
                     }
                 }
-            //}
-                renderMain(p, ctx);
+                renderMain(rendererContext);
+                NTxNodeRendererUtils.drawDebugBox(rendererContext.node(), rendererContext, rendererContext.graphics(), rendererContext.selfBounds());
             }
         } finally {
             if (nv != null) {
@@ -83,18 +83,18 @@ public abstract class NTxNodeRendererBase implements NTxNodeRenderer {
         }
     }
 
-    public abstract void renderMain(NTxNode p, NTxNodeRendererContext ctx);
+    public abstract void renderMain(NTxNodeRendererContext ctx);
 
     public NTxBounds2 bgBounds(NTxNode p, NTxNodeRendererContext ctx) {
         return ctx.selfBounds(p, null, null);
     }
 
-    public NTxBounds2 selfBounds(NTxNode t, NTxNodeRendererContext ctx) {
-        return NTxValueByName.selfBounds(t, null, null, ctx);
+    public NTxBounds2 selfBounds(NTxNodeRendererContext ctx) {
+        return NTxValueByName.selfBounds(ctx.node(), null, null, ctx);
     }
 
-    public NTxBounds2 defaultSelfBounds(NTxNode t, NTxNodeRendererContext ctx) {
-        return NTxValueByName.selfBounds(t, null, null, ctx);
+    public NTxBounds2 defaultSelfBounds(NTxNodeRendererContext ctx) {
+        return NTxValueByName.selfBounds(ctx.node(), null, null, ctx);
     }
 
 }
